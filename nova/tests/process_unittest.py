5c3d352cee5003395d078dcfe01e8f4743027074
Vishvananda Ishaya <vishvananda@gmail.com>
2010-07-26 12:49:21 -0700

    class based singleton for SharedPool

--- nova/tests/process_unittest.py
+++ nova/tests/process_unittest.py
@@ -118,7 +118,7 @@ class ProcessTestCase(test.TrialTestCase):
     def test_shared_pool_is_singleton(self):
         pool1 = process.SharedPool()
         pool2 = process.SharedPool()
-        self.assert_(id(pool1) == id(pool2))
+        self.assertEqual(id(pool1._instance), id(pool2._instance))
 
     def test_shared_pool_works_as_singleton(self):
         d1 = process.simple_execute('sleep 1')
