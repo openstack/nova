7c39435df527126411ccaf5f95c1f9d0828af107
Monty Taylor <mordred@inaugust.com>
2010-07-28 14:37:58 -0700

    Added ChangeLog generation.

--- setup.py
+++ setup.py
@@ -17,6 +17,23 @@
 #    under the License.
 
 from setuptools import setup, find_packages
+from setuptools.command.sdist import sdist
+
+import os
+import subprocess
+
+class local_sdist(sdist):
+    """Customized sdist hook - builds the ChangeLog file from VC first"""
+
+    def run(self):
+        if os.path.isdir('.bzr'):
+            # We're in a bzr branch
+            log_cmd = subprocess.Popen(["bzr","log","--gnu"],
+                                       stdout = subprocess.PIPE)
+            changelog = log_cmd.communicate()[0]
+            with open("ChangeLog", "w") as changelog_file:
+                changelog_file.write(changelog)
+        sdist.run(self)
 
 setup(name='nova',
       version='0.9.0',
@@ -24,6 +41,7 @@ setup(name='nova',
       author='OpenStack',
       author_email='nova@lists.launchpad.net',
       url='http://www.openstack.org/',
+      cmdclass = {'sdist': local_sdist},
       packages = find_packages(exclude=['bin','smoketests']),
       scripts=['bin/nova-api',
                'bin/nova-compute',
