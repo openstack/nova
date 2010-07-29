b01c531ba5cb408a630e992beda769032b135f6a
Ewan Mellor <ewan.mellor@citrix.com>
2010-07-29 01:21:50 +0100

    Move the reading of API parameters above the call to _get_image, so that they have a chance to take effect.

--- nova/endpoint/cloud.py
+++ nova/endpoint/cloud.py
@@ -518,13 +518,13 @@ class CloudController(object):
         kernel_id = image.get('kernelId', FLAGS.default_kernel)
         ramdisk_id = image.get('ramdiskId', FLAGS.default_ramdisk)
 
-        # make sure we have access to kernel and ramdisk
-        self._get_image(context, kernel_id)
-        self._get_image(context, ramdisk_id)
-
         # API parameters overrides of defaults
         kernel_id = kwargs.get('kernel_id', kernel_id)
         ramdisk_id = kwargs.get('ramdisk_id', ramdisk_id)
+
+        # make sure we have access to kernel and ramdisk
+        self._get_image(context, kernel_id)
+        self._get_image(context, ramdisk_id)
 
         logging.debug("Going to run instances...")
         reservation_id = utils.generate_uid('r')
