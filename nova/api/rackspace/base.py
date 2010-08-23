41e2e91ccfb1409f1ea47d92a9d15f47ab37e65d
Michael Gundlach <michael.gundlach@rackspace.com>
2010-08-23 16:43:25 -0400

    Merge fail

--- nova/api/rackspace/base.py
+++ nova/api/rackspace/base.py
@@ -27,4 +27,4 @@ class Controller(wsgi.Controller):
         if isinstance(instance, list):
             return {cls.entity_name: cls.render(instance)}
         else:
-            return { "TODO": "TODO" }
+            return {"TODO": "TODO"}
