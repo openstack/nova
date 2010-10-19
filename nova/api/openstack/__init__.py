86a8a5a2091dc0a36ccbe05f46609c82b1060291
Michael Gundlach <michael.gundlach@rackspace.com>
2010-10-19 19:37:42 -0400

    Fixes https://bugs.launchpad.net/nova/+bug/663551 by catching exceptions at the top level of the API and turning them into Faults.

--- nova/api/openstack/__init__.py
+++ nova/api/openstack/__init__.py
@@ -23,6 +23,7 @@ WSGI middleware for OpenStack API controllers.
 import json
 import time
 
+import logging
 import routes
 import webob.dec
 import webob.exc
@@ -52,6 +53,16 @@ class API(wsgi.Middleware):
     def __init__(self):
         app = AuthMiddleware(RateLimitingMiddleware(APIRouter()))
         super(API, self).__init__(app)
+
+    @webob.dec.wsgify
+    def __call__(self, req):
+        try:
+            return req.get_response(self.application)
+        except Exception as ex:
+            logging.warn("Caught error: %s" % str(ex))
+            exc = webob.exc.HTTPInternalServerError(explanation=str(ex))
+            return faults.Fault(exc)
+
 
 class AuthMiddleware(wsgi.Middleware):
     """Authorize the openstack API request or return an HTTP Forbidden."""
