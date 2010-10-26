8ccdae97558d9660a9a0fac8dad809a09cbd3c71
Vishvananda Ishaya <vishvananda@yahoo.com>
2010-10-25 17:20:10 -0700

    actually remove the conditional

--- nova/utils.py
+++ nova/utils.py
@@ -131,13 +131,9 @@ def runthis(prompt, cmd, check_exit_code=True):
 
 
 def generate_uid(topic, size=8):
-    if topic == "i":
-        # Instances have integer internal ids.
-        return random.randint(0, 2 ** 32 - 1)
-    else:
-        characters = '01234567890abcdefghijklmnopqrstuvwxyz'
-        choices = [random.choice(characters) for x in xrange(size)]
-        return '%s-%s' % (topic, ''.join(choices))
+    characters = '01234567890abcdefghijklmnopqrstuvwxyz'
+    choices = [random.choice(characters) for x in xrange(size)]
+    return '%s-%s' % (topic, ''.join(choices))
 
 
 def generate_mac():
