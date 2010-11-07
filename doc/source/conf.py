75841f22d39d90859c1157315c326b4e8a4a3b9c
Todd Willey <todd@ansolabs.com>
2010-11-07 17:18:41 -0500

    Fix include paths so setup.py build_sphinx works again.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,14 +17,15 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, '../')
-sys.path.insert(0, './')
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('./'))
 
 # -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'ext.nova_todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig']
+#extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig']
 todo_include_todos = True
 
 # Add any paths that contain templates here, relative to this directory.
