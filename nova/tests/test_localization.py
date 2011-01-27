c679e64d13a9ff8643d20316d3a96ed5fc27e0ca
Ed Leafe <ed@leafe.com>
2011-01-27 16:54:59 -0600

    Made changes based on code review

--- nova/tests/test_localization.py
+++ nova/tests/test_localization.py
@@ -1,25 +1,30 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+#    Copyright 2011 OpenStack LLC
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 import glob
 import logging
 import os
 import re
 import sys
+import unittest
 
 import nova
-from nova import test
-
-LOG = logging.getLogger('nova.tests.localization_unittest')
 
 
-class LocalizationTestCase(test.TestCase):
-    def setUp(self):
-        super(LocalizationTestCase, self).setUp()
-
-    def tearDown(self):
-        super(LocalizationTestCase, self).tearDown()
-
+class LocalizationTestCase(unittest.TestCase):
     def test_multiple_positional_format_placeholders(self):
         pat = re.compile("\W_\(")
         single_pat = re.compile("\W%\W")
