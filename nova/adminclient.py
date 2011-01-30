6ed93f6116ed092e64ceef9a255b46167099bfc3
Devin Carlen <devin.carlen@gmail.com>
2011-01-30 15:45:17 -0800

    pep8

--- nova/adminclient.py
+++ nova/adminclient.py
@@ -290,7 +290,9 @@ class NovaAdminClient(object):
     def get_user(self, name):
         """Grab a single user by name."""
         try:
-            return self.apiconn.get_object('DescribeUser', {'Name': name}, UserInfo)
+            return self.apiconn.get_object('DescribeUser',
+                                           {'Name': name},
+                                           UserInfo)
         except boto.exception.BotoServerError, e:
             if e.status == 400 and e.error_code == 'NotFound':
                 return None
