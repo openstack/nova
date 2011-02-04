563a77fd4aa80da9bddac5cf7f8f27ed2dedb39d
Ken Pepple <ken.pepple@gmail.com>
2011-02-03 17:52:19 -0800

    added seed data to migration

--- nova/db/sqlalchemy/migrate_repo/versions/003_cactus.py
+++ nova/db/sqlalchemy/migrate_repo/versions/003_cactus.py
@@ -15,9 +15,11 @@
 from sqlalchemy import *
 from migrate import *
 
-
+from nova import api
+from nova import db
 from nova import log as logging
 
+import time
 
 meta = MetaData()
 
@@ -25,17 +27,14 @@ meta = MetaData()
 #
 # New Tables
 #
-# Here are the old static instance types
-# INSTANCE_TYPES = {
-# 'm1.tiny': dict(memory_mb=512, vcpus=1, local_gb=0, flavorid=1),
-# 'm1.small': dict(memory_mb=2048, vcpus=1, local_gb=20, flavorid=2),
-# 'm1.medium': dict(memory_mb=4096, vcpus=2, local_gb=40, flavorid=3),
-# 'm1.large': dict(memory_mb=8192, vcpus=4, local_gb=80, flavorid=4),
-# 'm1.xlarge': dict(memory_mb=16384, vcpus=8, local_gb=160, flavorid=5)}
 instance_types = Table('instance_types', meta,
         Column('created_at', DateTime(timezone=False)),
+        Column('updated_at', DateTime(timezone=False)),
         Column('deleted_at', DateTime(timezone=False)),
         Column('deleted', Boolean(create_constraint=True, name=None)),
+        Column('name',
+               String(length=255, convert_unicode=False, assert_unicode=None,
+                      unicode_error=None, _warn_on_bytestring=False)),
         Column('id', Integer(),  primary_key=True, nullable=False),
         Column('memory_mb', Integer(),  nullable=False),
         Column('vcpus', Integer(),  nullable=False),
@@ -53,24 +52,32 @@ def upgrade(migrate_engine):
         instance_types.create()
     except Exception:
         logging.info(repr(table))
-        logging.exception('Exception while creating table')
+        logging.exception('Exception while creating instance_types table')
         raise
 
-    # TODO(ken-pepple) fix this to pre-populate the default EC2 types
-    #INSTANCE_TYPES = {
-    #    'm1.tiny': dict(memory_mb=512, vcpus=1, local_gb=0, flavorid=1),
-    #    'm1.small': dict(memory_mb=2048, vcpus=1, local_gb=20, flavorid=2),
-    #    'm1.medium': dict(memory_mb=4096, vcpus=2, local_gb=40, flavorid=3),
-    #    'm1.large': dict(memory_mb=8192, vcpus=4, local_gb=80, flavorid=4),
-    #    'm1.xlarge': dict(memory_mb=16384, vcpus=8, local_gb=160, flavorid=5)}
-    # for instance_type in INSTANCE_TYPES:
-    #	try:
-            # prepopulate tables with EC2 types
+    # Here are the old static instance types
+    INSTANCE_TYPES = {
+    'm1.tiny': dict(memory_mb=512, vcpus=1, local_gb=0, flavorid=1),
+    'm1.small': dict(memory_mb=2048, vcpus=1, local_gb=20, flavorid=2),
+    'm1.medium': dict(memory_mb=4096, vcpus=2, local_gb=40, flavorid=3),
+    'm1.large': dict(memory_mb=8192, vcpus=4, local_gb=80, flavorid=4),
+    'm1.xlarge': dict(memory_mb=16384, vcpus=8, local_gb=160, flavorid=5)}
+    try:
+        i = instance_types.insert()
+        for name, values in INSTANCE_TYPES.iteritems():
+            # FIXME(kpepple) should we be seeding created_at / updated_at ?
+            # the_time = time.strftime("%Y-%m-%d %H:%M:%S")
+            i.execute({'name': name, 'memory_mb': values["memory_mb"],
+                        'vcpus': values["vcpus"],
+                        'local_gb': values["local_gb"],
+                        'flavorid': values["flavorid"]})
+    except Exception:
+        logging.info(repr(table))
+        logging.exception('Exception while seeding instance_types table')
+        raise
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
-    #    # Operations to reverse the above upgrade go here.
-    #    for table in (instance_types):
-    #        table.drop()
-    pass
+    for table in (instance_types):
+        table.drop()
