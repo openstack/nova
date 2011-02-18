cd533e160e9c98a0c14b4e0bc32a6e94c7ab8657
Nirmal Ranganathan <nirmal.ranganathan@rackspace.coom>
2011-02-18 11:44:38 -0600

    Added Author and tests

--- nova/tests/test_compute.py
+++ nova/tests/test_compute.py
@@ -30,6 +30,7 @@ from nova import log as logging
 from nova import test
 from nova import utils
 from nova.auth import manager
+from nova.compute import instance_types
 
 
 LOG = logging.getLogger('nova.tests.compute')
@@ -266,3 +267,11 @@ class ComputeTestCase(test.TestCase):
         self.assertEqual(ret_val, None)
 
         self.compute.terminate_instance(self.context, instance_id)
+
+    def test_get_by_flavor_id(self):
+        type = instance_types.get_by_flavor_id(1)
+        self.assertEqual(type, 'm1.tiny')
+
+        type = instance_types.get_by_flavor_id("1")
+        self.assertEqual(type, 'm1.tiny')
+
