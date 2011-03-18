59d2a315b87fad6d88a31994546d99d859f1849b
Tushar Patil <tushar.vitthal.patil@gmail.com>
2011-03-17 18:26:20 -0700

    Fix for LP Bug #737240

--- smoketests/sysadmin_smoketests.py
+++ smoketests/sysadmin_smoketests.py
@@ -34,8 +34,6 @@ if os.path.exists(os.path.join(possible_topdir, 'nova', '__init__.py')):
 from smoketests import flags
 from smoketests import base
 
-
-
 FLAGS = flags.FLAGS
 flags.DEFINE_string('bundle_kernel', 'openwrt-x86-vmlinuz',
               'Local kernel file to use for bundling tests')
@@ -46,6 +44,8 @@ TEST_PREFIX = 'test%s' % int(random.random() * 1000000)
 TEST_BUCKET = '%s_bucket' % TEST_PREFIX
 TEST_KEY = '%s_key' % TEST_PREFIX
 TEST_GROUP = '%s_group' % TEST_PREFIX
+
+
 class ImageTests(base.UserSmokeTestCase):
     def test_001_can_bundle_image(self):
         self.assertTrue(self.bundle_image(FLAGS.bundle_image))
@@ -148,7 +148,8 @@ class InstanceTests(base.UserSmokeTestCase):
             self.fail('could not ping instance')
 
         if FLAGS.use_ipv6:
-            if not self.wait_for_ping(self.data['instance'].ip_v6, "ping6"):
+            if not self.wait_for_ping(self.data['instance'].dns_name_v6,
+                                      "ping6"):
                 self.fail('could not ping instance v6')
 
     def test_005_can_ssh_to_private_ip(self):
@@ -157,7 +158,7 @@ class InstanceTests(base.UserSmokeTestCase):
             self.fail('could not ssh to instance')
 
         if FLAGS.use_ipv6:
-            if not self.wait_for_ssh(self.data['instance'].ip_v6,
+            if not self.wait_for_ssh(self.data['instance'].dns_name_v6,
                                      TEST_KEY):
                 self.fail('could not ssh to instance v6')
 
@@ -286,8 +287,7 @@ class VolumeTests(base.UserSmokeTestCase):
 
 
 if __name__ == "__main__":
-    suites = {'image': unittest.makeSuite(ImageTests),
+    suites = {
               'instance': unittest.makeSuite(InstanceTests),
-              'volume': unittest.makeSuite(VolumeTests)
               }
     sys.exit(base.run_tests(suites))
