f988df6c6f29d6c885d44c6768297aaf489faf34
Dan Prince <dan.prince@rackspace.com>
2011-03-21 13:58:39 -0400

    Fix pep8 issues in nova/api/openstack/extensions.py.

--- nova/api/openstack/extensions.py
+++ nova/api/openstack/extensions.py
@@ -69,13 +69,13 @@ class ResponseExtensionController(wsgi.Controller):
         content_type = req.best_match_content_type()
         # currently response handlers are un-ordered
         for handler in self.handlers:
-            res=handler(res)
+            res = handler(res)
             try:
                 body = res.body
                 headers = res.headers
             except AttributeError:
                 body = self._serialize(res, content_type)
-                headers={"Content-Type": content_type}
+                headers = {"Content-Type": content_type}
             res = webob.Response()
             res.body = body
             res.headers = headers
