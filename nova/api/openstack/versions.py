186fab6781265b2dc92cb6049c11b390cb38b969
Brian Waldon <brian.waldon@rackspace.com>
2011-03-22 14:09:23 -0400

    fixing copyright

--- nova/api/openstack/versions.py
+++ nova/api/openstack/versions.py
@@ -1,7 +1,6 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# Copyright 2011 United States Government as represented by the
-# Administrator of the National Aeronautics and Space Administration.
+# Copyright 2011 OpenStack LLC.
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
