0dc2140d645d94d585fa8e3e5d189cd776574d28
Koji Iida <iida.koji@lab.ntt.co.jp>
2011-03-23 13:14:54 +0900

    Fix to avoid db migration failure in virtualenv

--- nova/db/sqlalchemy/migrate_repo/versions/012_add_ipv6_flatmanager.py
+++ nova/db/sqlalchemy/migrate_repo/versions/012_add_ipv6_flatmanager.py
@@ -26,6 +26,9 @@ meta = MetaData()
 # Just for the ForeignKey and column creation to succeed, these are not the
 # actual definitions of instances or services.
 #
+instances = Table('instances', meta,
+        Column('id', Integer(),  primary_key=True, nullable=False),
+        )
 
 #
 # Tables to alter
