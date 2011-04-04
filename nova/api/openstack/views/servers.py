a6c283f00d67a5172d8271d7e48bd58484ac6b96
Ken Pepple <ken.pepple@gmail.com>
2011-04-04 16:00:59 -0700

    openstack api metadata responses must be strings

--- nova/api/openstack/views/servers.py
+++ nova/api/openstack/views/servers.py
@@ -82,7 +82,8 @@ class ViewBuilder(object):
         # Return the metadata as a dictionary
         metadata = {}
         for item in inst.get('metadata', []):
-            metadata[item['key']] = item['value']
+            # metadata values must be strings per API
+            metadata[item['key']] = str(item['value'])
         inst_dict['metadata'] = metadata
 
         inst_dict['hostId'] = ''
