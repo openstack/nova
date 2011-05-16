e2a1258458e121f192d639d2804b3d3efa40495f
Justin Shepherd <jshepher@rackspace.com>
2011-05-16 08:26:39 -0500

    Removed tools/clean_vlans and tools/nova-debug from pep8 tests as they are shell scripts

--- run_tests.sh
+++ run_tests.sh
@@ -61,8 +61,7 @@ function run_pep8 {
   echo "Running pep8 ..."
   srcfiles=`find bin -type f ! -name "nova.conf*"`
   srcfiles+=" nova setup.py plugins/xenserver/xenapi/etc/xapi.d/plugins/glance"
-  srcfiles+=" tools/clean-vlans tools/esx/guest_tool.py tools/euca-get-ajax-console"
-  srcfiles+=" tools/install_venv.py tools/nova-debug"
+  srcfiles+=" tools/esx/guest_tool.py tools/euca-get-ajax-console tools/install_venv.py"
   pep8 --repeat --show-pep8 --show-source --exclude=vcsversion.py ${srcfiles}
 }
 
