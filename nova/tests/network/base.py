0708b01f628502947d7c2d128565cff2d97ac5b5
Jason Kölker <jason@koelker.net>
2011-06-01 14:14:25 -0500

    coose the network_manager based on instance variable

--- nova/tests/network/base.py
+++ nova/tests/network/base.py
@@ -15,11 +15,12 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-
+from nova import context
 from nova import db
 from nova import flags
 from nova import log as logging
 from nova import test
+from nova.auth import manager
 from nova.tests.db import fakes as db_fakes
 
 FLAGS = flags.FLAGS
@@ -31,11 +32,30 @@ class NetworkTestCase(test.TestCase):
         super(NetworkTestCase, self).setUp()
         self.flags(connection_type='fake',
                    fake_call=True,
-                   fake_network=True)
+                   fake_network=True,
+                   network_manager=self.network_manager)
         self.manager = manager.AuthManager()
         self.user = self.manager.create_user('netuser',
                                              'netuser',
                                              'netuser')
         self.projects = []
         db_fakes.stub_out_db_network_api(self.stubs)
+        self.network = utils.import_object(FLAGS.network_manager)
+        self.context = context.RequestContext(project=None, user=self.user)
 
+
+class TestFuncs(object):
+    def test_set_network_host(self):
+        host = "fake_test_host"
+        self.assertEqual(self.network.set_network_host(self.context, host),
+                         host)
+
+    def test_allocate_for_instance(self):
+        instance_id = 0
+        project_id = 0
+        type_id = 0
+        ip = self.network.allocate_from_instance(self.context,
+                                                 instance_id=instance_id,
+                                                 project_id=project_id,
+                                                 type_id=type_id)
+        print ip
