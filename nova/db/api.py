0a6aeacfedfd5e666e109b54c5c03908eeb47c31
Todd Willey <todd@ansolabs.com>
2011-06-11 18:38:44 -0400

    fix method chaining in database layer to pass right parameters.

--- nova/db/api.py
+++ nova/db/api.py
@@ -1006,12 +1006,12 @@ def provider_fw_rule_get_all(context):
 
 def provider_fw_rule_get_all_by_cidr(context, cidr):
     """Get all provider-level firewall rules."""
-    return IMPL.provider_fw_rule_get_all_by_cidr(context)
+    return IMPL.provider_fw_rule_get_all_by_cidr(context, cidr)
 
 
 def provider_fw_rule_destroy(context, rule_id):
     """Delete a provider firewall rule from the database."""
-    return IMPL.provider_fw_rule_destroy(context)
+    return IMPL.provider_fw_rule_destroy(context, rule_id)
 
 
 ###################
