220ddb2bd5413ea5fa2bff450f4fb3aba136e909
Isaku Yamahata <yamahata@valinux.co.jp>
2011-06-16 20:29:10 +0900

    api/ec2: check user permission for start/stop instances
    This patch adds precise permission check for start/stop instances.

--- nova/api/ec2/__init__.py
+++ nova/api/ec2/__init__.py
@@ -262,6 +262,8 @@ class Authorizer(wsgi.Middleware):
                 'TerminateInstances': ['projectmanager', 'sysadmin'],
                 'RebootInstances': ['projectmanager', 'sysadmin'],
                 'UpdateInstance': ['projectmanager', 'sysadmin'],
+                'StartInstances': ['projectmanager', 'sysadmin'],
+                'StopInstances': ['projectmanager', 'sysadmin'],
                 'DeleteVolume': ['projectmanager', 'sysadmin'],
                 'DescribeImages': ['all'],
                 'DeregisterImage': ['projectmanager', 'sysadmin'],
