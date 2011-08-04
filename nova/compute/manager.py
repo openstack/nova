5fe92058d0ee11a7e9ea1c8f56b7e9350cf703e4
Matt Dietz <matt.dietz@rackspace.com>
2011-08-04 22:18:52 +0000

    Revert migration now finishes

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -746,8 +746,8 @@ class ComputeManager(manager.SchedulerDependentManager):
                 instance_ref['host'])
         rpc.cast(context, topic,
                 {'method': 'finish_revert_resize',
-                 'args': { 'instance_id': instance_ref['uuid'],
-                           'migration_id': migration_ref['id']},
+                 'args': {'instance_id': instance_ref['uuid'],
+                          'migration_id': migration_ref['id']},
                 })
 
     @exception.wrap_exception(notifier=notifier, publisher_id=publisher_id())
