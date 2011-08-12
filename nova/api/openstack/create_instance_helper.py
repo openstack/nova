91eaa647506a2e343e8c689289529eafea0bc9d3
Christopher MacGown <chris@pistoncloud.com>
2011-08-12 14:33:27 -0700

    Fix ugly little violations before someone says anything

--- nova/api/openstack/create_instance_helper.py
+++ nova/api/openstack/create_instance_helper.py
@@ -178,8 +178,6 @@ class CreateInstanceHelper(object):
     def _handle_quota_error(self, error):
         """
         Reraise quota errors as api-specific http exceptions
-
-
         """
         if error.code == "OnsetFileLimitExceeded":
             expl = _("Personality file limit exceeded")
