3202b7a9193796170fbb25a793e40ff14f9b9621
Scott Moser <smoser@ubuntu.com>
2011-09-08 18:02:02 -0700

    put key into meta-data, not top level 'data'

--- nova/api/ec2/cloud.py
+++ nova/api/ec2/cloud.py
@@ -337,8 +337,9 @@ class CloudController(object):
 
         # public-keys should only show up if it is non-empty (if user specified one)
         if instance_ref['key_name']:
-            data['public-keys'] = {'0': {'_name': instance_ref['key_name'],
-                          'openssh-key': instance_ref['key_data']}}
+            data['meta-data']['public-keys'] = {
+                '0': {'_name': instance_ref['key_name'],
+                      'openssh-key': instance_ref['key_data']}}
 
         for image_type in ['kernel', 'ramdisk']:
             if instance_ref.get('%s_id' % image_type):
