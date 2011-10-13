6835f46738d286591990819356b02670d7524257
Josh Kearney <josh@jk0.org>
2011-10-13 14:15:37 -0500

    Added vcpu_weight to models.
    Fixes bug 873610.
    
    Change-Id: Iba4952b5ef29ade2795e78b55baf41f7063ca136

--- nova/db/sqlalchemy/migrate_repo/versions/051_add_vcpu_weight_to_instance_types.py
+++ nova/db/sqlalchemy/migrate_repo/versions/051_add_vcpu_weight_to_instance_types.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from sqlalchemy import Column, Integer, MetaData, Table, Boolean
+from sqlalchemy import Column, Integer, MetaData, Table
 
 meta = MetaData()
 
