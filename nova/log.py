09004e02272757bfd03b54c18b897dcf76ac010a
Unmesh Gurjar <unmesh.gurjar@vertex.co.in>
2011-12-06 19:33:52 +0530

    Added server name in log messages.

--- nova/log.py
+++ nova/log.py
@@ -46,12 +46,12 @@ from nova import version
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('logging_context_format_string',
-                    '%(asctime)s %(levelname)s %(name)s '
+                    '%(asctime)s %(server_name)s %(levelname)s %(name)s '
                     '[%(request_id)s %(user_id)s '
                     '%(project_id)s] %(message)s',
                     'format string to use for log messages with context')
 flags.DEFINE_string('logging_default_format_string',
-                    '%(asctime)s %(levelname)s %(name)s [-] '
+                    '%(asctime)s %(server_name)s %(levelname)s %(name)s [-] '
                     '%(message)s',
                     'format string to use for log messages without context')
 flags.DEFINE_string('logging_debug_format_suffix',
@@ -164,6 +164,8 @@ class NovaLogger(logging.Logger):
             context = getattr(local.store, 'context', None)
         if context:
             extra.update(_dictify_context(context))
+        global _binary_name
+        extra['server_name'] = _binary_name
         extra.update({"nova_version": version.version_string_with_vcs()})
         return logging.Logger._log(self, level, msg, args, exc_info, extra)
 
@@ -325,6 +327,8 @@ def reset():
 def setup():
     """Setup nova logging."""
     if not isinstance(logging.root, NovaRootLogger):
+        global _binary_name
+        _binary_name = _get_binary_name()
         logging._acquireLock()
         for handler in logging.root.handlers:
             logging.root.removeHandler(handler)
