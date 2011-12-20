dc2c46430158496878255dd2e4d6416804d7c622
Brad Hall <brad@nicira.com>
2011-12-19 19:02:47 -0800

    Add support for port security to QuantumManager
    If enabled, QuantumManager will now pass in the allowed_address_pairs that
    some quantum plugins understand in order to enforce port security on a
    given port.  Any plugins that don't understand the extra fields will just
    ignore them.
    
    Change-Id: I640658036789b319ecefbb5e7dcdcf6b4f4ab34e

--- nova/network/quantum/manager.py
+++ nova/network/quantum/manager.py
@@ -49,6 +49,9 @@ flags.DEFINE_bool('use_melange_mac_generation', False,
 flags.DEFINE_bool('quantum_use_dhcp', False,
                     'Whether or not to enable DHCP for networks')
 
+flags.DEFINE_bool('quantum_use_port_security', False,
+                  'Whether or not to enable port security')
+
 
 class QuantumManager(manager.FlatManager):
     """NetworkManager class that communicates with a Quantum service
@@ -282,14 +285,20 @@ class QuantumManager(manager.FlatManager):
             rxtx_factor = instance_type['rxtx_factor']
             nova_id = self._get_nova_id(context)
             q_tenant_id = project_id or FLAGS.quantum_default_tenant_id
+            # Tell the ipam library to allocate an IP
+            ip = self.ipam.allocate_fixed_ip(context, project_id,
+                    quantum_net_id, vif_rec)
+            pairs = []
+            # Set up port security if enabled
+            if FLAGS.quantum_use_port_security:
+                pairs = [{'mac_address': vif_rec['address'],
+                          'ip_address': ip}]
             self.q_conn.create_and_attach_port(q_tenant_id, quantum_net_id,
                                                vif_rec['uuid'],
                                                vm_id=instance['uuid'],
                                                rxtx_factor=rxtx_factor,
-                                               nova_id=nova_id)
-            # Tell melange to allocate an IP
-            ip = self.ipam.allocate_fixed_ip(context, project_id,
-                    quantum_net_id, vif_rec)
+                                               nova_id=nova_id,
+                                               allowed_address_pairs=pairs)
             # Set up/start the dhcp server for this network if necessary
             if FLAGS.quantum_use_dhcp:
                 self.enable_dhcp(context, quantum_net_id, network_ref,
