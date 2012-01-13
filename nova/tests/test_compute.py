021b4d576c834c120c8e54df4691a49880261f14
Chris Behrens <cbehrens@codestud.com>
2012-01-12 19:11:53 -0800

    Remove skipping of 2 tests
    This fixes bug 892005.  Vish put in the real fix, which is to check for
    Mock objects in to_primitive.  This patch re-enables 2 skipped tests that
    work now.
    
    Change-Id: I0b193cf2df987a10ea16f41ae9f39e96426c496a

--- nova/tests/test_compute.py
+++ nova/tests/test_compute.py
@@ -821,7 +821,6 @@ class ComputeTestCase(BaseTestCase):
                           instance['uuid'])
         self.compute.terminate_instance(self.context, instance['uuid'])
 
-    @test.skip_test('test fails: lp892005')
     def test_instance_set_to_error_on_uncaught_exception(self):
         """Test that instance is set to error state when exception is raised"""
         instance = self._create_fake_instance()
@@ -850,7 +849,6 @@ class ComputeTestCase(BaseTestCase):
 
         self.compute.terminate_instance(self.context, instance['uuid'])
 
-    @test.skip_test('test fails: lp892005')
     def test_network_is_deallocated_on_spawn_failure(self):
         """When a spawn fails the network must be deallocated"""
         instance = self._create_fake_instance()
