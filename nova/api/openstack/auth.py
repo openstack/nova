e48106a2a5086c933d98043952d63c50c25df10b
Pádraig Brady <pbrady@redhat.com>
2012-01-23 15:49:08 +0000

    Bug #920497 fix X-Server-Management-Url for v1.0 noauth API
    This regression was introduced in commit 2aa5bb96,
    which discounted the v1.0 case
    
    Change-Id: I44e03fbea6b79b1c2e2705b1f39e6c7aaaed9265

--- nova/api/openstack/auth.py
+++ nova/api/openstack/auth.py
@@ -43,9 +43,12 @@ class NoAuthMiddleware(wsgi.Middleware):
     @webob.dec.wsgify(RequestClass=wsgi.Request)
     def __call__(self, req):
         if 'X-Auth-Token' not in req.headers:
+            os_url = req.url
+            version = common.get_version_from_href(os_url)
             user_id = req.headers.get('X-Auth-User', 'admin')
             project_id = req.headers.get('X-Auth-Project-Id', 'admin')
-            os_url = os.path.join(req.url, project_id)
+            if version == '1.1':
+                os_url = os.path.join(os_url, project_id)
             res = webob.Response()
             # NOTE(vish): This is expecting and returning Auth(1.1), whereas
             #             keystone uses 2.0 auth.  We should probably allow

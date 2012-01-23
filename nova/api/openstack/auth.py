openstack/nova

commit 2aa5bb969256b5a447b37704d9a2076f2c5bf48d

Updated NoAuth to account for requests ending in /
Fixes bug 882742

(cherry picked from commit 566f564a60fc5309441d6624fc70389ffc28773f)

Change-Id: I19d527c5d16f73de2841e9d69872da6979f71886

nova/api/openstack/auth.py
@@ -16,6 +16,7 @@
 #    under the License.
 
 import hashlib
+import os
 import time
 
 import webob.exc
@@ -42,12 +43,9 @@ class NoAuthMiddleware(wsgi.Middleware):
     @webob.dec.wsgify(RequestClass=wsgi.Request)
     def __call__(self, req):
         if 'X-Auth-Token' not in req.headers:
-            os_url = req.url
-            version = common.get_version_from_href(os_url)
             user_id = req.headers.get('X-Auth-User', 'admin')
             project_id = req.headers.get('X-Auth-Project-Id', 'admin')
-            if version == '1.1':
-                os_url += '/' + project_id
+            os_url = os.path.join(req.url, project_id)
             res = webob.Response()
             # NOTE(vish): This is expecting and returning Auth(1.1), whereas
             #             keystone uses 2.0 auth.  We should probably allow

commit e48106a2a5086c933d98043952d63c50c25df10b

Bug #920497 fix X-Server-Management-Url for v1.0 noauth API
This regression was introduced in commit 2aa5bb96,
which discounted the v1.0 case

Change-Id: I44e03fbea6b79b1c2e2705b1f39e6c7aaaed9265

Authors
@@ -103,6 +103,7 @@ Naveed Massjouni <naveedm9@gmail.com>
 Nikolay Sokolov <nsokolov@griddynamics.com>
 Nirmal Ranganathan <nirmal.ranganathan@rackspace.com>
 Ollie Leahy <oliver.leahy@hp.com>
+Pádraig Brady <pbrady@redhat.com>
 Paul Voccio <paul@openstack.org>
 Renuka Apte <renuka.apte@citrix.com>
 Ricardo Carrillo Cruz <emaildericky@gmail.com>

nova/api/openstack/auth.py
@@ -43,9 +43,12 @@ class NoAuthMiddleware(wsgi.Middleware):
     @webob.dec.wsgify(RequestClass=wsgi.Request)
     def __call__(self, req):
         if 'X-Auth-Token' not in req.headers:
+            os_url = req.url
+            version = common.get_version_from_href(os_url)
             user_id = req.headers.get('X-Auth-User', 'admin')
             project_id = req.headers.get('X-Auth-Project-Id', 'admin')
-            os_url = os.path.join(req.url, project_id)
+            if version == '1.1':
+                os_url = os.path.join(os_url, project_id)
             res = webob.Response()
             # NOTE(vish): This is expecting and returning Auth(1.1), whereas
             #             keystone uses 2.0 auth.  We should probably allow