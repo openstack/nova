702a4743f05370c16c92094fc43de14168031e96
Monty Taylor <mordred@inaugust.com>
2012-02-25 12:44:28 -0800

    Get rid of all of the autodoc import errors.
    This hasn't worked right in a bazillion years.
    
    Change-Id: I4db20ce97f920cdb7254d51e18a0630b0283b39d

--- doc/ext/nova_autodoc.py
+++ doc/ext/nova_autodoc.py
@@ -7,8 +7,6 @@ from nova import utils
 
 
 def setup(app):
-    rootdir = os.path.abspath(app.srcdir + '/..')
-    print "**Autodocumenting from %s" % rootdir
-    os.chdir(rootdir)
-    rv = utils.execute('./generate_autodoc_index.sh')
+    print "**Autodocumenting from %s" % os.path.abspath(os.curdir)
+    rv = utils.execute('./doc/generate_autodoc_index.sh')
     print rv[0]
