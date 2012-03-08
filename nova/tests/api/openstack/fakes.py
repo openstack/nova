68f5b9e4bef40810df4a061b95541e79de72d6f3
Alex Meade <alex.meade@rackspace.com>
2012-03-08 19:08:59 +0000

    Change a fake classes variable to something other than id.
    Removes an old FIXME
    
    Change-Id: I980e0a06bf8b3ca4a7c4ad0bc09a63bdba17c0b6

--- nova/tests/api/openstack/fakes.py
+++ nova/tests/api/openstack/fakes.py
@@ -275,15 +275,14 @@ def stub_out_glance(stubs):
 
 
 class FakeToken(object):
-    # FIXME(sirp): let's not use id here
-    id = 0
+    id_count = 0
 
     def __getitem__(self, key):
         return getattr(self, key)
 
     def __init__(self, **kwargs):
-        FakeToken.id += 1
-        self.id = FakeToken.id
+        FakeToken.id_count += 1
+        self.id = FakeToken.id_count
         for k, v in kwargs.iteritems():
             setattr(self, k, v)
 
