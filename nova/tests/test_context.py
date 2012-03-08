eba95d1e42992056edf6f0d6f84e8e230f331cd7
Chris Behrens <cbehrens@codestud.com>
2012-03-08 00:07:30 -0800

    Add kwargs to RequestContext __init__
    Fixes bug 949726
    
    This allows processing of rpc messages that contain older versions of
    RequestContext to not raise an exception.
    
    Change-Id: I4891a44280fcb8accf0cef1c00c1123029abcc96

--- nova/tests/test_context.py
+++ nova/tests/test_context.py
@@ -44,3 +44,17 @@ class ContextTestCase(test.TestCase):
                           '111',
                           '222',
                           read_deleted=True)
+
+    def test_extra_args_to_context_get_logged(self):
+        info = {}
+
+        def fake_warn(log_msg):
+            info['log_msg'] = log_msg
+
+        self.stubs.Set(context.LOG, 'warn', fake_warn)
+
+        c = context.RequestContext('user', 'project',
+                extra_arg1='meow', extra_arg2='wuff')
+        self.assertTrue(c)
+        self.assertIn("'extra_arg1': 'meow'", info['log_msg'])
+        self.assertIn("'extra_arg2': 'wuff'", info['log_msg'])
