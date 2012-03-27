e064a4ea750a6237dabf03202b1dcb6fa435c7f6
Unmesh Gurjar <unmesh.gurjar@vertex.co.in>
2012-03-27 10:43:02 +0530

    Added img metadata validation. Fixes bug 962117.
    1. Added validation for image metadata.
    2. Renamed the method 'check_img_metadata_quota_limit' to
    'check_img_metadata_quota_limit' since it also validates the image metadata.
    3. Added unit test cases.
    
    Change-Id: I0e9dad97c03070363d14977897701e146870e41a

--- nova/api/openstack/compute/image_metadata.py
+++ nova/api/openstack/compute/image_metadata.py
@@ -64,7 +64,8 @@ class Controller(object):
         if 'metadata' in body:
             for key, value in body['metadata'].iteritems():
                 image['properties'][key] = value
-        common.check_img_metadata_quota_limit(context, image['properties'])
+        common.check_img_metadata_properties_quota(context,
+                                                   image['properties'])
         self.image_service.update(context, image_id, image, None)
         return dict(metadata=image['properties'])
 
@@ -88,7 +89,8 @@ class Controller(object):
 
         image = self._get_image(context, image_id)
         image['properties'][id] = meta[id]
-        common.check_img_metadata_quota_limit(context, image['properties'])
+        common.check_img_metadata_properties_quota(context,
+                                                   image['properties'])
         self.image_service.update(context, image_id, image, None)
         return dict(meta=meta)
 
@@ -98,7 +100,7 @@ class Controller(object):
         context = req.environ['nova.context']
         image = self._get_image(context, image_id)
         metadata = body.get('metadata', {})
-        common.check_img_metadata_quota_limit(context, metadata)
+        common.check_img_metadata_properties_quota(context, metadata)
         image['properties'] = metadata
         self.image_service.update(context, image_id, image, None)
         return dict(metadata=metadata)
