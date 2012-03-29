facb936f0bfc6c78fdce93785078e78223b0ddf7
Dan Prince <dprince@redhat.com>
2012-03-28 22:00:11 -0400

    Update KillFilter to handle 'deleted' exe's.
    Updates KillFilter so that it handles the case where the executable
    linked to by /proc/PID/exe is updated or deleted.
    
    Fixes LP Bug #967931.
    
    Also added a unit test to test that 'deleted' exe's are
    filtered correctly.
    
    (cherry picked from commit b24c11b and commit 3d28e3d)
    
    Change-Id: I368a01383bf62b64b7579d573b8b84640dec03ae

--- nova/rootwrap/filters.py
+++ nova/rootwrap/filters.py
@@ -117,6 +117,10 @@ class KillFilter(CommandFilter):
                 return False
         try:
             command = os.readlink("/proc/%d/exe" % int(args[1]))
+            # NOTE(dprince): /proc/PID/exe may have ' (deleted)' on
+            # the end if an executable is updated or deleted
+            if command.endswith(" (deleted)"):
+                command = command[:command.rindex(" ")]
             if command not in self.args[1]:
                 # Affected executable not in accepted list
                 return False
