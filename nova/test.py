70a712921f1d9253653ebe0d25a2c23d5cf5d750
Russell Bryant <rbryant@redhat.com>
2012-04-02 18:23:09 -0400

    Remove nova.rpc.impl_carrot.
    This module was marked as deprecated and scheduled for removal in Essex.
    Remove it now that Folsom development is open.  nova.rpc.impl_kombu
    should be used instead.
    
    This patch also removes nova.testing.fake.rabbit, since as far as I can
    tell, it isn't used anymore and was the last thing still using the
    carrot dependency.
    
    Change-Id: I8cfb2d09ee5eed439ec1d152261f7097faf08ad6

--- nova/test.py
+++ nova/test.py
@@ -39,7 +39,6 @@ from nova import log as logging
 from nova.openstack.common import cfg
 from nova import utils
 from nova import service
-from nova.testing.fake import rabbit
 from nova.tests import reset_db
 from nova.virt import fake
 
@@ -149,10 +148,6 @@ class TestCase(unittest.TestCase):
             self.mox.VerifyAll()
             super(TestCase, self).tearDown()
         finally:
-            # Clean out fake_rabbit's queue if we used it
-            if FLAGS.fake_rabbit:
-                rabbit.reset_all()
-
             if FLAGS.connection_type == 'fake':
                 if hasattr(fake.FakeConnection, '_instance'):
                     del fake.FakeConnection._instance
