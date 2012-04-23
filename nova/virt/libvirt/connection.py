openstack/nova

commit a837f92e39c74f1a97e358f1a8ecfcdb0915bf6c

Fix libvirt rescue.
Updates libvirt connection.py so that it chowns console.log
correctly when performing a rescue.

Fixes LP Bug #968587.

Change-Id: I38a97df92330a9f20f43e9e4598da3375c6a22dc

nova/virt/libvirt/connection.py
@@ -955,9 +955,7 @@ class LibvirtConnection(driver.ComputeDriver):
         else:
             raise exception.Error(_("Guest does not have a console available"))
 
-        console_log = os.path.join(FLAGS.instances_path, instance['name'],
-                                   'console.log')
-        libvirt_utils.chown(console_log, os.getuid())
+        self._chown_console_log_for_instance(instance['name'])
         data = self._flush_libvirt_console(pty)
         fpath = self._append_to_file(data, console_log)
 
@@ -1099,6 +1097,13 @@ class LibvirtConnection(driver.ComputeDriver):
         libvirt_utils.create_image('raw', target, '%dM' % swap_mb)
         libvirt_utils.mkfs('swap', target)
 
+    @staticmethod
+    def _chown_console_log_for_instance(instance_name):
+        console_log = os.path.join(FLAGS.instances_path, instance_name,
+                                   'console.log')
+        if os.path.exists(console_log):
+            libvirt_utils.chown(console_log, os.getuid())
+
     def _create_image(self, context, instance, libvirt_xml, suffix='',
                       disk_images=None, network_info=None,
                       block_device_info=None):
@@ -1120,6 +1125,9 @@ class LibvirtConnection(driver.ComputeDriver):
         if FLAGS.libvirt_type == 'lxc':
             container_dir = '%s/rootfs' % basepath(suffix='')
             libvirt_utils.ensure_tree(container_dir)
+
+        # NOTE(dprince): for rescue console.log may already exist... chown it.
+        self._chown_console_log_for_instance(instance['name'])
 
         # NOTE(vish): No need add the suffix to console.log
         libvirt_utils.write_to_file(basepath('console.log', ''), '', 007)

commit b2247590ae422588db541f279c7b137e36ee836f

Fixes bug 987335.
Revert bug introduced by commit a837f92e that removed
console_log from get_console_output()

Change-Id: I22a14b5f50c2df0486420b38137328ac87844c1f

nova/virt/libvirt/connection.py
@@ -982,6 +982,7 @@ class LibvirtConnection(driver.ComputeDriver):
 
         self._chown_console_log_for_instance(instance['name'])
         data = self._flush_libvirt_console(pty)
+        console_log = self._get_console_log_path(instance_name)
         fpath = self._append_to_file(data, console_log)
 
         return libvirt_utils.load_file(fpath)
@@ -1132,9 +1133,12 @@ class LibvirtConnection(driver.ComputeDriver):
         libvirt_utils.mkfs('swap', target)
 
     @staticmethod
-    def _chown_console_log_for_instance(instance_name):
-        console_log = os.path.join(FLAGS.instances_path, instance_name,
-                                   'console.log')
+    def _get_console_log_path(instance_name):
+        return os.path.join(FLAGS.instances_path, instance_name,
+                'console.log')
+
+    def _chown_console_log_for_instance(self, instance_name):
+        console_log = self._get_console_log_path(instance_name)
         if os.path.exists(console_log):
             libvirt_utils.chown(console_log, os.getuid())
 