9ec9fdce626d5e2937c6e0664621ad590f958e13
Russell Bryant <rbryant@redhat.com>
2012-05-04 22:56:11 -0400

    Add version to consoleauth rpc API.
    Part of blueprint versioned-rpc-apis.
    
    Change-Id: I9682bdbd06d744141b94385992b37cd2e55b8f5e

---
+++ nova/consoleauth/rpcapi.py
@@ -0,0 +1,53 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012, Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Client side of the consoleauth RPC API.
+"""
+
+from nova import flags
+import nova.rpc.proxy
+
+
+FLAGS = flags.FLAGS
+
+
+class ConsoleAuthAPI(nova.rpc.proxy.RpcProxy):
+    '''Client side of the consoleauth rpc API.
+
+    API version history:
+
+        1.0 - Initial version.
+    '''
+
+    RPC_API_VERSION = '1.0'
+
+    def __init__(self):
+        super(ConsoleAuthAPI, self).__init__(topic=FLAGS.consoleauth_topic,
+                                      default_version=self.RPC_API_VERSION)
+
+    def authorize_console(self, ctxt, token, console_type, host, port,
+                          internal_access_path):
+        # The remote side doesn't return anything, but we want to block
+        # until it completes.
+        return self.call(ctxt,
+                self.make_msg('authorize_console',
+                              token=token, console_type=console_type,
+                              host=host, port=port,
+                              internal_access_path=internal_access_path))
+
+    def check_token(self, ctxt, token):
+        return self.call(ctxt, self.make_msg('check_token', token=token))
