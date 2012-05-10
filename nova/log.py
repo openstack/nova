f6088545d2569423f4911bcddccc3a7eac36baf2
Alessio Ababilov <aababilov@griddynamics.com>
2012-05-10 12:52:03 +0300

    Setup logging, particularly for keystone middleware
    Fixes bug 979282 and bug 988951
    
    Change-Id: I895d00ac9648e8b3cb3ad1bf141c9bfe02ef3680

--- nova/log.py
+++ nova/log.py
@@ -71,6 +71,7 @@ log_opts = [
                   'sqlalchemy=WARN',
                   'boto=WARN',
                   'suds=INFO',
+                  'keystone=INFO',
                   'eventlet.wsgi.server=WARN'
                   ],
                 help='list of logger=LEVEL pairs'),
@@ -384,6 +385,8 @@ def _setup_logging_from_flags():
         level = logging.getLevelName(level_name)
         logger = logging.getLogger(mod)
         logger.setLevel(level)
+        for handler in nova_root.handlers:
+            logger.addHandler(handler)
 
     # NOTE(jkoelker) Clear the handlers for the root logger that was setup
     #                by basicConfig in nova/__init__.py and install the
