openstack/nova

commit ba76b954e69de56f76f9db5cade1780bc351be67

Make use of openstack.common.jsonutils.
This patch imports jsonutils from openstack-common.  It removes the
equivalent code from nova.utils and then converts the code base to use
jsonutils.  The primary motivator for this change was to remove the rest
of the dependencies from nova.rpc on nova.utils.

Change-Id: If43658b9b098ed56cba018c81be268b8c3e2916a

nova/network/quantum/client.py
@@ -21,7 +21,7 @@ import json
 import socket
 import urllib
 
-from nova import utils
+from nova.openstack.common import jsonutils
 
 
 # FIXME(danwent): All content in this file should be removed once the
@@ -35,14 +35,11 @@ class JSONSerializer(object):
     the standard serializer from the quantum library.
     """
     def serialize(self, data, content_type):
-        try:
-            return json.dumps(data)
-        except TypeError:
-            pass
-        return json.dumps(utils.to_primitive(data))
+        jsonutils.dumps(data)
 
     def deserialize(self, data, content_type):
-        return json.loads(data)
+        return jsonutils.loads(data)
+
 
 # Quantum API v1.0 uses 420 + 430 for network + port not found
 # Quantum API v1.1 uses 404 for network + port not found

nova/api/ec2/__init__.py
@@ -38,6 +38,7 @@ from nova import flags
 from nova import log as logging
 from nova.openstack.common import cfg
 from nova.openstack.common import importutils
+from nova.openstack.common import jsonutils
 from nova import utils
 from nova import wsgi
 
@@ -226,7 +227,7 @@ class EC2Token(wsgi.Middleware):
                                         'path': req.path,
                                         'params': auth_params,
                                        }}}
-        creds_json = utils.dumps(creds)
+        creds_json = jsonutils.dumps(creds)
         headers = {'Content-Type': 'application/json'}
 
         # Disable "has no x member" pylint error
@@ -245,7 +246,7 @@ class EC2Token(wsgi.Middleware):
         #             having keystone return token, tenant,
         #             user, and roles from this call.
 
-        result = utils.loads(response)
+        result = jsonutils.loads(response)
         try:
             token_id = result['access']['token']['id']
         except (AttributeError, KeyError), e:
@@ -289,7 +290,7 @@ class EC2KeystoneAuth(wsgi.Middleware):
             creds = {'ec2Credentials': cred_dict}
         else:
             creds = {'auth': {'OS-KSEC2:ec2Credentials': cred_dict}}
-        creds_json = utils.dumps(creds)
+        creds_json = jsonutils.dumps(creds)
         headers = {'Content-Type': 'application/json'}
 
         o = urlparse.urlparse(FLAGS.keystone_ec2_url)
@@ -306,7 +307,7 @@ class EC2KeystoneAuth(wsgi.Middleware):
             else:
                 msg = _("Failure communicating with keystone")
             return ec2_error(req, request_id, "Unauthorized", msg)
-        result = utils.loads(data)
+        result = jsonutils.loads(data)
         conn.close()
 
         try:
@@ -645,7 +646,7 @@ class Executor(wsgi.Application):
                     env.pop(k)
 
             LOG.exception(_('Unexpected error raised: %s'), unicode(ex))
-            LOG.error(_('Environment: %s') % utils.dumps(env))
+            LOG.error(_('Environment: %s') % jsonutils.dumps(env))
             return ec2_error(req, request_id, 'UnknownError',
                              _('An unknown error has occurred. '
                                'Please try your request again.'))

nova/api/openstack/wsgi.py
@@ -26,7 +26,7 @@ import webob
 
 from nova import exception
 from nova import log as logging
-from nova import utils
+from nova.openstack.common import jsonutils
 from nova import wsgi
 
 
@@ -130,7 +130,7 @@ class JSONDeserializer(TextDeserializer):
 
     def _from_json(self, datastring):
         try:
-            return utils.loads(datastring)
+            return jsonutils.loads(datastring)
         except ValueError:
             msg = _("cannot understand JSON")
             raise exception.MalformedRequestBody(reason=msg)
@@ -242,7 +242,7 @@ class JSONDictSerializer(DictSerializer):
     """Default JSON request body serialization"""
 
     def default(self, data):
-        return utils.dumps(data)
+        return jsonutils.dumps(data)
 
 
 class XMLDictSerializer(DictSerializer):
@@ -533,7 +533,7 @@ def action_peek_json(body):
     """Determine action to invoke."""
 
     try:
-        decoded = utils.loads(body)
+        decoded = jsonutils.loads(body)
     except ValueError:
         msg = _("cannot understand JSON")
         raise exception.MalformedRequestBody(reason=msg)

nova/compute/api.py
@@ -40,6 +40,7 @@ import nova.image
 from nova import log as logging
 from nova import network
 from nova.openstack.common import cfg
+from nova.openstack.common import jsonutils
 import nova.policy
 from nova import quota
 from nova import rpc
@@ -652,7 +653,7 @@ class API(BaseAPI):
                 locals())
 
         request_spec = {
-            'image': utils.to_primitive(image),
+            'image': jsonutils.to_primitive(image),
             'instance_properties': base_options,
             'instance_type': instance_type,
             'num_instances': num_instances,
@@ -1482,7 +1483,7 @@ class API(BaseAPI):
             "instance_type_id": new_instance_type['id'],
             "image": image,
             "update_db": False,
-            "request_spec": utils.to_primitive(request_spec),
+            "request_spec": jsonutils.to_primitive(request_spec),
             "filter_properties": filter_properties,
         }
         self._cast_scheduler_message(context,

nova/compute/manager.py
@@ -64,6 +64,7 @@ from nova.notifier import api as notifier
 from nova.openstack.common import cfg
 from nova.openstack.common import excutils
 from nova.openstack.common import importutils
+from nova.openstack.common import jsonutils
 from nova import rpc
 from nova import utils
 from nova.virt import driver
@@ -397,7 +398,7 @@ class ComputeManager(manager.SchedulerDependentManager):
                                                  bdm['device_name'])
                 self.db.block_device_mapping_update(
                         context, bdm['id'],
-                        {'connection_info': utils.dumps(cinfo)})
+                        {'connection_info': jsonutils.dumps(cinfo)})
                 block_device_mapping.append({'connection_info': cinfo,
                                              'mount_device':
                                              bdm['device_name']})
@@ -635,7 +636,7 @@ class ComputeManager(manager.SchedulerDependentManager):
         bdms = self._get_instance_volume_bdms(context, instance_uuid)
         block_device_mapping = []
         for bdm in bdms:
-            cinfo = utils.loads(bdm['connection_info'])
+            cinfo = jsonutils.loads(bdm['connection_info'])
             block_device_mapping.append({'connection_info': cinfo,
                                          'mount_device':
                                          bdm['device_name']})
@@ -1775,7 +1776,7 @@ class ComputeManager(manager.SchedulerDependentManager):
                                mountpoint)
         values = {
             'instance_uuid': instance_ref['uuid'],
-            'connection_info': utils.dumps(connection_info),
+            'connection_info': jsonutils.dumps(connection_info),
             'device_name': mountpoint,
             'delete_on_termination': False,
             'virtual_name': None,
@@ -1799,7 +1800,7 @@ class ComputeManager(manager.SchedulerDependentManager):
         if instance_name not in self.driver.list_instances():
             LOG.warn(_('Detaching volume from unknown instance'),
                      context=context, instance=instance)
-        self.driver.detach_volume(utils.loads(bdm['connection_info']),
+        self.driver.detach_volume(jsonutils.loads(bdm['connection_info']),
                                   instance_name,
                                   mp)
 

nova/network/manager.py
@@ -65,6 +65,7 @@ from nova.network import model as network_model
 from nova.notifier import api as notifier
 from nova.openstack.common import cfg
 from nova.openstack.common import importutils
+from nova.openstack.common import jsonutils
 import nova.policy
 from nova import quota
 from nova import rpc
@@ -194,7 +195,7 @@ class RPCAllocateFixedIP(object):
                 host = rpc.call(context, FLAGS.network_topic,
                                 {'method': 'set_network_host',
                                  'args': {'network_ref':
-                                 utils.to_primitive(network)}})
+                                 jsonutils.to_primitive(network)}})
             if host != self.host:
                 # need to call allocate_fixed_ip to correct network host
                 topic = self.db.queue_get_for(context,

nova/notifier/api.py
@@ -21,6 +21,7 @@ from nova import flags
 from nova import log as logging
 from nova.openstack.common import cfg
 from nova.openstack.common import importutils
+from nova.openstack.common import jsonutils
 from nova import utils
 
 
@@ -121,7 +122,7 @@ def notify(context, publisher_id, event_type, priority, payload):
                  _('%s not in valid priorities') % priority)
 
     # Ensure everything is JSON serializable.
-    payload = utils.to_primitive(payload, convert_instances=True)
+    payload = jsonutils.to_primitive(payload, convert_instances=True)
 
     driver = importutils.import_module(FLAGS.notification_driver)
     msg = dict(message_id=str(uuid.uuid4()),

nova/openstack/common/jsonutils.py
@@ -1,0 +1,133 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# Copyright 2011 Justin Santa Barbara
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+'''
+JSON related utilities.
+
+This module provides a few things:
+
+    1) A handy function for getting an object down to something that can be
+    JSON serialized.  See to_primitive().
+
+    2) Wrappers around loads() and dumps().  The dumps() wrapper will
+    automatically use to_primitive() for you if needed.
+
+    3) This sets up anyjson to use the loads() and dumps() wrappers if anyjson
+    is available.
+'''
+
+
+import datetime
+import inspect
+import itertools
+import json
+
+
+def to_primitive(value, convert_instances=False, level=0):
+    """Convert a complex object into primitives.
+
+    Handy for JSON serialization. We can optionally handle instances,
+    but since this is a recursive function, we could have cyclical
+    data structures.
+
+    To handle cyclical data structures we could track the actual objects
+    visited in a set, but not all objects are hashable. Instead we just
+    track the depth of the object inspections and don't go too deep.
+
+    Therefore, convert_instances=True is lossy ... be aware.
+
+    """
+    nasty = [inspect.ismodule, inspect.isclass, inspect.ismethod,
+             inspect.isfunction, inspect.isgeneratorfunction,
+             inspect.isgenerator, inspect.istraceback, inspect.isframe,
+             inspect.iscode, inspect.isbuiltin, inspect.isroutine,
+             inspect.isabstract]
+    for test in nasty:
+        if test(value):
+            return unicode(value)
+
+    # value of itertools.count doesn't get caught by inspects
+    # above and results in infinite loop when list(value) is called.
+    if type(value) == itertools.count:
+        return unicode(value)
+
+    # FIXME(vish): Workaround for LP bug 852095. Without this workaround,
+    #              tests that raise an exception in a mocked method that
+    #              has a @wrap_exception with a notifier will fail. If
+    #              we up the dependency to 0.5.4 (when it is released) we
+    #              can remove this workaround.
+    if getattr(value, '__module__', None) == 'mox':
+        return 'mock'
+
+    if level > 3:
+        return '?'
+
+    # The try block may not be necessary after the class check above,
+    # but just in case ...
+    try:
+        if isinstance(value, (list, tuple)):
+            o = []
+            for v in value:
+                o.append(to_primitive(v, convert_instances=convert_instances,
+                                      level=level))
+            return o
+        elif isinstance(value, dict):
+            o = {}
+            for k, v in value.iteritems():
+                o[k] = to_primitive(v, convert_instances=convert_instances,
+                                    level=level)
+            return o
+        elif isinstance(value, datetime.datetime):
+            return str(value)
+        elif hasattr(value, 'iteritems'):
+            return to_primitive(dict(value.iteritems()),
+                                convert_instances=convert_instances,
+                                level=level)
+        elif hasattr(value, '__iter__'):
+            return to_primitive(list(value), level)
+        elif convert_instances and hasattr(value, '__dict__'):
+            # Likely an instance of something. Watch for cycles.
+            # Ignore class member vars.
+            return to_primitive(value.__dict__,
+                                convert_instances=convert_instances,
+                                level=level + 1)
+        else:
+            return value
+    except TypeError, e:
+        # Class objects are tricky since they may define something like
+        # __iter__ defined but it isn't callable as list().
+        return unicode(value)
+
+
+def dumps(value):
+    return json.dumps(value, default=to_primitive)
+
+
+def loads(s):
+    return json.loads(s)
+
+
+try:
+    import anyjson
+except ImportError:
+    pass
+else:
+    anyjson._modules.append((__name__, 'dumps', TypeError,
+                                       'loads', ValueError))
+    anyjson.force_implementation(__name__)

commit a5d06c2d12135f91aa2616b3bb8d28aa14c217ca

fixes typo that completely broken Quantum/Nova integration
bug 1000887

fixes bug introduced in ba76b954e69de56f76f9db5cade1780bc351be67 that
prevented the serializer in the Quantum client code from correctly
serializing request body data.  Instead, the serializer always returned
None, meaning that requests would have no body, causing the Quantum
server to return a 400.

Change-Id: I410fb408435da8774f418f0e62ffb59eac2354f3

nova/network/quantum/client.py
@@ -35,7 +35,7 @@ class JSONSerializer(object):
     the standard serializer from the quantum library.
     """
     def serialize(self, data, content_type):
-        jsonutils.dumps(data)
+        return jsonutils.dumps(data)
 
     def deserialize(self, data, content_type):
         return jsonutils.loads(data)