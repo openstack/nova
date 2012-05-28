ff6b72d66ac18331421aedc7f85adefb98a288d7
Matthew Sherborne <msherborne@gmail.com>
2012-05-29 02:37:09 +1000

    Revert "Get rid of all of the autodoc import errors."
    This reverts commit 702a4743f05370c16c92094fc43de14168031e96.
    
    Following the intructions in doc/README.rst and running:
    
        cd doc
        make
    
    The reverted commit, makes things break.
    
    But without that commit, things work.
    
    I assume that there's probably some other way to generate the docs that I don't know about. If that is the case, then the README.rst needs updating.
    
    I asked the commiter if he has any more info to share: https://github.com/openstack/nova/commit/702a4743f05370c16c92094fc43de14168031e96#commitcomment-1384438

--- doc/ext/nova_autodoc.py
+++ doc/ext/nova_autodoc.py
@@ -7,6 +7,8 @@ from nova import utils
 
 
 def setup(app):
-    print "**Autodocumenting from %s" % os.path.abspath(os.curdir)
-    rv = utils.execute('./doc/generate_autodoc_index.sh')
+    rootdir = os.path.abspath(app.srcdir + '/..')
+    print "**Autodocumenting from %s" % rootdir
+    os.chdir(rootdir)
+    rv = utils.execute('./generate_autodoc_index.sh')
     print rv[0]
