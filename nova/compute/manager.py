openstack/nova

commit 957c4ff741b4029cf4bad5087000fba91f0ce938

Cleans up extraneous volume_api calls
 * Partially implements blueprint volume-decoupling
 * Deletes update() which should not be public
 * Removes extra call to update() in rollback live migration that
   was missed in an earlier patch
 * Deletes remove_from_compute() in preference to compute doing
   the call to the destination host directly
 * Removes wait_creation and moves the polling to the one place
   it is used

Change-Id: I63160e0fd048f82937a322bd068c3a9ff4f60336

nova/compute/manager.py
@@ -399,7 +399,12 @@ class ComputeManager(manager.SchedulerDependentManager):
                                              '', '', snapshot)
                 # TODO(yamahata): creating volume simultaneously
                 #                 reduces creation time?
-                self.volume_api.wait_creation(context, vol)
+                # TODO(yamahata): eliminate dumb polling
+                while True:
+                    volume = self.volume_api.get(context, vol['id'])
+                    if volume['status'] != 'creating':
+                        break
+                    greenthread.sleep(1)
                 self.db.block_device_mapping_update(
                     context, bdm['id'], {'volume_id': vol['id']})
                 bdm['volume_id'] = vol['id']
@@ -2216,11 +2221,11 @@ class ComputeManager(manager.SchedulerDependentManager):
                                                   instance_ref['uuid']):
             volume_id = bdm['volume_id']
             volume = self.volume_api.get(context, volume_id)
-            self.volume_api.update(context, volume, {'status': 'in-use'})
-            self.volume_api.remove_from_compute(context,
-                                                volume,
-                                                instance_ref['id'],
-                                                dest)
+            rpc.call(context,
+                     rpc.queue_get_for(context, FLAGS.compute_topic, host),
+                     {"method": "remove_volume_connection",
+                      "args": {'instance_id': instance_ref['id'],
+                               'volume_id': volume['id']}})
 
         # Block migration needs empty image at destination host
         # before migration starts, so if any failure occurs,

nova/volume/api.py
@@ -22,8 +22,6 @@ Handles all requests relating to volumes.
 
 import functools
 
-from eventlet import greenthread
-
 from nova.db import base
 from nova import exception
 from nova import flags
@@ -122,15 +120,6 @@ class API(base.Base):
                            "reservations": reservations}})
         return volume
 
-    # TODO(yamahata): eliminate dumb polling
-    def wait_creation(self, context, volume):
-        volume_id = volume['id']
-        while True:
-            volume = self.get(context, volume_id)
-            if volume['status'] != 'creating':
-                return
-            greenthread.sleep(1)
-
     @wrap_check_policy
     def delete(self, context, volume):
         volume_id = volume['id']
@@ -155,10 +144,6 @@ class API(base.Base):
                  rpc.queue_get_for(context, FLAGS.volume_topic, host),
                  {"method": "delete_volume",
                   "args": {"volume_id": volume_id}})
-
-    @wrap_check_policy
-    def update(self, context, volume, fields):
-        self.db.volume_update(context, volume['id'], fields)
 
     def get(self, context, volume_id):
         rv = self.db.volume_get(context, volume_id)
@@ -235,22 +220,16 @@ class API(base.Base):
             msg = _("already detached")
             raise exception.InvalidVolume(reason=msg)
 
-    def remove_from_compute(self, context, volume, instance_id, host):
-        """Remove volume from specified compute host."""
-        rpc.call(context,
-                 rpc.queue_get_for(context, FLAGS.compute_topic, host),
-                 {"method": "remove_volume_connection",
-                  "args": {'instance_id': instance_id,
-                           'volume_id': volume['id']}})
-
     @wrap_check_policy
     def reserve_volume(self, context, volume):
-        self.update(context, volume, {"status": "attaching"})
+        self.db.volume_update(context, volume['id'], {"status": "attaching"})
 
     @wrap_check_policy
     def unreserve_volume(self, context, volume):
         if volume['status'] == "attaching":
-            self.update(context, volume, {"status": "available"})
+            self.db.volume_update(context,
+                                  volume['id'],
+                                  {"status": "available"})
 
     @wrap_check_policy
     def attach(self, context, volume, instance_uuid, mountpoint):

commit 164d233371fa1acbe695136df30366c4547604d0

Fix destination host for remove_volume_connection.
This patch fixes a regression in
957c4ff741b4029cf4bad5087000fba91f0ce938.  The destination host was
accidentally changed to 'host' when it should have been 'dest'.

Change-Id: I2b262c4aedc2caa0c98c7660853358fc59935b63

nova/compute/manager.py
@@ -2222,7 +2222,7 @@ class ComputeManager(manager.SchedulerDependentManager):
             volume_id = bdm['volume_id']
             volume = self.volume_api.get(context, volume_id)
             rpc.call(context,
-                     rpc.queue_get_for(context, FLAGS.compute_topic, host),
+                     rpc.queue_get_for(context, FLAGS.compute_topic, dest),
                      {"method": "remove_volume_connection",
                       "args": {'instance_id': instance_ref['id'],
                                'volume_id': volume['id']}})