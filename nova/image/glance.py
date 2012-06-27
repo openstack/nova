8c1eee19d13043cde66c18c64122b8352643ec70
MotoKen <motokentsai@gmail.com>
2012-06-27 09:51:30 +0800

    Adds missing 'owner' attribute to image.
    Fixes bug 1018216.
    
    Change-Id: I5540e0b8ab1df1904778e3edc250369b0930987e

--- nova/image/glance.py
+++ nova/image/glance.py
@@ -452,7 +452,7 @@ def _convert_to_string(metadata):
 
 
 def _limit_attributes(image_meta):
-    IMAGE_ATTRIBUTES = ['size', 'disk_format',
+    IMAGE_ATTRIBUTES = ['size', 'disk_format', 'owner',
                         'container_format', 'checksum', 'id',
                         'name', 'created_at', 'updated_at',
                         'deleted_at', 'deleted', 'status',
