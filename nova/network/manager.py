a253151a43c74ad838a5cde6fcd25ffdbe44bb15
Alessio Ababilov <aababilov@griddynamics.com>
2012-07-12 11:35:43 +0300

    overriden VlanManager.create_networks must return a result
    Fix bug #1022469
    
    Change-Id: Iaaa016ea61dd8d26dcd66603abb6f66d22a15bb5

--- nova/network/manager.py
+++ nova/network/manager.py
@@ -1903,7 +1903,8 @@ class VlanManager(RPCAllocateFixedIP, FloatingIP, NetworkManager):
                   '%(num_networks)s. Network size is %(network_size)s') %
                   kwargs)
 
-        NetworkManager.create_networks(self, context, vpn=True, **kwargs)
+        return NetworkManager.create_networks(
+            self, context, vpn=True, **kwargs)
 
     def _setup_network_on_host(self, context, network):
         """Sets up network on this host."""
