109a4321d4488cd8a6c3be8b8cfcb7baad819de6
Rick Harris <rconradharris@gmail.com>
2012-07-23 16:59:52 +0000

    Log snapshot UUID and not OpaqueRef.
    The OpaqueRef doesn't help troubleshooting since it can't be easily used
    to identify an image after the session finishes. UUID, which is
    invariant, is much more useful.
    
    Change-Id: I3b802e41e583bee84ee6f5d05e1ffc581761571d

--- nova/virt/xenapi/vm_utils.py
+++ nova/virt/xenapi/vm_utils.py
@@ -487,10 +487,8 @@ def snapshot_attached_here(session, instance, vm_ref, label):
 
 def _create_snapshot(session, instance, vm_ref, label):
     """Creates Snapshot (Template) VM, Snapshot VBD, Snapshot VDI,
-    Snapshot VHD"""
-    LOG.debug(_("Snapshotting with label '%(label)s'"), locals(),
-              instance=instance)
-
+    Snapshot VHD
+    """
     vm_vdi_ref, vm_vdi_rec = get_vdi_for_vm_safely(session, vm_ref)
 
     original_parent_uuid = _get_vhd_parent_uuid(session, vm_vdi_ref)
@@ -499,8 +497,8 @@ def _create_snapshot(session, instance, vm_ref, label):
     template_vdi_rec = get_vdi_for_vm_safely(session, template_vm_ref)[1]
     template_vdi_uuid = template_vdi_rec["uuid"]
 
-    LOG.debug(_('Created snapshot %(template_vm_ref)s'), locals(),
-              instance=instance)
+    LOG.debug(_("Created snapshot %(template_vdi_uuid)s with label"
+                " '%(label)s'"), locals(), instance=instance)
 
     sr_ref = vm_vdi_rec["SR"]
     parent_uuid, base_uuid = _wait_for_vhd_coalesce(
