ced45de5984f1d89c238f70337cc0da2a150383f
Vishvananda Ishaya <vishvananda@gmail.com>
2012-08-03 18:44:56 -0700

    Adds ability to inherit wsgi extensions
    Needed for blueprint disable-server-extensions
    
    It is possible to create a wsgi resource that extends another
    resource by using the same controller, but any extensions that
    have been applied to the original resource will have no affect.
    
    This adds a new parameter to ResourceExtension that allows it
    to inherit extensions from another resource. This is necessary
    because we are moving key funcionality of the /servers resource
    into extensions, and some other resources (like create-server-ext)
    extend that functionality. This way we can keep the functionality
    of the other extensions.
    
    Change-Id: I21b4c2569c35d59c1f466642355564084a277aea

--- nova/api/openstack/extensions.py
+++ nova/api/openstack/extensions.py
@@ -289,9 +289,9 @@ class ControllerExtension(object):
 class ResourceExtension(object):
     """Add top level resources to the OpenStack API in nova."""
 
-    def __init__(self, collection, controller, parent=None,
+    def __init__(self, collection, controller=None, parent=None,
                  collection_actions=None, member_actions=None,
-                 custom_routes_fn=None):
+                 custom_routes_fn=None, inherits=None):
         if not collection_actions:
             collection_actions = {}
         if not member_actions:
@@ -302,6 +302,7 @@ class ResourceExtension(object):
         self.collection_actions = collection_actions
         self.member_actions = member_actions
         self.custom_routes_fn = custom_routes_fn
+        self.inherits = inherits
 
 
 def wrap_errors(fn):
