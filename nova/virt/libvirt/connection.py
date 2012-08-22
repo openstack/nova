86fb73623f80b6549c46d5b2af35fa6390070c20
Dan Smith <danms@us.ibm.com>
2012-08-22 13:54:35 -0400

    Fix exception handling in libvirt attach_volume()
    Currently, the real reason for a failure is dropped when attempting
    to unroll the volume connection (with the exception of when it is
    VIR_ERR_OPERATION_FAILED). This change uses save_and_reraise_exception()
    to correct that so that the actual reason for failure gets logged.
    
    Fixes bug 1029463
    
    Change-Id: Id47db565c4fb5a88d1a263600b41706dd3419726
    Signed-off-by: Aaron Rosen <arosen@nicira.com>

--- nova/virt/libvirt/connection.py
+++ nova/virt/libvirt/connection.py
@@ -537,15 +537,17 @@ class LibvirtConnection(driver.ComputeDriver):
             try:
                 virt_dom.attachDevice(xml)
             except Exception, ex:
-                self.volume_driver_method('disconnect_volume',
-                                           connection_info,
-                                           mount_device)
-
                 if isinstance(ex, libvirt.libvirtError):
                     errcode = ex.get_error_code()
                     if errcode == libvirt.VIR_ERR_OPERATION_FAILED:
+                        self.volume_driver_method('disconnect_volume',
+                                                   connection_info,
+                                                   mount_device)
                         raise exception.DeviceIsBusy(device=mount_device)
-                raise
+                with utils.save_and_reraise_exception():
+                    self.volume_driver_method('disconnect_volume',
+                                               connection_info,
+                                               mount_device)
 
     @staticmethod
     def _get_disk_xml(xml, device):
