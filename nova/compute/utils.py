91734bad9139555294fe088d2c2d77a9712652ab
Vishvananda Ishaya <vishvananda@gmail.com>
2012-09-17 16:02:06 -0700

    Fixes error handling during schedule_run_instance
    If there are not enough hosts available during a multi-instance launch,
    every failing instance should be updated to error state, instead of
    just the first instance. Currently only the first instance is set
    to Error and the rest stay in building.
    
    This patch makes a number of fixes to error handling during scheduling.
    
     * Moves instance faults into compute utils so they can be created
       from the scheduler.
     * Moves error handling into the driver so that each instance can be
       updated separately.
     * Sets an instance fault for failed scheduling
     * Sets task state back to none if there is a scheduling failure
     * Modifies chance scheduler to stop returning a list of instances
       as it is not used.
     * Modifies tests to check for these states.
    
    In addition to the included tests, the code was manually verified on
    a devstack install
    
    Fixes bug 1051066
    Fixes bug 1019017
    
    Change-Id: I49267ce4a21e2f7cc7a996fb2ed5d625f6794730

--- nova/compute/utils.py
+++ nova/compute/utils.py
@@ -18,6 +18,7 @@
 
 import re
 import string
+import traceback
 
 from nova import block_device
 from nova import db
@@ -31,6 +32,27 @@ from nova import utils
 
 FLAGS = flags.FLAGS
 LOG = log.getLogger(__name__)
+
+
+def add_instance_fault_from_exc(context, instance_uuid, fault, exc_info=None):
+    """Adds the specified fault to the database."""
+
+    code = 500
+    if hasattr(fault, "kwargs"):
+        code = fault.kwargs.get('code', 500)
+
+    details = unicode(fault)
+    if exc_info and code == 500:
+        tb = exc_info[2]
+        details += '\n' + ''.join(traceback.format_tb(tb))
+
+    values = {
+        'instance_uuid': instance_uuid,
+        'code': code,
+        'message': fault.__class__.__name__,
+        'details': unicode(details),
+    }
+    db.instance_fault_create(context, values)
 
 
 def get_device_name_for_instance(context, instance, device):
