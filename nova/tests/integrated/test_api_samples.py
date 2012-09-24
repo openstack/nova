7090ef94372c9ed1da4b11664611da240d74a841
Nikola Dipanov <ndipanov@redhat.com>
2012-09-24 17:09:52 +0200

    Integration tests virtual interfaces API extension
    Adds tests and templates for testing of the virtual interfaces
    API extension. Tests both json and XML API functionality.
    
    Change-Id: I95f4e6667c3c2df123246d73498f13d826e3866a

--- nova/tests/integrated/test_api_samples.py
+++ nova/tests/integrated/test_api_samples.py
@@ -964,3 +964,23 @@ class RescueJsonTest(ServersSampleBase):
 
 class RescueXmlTest(RescueJsonTest):
     ctype = 'xml'
+
+
+class VirtualInterfacesJsonTest(ServersSampleBase):
+    extension_name = ("nova.api.openstack.compute.contrib"
+                     ".virtual_interfaces.Virtual_interfaces")
+
+    def test_vifs_list(self):
+        uuid = self._post_server()
+
+        response = self._do_get('servers/%s/os-virtual-interfaces' % uuid)
+        self.assertEqual(response.status, 200)
+
+        subs = self._get_regexes()
+        subs['mac_addr'] = '(?:[a-f0-9]{2}:){5}[a-f0-9]{2}'
+
+        self._verify_response('vifs-list-resp', subs, response)
+
+
+class VirtualInterfacesXmlTest(VirtualInterfacesJsonTest):
+    ctype = 'xml'
