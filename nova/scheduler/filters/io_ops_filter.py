2c50abe05af33d7150cbeec40bcdf31a626c0844
Brian Elliott <brian.elliott@rackspace.com>
2012-09-28 02:54:51 +0000

    Max I/O ops per host scheduler filter
    Provide ability to filter hosts based on the number of concurrent
    build, resize, and image snapshot operations in progress.
    
    FLAGS.max_io_ops_per_host is used to control the number of concurrent
    permitted operations.
    
    Change-Id: I77feca00a6b6b248de8423cef7fb8da2cf704930

---
+++ nova/scheduler/filters/io_ops_filter.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2012 OpenStack, LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nova import flags
+from nova.openstack.common import cfg
+from nova.openstack.common import log as logging
+from nova.scheduler import filters
+
+LOG = logging.getLogger(__name__)
+
+max_io_ops_per_host_opt = cfg.IntOpt("max_io_ops_per_host",
+        default=8,
+        help="Ignore hosts that have too many builds/resizes/snaps/migrations")
+
+FLAGS = flags.FLAGS
+FLAGS.register_opt(max_io_ops_per_host_opt)
+
+
+class IoOpsFilter(filters.BaseHostFilter):
+    """Filter out hosts with too many concurrent I/O operations"""
+
+    def host_passes(self, host_state, filter_properties):
+        """Use information about current vm and task states collected from
+        compute node statistics to decide whether to filter.
+        """
+        num_io_ops = host_state.num_io_ops
+        max_io_ops = FLAGS.max_io_ops_per_host
+        passes = num_io_ops < max_io_ops
+        if not passes:
+            LOG.debug(_("%(host_state)s fails I/O ops check: Max IOs per host "
+                        "is set to %(max_io_ops)s"), locals())
+        return passes
