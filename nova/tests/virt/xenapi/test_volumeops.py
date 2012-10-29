bbee6536d98f0ad7e20f302ff1936cfb9cda0c2c
Mate Lakat <mate.lakat@citrix.com>
2012-10-29 13:51:34 +0000

    refactor: extract method: connect_volume
    Related to blueprint xenapi-volume-drivers
    
    Extract connect_volume call from attach_volume, and a test for
    attach_volume. By extracting this call, it will be easier to implement
    other drivers, by providing an alternate implementation for this
    extracted method.
    
    Change-Id: Ie5a17ec7fada26a9df5ba8a29ed0dadeb02516e8

---
+++ nova/tests/virt/xenapi/test_volumeops.py
@@ -0,0 +1,40 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2012 Citrix Systems, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nova import test
+from nova.virt.xenapi import volumeops
+import unittest
+
+
+class VolumeAttachTestCase(test.TestCase):
+    def test_connect_volume_call(self):
+        ops = volumeops.VolumeOps('session')
+        self.mox.StubOutWithMock(ops, 'connect_volume')
+        self.mox.StubOutWithMock(volumeops.vm_utils, 'vm_ref_or_raise')
+        self.mox.StubOutWithMock(volumeops.volume_utils, 'get_device_number')
+
+        volumeops.vm_utils.vm_ref_or_raise('session', 'instance_1').AndReturn(
+            'vmref')
+
+        volumeops.volume_utils.get_device_number('mountpoint').AndReturn(
+            'devnumber')
+
+        ops.connect_volume('conn_data', 'devnumber', 'instance_1', 'vmref')
+
+        self.mox.ReplayAll()
+        ops.attach_volume(
+            dict(driver_volume_type='iscsi', data='conn_data'),
+            'instance_1', 'mountpoint')
