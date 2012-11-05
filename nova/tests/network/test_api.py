openstack/nova

commit d96102b6251d489574132104f8ad9db110286415

Add rxtx_factor to network migration logic
Currently, when an instance is migrated from one host to another, the
only action the network API is able to take is moving floating IP
addresses.  We also need to be able to update the rxtx_factor value
to update QoS settings.  This change makes sure that the network APIs
will be passed the rxtx_factor value during a migration.

Change-Id: If4e1e96b84ee082c2d277c3d91da7df0325fc1ff

nova/network/api.py
@@ -21,6 +21,7 @@ import functools
 import inspect
 
 from nova.db import base
+from nova import exception
 from nova import flags
 from nova.network import model as network_model
 from nova.network import rpcapi as network_rpcapi
@@ -324,8 +325,11 @@ class API(base.Base):
         self.network_rpcapi.setup_networks_on_host(context, **args)
 
     def _is_multi_host(self, context, instance):
-        fixed_ips = self.db.fixed_ip_get_by_instance(context, instance['uuid'])
-
+        try:
+            fixed_ips = self.db.fixed_ip_get_by_instance(context,
+                                                         instance['uuid'])
+        except exception.FixedIpNotFoundForInstance:
+            return False
         network = self.db.network_get(context, fixed_ips[0]['network_id'],
                                       project_only=True)
         return network['multi_host']
@@ -335,22 +339,36 @@ class API(base.Base):
                                                             instance['uuid'])
         return [floating_ip['address'] for floating_ip in floating_ips]
 
-    def migrate_instance_start(self, context, instance, host):
+    def migrate_instance_start(self, context, instance, migration):
         """Start to migrate the network of an instance"""
-        if self._is_multi_host(context, instance):
-            addresses = self._get_floating_ip_addresses(context, instance)
-            if addresses:
-                self.network_rpcapi.migrate_instance_start(context,
-                                                           instance['uuid'],
-                                                           addresses,
-                                                           host)
+        args = dict(
+            instance_uuid=instance['uuid'],
+            rxtx_factor=instance['instance_type']['rxtx_factor'],
+            project_id=instance['project_id'],
+            source_compute=migration['source_compute'],
+            dest_compute=migration['dest_compute'],
+            floating_addresses=None,
+        )
 
-    def migrate_instance_finish(self, context, instance, dest):
-        """Finish migrating the network of an instance"""
         if self._is_multi_host(context, instance):
-            addresses = self._get_floating_ip_addresses(context, instance)
-            if addresses:
-                self.network_rpcapi.migrate_instance_finish(context,
-                                                            instance['uuid'],
-                                                            addresses,
-                                                            dest)
+            args['floating_addresses'] = \
+                self._get_floating_ip_addresses(context, instance)
+
+        self.network_rpcapi.migrate_instance_start(context, **args)
+
+    def migrate_instance_finish(self, context, instance, migration):
+        """Finish migrating the network of an instance"""
+        args = dict(
+            instance_uuid=instance['uuid'],
+            rxtx_factor=instance['instance_type']['rxtx_factor'],
+            project_id=instance['project_id'],
+            source_compute=migration['source_compute'],
+            dest_compute=migration['dest_compute'],
+            floating_addresses=None,
+        )
+
+        if self._is_multi_host(context, instance):
+            args['floating_addresses'] = \
+                self._get_floating_ip_addresses(context, instance)
+
+        self.network_rpcapi.migrate_instance_finish(context, **args)

nova/network/rpcapi.py
@@ -34,6 +34,7 @@ class NetworkAPI(rpc_proxy.RpcProxy):
 
         1.0 - Initial version.
         1.1 - Adds migrate_instance_[start|finish]
+        1.2 - Make migrate_instance_[start|finish] a little more flexible
     '''
 
     #
@@ -264,19 +265,32 @@ class NetworkAPI(rpc_proxy.RpcProxy):
         self.cast(ctxt, self.make_msg('release_fixed_ip', address=address),
                   topic=rpc.queue_get_for(ctxt, self.topic, host))
 
-    def migrate_instance_start(self, ctxt, instance_uuid,
-                               floating_addresses, host):
-        return self.call(ctxt, self.make_msg('migrate_instance_start',
-                                             instance_uuid=instance_uuid,
-                                    floating_addresses=floating_addresses),
-                         topic=rpc.queue_get_for(ctxt, self.topic, host),
-                         version='1.1')
+    def migrate_instance_start(self, ctxt, instance_uuid, rxtx_factor,
+                               project_id, source_compute, dest_compute,
+                               floating_addresses):
+        return self.call(ctxt, self.make_msg(
+                'migrate_instance_start',
+                instance_uuid=instance_uuid,
+                rxtx_factor=rxtx_factor,
+                project_id=project_id,
+                source=source_compute,
+                dest=dest_compute,
+                floating_addresses=floating_addresses),
+                         topic=rpc.queue_get_for(ctxt, self.topic,
+                                                 dest_compute),
+                         version='1.2')
 
-    def migrate_instance_finish(self, ctxt, instance_uuid,
-                                floating_addresses, dest):
-        return self.call(ctxt, self.make_msg('migrate_instance_finish',
-                                             instance_uuid=instance_uuid,
-                                    floating_addresses=floating_addresses,
-                                             host=dest),
-                         topic=rpc.queue_get_for(ctxt, self.topic, dest),
-                         version='1.1')
+    def migrate_instance_finish(self, ctxt, instance_uuid, rxtx_factor,
+                                project_id, source_compute, dest_compute,
+                                floating_addresses):
+        return self.call(ctxt, self.make_msg(
+                'migrate_instance_finish',
+                instance_uuid=instance_uuid,
+                rxtx_factor=rxtx_factor,
+                project_id=project_id,
+                source=source_compute,
+                dest=dest_compute,
+                floating_addresses=floating_addresses),
+                         topic=rpc.queue_get_for(ctxt, self.topic,
+                                                 dest_compute),
+                         version='1.2')

nova/network/quantumv2/api.py
@@ -508,13 +508,15 @@ class API(base.Base):
         fip = self._get_floating_ip_by_address(client, address)
         client.update_floatingip(fip['id'], {'floatingip': {'port_id': None}})
 
-    def migrate_instance_start(self, context, instance, host):
+    def migrate_instance_start(self, context, instance_uuid, rxtx_factor,
+                               project_id, source, dest, floating_addresses):
         """Start to migrate the network of an instance"""
         # NOTE(wenjianhn): just pass to make migrate instance doesn't
         # raise for now.
         pass
 
-    def migrate_instance_finish(self, context, instance, dest):
+    def migrate_instance_finish(self, context, instance_uuid, rxtx_factor,
+                                project_id, source, dest, floating_addresses):
         """Finish migrating the network of an instance"""
         # NOTE(wenjianhn): just pass to make migrate instance doesn't
         # raise for now.

nova/compute/manager.py
@@ -1486,10 +1486,8 @@ class ComputeManager(manager.SchedulerDependentManager):
             self.network_api.setup_networks_on_host(context, instance,
                                                     teardown=True)
 
-            if migration_ref['dest_compute'] != \
-                                   migration_ref['source_compute']:
-                self.network_api.migrate_instance_start(context, instance,
-                                                migration_ref['dest_compute'])
+            self.network_api.migrate_instance_start(context, instance,
+                                                    migration_ref)
 
             network_info = self._get_instance_nw_info(context, instance)
             block_device_info = self._get_instance_volume_block_device_info(
@@ -1548,11 +1546,6 @@ class ComputeManager(manager.SchedulerDependentManager):
                                        self._legacy_nw_info(network_info),
                                        block_device_info)
 
-            if migration_ref['dest_compute'] != \
-                                    migration_ref['source_compute']:
-                self.network_api.migrate_instance_finish(context, instance,
-                                            migration_ref['source_compute'])
-
             # Just roll back the record. There's no need to resize down since
             # the 'old' VM already has the preferred attributes
             self._instance_update(context,
@@ -1563,10 +1556,15 @@ class ComputeManager(manager.SchedulerDependentManager):
                                   ephemeral_gb=instance_type['ephemeral_gb'],
                                   instance_type_id=instance_type['id'],
                                   launched_at=timeutils.utcnow(),
-                                  vm_state=vm_states.ACTIVE,
-                                  task_state=None,
                                   expected_task_state=task_states.
                                       RESIZE_REVERTING)
+
+            self.network_api.migrate_instance_finish(context, instance,
+                                                     migration_ref)
+
+            self._instance_update(context, instance['uuid'],
+                                  vm_state=vm_states.ACTIVE,
+                                  task_state=None)
 
             self.db.migration_update(elevated, migration_id,
                     {'status': 'reverted'})
@@ -1673,9 +1671,8 @@ class ComputeManager(manager.SchedulerDependentManager):
 
             self._terminate_volume_connections(context, instance)
 
-            if migration['dest_compute'] != migration['source_compute']:
-                self.network_api.migrate_instance_start(context, instance,
-                                                           self.host)
+            self.network_api.migrate_instance_start(context, instance,
+                                                    migration)
 
             migration = self.db.migration_update(elevated,
                                                  migration['id'],
@@ -1725,9 +1722,8 @@ class ComputeManager(manager.SchedulerDependentManager):
         self.network_api.setup_networks_on_host(context, instance,
                                                 migration['dest_compute'])
 
-        if migration['dest_compute'] != migration['source_compute']:
-            self.network_api.migrate_instance_finish(context, instance,
-                                                     migration['dest_compute'])
+        self.network_api.migrate_instance_finish(context, instance,
+                                                 migration)
 
         network_info = self._get_instance_nw_info(context, instance)
 

nova/network/manager.py
@@ -663,8 +663,13 @@ class FloatingIP(object):
         return False if floating_ip.get('fixed_ip_id') else True
 
     @wrap_check_policy
-    def migrate_instance_start(self, context, instance_uuid,
-                               floating_addresses):
+    def migrate_instance_start(self, context, instance_uuid, rxtx_factor,
+                               project_id, source, dest, floating_addresses):
+        # We only care if floating_addresses are provided and we're
+        # switching hosts
+        if not floating_addresses or source == dest:
+            return
+
         LOG.info(_("Starting migration network for instance"
                    " %(instance_uuid)s"), locals())
         for address in floating_addresses:
@@ -691,8 +696,13 @@ class FloatingIP(object):
                                        {'host': None})
 
     @wrap_check_policy
-    def migrate_instance_finish(self, context, instance_uuid,
-                                floating_addresses, host):
+    def migrate_instance_finish(self, context, instance_uuid, rxtx_factor,
+                                project_id, source, dest, floating_addresses):
+        # We only care if floating_addresses are provided and we're
+        # switching hosts
+        if not floating_addresses or source == dest:
+            return
+
         LOG.info(_("Finishing migration network for instance"
                    " %(instance_uuid)s"), locals())
 
@@ -708,7 +718,7 @@ class FloatingIP(object):
 
             self.db.floating_ip_update(context,
                                        floating_ip['address'],
-                                       {'host': host})
+                                       {'host': dest})
 
             interface = FLAGS.public_interface or floating_ip['interface']
             fixed_ip = self.db.fixed_ip_get(context,
@@ -1943,6 +1953,14 @@ class FlatManager(NetworkManager):
     def get_floating_ips_by_fixed_address(self, context, fixed_address):
         """Returns the floating IPs associated with a fixed_address"""
         return []
+
+    def migrate_instance_start(self, context, instance_uuid, rxtx_factor,
+                               project_id, source, dest, floating_addresses):
+        pass
+
+    def migrate_instance_finish(self, context, instance_uuid, rxtx_factor,
+                                project_id, source, dest, floating_addresses):
+        pass
 
 
 class FlatDHCPManager(RPCAllocateFixedIP, FloatingIP, NetworkManager):

nova/tests/network/test_manager.py
@@ -1667,7 +1667,9 @@ class FloatingIPTestCase(test.TestCase):
         self.mox.ReplayAll()
         floating_ip_addresses = ['172.24.4.23', '172.24.4.24', '172.24.4.25']
         self.network.migrate_instance_start(self.context, FAKEUUID,
-                                               floating_ip_addresses)
+                                            3, self.project_id,
+                                            'fake_source', 'fake_dest',
+                                            floating_ip_addresses)
 
         self.assertEqual(called['count'], 2)
 
@@ -1703,8 +1705,9 @@ class FloatingIPTestCase(test.TestCase):
         self.mox.ReplayAll()
         floating_ip_addresses = ['172.24.4.23', '172.24.4.24', '172.24.4.25']
         self.network.migrate_instance_finish(self.context, FAKEUUID,
-                                               floating_ip_addresses,
-                                               'fake_dest')
+                                             3, self.project_id,
+                                             'fake_source', 'fake_dest',
+                                             floating_ip_addresses)
 
         self.assertEqual(called['count'], 2)
 

commit b3843021b78a5150d8fd577fda523a52f8ce5b26

Fix migrations when not using multi-host network
Commit d96102b6251d caused rpc.calls to network manager to occur for
non-multi-host configs, but they get sent to the
network.<destination_compute> topic when they should just go to
'network'.

Fixes bug 1075245

Change-Id: If2bb8b3897efb59b8dc41ea136b08146e4e56270

nova/network/api.py
@@ -353,6 +353,7 @@ class API(base.Base):
         if self._is_multi_host(context, instance):
             args['floating_addresses'] = \
                 self._get_floating_ip_addresses(context, instance)
+            args['host'] = migration['dest_compute']
 
         self.network_rpcapi.migrate_instance_start(context, **args)
 
@@ -370,5 +371,6 @@ class API(base.Base):
         if self._is_multi_host(context, instance):
             args['floating_addresses'] = \
                 self._get_floating_ip_addresses(context, instance)
+            args['host'] = migration['dest_compute']
 
         self.network_rpcapi.migrate_instance_finish(context, **args)

nova/network/rpcapi.py
@@ -267,7 +267,11 @@ class NetworkAPI(rpc_proxy.RpcProxy):
 
     def migrate_instance_start(self, ctxt, instance_uuid, rxtx_factor,
                                project_id, source_compute, dest_compute,
-                               floating_addresses):
+                               floating_addresses, host=None):
+        if host is not None:
+            topic = rpc.queue_get_for(ctxt, self.topic, host)
+        else:
+            topic = self.topic
         return self.call(ctxt, self.make_msg(
                 'migrate_instance_start',
                 instance_uuid=instance_uuid,
@@ -276,13 +280,16 @@ class NetworkAPI(rpc_proxy.RpcProxy):
                 source=source_compute,
                 dest=dest_compute,
                 floating_addresses=floating_addresses),
-                         topic=rpc.queue_get_for(ctxt, self.topic,
-                                                 dest_compute),
-                         version='1.2')
+                topic=topic,
+                version='1.2')
 
     def migrate_instance_finish(self, ctxt, instance_uuid, rxtx_factor,
                                 project_id, source_compute, dest_compute,
-                                floating_addresses):
+                                floating_addresses, host=None):
+        if host is not None:
+            topic = rpc.queue_get_for(ctxt, self.topic, host)
+        else:
+            topic = self.topic
         return self.call(ctxt, self.make_msg(
                 'migrate_instance_finish',
                 instance_uuid=instance_uuid,
@@ -291,6 +298,5 @@ class NetworkAPI(rpc_proxy.RpcProxy):
                 source=source_compute,
                 dest=dest_compute,
                 floating_addresses=floating_addresses),
-                         topic=rpc.queue_get_for(ctxt, self.topic,
-                                                 dest_compute),
-                         version='1.2')
+                topic=topic,
+                version='1.2')

nova/tests/network/test_api.py
@@ -19,6 +19,7 @@
 
 from nova import context
 from nova import network
+from nova.network import rpcapi as network_rpcapi
 from nova.openstack.common import rpc
 from nova import test
 
@@ -78,3 +79,66 @@ class ApiTestCase(test.TestCase):
 
     def test_associate_unassociated_floating_ip(self):
         self._do_test_associate_floating_ip(None)
+
+    def _stub_migrate_instance_calls(self, method, multi_host, info):
+        fake_instance_type = {'rxtx_factor': 'fake_factor'}
+        fake_instance = {'uuid': 'fake_uuid',
+                         'instance_type': fake_instance_type,
+                         'project_id': 'fake_project_id'}
+        fake_migration = {'source_compute': 'fake_compute_source',
+                          'dest_compute': 'fake_compute_dest'}
+
+        def fake_mig_inst_method(*args, **kwargs):
+            info['kwargs'] = kwargs
+
+        def fake_is_multi_host(*args, **kwargs):
+            return multi_host
+
+        def fake_get_floaters(*args, **kwargs):
+            return ['fake_float1', 'fake_float2']
+
+        self.stubs.Set(network_rpcapi.NetworkAPI, method,
+                fake_mig_inst_method)
+        self.stubs.Set(self.network_api, '_is_multi_host',
+                fake_is_multi_host)
+        self.stubs.Set(self.network_api, '_get_floating_ip_addresses',
+                fake_get_floaters)
+
+        expected = {'instance_uuid': 'fake_uuid',
+                    'source_compute': 'fake_compute_source',
+                    'dest_compute': 'fake_compute_dest',
+                    'rxtx_factor': 'fake_factor',
+                    'project_id': 'fake_project_id',
+                    'floating_addresses': None}
+        if multi_host:
+            expected['host'] = 'fake_compute_dest'
+            expected['floating_addresses'] = ['fake_float1', 'fake_float2']
+        return fake_instance, fake_migration, expected
+
+    def test_migrate_instance_start_with_multhost(self):
+        info = {'kwargs': {}}
+        arg1, arg2, expected = self._stub_migrate_instance_calls(
+                'migrate_instance_start', True, info)
+        self.network_api.migrate_instance_start(self.context, arg1, arg2)
+        self.assertEqual(info['kwargs'], expected)
+
+    def test_migrate_instance_start_without_multhost(self):
+        info = {'kwargs': {}}
+        arg1, arg2, expected = self._stub_migrate_instance_calls(
+                'migrate_instance_start', False, info)
+        self.network_api.migrate_instance_start(self.context, arg1, arg2)
+        self.assertEqual(info['kwargs'], expected)
+
+    def test_migrate_instance_finish_with_multhost(self):
+        info = {'kwargs': {}}
+        arg1, arg2, expected = self._stub_migrate_instance_calls(
+                'migrate_instance_finish', True, info)
+        self.network_api.migrate_instance_finish(self.context, arg1, arg2)
+        self.assertEqual(info['kwargs'], expected)
+
+    def test_migrate_instance_finish_without_multhost(self):
+        info = {'kwargs': {}}
+        arg1, arg2, expected = self._stub_migrate_instance_calls(
+                'migrate_instance_finish', False, info)
+        self.network_api.migrate_instance_finish(self.context, arg1, arg2)
+        self.assertEqual(info['kwargs'], expected)

nova/tests/network/test_rpcapi.py
@@ -36,11 +36,17 @@ class NetworkRpcAPITestCase(test.TestCase):
         expected_version = kwargs.pop('version', rpcapi.BASE_RPC_API_VERSION)
         expected_topic = FLAGS.network_topic
         expected_msg = rpcapi.make_msg(method, **kwargs)
+        if 'source_compute' in expected_msg['args']:
+            # Fix up for migrate_instance_* calls.
+            args = expected_msg['args']
+            args['source'] = args.pop('source_compute')
+            args['dest'] = args.pop('dest_compute')
         targeted_methods = [
             'lease_fixed_ip', 'release_fixed_ip', 'rpc_setup_network_on_host',
             '_rpc_allocate_fixed_ip', 'deallocate_fixed_ip',
             '_associate_floating_ip', '_disassociate_floating_ip',
-            'lease_fixed_ip', 'release_fixed_ip'
+            'lease_fixed_ip', 'release_fixed_ip',
+            'migrate_instance_start', 'migrate_instance_finish',
         ]
         if method in targeted_methods and 'host' in kwargs:
             if method != 'deallocate_fixed_ip':
@@ -258,3 +264,45 @@ class NetworkRpcAPITestCase(test.TestCase):
     def test_release_fixed_ip(self):
         self._test_network_api('release_fixed_ip', rpc_method='cast',
                 address='fake_addr', host='fake_host')
+
+    def test_migrate_instance_start(self):
+        self._test_network_api('migrate_instance_start', rpc_method='call',
+                instance_uuid='fake_instance_uuid',
+                rxtx_factor='fake_factor',
+                project_id='fake_project',
+                source_compute='fake_src_compute',
+                dest_compute='fake_dest_compute',
+                floating_addresses='fake_floating_addresses',
+                version='1.2')
+
+    def test_migrate_instance_start_multi_host(self):
+        self._test_network_api('migrate_instance_start', rpc_method='call',
+                instance_uuid='fake_instance_uuid',
+                rxtx_factor='fake_factor',
+                project_id='fake_project',
+                source_compute='fake_src_compute',
+                dest_compute='fake_dest_compute',
+                floating_addresses='fake_floating_addresses',
+                host='fake_host',
+                version='1.2')
+
+    def test_migrate_instance_finish(self):
+        self._test_network_api('migrate_instance_finish', rpc_method='call',
+                instance_uuid='fake_instance_uuid',
+                rxtx_factor='fake_factor',
+                project_id='fake_project',
+                source_compute='fake_src_compute',
+                dest_compute='fake_dest_compute',
+                floating_addresses='fake_floating_addresses',
+                version='1.2')
+
+    def test_migrate_instance_finish_multi_host(self):
+        self._test_network_api('migrate_instance_finish', rpc_method='call',
+                instance_uuid='fake_instance_uuid',
+                rxtx_factor='fake_factor',
+                project_id='fake_project',
+                source_compute='fake_src_compute',
+                dest_compute='fake_dest_compute',
+                floating_addresses='fake_floating_addresses',
+                host='fake_host',
+                version='1.2')