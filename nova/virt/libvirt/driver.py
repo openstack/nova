5d4cd7ea4fe36ff762a6426c58fceb03409d5729
Yun Mao <yunmao@gmail.com>
2012-11-12 14:02:34 -0500

    Rename config to vconfig
    config doesn't have member named LibvirtConfigCPUFeature.
    
    Part of bug 1078062
    
    Change-Id: I115f3351b42173a00e2b7d02cb0fc3d76b9eb4e5

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -2357,7 +2357,7 @@ class LibvirtDriver(driver.ComputeDriver):
         cpu.cores = info['topology']['cores']
         cpu.threads = info['topology']['threads']
         for f in info['features']:
-            cpu.add_feature(config.LibvirtConfigCPUFeature(f))
+            cpu.add_feature(vconfig.LibvirtConfigCPUFeature(f))
 
         u = "http://libvirt.org/html/libvirt-libvirt.html#virCPUCompareResult"
         m = _("CPU doesn't have compatibility.\n\n%(ret)s\n\nRefer to %(u)s")
