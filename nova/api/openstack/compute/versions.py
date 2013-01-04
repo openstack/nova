c7221d046008f6bc980b6cb97f868e9ef8655070
Sean Dague <sdague@linux.vnet.ibm.com>
2013-01-04 13:01:06 -0500

    update version urls to working v2 urls
    the GET /v2/ API call is documented to return urls to both
    wadl and pdf documents. However our call returned non working
    urls to 1.1 versions of those documents. Fix this.
    
    Also fix the unit tests to actually test the output of versions.
    Previously we were over stubbing the test_verions.py so it was
    testing only itself, and not the output you'd actually get from
    the real versions.py module.
    
    Renamed the variables used for the expected variables to reduce
    confusion about what is an expected variable vs. the variables
    coming from versions.py to try to ensure we don't have cicular
    always passing tests in the future.
    
    Fixes bug #1076109
    
    Change-Id: I1047b8c48ff053eed5ad4afa8eae5833f70d0893

--- nova/api/openstack/compute/versions.py
+++ nova/api/openstack/compute/versions.py
@@ -26,9 +26,9 @@ from nova.openstack.common import timeutils
 LINKS = {
    'v2.0': {
        'pdf': 'http://docs.openstack.org/'
-               'api/openstack-compute/1.1/os-compute-devguide-1.1.pdf',
+               'api/openstack-compute/2/os-compute-devguide-2.pdf',
        'wadl': 'http://docs.openstack.org/'
-               'api/openstack-compute/1.1/wadl/os-compute-1.1.wadl',
+               'api/openstack-compute/2/wadl/os-compute-2.wadl'
     },
 }
 
