24fffd9d8b77e9b71e8013fc22c172f76bb4e84c
Vishvananda Ishaya <vishvananda@gmail.com>
2013-01-24 10:07:33 +0000

    validate specified volumes to boot from at the API layer
    This causes the create to fail fast instead of putting
    the instance into error later in the process.
    
    Related to bug: 1069904
    Change-Id: I5f7c8d20d3ebf33ce1ce64bf0a8418bd2b5a6411

--- nova/exception.py
+++ nova/exception.py
@@ -226,6 +226,20 @@ class Invalid(NovaException):
     code = 400
 
 
+class InvalidBDM(Invalid):
+    message = _("Block Device Mapping is Invalid.")
+
+
+class InvalidBDMSnapshot(InvalidBDM):
+    message = _("Block Device Mapping is Invalid: "
+                "failed to get snapshot %(id)s.")
+
+
+class InvalidBDMVolume(InvalidBDM):
+    message = _("Block Device Mapping is Invalid: "
+                "failed to get volume %(id)s.")
+
+
 class VolumeUnattached(Invalid):
     message = _("Volume %(volume_id)s is not attached to anything")
 
