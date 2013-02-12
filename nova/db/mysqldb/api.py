317e7904f5ba6cacb175500db167672131d2631a
Chris Behrens <cbehrens@codestud.com>
2013-02-12 16:35:33 +0000

    MYSQLDB: Add bw_usage_update
    Adds implementation for bw_usage_update
    
    Implements bp/db-mysqldb

--- nova/db/mysqldb/api.py
+++ nova/db/mysqldb/api.py
@@ -23,10 +23,12 @@ here.
 """
 from eventlet import tpool
 
+from nova.db import utils as dbutils
 from nova.db.mysqldb import connection
 from nova.db.sqlalchemy import api as sqlalchemy_api
 from nova.openstack.common import cfg
 from nova.openstack.common import log as logging
+from nova.openstack.common import timeutils
 
 
 mysqldb_opts = [
@@ -53,12 +55,51 @@ def _tpool_enabled(f):
     return wrapped
 
 
-class API(object):
-    # TODO(belliott) mysql raw methods to be implemented here.
+def _datestr(dt):
+    return dt.strftime('%Y-%m-%d %H:%M:%S')
 
+
+class API(object):
     def __init__(self):
         self.pool = connection.ConnectionPool()
 
     def __getattr__(self, key):
         # forward unimplemented method to sqlalchemy backend:
         return getattr(sqlalchemy_api, key)
+
+    @_tpool_enabled
+    @dbutils.require_context
+    def bw_usage_update(self, context, uuid, mac, start_period, bw_in, bw_out,
+                        last_ctr_in, last_ctr_out, last_refreshed=None):
+
+        if last_refreshed is None:
+            last_refreshed = timeutils.utcnow()
+
+        values = {'last_refreshed': last_refreshed,
+                  'last_ctr_in': last_ctr_in,
+                  'last_ctr_out': last_ctr_out,
+                  'bw_in': bw_in,
+                  'bw_out': bw_out,
+                  'updated_at': timeutils.utcnow}
+
+        with self.pool.get() as conn:
+            sql = """UPDATE bw_usage_cache SET bw_in=%s, bw_out=%s,
+                         last_ctr_in=%s, last_ctr_out=%s
+                     WHERE bw_usage_cache.start_period = %s AND
+                           bw_usage_cache.uuid = %s AND
+                           bw_usage_cache.mac = %s"""
+            args = (bw_in, bw_out, last_ctr_in, last_ctr_out,
+                    _datestr(start_period), uuid, mac)
+            num_rows_affected = conn.execute(sql, args)
+            if num_rows_affected > 0:
+                return
+
+        values.pop('updated_at')
+        values['created_at'] = timeutils.utcnow()
+        values['start_period'] = start_period
+        values['uuid'] = uuid
+        values['mac'] = mac
+        # Start a new transaction.  UPDATE + INSERT can cause a deadlock
+        # if mixed into the same transaction.
+        with self.pool.get() as conn:
+            conn.insert('bw_usage_cache', values)
