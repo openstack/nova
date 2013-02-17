openstack/nova

commit 7c265343159bb76e0322f150f8d194ecd342b637

ensure atomic manipulation of libvirt disk images
This pattern could probably be used elsewhere,
but only libvirt disk images are considered for now.
This change ensures there are no stale files left
anywhere in the path from glance, through the libvirt image cache.
These could cause subsequent operational errors either
directly or indirectly through disk wastage.

* nova/utils.py: Add a new remove_path_on_error() context manager
that is used to remove the passed PATH on a raised exception.
* nova/virt/images.py: Ensure temporary downloaded and
converted images are protected.
* nova/virt/libvirt/connection.py: Ensure all the images in
the image cache and instance dirs are protected.

Change-Id: I81a5407665a6998128c0dee41387ef00ebddeb4d

nova/virt/images.py
@@ -51,18 +51,10 @@ def fetch(context, image_href, path, _user_id, _project_id):
     #             checked before we got here.
     (image_service, image_id) = nova.image.get_image_service(context,
                                                              image_href)
-    try:
+    with utils.remove_path_on_error(path):
         with open(path, "wb") as image_file:
             metadata = image_service.get(context, image_id, image_file)
-    except Exception:
-        with utils.save_and_reraise_exception():
-            try:
-                os.unlink(path)
-            except OSError, e:
-                if e.errno != errno.ENOENT:
-                    LOG.warn("unable to remove stale image '%s': %s" %
-                             (path, e.strerror))
-    return metadata
+            return metadata
 
 
 def fetch_to_raw(context, image_href, path, user_id, project_id):
@@ -85,37 +77,36 @@ def fetch_to_raw(context, image_href, path, user_id, project_id):
 
         return(data)
 
-    data = _qemu_img_info(path_tmp)
+    with utils.remove_path_on_error(path_tmp):
+        data = _qemu_img_info(path_tmp)
 
-    fmt = data.get("file format")
-    if fmt is None:
-        os.unlink(path_tmp)
-        raise exception.ImageUnacceptable(
-            reason=_("'qemu-img info' parsing failed."), image_id=image_href)
+        fmt = data.get("file format")
+        if fmt is None:
+            raise exception.ImageUnacceptable(
+                reason=_("'qemu-img info' parsing failed."),
+                image_id=image_href)
 
-    if "backing file" in data:
-        backing_file = data['backing file']
-        os.unlink(path_tmp)
-        raise exception.ImageUnacceptable(image_id=image_href,
-            reason=_("fmt=%(fmt)s backed by: %(backing_file)s") % locals())
-
-    if fmt != "raw" and FLAGS.force_raw_images:
-        staged = "%s.converted" % path
-        LOG.debug("%s was %s, converting to raw" % (image_href, fmt))
-        out, err = utils.execute('qemu-img', 'convert', '-O', 'raw',
-                                 path_tmp, staged)
-        os.unlink(path_tmp)
-
-        data = _qemu_img_info(staged)
-        if data.get('file format', None) != "raw":
-            os.unlink(staged)
+        if "backing file" in data:
+            backing_file = data['backing file']
             raise exception.ImageUnacceptable(image_id=image_href,
-                reason=_("Converted to raw, but format is now %s") %
-                data.get('file format', None))
+                reason=_("fmt=%(fmt)s backed by: %(backing_file)s") % locals())
 
-        os.rename(staged, path)
+        if fmt != "raw" and FLAGS.force_raw_images:
+            staged = "%s.converted" % path
+            LOG.debug("%s was %s, converting to raw" % (image_href, fmt))
+            with utils.remove_path_on_error(staged):
+                out, err = utils.execute('qemu-img', 'convert', '-O', 'raw',
+                                         path_tmp, staged)
 
-    else:
-        os.rename(path_tmp, path)
+                data = _qemu_img_info(staged)
+                if data.get('file format', None) != "raw":
+                    raise exception.ImageUnacceptable(image_id=image_href,
+                        reason=_("Converted to raw, but format is now %s") %
+                        data.get('file format', None))
+
+                os.rename(staged, path)
+
+        else:
+            os.rename(path_tmp, path)
 
     return metadata

nova/utils.py
@@ -21,6 +21,7 @@
 
 import contextlib
 import datetime
+import errno
 import functools
 import hashlib
 import inspect
@@ -1014,8 +1015,8 @@ def cleanup_file_locks():
             continue
         try:
             stat_info = os.stat(os.path.join(FLAGS.lock_path, filename))
-        except OSError as (errno, strerror):
-            if errno == 2:  # doesn't exist
+        except OSError as e:
+            if e.errno == errno.ENOENT:
                 continue
             else:
                 raise
@@ -1034,8 +1035,8 @@ def delete_if_exists(pathname):
 
     try:
         os.unlink(pathname)
-    except OSError as (errno, strerror):
-        if errno == 2:  # doesn't exist
+    except OSError as e:
+        if e.errno == errno.ENOENT:
             return
         else:
             raise
@@ -1343,6 +1344,18 @@ def logging_error(message):
     except Exception as error:
         with save_and_reraise_exception():
             LOG.exception(message)
+
+
+@contextlib.contextmanager
+def remove_path_on_error(path):
+    """Protect code that wants to operate on PATH atomically.
+    Any exception will cause PATH to be removed.
+    """
+    try:
+        yield
+    except Exception:
+        with save_and_reraise_exception():
+            delete_if_exists(path)
 
 
 def make_dev_path(dev, partition=None, base='/dev'):

nova/virt/libvirt/connection.py
@@ -1047,7 +1047,8 @@ class LibvirtConnection(driver.ComputeDriver):
             @utils.synchronized(fname)
             def call_if_not_exists(base, fn, *args, **kwargs):
                 if not os.path.exists(base):
-                    fn(target=base, *args, **kwargs)
+                    with utils.remove_path_on_error(base):
+                        fn(target=base, *args, **kwargs)
 
             if cow or not generating:
                 call_if_not_exists(base, fn, *args, **kwargs)
@@ -1063,8 +1064,9 @@ class LibvirtConnection(driver.ComputeDriver):
                         size_gb = size / (1024 * 1024 * 1024)
                         cow_base += "_%d" % size_gb
                         if not os.path.exists(cow_base):
-                            libvirt_utils.copy_image(base, cow_base)
-                            disk.extend(cow_base, size)
+                            with utils.remove_path_on_error(cow_base):
+                                libvirt_utils.copy_image(base, cow_base)
+                                disk.extend(cow_base, size)
                     libvirt_utils.create_cow_image(cow_base, target)
                 elif not generating:
                     libvirt_utils.copy_image(base, target)
@@ -1074,7 +1076,8 @@ class LibvirtConnection(driver.ComputeDriver):
                     if size:
                         disk.extend(target, size)
 
-            copy_and_extend(cow, generating, base, target, size)
+            with utils.remove_path_on_error(target):
+                copy_and_extend(cow, generating, base, target, size)
 
     @staticmethod
     def _create_local(target, local_size, unit='G',
@@ -1234,8 +1237,9 @@ class LibvirtConnection(driver.ComputeDriver):
                               project_id=instance['project_id'],)
         elif config_drive:
             label = 'config'
-            self._create_local(basepath('disk.config'), 64, unit='M',
-                               fs_format='msdos', label=label)  # 64MB
+            with utils.remove_path_on_error(basepath('disk.config')):
+                self._create_local(basepath('disk.config'), 64, unit='M',
+                                   fs_format='msdos', label=label)  # 64MB
 
         if instance['key_data']:
             key = str(instance['key_data'])

commit 1501a4e4f75efb63385a1dadc728c2ef245bf7de

remove intermediate libvirt downloaded images
* nova/virt/images.py (fetch_to_raw): When converting a downloaded
image to raw, ensure we delete the original download image.
I.E. the .part file.  The regression was introduced in 7c265343.
* nova/tests/test_libvirt.py (test_fetch_raw_image): A new test
for this function which wasn't tested previously.

Fixes bug: 1125068
Change-Id: Ie6f2cff1a554b8500fd66f0832566ae1b317be40

nova/virt/images.py
@@ -225,6 +225,7 @@ def fetch_to_raw(context, image_href, path, user_id, project_id):
             LOG.debug("%s was %s, converting to raw" % (image_href, fmt))
             with utils.remove_path_on_error(staged):
                 convert_image(path_tmp, staged, 'raw')
+                os.unlink(path_tmp)
 
                 data = qemu_img_info(staged)
                 if data.file_format != "raw":
@@ -233,6 +234,5 @@ def fetch_to_raw(context, image_href, path, user_id, project_id):
                         data.file_format)
 
                 os.rename(staged, path)
-
         else:
             os.rename(path_tmp, path)

nova/tests/test_libvirt.py
@@ -1278,7 +1278,7 @@ class LibvirtConnTestCase(test.TestCase):
         def convert_image(source, dest, out_format):
             libvirt_driver.libvirt_utils.files[dest] = ''
 
-        images.convert_image = convert_image
+        self.stubs.Set(images, 'convert_image', convert_image)
 
         self.mox.ReplayAll()
 
@@ -1330,7 +1330,7 @@ class LibvirtConnTestCase(test.TestCase):
         def convert_image(source, dest, out_format):
             libvirt_driver.libvirt_utils.files[dest] = ''
 
-        images.convert_image = convert_image
+        self.stubs.Set(images, 'convert_image', convert_image)
 
         self.mox.ReplayAll()
 
@@ -4157,6 +4157,77 @@ disk size: 4.4M''', ''))
         self.mox.ReplayAll()
         libvirt_utils.fetch_image(context, target, image_id,
                                   user_id, project_id)
+
+    def test_fetch_raw_image(self):
+
+        def fake_execute(*cmd, **kwargs):
+            self.executes.append(cmd)
+            return None, None
+
+        def fake_rename(old, new):
+            self.executes.append(('mv', old, new))
+
+        def fake_unlink(path):
+            self.executes.append(('rm', path))
+
+        def fake_rm_on_errror(path):
+            self.executes.append(('rm', '-f', path))
+
+        def fake_qemu_img_info(path):
+            class FakeImgInfo(object):
+                pass
+
+            file_format = path.split('.')[-1]
+            if file_format == 'part':
+                file_format = path.split('.')[-2]
+            elif file_format == 'converted':
+                file_format = 'raw'
+            if 'backing' in path:
+                backing_file = 'backing'
+            else:
+                backing_file = None
+
+            FakeImgInfo.file_format = file_format
+            FakeImgInfo.backing_file = backing_file
+
+            return FakeImgInfo()
+
+        self.stubs.Set(utils, 'execute', fake_execute)
+        self.stubs.Set(os, 'rename', fake_rename)
+        self.stubs.Set(os, 'unlink', fake_unlink)
+        self.stubs.Set(images, 'fetch', lambda *_: None)
+        self.stubs.Set(images, 'qemu_img_info', fake_qemu_img_info)
+        self.stubs.Set(utils, 'delete_if_exists', fake_rm_on_errror)
+
+        context = 'opaque context'
+        image_id = '4'
+        user_id = 'fake'
+        project_id = 'fake'
+
+        target = 't.qcow2'
+        self.executes = []
+        expected_commands = [('qemu-img', 'convert', '-O', 'raw',
+                              't.qcow2.part', 't.qcow2.converted'),
+                             ('rm', 't.qcow2.part'),
+                             ('mv', 't.qcow2.converted', 't.qcow2')]
+        images.fetch_to_raw(context, image_id, target, user_id, project_id)
+        self.assertEqual(self.executes, expected_commands)
+
+        target = 't.raw'
+        self.executes = []
+        expected_commands = [('mv', 't.raw.part', 't.raw')]
+        images.fetch_to_raw(context, image_id, target, user_id, project_id)
+        self.assertEqual(self.executes, expected_commands)
+
+        target = 'backing.qcow2'
+        self.executes = []
+        expected_commands = [('rm', '-f', 'backing.qcow2.part')]
+        self.assertRaises(exception.ImageUnacceptable,
+                          images.fetch_to_raw,
+                          context, image_id, target, user_id, project_id)
+        self.assertEqual(self.executes, expected_commands)
+
+        del self.executes
 
     def test_get_disk_backing_file(self):
         with_actual_path = False