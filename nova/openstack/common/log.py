8ca04fc9a1bea39bc19543d2aca554d6fc30cb13
Sean Dague <sdague@linux.vnet.ibm.com>
2013-02-20 08:43:07 -0500

    sync oslo log updates
    this helps address the issue where syslog gets overloaded and we
    loose the ability to figure out what's going on in clients like
    nova-manage
    
    Change-Id: Iedde5ed4e8e23e204be4fa7a19e9514acf2ebf86

--- nova/openstack/common/log.py
+++ nova/openstack/common/log.py
@@ -325,16 +325,11 @@ def _create_logging_excepthook(product_name):
 
 def setup(product_name):
     """Setup logging."""
-    sys.excepthook = _create_logging_excepthook(product_name)
-
     if CONF.log_config:
-        try:
-            logging.config.fileConfig(CONF.log_config)
-        except Exception:
-            traceback.print_exc()
-            raise
+        logging.config.fileConfig(CONF.log_config)
     else:
         _setup_logging_from_conf(product_name)
+    sys.excepthook = _create_logging_excepthook(product_name)
 
 
 def set_defaults(logging_context_format_string):
