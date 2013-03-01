23e9b6e9846237acf89a74fdea50a69528a28ef6
Russell Bryant <rbryant@redhat.com>
2013-03-01 11:40:39 -0500

    Tone down logging while waiting for conductor.
    When nova-compute starts, it sits in a loop waiting for nova-conductor
    to be responsive, since it is required for nova-compute to be able to
    complete its startup process.  If a ping request times out, don't leave
    a traceback mess in the log.  This is an expected type of failure that
    is handled.  Just leave it as a warning in case the problem really is
    that nova-conductor hasn't been started.
    
    Fix bug 1137994.
    
    Change-Id: I0754fbd6faf35a09b8733d3ced77871268abf1e8

--- nova/conductor/api.py
+++ nova/conductor/api.py
@@ -364,7 +364,7 @@ class API(object):
                 self.ping(context, '1.21 GigaWatts', timeout=timeout)
                 break
             except rpc_common.Timeout as e:
-                LOG.exception(_('Timed out waiting for nova-conductor. '
+                LOG.warning(_('Timed out waiting for nova-conductor. '
                                 'Is it running? Or did this service start '
                                 'before nova-conductor?'))
 
