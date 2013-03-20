f8d25feed1f92437aa0e90e23ad697a2aa05e278
Mate Lakat <mate.lakat@citrix.com>
2013-03-20 14:58:32 +0000

    xenapi: rpmbuild fixes
    Fixes bug 1157695
    
    This fix removes the parted dependency from the rpm specification file,
    and also gets rid of the assumption, that the build-rpm.sh is executed
    at a specific directory.
    
    Change-Id: I1dcf9096a783a88bc09af0f4f4d5541a7e505748

--- plugins/xenserver/xenapi/contrib/build-rpm.sh
+++ plugins/xenserver/xenapi/contrib/build-rpm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PACKAGE=openstack-xen-plugins
-RPMBUILD_DIR=$PWD/rpmbuild
+THIS_DIR=$(cd $(dirname $0) && pwd)
+RPMBUILD_DIR="$THIS_DIR/rpmbuild"
+
 if [ ! -d $RPMBUILD_DIR ]; then
     echo $RPMBUILD_DIR is missing
     exit 1
@@ -13,7 +15,7 @@ done
 
 rm -rf /tmp/$PACKAGE
 mkdir /tmp/$PACKAGE
-cp -r ../etc/xapi.d /tmp/$PACKAGE
+cp -r "$THIS_DIR/../etc/xapi.d" /tmp/$PACKAGE
 tar czf $RPMBUILD_DIR/SOURCES/$PACKAGE.tar.gz -C /tmp $PACKAGE
 
 rpmbuild -ba --nodeps --define "_topdir $RPMBUILD_DIR" \
