5df18f6b915cf49ac8559274a631f1366eab00fd
David Ripton <dripton@redhat.com>
2013-03-21 18:07:04 -0400

    Add a comment to placeholder migrations.
    We've had a review that tried to reuse migration 162 rather than
    skipping to 172.  Maybe a comment will prevent that in the future.
    
    Change-Id: Ib8b73260df593d13a81ce65c85a302a54668b95e

--- nova/db/sqlalchemy/migrate_repo/versions/170_placeholder.py
+++ nova/db/sqlalchemy/migrate_repo/versions/170_placeholder.py
@@ -1,3 +1,11 @@
+# This is a placeholder for Grizzly backports.
+# Do not use this number for new Havana work.  New Havana work starts after
+# all the placeholders.
+#
+# See https://blueprints.launchpad.net/nova/+spec/backportable-db-migrations
+# http://lists.openstack.org/pipermail/openstack-dev/2013-March/006827.html
+
+
 def upgrade(migrate_engine):
     pass
 
