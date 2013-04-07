f2e49ad3128856785f6ea460b1968bf03dc87e99
Michael Still <mikal@stillhq.com>
2013-04-08 07:46:34 +1000

    Import eventlet in __init__.py
    All of this is because if dnspython is present in your environment then
    eventlet monkeypatches socket.getaddrinfo() with an implementation
    which doesn't work for IPv6. To turn this off you need to set an
    environment variable before you do the import.
    
    Resolves bug 1164822.
    
    Change-Id: I2382db7b3ac4a2d940e0c45dfe3d48f499de2f16

--- nova/tests/__init__.py
+++ nova/tests/__init__.py
@@ -24,6 +24,26 @@
    :platform: Unix
 """
 
+# TODO(mikal): move eventlet imports to nova.__init__ once we move to PBR
+import os
+import sys
+
+# NOTE(mikal): All of this is because if dnspython is present in your
+# environment then eventlet monkeypatches socket.getaddrinfo() with an
+# implementation which doesn't work for IPv6. What we're checking here is
+# that the magic environment variable was set when the import happened.
+if ('eventlet' in sys.modules and
+    os.environ.get('EVENTLET_NO_GREENDNS', '').lower() != 'yes'):
+    raise ImportError('eventlet imported before nova/cmd/__init__ '
+                      '(env var set to %s)'
+                      % os.environ.get('EVENTLET_NO_GREENDNS'))
+
+os.environ['EVENTLET_NO_GREENDNS'] = 'yes'
+
+import eventlet
+
+eventlet.monkey_patch(os=False)
+
 # See http://code.google.com/p/python-nose/issues/detail?id=373
 # The code below enables nosetests to work with i18n _() blocks
 import __builtin__
