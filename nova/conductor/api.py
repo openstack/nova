52663f7bc6388e619c138dced7f4d236de4eebbd
Dan Smith <danms@us.ibm.com>
2013-04-08 16:13:30 -0700

    Change DB API instance functions for selective metadata fetching
    This makes the DB API for instance_get_all() variants able to take a
    columns_to_join parameter like the base instance_get_all() function,
    which provides control over whether we go to the trouble of pulling
    the metadata out of the database before we return the instances.
    
    Related to bug 1164737
    
    (cherry-picked from ba9cd2a545a0c198990f649b6a2985a22102930f)
    Change-Id: Ie7c012e2e12bb91ef4294cdc90d828af255b67c5

--- nova/conductor/api.py
+++ nova/conductor/api.py
@@ -76,19 +76,22 @@ class LocalAPI(object):
     def instance_get_all(self, context):
         return self._manager.instance_get_all(context)
 
-    def instance_get_all_by_host(self, context, host):
-        return self._manager.instance_get_all_by_host(context, host)
+    def instance_get_all_by_host(self, context, host, columns_to_join=None):
+        return self._manager.instance_get_all_by_host(context, host,
+                                                      columns_to_join)
 
     def instance_get_all_by_host_and_node(self, context, host, node):
         return self._manager.instance_get_all_by_host(context, host, node)
 
     def instance_get_all_by_filters(self, context, filters,
                                     sort_key='created_at',
-                                    sort_dir='desc'):
+                                    sort_dir='desc',
+                                    columns_to_join=None):
         return self._manager.instance_get_all_by_filters(context,
                                                          filters,
                                                          sort_key,
-                                                         sort_dir)
+                                                         sort_dir,
+                                                         columns_to_join)
 
     def instance_get_all_hung_in_rebooting(self, context, timeout):
         return self._manager.instance_get_all_hung_in_rebooting(context,
@@ -398,8 +401,9 @@ class API(object):
     def instance_get_all(self, context):
         return self.conductor_rpcapi.instance_get_all(context)
 
-    def instance_get_all_by_host(self, context, host):
-        return self.conductor_rpcapi.instance_get_all_by_host(context, host)
+    def instance_get_all_by_host(self, context, host, columns_to_join=None):
+        return self.conductor_rpcapi.instance_get_all_by_host(
+            context, host, columns_to_join=columns_to_join)
 
     def instance_get_all_by_host_and_node(self, context, host, node):
         return self.conductor_rpcapi.instance_get_all_by_host(context,
@@ -407,11 +411,10 @@ class API(object):
 
     def instance_get_all_by_filters(self, context, filters,
                                     sort_key='created_at',
-                                    sort_dir='desc'):
-        return self.conductor_rpcapi.instance_get_all_by_filters(context,
-                                                                 filters,
-                                                                 sort_key,
-                                                                 sort_dir)
+                                    sort_dir='desc',
+                                    columns_to_join=None):
+        return self.conductor_rpcapi.instance_get_all_by_filters(
+            context, filters, sort_key, sort_dir, columns_to_join)
 
     def instance_get_all_hung_in_rebooting(self, context, timeout):
         return self.conductor_rpcapi.instance_get_all_hung_in_rebooting(
--- nova/tests/conductor/test_conductor.py
+++ nova/tests/conductor/test_conductor.py
@@ -667,7 +667,8 @@ class ConductorTestCase(_BaseTestCase, test.TestCase):
         filters = {'foo': 'bar'}
         self.mox.StubOutWithMock(db, 'instance_get_all_by_filters')
         db.instance_get_all_by_filters(self.context, filters,
-                                       'fake-key', 'fake-sort')
+                                       'fake-key', 'fake-sort',
+                                       columns_to_join=None)
         self.mox.ReplayAll()
         self.conductor.instance_get_all_by_filters(self.context, filters,
                                                    'fake-key', 'fake-sort')
@@ -676,7 +677,7 @@ class ConductorTestCase(_BaseTestCase, test.TestCase):
         self.mox.StubOutWithMock(db, 'instance_get_all_by_host')
         self.mox.StubOutWithMock(db, 'instance_get_all_by_host_and_node')
         db.instance_get_all_by_host(self.context.elevated(),
-                                    'host').AndReturn('result')
+                                    'host', None).AndReturn('result')
         db.instance_get_all_by_host_and_node(self.context.elevated(), 'host',
                                              'node').AndReturn('result')
         self.mox.ReplayAll()
@@ -823,7 +824,8 @@ class ConductorRPCAPITestCase(_BaseTestCase, test.TestCase):
         filters = {'foo': 'bar'}
         self.mox.StubOutWithMock(db, 'instance_get_all_by_filters')
         db.instance_get_all_by_filters(self.context, filters,
-                                       'fake-key', 'fake-sort')
+                                       'fake-key', 'fake-sort',
+                                       columns_to_join=None)
         self.mox.ReplayAll()
         self.conductor.instance_get_all_by_filters(self.context, filters,
                                                    'fake-key', 'fake-sort')
@@ -970,7 +972,8 @@ class ConductorAPITestCase(_BaseTestCase, test.TestCase):
         self.mox.StubOutWithMock(db, 'instance_get_all_by_filters')
         db.instance_get_all(self.context)
         db.instance_get_all_by_filters(self.context, {'name': 'fake-inst'},
-                                       'updated_at', 'asc')
+                                       'updated_at', 'asc',
+                                       columns_to_join=None)
         self.mox.ReplayAll()
         self.conductor.instance_get_all(self.context)
         self.conductor.instance_get_all_by_filters(self.context,
@@ -1046,13 +1049,19 @@ class ConductorAPITestCase(_BaseTestCase, test.TestCase):
         result = self.conductor.service_update(self.context, {'id': ''}, {})
         self.assertEqual(result, 'fake-result')
 
-    def test_instance_get_all_by_host(self):
-        self._test_stubbed('instance_get_all_by_host',
-                           self.context.elevated(), 'host')
-
     def test_instance_get_all_by_host_and_node(self):
         self._test_stubbed('instance_get_all_by_host_and_node',
                            self.context.elevated(), 'host', 'node')
+
+    def test_instance_get_all_by_host(self):
+        self.mox.StubOutWithMock(db, 'instance_get_all_by_host')
+        self.mox.StubOutWithMock(db, 'instance_get_all_by_host_and_node')
+        db.instance_get_all_by_host(self.context.elevated(), 'host',
+                                    None).AndReturn('fake-result')
+        self.mox.ReplayAll()
+        result = self.conductor.instance_get_all_by_host(self.context,
+                                                         'host')
+        self.assertEqual(result, 'fake-result')
 
     def test_ping(self):
         timeouts = []
