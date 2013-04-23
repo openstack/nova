3546cdeb0548e03e71004914b2ab651a316fe047
Yaguang Tang <yaguang.tang@canonical.com>
2013-04-23 18:55:19 +0800

    Support FlatDHCP network for VMware Hyper.
    Auto create bridge when using FlatDHCP network, Add unittests for vif.
    
    Fix bug #1171783
    Fix bug #1177635
    
    Change-Id: Ibb75c8e3174283f0f5e94bacfd61ee93e8bd6227
    (cherry picked from commit bc66b8aa968a0c0b74e2f21ed0fc856f8933c6d3)

--- nova/virt/vmwareapi/vif.py
+++ nova/virt/vmwareapi/vif.py
@@ -20,11 +20,8 @@
 from oslo.config import cfg
 
 from nova import exception
-from nova.openstack.common import log as logging
 from nova.virt.vmwareapi import network_util
 
-
-LOG = logging.getLogger(__name__)
 
 CONF = cfg.CONF
 
@@ -37,48 +34,51 @@ vmwareapi_vif_opts = [
 CONF.register_opts(vmwareapi_vif_opts)
 
 
-def ensure_vlan_bridge(session, vif, cluster=None):
+def ensure_vlan_bridge(session, vif, cluster=None, create_vlan=True):
     """Create a vlan and bridge unless they already exist."""
     vlan_num = vif['network'].get_meta('vlan')
     bridge = vif['network']['bridge']
     vlan_interface = CONF.vmwareapi_vlan_interface
 
-    # Check if the vlan_interface physical network adapter exists on the
-    # host.
-    if not network_util.check_if_vlan_interface_exists(session,
-                                                       vlan_interface,
-                                                       cluster):
-        raise exception.NetworkAdapterNotFound(adapter=vlan_interface)
-
+    network_ref = network_util.get_network_with_the_name(session, bridge,
+                                                         cluster)
     # Get the vSwitch associated with the Physical Adapter
     vswitch_associated = network_util.get_vswitch_for_vlan_interface(
-                                        session, vlan_interface, cluster)
+                                    session, vlan_interface, cluster)
     if vswitch_associated is None:
         raise exception.SwitchNotFoundForNetworkAdapter(
             adapter=vlan_interface)
-    # Check whether bridge already exists and retrieve the the ref of the
-    # network whose name_label is "bridge"
-    network_ref = network_util.get_network_with_the_name(session, bridge,
-                                                         cluster)
-    if network_ref is None:
+    # Check if the vlan_interface physical network adapter exists on the
+    # host.
+    if not network_util.check_if_vlan_interface_exists(session,
+                                        vlan_interface, cluster):
+        raise exception.NetworkAdapterNotFound(adapter=vlan_interface)
+    if create_vlan:
+
+        if network_ref is None:
         # Create a port group on the vSwitch associated with the
         # vlan_interface corresponding physical network adapter on the ESX
         # host.
-        network_util.create_port_group(session, bridge,
+            network_util.create_port_group(session, bridge,
                                        vswitch_associated, vlan_num,
                                        cluster)
-    else:
-        # Get the vlan id and vswitch corresponding to the port group
-        _get_pg_info = network_util.get_vlanid_and_vswitch_for_portgroup
-        pg_vlanid, pg_vswitch = _get_pg_info(session, bridge, cluster)
+        else:
+            # Get the vlan id and vswitch corresponding to the port group
+            _get_pg_info = network_util.get_vlanid_and_vswitch_for_portgroup
+            pg_vlanid, pg_vswitch = _get_pg_info(session, bridge, cluster)
 
-        # Check if the vswitch associated is proper
-        if pg_vswitch != vswitch_associated:
-            raise exception.InvalidVLANPortGroup(
-                bridge=bridge, expected=vswitch_associated,
-                actual=pg_vswitch)
+            # Check if the vswitch associated is proper
+            if pg_vswitch != vswitch_associated:
+                raise exception.InvalidVLANPortGroup(
+                    bridge=bridge, expected=vswitch_associated,
+                    actual=pg_vswitch)
 
-        # Check if the vlan id is proper for the port group
-        if pg_vlanid != vlan_num:
-            raise exception.InvalidVLANTag(bridge=bridge, tag=vlan_num,
+            # Check if the vlan id is proper for the port group
+            if pg_vlanid != vlan_num:
+                raise exception.InvalidVLANTag(bridge=bridge, tag=vlan_num,
                                            pgroup=pg_vlanid)
+    else:
+        if network_ref is None:
+            network_util.create_port_group(session, bridge,
+                                       vswitch_associated, 0,
+                                       cluster)
