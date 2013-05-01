498ce526c1c2ce1ad1e17af9fc4a3ff67b0b09a4
Rick Harris <rconradharris@gmail.com>
2013-05-01 21:47:35 +0000

    xenapi: Don't swallow missing SR exception
    The existing code would catch a missing SR exception and bail out of the
    `update_status` method before finishing, causing required keys to be missing.
    The end-result was code breaking later on when it tried to access these keys.
    
    Since an SR is required to generate these required stats keys, we shouldn't
    swallow the StorageRepositoryNotFound exception and instead should fail-fast.
    
    Fixes bug 1175357
    
    Change-Id: I3d65ae807fb2d86277bc0061c1967a87792b6662
    (cherry picked from commit b6aac988e46147154d49e938ecf9b67831613636)

--- nova/virt/xenapi/host.py
+++ nova/virt/xenapi/host.py
@@ -148,13 +148,7 @@ class HostState(object):
         LOG.debug(_("Updating host stats"))
         data = call_xenhost(self._session, "host_data", {})
         if data:
-            try:
-                # Get the SR usage
-                sr_ref = vm_utils.safe_find_sr(self._session)
-            except exception.NotFound as e:
-                # No SR configured
-                LOG.error(_("Unable to get SR for this host: %s") % e)
-                return
+            sr_ref = vm_utils.safe_find_sr(self._session)
             self._session.call_xenapi("SR.scan", sr_ref)
             sr_rec = self._session.call_xenapi("SR.get_record", sr_ref)
             total = int(sr_rec["physical_size"])
