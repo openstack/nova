1985b176adff8252886c09f36c075af4144cb95f
kirankv <kiran-kumar.vaddi@hp.com>
2013-05-17 07:14:44 -0700

    Fix VMwareVCdriver reporting incorrect stats
    fix bug #1180779
    
    VMware nova compute drivers VMwareESXDriver and VMwareVCDriver
    currently report stats for the column local_gb in MB instead of in GB
    The fix would be to modify vmwareapi/host.py to ensure the values
    reported for disk space are calculated in GB instead of MB.
    Fix is done by further dividing the existing MB value by 1024 to
    convert to GB.
    The unit tests to check for capacity and freespace have been modified
    to check for GB.
    
    Change-Id: I0610047ee2bddefe0ccb46568c0f48de4c0941af

--- nova/virt/vmwareapi/fake.py
+++ nova/virt/vmwareapi/fake.py
@@ -1,5 +1,6 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 # Copyright (c) 2012 VMware, Inc.
 # Copyright (c) 2011 Citrix Systems, Inc.
 # Copyright 2011 OpenStack Foundation
@@ -255,8 +256,8 @@ class Datastore(ManagedObject):
         super(Datastore, self).__init__("Datastore")
         self.set("summary.type", "VMFS")
         self.set("summary.name", "fake-ds")
-        self.set("summary.capacity", 1024 * 1024 * 1024)
-        self.set("summary.freeSpace", 500 * 1024 * 1024)
+        self.set("summary.capacity", 1024 * 1024 * 1024 * 1024)
+        self.set("summary.freeSpace", 500 * 1024 * 1024 * 1024)
 
 
 class HostNetworkSystem(ManagedObject):
