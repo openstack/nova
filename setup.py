8e6edd2fe9845ee0d93600ebfcf90ce029bd1f70
Adam Gandelman <adamg@canonical.com>
2013-06-06 12:09:02 -0700

    Bump stable/grizzly next version to 2013.1.3
    Change-Id: Ie9b5fb202effaa2d42270d64642369adf7b11840

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ project = 'nova'
 
 setuptools.setup(
       name=project,
-      version=common_setup.get_version(project, '2013.1.2'),
+      version=common_setup.get_version(project, '2013.1.3'),
       description='cloud computing fabric controller',
       author='OpenStack',
       author_email='nova@lists.launchpad.net',
