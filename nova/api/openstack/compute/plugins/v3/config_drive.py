57b3fc171e09d74fea48c3813b82d9f74a23d11b
Chris Yeoh <cyeoh@au1.ibm.com>
2013-06-07 14:45:07 +0930

    Port config drive API to v3 Part 1
    This changeset only copies the v2 files (implementation and test)
    into the appropriate v3 directories unchanged. The copy as-is
    will not be loaded by either the v2 or v3 extension loaders. The
    second changeset will then make the changes required for it to
    work as a v3 extension.
    
    This is being done in order to make reviewing of extension porting
    easier as gerrit will display only what is actually changed for v3
    rather than entirely new files
    
    Partially implements blueprint nova-v3-api
    
    Change-Id: I9a4b31ef06bbf05c25b4a5e96f5cee2ce4d80d2f

---
+++ nova/api/openstack/compute/plugins/v3/config_drive.py
@@ -0,0 +1,84 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Config Drive extension."""
+
+from nova.api.openstack.compute import servers
+from nova.api.openstack import extensions
+from nova.api.openstack import wsgi
+from nova.api.openstack import xmlutil
+
+authorize = extensions.soft_extension_authorizer('compute', 'config_drive')
+
+
+class ServerConfigDriveTemplate(xmlutil.TemplateBuilder):
+    def construct(self):
+        root = xmlutil.TemplateElement('server')
+        root.set('config_drive', 'config_drive')
+        return xmlutil.SlaveTemplate(root, 1)
+
+
+class ServersConfigDriveTemplate(xmlutil.TemplateBuilder):
+    def construct(self):
+        root = xmlutil.TemplateElement('servers')
+        elem = xmlutil.SubTemplateElement(root, 'server', selector='servers')
+        elem.set('config_drive', 'config_drive')
+        return xmlutil.SlaveTemplate(root, 1)
+
+
+class Controller(servers.Controller):
+
+    def _add_config_drive(self, req, servers):
+        for server in servers:
+            db_server = req.get_db_instance(server['id'])
+            # server['id'] is guaranteed to be in the cache due to
+            # the core API adding it in its 'show'/'detail' methods.
+            server['config_drive'] = db_server['config_drive']
+
+    def _show(self, req, resp_obj):
+        if 'server' in resp_obj.obj:
+            resp_obj.attach(xml=ServerConfigDriveTemplate())
+            server = resp_obj.obj['server']
+            self._add_config_drive(req, [server])
+
+    @wsgi.extends
+    def show(self, req, resp_obj, id):
+        context = req.environ['nova.context']
+        if authorize(context):
+            self._show(req, resp_obj)
+
+    @wsgi.extends
+    def detail(self, req, resp_obj):
+        context = req.environ['nova.context']
+        if 'servers' in resp_obj.obj and authorize(context):
+            resp_obj.attach(xml=ServersConfigDriveTemplate())
+            servers = resp_obj.obj['servers']
+            self._add_config_drive(req, servers)
+
+
+class Config_drive(extensions.ExtensionDescriptor):
+    """Config Drive Extension."""
+
+    name = "ConfigDrive"
+    alias = "os-config-drive"
+    namespace = "http://docs.openstack.org/compute/ext/config_drive/api/v1.1"
+    updated = "2012-07-16T00:00:00+00:00"
+
+    def get_controller_extensions(self):
+        controller = Controller(self.ext_mgr)
+        extension = extensions.ControllerExtension(self, 'servers', controller)
+        return [extension]
