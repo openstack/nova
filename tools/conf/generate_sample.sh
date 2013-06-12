8781fccec83054ce9d10493b1791a1b8b55b41a5
Zhongyue Luo <zhongyue.nah@intel.com>
2013-06-12 14:29:21 +0800

    Refactor conf sample generator script
    Optimized grep calls to not create a grep process per file.
    Defined variables to enhance readability.
    
    Change-Id: Ic873f2c3b600a737ce54444dec973720c8813285

--- tools/conf/generate_sample.sh
+++ tools/conf/generate_sample.sh
@@ -3,7 +3,6 @@
 
 # Copyright 2012 SINA Corporation
 # All Rights Reserved.
-# Author: Zhongyue Luo <lzyeval@gmail.com>
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -16,16 +15,19 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+#
+# @author: Zhongyue Luo, Intel Corporation.
+#
 
-FILES=$(find nova -type f -name "*.py" ! -path "nova/tests/*" -exec \
-    grep -l "Opt(" {} \; | sort -u)
+FILES=$(find nova -type f -name "*.py" ! -path "nova/tests/*" \
+        -exec grep -l "Opt(" {} + | sort -u)
 BINS=$(echo bin/nova-* | grep -v nova-rootwrap)
 
 export EVENTLET_NO_GREENDNS=yes
 
-PYTHONPATH=./:${PYTHONPATH} \
-    python $(dirname "$0")/../../nova/openstack/common/config/generator.py ${FILES} ${BINS} > \
-    etc/nova/nova.conf.sample
+MODULEPATH=$(dirname "$0")/../../nova/openstack/common/config/generator.py
+OUTPUTPATH=etc/nova/nova.conf.sample
+PYTHONPATH=./:${PYTHONPATH} python $MODULEPATH $FILES $BINS > $OUTPUTPATH
 
 # Remove compiled files created by imp.import_source()
 for bin in ${BINS}; do
