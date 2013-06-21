b01136e9f56376167a97ba3641184c631917733e
Chris Yeoh <cyeoh@au1.ibm.com>
2013-06-21 14:48:13 +0930

    Port extended status extension to v3 API Part 1
    This changeset only copies the v2 implementation file into the
    appropriate v3 directory unchanged. The copy as-is will not be
    loaded by either the v2 or v3 extension loaders. The second
    changeset will then make the changes required for it to work as a
    v3 extension.
    
    This is being done in order to make reviewing of extension porting
    easier as gerrit will display only what is actually changed for v3
    rather than entirely new files.
    
    Partially implements blueprint nova-v3-api
    
    Change-Id: Ia5b80b70a7a459bf973de1c1aea8dc650e763b8d

---
+++ nova/api/openstack/compute/plugins/v3/extended_status.py
@@ -0,0 +1,99 @@
+#   Copyright 2011 OpenStack Foundation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+"""The Extended Status Admin API extension."""
+
+from nova.api.openstack import extensions
+from nova.api.openstack import wsgi
+from nova.api.openstack import xmlutil
+from nova import compute
+
+authorize = extensions.soft_extension_authorizer('compute', 'extended_status')
+
+
+class ExtendedStatusController(wsgi.Controller):
+    def __init__(self, *args, **kwargs):
+        super(ExtendedStatusController, self).__init__(*args, **kwargs)
+        self.compute_api = compute.API()
+
+    def _extend_server(self, server, instance):
+        for state in ['task_state', 'vm_state', 'power_state']:
+            key = "%s:%s" % (Extended_status.alias, state)
+            server[key] = instance[state]
+
+    @wsgi.extends
+    def show(self, req, resp_obj, id):
+        context = req.environ['nova.context']
+        if authorize(context):
+            # Attach our slave template to the response object
+            resp_obj.attach(xml=ExtendedStatusTemplate())
+            server = resp_obj.obj['server']
+            db_instance = req.get_db_instance(server['id'])
+            # server['id'] is guaranteed to be in the cache due to
+            # the core API adding it in its 'show' method.
+            self._extend_server(server, db_instance)
+
+    @wsgi.extends
+    def detail(self, req, resp_obj):
+        context = req.environ['nova.context']
+        if authorize(context):
+            # Attach our slave template to the response object
+            resp_obj.attach(xml=ExtendedStatusesTemplate())
+            servers = list(resp_obj.obj['servers'])
+            for server in servers:
+                db_instance = req.get_db_instance(server['id'])
+                # server['id'] is guaranteed to be in the cache due to
+                # the core API adding it in its 'detail' method.
+                self._extend_server(server, db_instance)
+
+
+class Extended_status(extensions.ExtensionDescriptor):
+    """Extended Status support."""
+
+    name = "ExtendedStatus"
+    alias = "OS-EXT-STS"
+    namespace = ("http://docs.openstack.org/compute/ext/"
+                 "extended_status/api/v1.1")
+    updated = "2011-11-03T00:00:00+00:00"
+
+    def get_controller_extensions(self):
+        controller = ExtendedStatusController()
+        extension = extensions.ControllerExtension(self, 'servers', controller)
+        return [extension]
+
+
+def make_server(elem):
+    elem.set('{%s}task_state' % Extended_status.namespace,
+             '%s:task_state' % Extended_status.alias)
+    elem.set('{%s}power_state' % Extended_status.namespace,
+             '%s:power_state' % Extended_status.alias)
+    elem.set('{%s}vm_state' % Extended_status.namespace,
+             '%s:vm_state' % Extended_status.alias)
+
+
+class ExtendedStatusTemplate(xmlutil.TemplateBuilder):
+    def construct(self):
+        root = xmlutil.TemplateElement('server', selector='server')
+        make_server(root)
+        return xmlutil.SlaveTemplate(root, 1, nsmap={
+            Extended_status.alias: Extended_status.namespace})
+
+
+class ExtendedStatusesTemplate(xmlutil.TemplateBuilder):
+    def construct(self):
+        root = xmlutil.TemplateElement('servers')
+        elem = xmlutil.SubTemplateElement(root, 'server', selector='servers')
+        make_server(elem)
+        return xmlutil.SlaveTemplate(root, 1, nsmap={
+            Extended_status.alias: Extended_status.namespace})
---
+++ nova/tests/api/openstack/compute/plugins/v3/test_extended_status.py
@@ -0,0 +1,118 @@
+# Copyright 2011 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from lxml import etree
+import webob
+
+from nova.api.openstack.compute.contrib import extended_status
+from nova import compute
+from nova import exception
+from nova.openstack.common import jsonutils
+from nova import test
+from nova.tests.api.openstack import fakes
+
+UUID1 = '00000000-0000-0000-0000-000000000001'
+UUID2 = '00000000-0000-0000-0000-000000000002'
+UUID3 = '00000000-0000-0000-0000-000000000003'
+
+
+def fake_compute_get(*args, **kwargs):
+    return fakes.stub_instance(1, uuid=UUID3, task_state="kayaking",
+            vm_state="slightly crunchy", power_state="empowered")
+
+
+def fake_compute_get_all(*args, **kwargs):
+    return [
+        fakes.stub_instance(1, uuid=UUID1, task_state="task-1",
+                vm_state="vm-1", power_state="power-1"),
+        fakes.stub_instance(2, uuid=UUID2, task_state="task-2",
+                vm_state="vm-2", power_state="power-2"),
+    ]
+
+
+class ExtendedStatusTest(test.TestCase):
+    content_type = 'application/json'
+    prefix = 'OS-EXT-STS:'
+
+    def setUp(self):
+        super(ExtendedStatusTest, self).setUp()
+        fakes.stub_out_nw_api(self.stubs)
+        self.stubs.Set(compute.api.API, 'get', fake_compute_get)
+        self.stubs.Set(compute.api.API, 'get_all', fake_compute_get_all)
+        self.flags(
+            osapi_compute_extension=[
+                'nova.api.openstack.compute.contrib.select_extensions'],
+            osapi_compute_ext_list=['Extended_status'])
+
+    def _make_request(self, url):
+        req = webob.Request.blank(url)
+        req.headers['Accept'] = self.content_type
+        res = req.get_response(fakes.wsgi_app(init_only=('servers',)))
+        return res
+
+    def _get_server(self, body):
+        return jsonutils.loads(body).get('server')
+
+    def _get_servers(self, body):
+        return jsonutils.loads(body).get('servers')
+
+    def assertServerStates(self, server, vm_state, power_state, task_state):
+        self.assertEqual(server.get('%svm_state' % self.prefix), vm_state)
+        self.assertEqual(server.get('%spower_state' % self.prefix),
+                         power_state)
+        self.assertEqual(server.get('%stask_state' % self.prefix), task_state)
+
+    def test_show(self):
+        url = '/v2/fake/servers/%s' % UUID3
+        res = self._make_request(url)
+
+        self.assertEqual(res.status_int, 200)
+        self.assertServerStates(self._get_server(res.body),
+                                vm_state='slightly crunchy',
+                                power_state='empowered',
+                                task_state='kayaking')
+
+    def test_detail(self):
+        url = '/v2/fake/servers/detail'
+        res = self._make_request(url)
+
+        self.assertEqual(res.status_int, 200)
+        for i, server in enumerate(self._get_servers(res.body)):
+            self.assertServerStates(server,
+                                    vm_state='vm-%s' % (i + 1),
+                                    power_state='power-%s' % (i + 1),
+                                    task_state='task-%s' % (i + 1))
+
+    def test_no_instance_passthrough_404(self):
+
+        def fake_compute_get(*args, **kwargs):
+            raise exception.InstanceNotFound(instance_id='fake')
+
+        self.stubs.Set(compute.api.API, 'get', fake_compute_get)
+        url = '/v2/fake/servers/70f6db34-de8d-4fbd-aafb-4065bdfa6115'
+        res = self._make_request(url)
+
+        self.assertEqual(res.status_int, 404)
+
+
+class ExtendedStatusXmlTest(ExtendedStatusTest):
+    content_type = 'application/xml'
+    prefix = '{%s}' % extended_status.Extended_status.namespace
+
+    def _get_server(self, body):
+        return etree.XML(body)
+
+    def _get_servers(self, body):
+        return etree.XML(body).getchildren()
