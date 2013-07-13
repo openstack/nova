5685ea870508b9153179526e28daaf7a9328d373
Kieran Spear <kispear@gmail.com>
2013-07-14 01:08:52 +1000

    Fix aggregate_get_by_host host filtering
    aggregate_get_by_host never actually joins the _hosts relationship,
    so aggregates with non-matching hosts are still included in the output.
    
    This change adds the join and improves the tests to cover the bug.
    
    Fixes bug 1201277.
    
    Conflicts:
    
    	nova/db/sqlalchemy/api.py
    	nova/tests/db/test_db_api.py
    
    Change-Id: I87ebfe3de193b9ae47e3a2c8350a49d9cc863fb0
    (cherry picked from commit 13513a182a54dffb62900a8464529459dd836136)

--- nova/db/sqlalchemy/api.py
+++ nova/db/sqlalchemy/api.py
@@ -4346,8 +4346,16 @@ def aggregate_get(context, aggregate_id):
 
 @require_admin_context
 def aggregate_get_by_host(context, host, key=None):
-    query = _aggregate_get_query(context, models.Aggregate,
-            models.AggregateHost.host, host)
+    """Return rows that match host (mandatory) and metadata key (optional).
+
+    :param host matches host, and is required.
+    :param key Matches metadata key, if not None.
+    """
+    query = model_query(context, models.Aggregate)
+    query = query.options(joinedload('_hosts'))
+    query = query.options(joinedload('_metadata'))
+    query = query.join('_hosts')
+    query = query.filter(models.AggregateHost.host == host)
 
     if key:
         query = query.join("_metadata").filter(
