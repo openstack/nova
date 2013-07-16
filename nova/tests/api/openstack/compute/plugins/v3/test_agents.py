aff272eff9042698fe674e7ef80670627d26f50e
He Jie Xu <xuhj@linux.vnet.ibm.com>
2013-07-16 14:50:59 +0800

    Fix extensions agent follow API v3 rules
    * Add decorator expected_errors
    * Use AgentBuildExists instead of generic exception catching in create
    * Use HTTPBadRequest instead of HTTPUnprocessableEntity
    * Rename the request parameter 'para' to 'agent' for update
    
    Partially implements bp v3-api-extension-versioning
    
    DocImpact
    
    Change-Id: I45203203b8396517ef94e604366183ce93810205

--- nova/tests/api/openstack/compute/plugins/v3/test_agents.py
+++ nova/tests/api/openstack/compute/plugins/v3/test_agents.py
@@ -11,12 +11,13 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
+from webob import exc
 
 from nova.api.openstack.compute.plugins.v3 import agents
 from nova import context
 from nova import db
 from nova.db.sqlalchemy import models
+from nova import exception
 from nova import test
 
 fake_agents_list = [{'hypervisor': 'kvm', 'os': 'win',
@@ -82,6 +83,10 @@ class FakeRequestWithHypervisor(object):
     GET = {'hypervisor': 'kvm'}
 
 
+def fake_agent_build_create_with_exited_agent(context, values):
+    raise exception.AgentBuildExists(**values)
+
+
 class AgentsTest(test.TestCase):
 
     def setUp(self):
@@ -115,6 +120,90 @@ class AgentsTest(test.TestCase):
                     'agent_id': 1}}
         res_dict = self.controller.create(req, body)
         self.assertEqual(res_dict, response)
+
+    def test_agents_create_with_existed_agent(self):
+        self.stubs.Set(db, 'agent_build_create',
+                       fake_agent_build_create_with_exited_agent)
+        req = FakeRequest()
+        body = {'agent': {'hypervisor': 'kvm',
+                'os': 'win',
+                'architecture': 'x86',
+                'version': '7.0',
+                'url': 'xxx://xxxx/xxx/xxx',
+                'md5hash': 'add6bb58e139be103324d04d82d8f545'}}
+        self.assertRaises(exc.HTTPConflict, self.controller.create, req, body)
+
+    def test_agents_create_without_md5hash(self):
+        req = FakeRequest()
+        body = {'agent': {'hypervisor': 'kvm',
+                'os': 'win',
+                'architecture': 'x86',
+                'version': '7.0',
+                'url': 'xxx://xxxx/xxx/xxx'}}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.create,
+                          req, body)
+
+    def test_agents_create_without_url(self):
+        req = FakeRequest()
+        body = {'agent': {'hypervisor': 'kvm',
+                'os': 'win',
+                'architecture': 'x86',
+                'version': '7.0',
+                'md5hash': 'add6bb58e139be103324d04d82d8f545'}}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.create,
+                          req, body)
+
+    def test_agents_create_without_version(self):
+        req = FakeRequest()
+        body = {'agent': {'hypervisor': 'kvm',
+                'os': 'win',
+                'architecture': 'x86',
+                'url': 'xxx://xxxx/xxx/xxx',
+                'md5hash': 'add6bb58e139be103324d04d82d8f545'}}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.create,
+                          req, body)
+
+    def test_agents_create_without_architecture(self):
+        req = FakeRequest()
+        body = {'agent': {'hypervisor': 'kvm',
+                'os': 'win',
+                'version': '7.0',
+                'url': 'xxx://xxxx/xxx/xxx',
+                'md5hash': 'add6bb58e139be103324d04d82d8f545'}}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.create,
+                          req, body)
+
+    def test_agents_create_without_os(self):
+        req = FakeRequest()
+        body = {'agent': {'hypervisor': 'kvm',
+                'architecture': 'x86',
+                'version': '7.0',
+                'url': 'xxx://xxxx/xxx/xxx',
+                'md5hash': 'add6bb58e139be103324d04d82d8f545'}}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.create,
+                          req, body)
+
+    def test_agents_create_without_hypervisor(self):
+        req = FakeRequest()
+        body = {'agent': {'os': 'win',
+                'architecture': 'x86',
+                'version': '7.0',
+                'url': 'xxx://xxxx/xxx/xxx',
+                'md5hash': 'add6bb58e139be103324d04d82d8f545'}}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.create,
+                          req, body)
+
+    def test_agents_create_with_wrong_type(self):
+        req = FakeRequest()
+        body = {'agent': None}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.create,
+                          req, body)
+
+    def test_agents_create_with_empty_type(self):
+        req = FakeRequest()
+        body = {}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.create,
+                          req, body)
 
     def test_agents_delete(self):
         req = FakeRequest()
@@ -170,7 +259,7 @@ class AgentsTest(test.TestCase):
 
     def test_agents_update(self):
         req = FakeRequest()
-        body = {'para': {'version': '7.0',
+        body = {'agent': {'version': '7.0',
                 'url': 'xxx://xxxx/xxx/xxx',
                 'md5hash': 'add6bb58e139be103324d04d82d8f545'}}
         response = {'agent': {'agent_id': 1,
@@ -179,3 +268,34 @@ class AgentsTest(test.TestCase):
                     'md5hash': 'add6bb58e139be103324d04d82d8f545'}}
         res_dict = self.controller.update(req, 1, body)
         self.assertEqual(res_dict, response)
+
+    def test_agents_update_without_md5hash(self):
+        req = FakeRequest()
+        body = {'agent': {'version': '7.0',
+                'url': 'xxx://xxxx/xxx/xxx'}}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.update,
+                          req, 1, body)
+
+    def test_agents_update_without_url(self):
+        req = FakeRequest()
+        body = {'agent': {'version': '7.0'}}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.update,
+                          req, 1, body)
+
+    def test_agents_update_without_version(self):
+        req = FakeRequest()
+        body = {'agent': {}}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.update,
+                          req, 1, body)
+
+    def test_agents_update_with_wrong_type(self):
+        req = FakeRequest()
+        body = {'agent': None}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.update,
+                          req, 1, body)
+
+    def test_agents_update_with_empty(self):
+        req = FakeRequest()
+        body = {}
+        self.assertRaises(exc.HTTPBadRequest, self.controller.update,
+                          req, 1, body)
