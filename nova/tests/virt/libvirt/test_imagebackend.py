c246fb37e20197be27264a36515725233085df1d
Jiajun Liu <jiajun@unitedstack.com>
2013-08-22 11:45:18 +0800

    fallocate image only when user has write access
    Libvirt will change instance's ownership when the instance became power
    off which will make nova-compute service unable to hard reboot instance
    bacause the nova-compute service has not write access. This commit will
    allocate space for image only the first time we create image.
    
    fixes bug 1200113
    
    Change-Id: Id81b09ade5f870665f5ffabab3cde144c6dec210

--- nova/tests/virt/libvirt/test_imagebackend.py
+++ nova/tests/virt/libvirt/test_imagebackend.py
@@ -139,6 +139,7 @@ class _ImageTestCase(object):
             return
 
         self.stubs.Set(os.path, 'exists', lambda _: True)
+        self.stubs.Set(os, 'access', lambda p, w: True)
 
         # Call twice to verify testing fallocate is only called once.
         image.cache(fake_fetch, self.TEMPLATE_PATH, self.SIZE)
@@ -148,6 +149,26 @@ class _ImageTestCase(object):
             ['fallocate -n -l 1 %s.fallocate_test' % self.PATH,
              'fallocate -n -l %s %s' % (self.SIZE, self.PATH),
              'fallocate -n -l %s %s' % (self.SIZE, self.PATH)])
+
+    def test_prealloc_image_without_write_access(self):
+        CONF.set_override('preallocate_images', 'space')
+
+        fake_processutils.fake_execute_clear_log()
+        fake_processutils.stub_out_processutils_execute(self.stubs)
+        image = self.image_class(self.INSTANCE, self.NAME)
+
+        def fake_fetch(target, *args, **kwargs):
+            return
+
+        self.stubs.Set(image, 'check_image_exists', lambda: True)
+        self.stubs.Set(image, '_can_fallocate', lambda: True)
+        self.stubs.Set(os.path, 'exists', lambda _: True)
+        self.stubs.Set(os, 'access', lambda p, w: False)
+
+        # Testing fallocate is only called when user has write access.
+        image.cache(fake_fetch, self.TEMPLATE_PATH, self.SIZE)
+
+        self.assertEqual(fake_processutils.fake_execute_get_log(), [])
 
 
 class RawTestCase(_ImageTestCase, test.NoDBTestCase):
