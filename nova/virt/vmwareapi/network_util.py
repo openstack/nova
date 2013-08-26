8a86025a48570c6bfae9b221f1688f996019a67f
Gary Kotton <gkotton@vmware.com>
2013-08-26 08:06:40 -0700

    VMware: clean up get_network_with_the_name
    By default the method returns None. No need to do this explicitly.
    
    Change-Id: Ie18300bdfaaec8d9c813352fe42a2aebec730c45

--- nova/virt/vmwareapi/network_util.py
+++ nova/virt/vmwareapi/network_util.py
@@ -79,8 +79,6 @@ def get_network_with_the_name(session, network_name="vmnet0", cluster=None):
                 network_obj['name'] = network_name
     if (len(network_obj) > 0):
         return network_obj
-    else:
-        return None
 
 
 def get_vswitch_for_vlan_interface(session, vlan_interface, cluster=None):
