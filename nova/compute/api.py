pombredanne/nova-1

Assisted snapshots compute API plumbing
This patch adds the plumbing necessary to get assisted volume snapshot
calls down from compute.api down to a compute host.

Part of blueprint qemu-assisted-snapshots

Change-Id: I18d89b16c3e4bbbb2cdf3fe70c958f7e671e78ec

nova/compute/api.py
@@ -2865,6 +2865,28 @@ class API(base.Base):
         """Get all migrations for the given filters."""
         return self.db.migration_get_all_by_filters(context, filters)
 
+    @wrap_check_policy
+    def volume_snapshot_create(self, context, volume_id, create_info):
+        bdm = self.db.block_device_mapping_get_by_volume_id(context,
+                volume_id, ['instance'])
+        self.compute_rpcapi.volume_snapshot_create(context, bdm['instance'],
+                volume_id, create_info)
+        snapshot = {
+            'snapshot': {
+                'id': create_info.get('id'),
+                'volume_id': volume_id
+            }
+        }
+        return snapshot
+
+    @wrap_check_policy
+    def volume_snapshot_delete(self, context, volume_id, snapshot_id,
+                               delete_info):
+        bdm = self.db.block_device_mapping_get_by_volume_id(context,
+                volume_id, ['instance'])
+        self.compute_rpcapi.volume_snapshot_delete(context, bdm['instance'],
+                volume_id, snapshot_id, delete_info)
+
 
 class HostAPI(base.Base):
     """Sub-set of the Compute Manager API for managing host operations."""

nova/tests/compute/test_compute_api.py
@@ -1091,6 +1091,68 @@ class _ComputeAPIUnitTestMixIn(object):
                                      volumes[old_volume_id],
                                      volumes[new_volume_id])
 
+    def test_volume_snapshot_create(self):
+        volume_id = '1'
+        create_info = {'id': 'eyedee'}
+        fake_bdm = {
+            'instance': {
+                'uuid': 'fake_uuid',
+                'vm_state': vm_states.ACTIVE,
+            },
+        }
+
+        def fake_get_bdm(context, _volume_id, columns_to_join):
+            self.assertEqual(volume_id, _volume_id)
+            return fake_bdm
+
+        self.stubs.Set(self.compute_api.db,
+                'block_device_mapping_get_by_volume_id', fake_get_bdm)
+        self.mox.StubOutWithMock(self.compute_api.compute_rpcapi,
+                'volume_snapshot_create')
+
+        self.compute_api.compute_rpcapi.volume_snapshot_create(self.context,
+                fake_bdm['instance'], volume_id, create_info)
+
+        self.mox.ReplayAll()
+
+        snapshot = self.compute_api.volume_snapshot_create(self.context,
+                volume_id, create_info)
+
+        expected_snapshot = {
+            'snapshot': {
+                'id': create_info['id'],
+                'volume_id': volume_id,
+            },
+        }
+        self.assertEqual(snapshot, expected_snapshot)
+
+    def test_volume_snapshot_delete(self):
+        volume_id = '1'
+        snapshot_id = '2'
+        fake_bdm = {
+            'instance': {
+                'uuid': 'fake_uuid',
+                'vm_state': vm_states.ACTIVE,
+            },
+        }
+
+        def fake_get_bdm(context, _volume_id, columns_to_join):
+            self.assertEqual(volume_id, _volume_id)
+            return fake_bdm
+
+        self.stubs.Set(self.compute_api.db,
+                'block_device_mapping_get_by_volume_id', fake_get_bdm)
+        self.mox.StubOutWithMock(self.compute_api.compute_rpcapi,
+                'volume_snapshot_delete')
+
+        self.compute_api.compute_rpcapi.volume_snapshot_delete(self.context,
+                fake_bdm['instance'], volume_id, snapshot_id, {})
+
+        self.mox.ReplayAll()
+
+        self.compute_api.volume_snapshot_delete(self.context, volume_id,
+                snapshot_id, {})
+
 
 class ComputeAPIUnitTestCase(_ComputeAPIUnitTestMixIn, test.NoDBTestCase):
     def setUp(self):

openstack/nova

nova/compute/api.py
@@ -2889,6 +2889,28 @@ class API(base.Base):
         """Get all migrations for the given filters."""
         return migration_obj.MigrationList.get_by_filters(context, filters)
 
+    @wrap_check_policy
+    def volume_snapshot_create(self, context, volume_id, create_info):
+        bdm = self.db.block_device_mapping_get_by_volume_id(context,
+                volume_id, ['instance'])
+        self.compute_rpcapi.volume_snapshot_create(context, bdm['instance'],
+                volume_id, create_info)
+        snapshot = {
+            'snapshot': {
+                'id': create_info.get('id'),
+                'volumeId': volume_id
+            }
+        }
+        return snapshot
+
+    @wrap_check_policy
+    def volume_snapshot_delete(self, context, volume_id, snapshot_id,
+                               delete_info):
+        bdm = self.db.block_device_mapping_get_by_volume_id(context,
+                volume_id, ['instance'])
+        self.compute_rpcapi.volume_snapshot_delete(context, bdm['instance'],
+                volume_id, snapshot_id, delete_info)
+
 
 class HostAPI(base.Base):
     """Sub-set of the Compute Manager API for managing host operations."""

nova/tests/compute/test_compute_api.py
@@ -1320,6 +1320,68 @@ class _ComputeAPIUnitTestMixIn(object):
     def test_get_minram_mindisk_other_fails(self):
         self._test_get_minram_mindisk_params('other', show_fails=True)
 
+    def test_volume_snapshot_create(self):
+        volume_id = '1'
+        create_info = {'id': 'eyedee'}
+        fake_bdm = {
+            'instance': {
+                'uuid': 'fake_uuid',
+                'vm_state': vm_states.ACTIVE,
+            },
+        }
+
+        def fake_get_bdm(context, _volume_id, columns_to_join):
+            self.assertEqual(volume_id, _volume_id)
+            return fake_bdm
+
+        self.stubs.Set(self.compute_api.db,
+                'block_device_mapping_get_by_volume_id', fake_get_bdm)
+        self.mox.StubOutWithMock(self.compute_api.compute_rpcapi,
+                'volume_snapshot_create')
+
+        self.compute_api.compute_rpcapi.volume_snapshot_create(self.context,
+                fake_bdm['instance'], volume_id, create_info)
+
+        self.mox.ReplayAll()
+
+        snapshot = self.compute_api.volume_snapshot_create(self.context,
+                volume_id, create_info)
+
+        expected_snapshot = {
+            'snapshot': {
+                'id': create_info['id'],
+                'volumeId': volume_id,
+            },
+        }
+        self.assertEqual(snapshot, expected_snapshot)
+
+    def test_volume_snapshot_delete(self):
+        volume_id = '1'
+        snapshot_id = '2'
+        fake_bdm = {
+            'instance': {
+                'uuid': 'fake_uuid',
+                'vm_state': vm_states.ACTIVE,
+            },
+        }
+
+        def fake_get_bdm(context, _volume_id, columns_to_join):
+            self.assertEqual(volume_id, _volume_id)
+            return fake_bdm
+
+        self.stubs.Set(self.compute_api.db,
+                'block_device_mapping_get_by_volume_id', fake_get_bdm)
+        self.mox.StubOutWithMock(self.compute_api.compute_rpcapi,
+                'volume_snapshot_delete')
+
+        self.compute_api.compute_rpcapi.volume_snapshot_delete(self.context,
+                fake_bdm['instance'], volume_id, snapshot_id, {})
+
+        self.mox.ReplayAll()
+
+        self.compute_api.volume_snapshot_delete(self.context, volume_id,
+                snapshot_id, {})
+
 
 class ComputeAPIUnitTestCase(_ComputeAPIUnitTestMixIn, test.NoDBTestCase):
     def setUp(self):