bac116e29e8ce28558e530848274f56198058920
Matt Odden <mrodden@us.ibm.com>
2013-09-04 07:43:29 +0000

    Use system locale for default request language
    Remove en_US as the default language when no header is provided, and use
    None instead. Upon translation None will be defaulted to system locale
    as it was before API translation changes.
    
    fixes bug 1214476
    
    Change-Id: Ic33c94c29993ceb944d7c22d2e754cb938db7503

--- nova/api/openstack/wsgi.py
+++ nova/api/openstack/wsgi.py
@@ -179,10 +179,15 @@ class Request(webob.Request):
         return content_type
 
     def best_match_language(self):
-        """Determine language for returned response."""
+        """Determine the best available language for the request.
+
+        :returns: the best language match or None if the 'Accept-Language'
+                  header was not available in the request.
+        """
+        if not self.accept_language:
+            return None
         return self.accept_language.best_match(
-                gettextutils.get_available_languages('nova'),
-                default_match='en_US')
+                gettextutils.get_available_languages('nova'))
 
 
 class ActionDispatcher(object):
