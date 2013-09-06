573ebe4b41d2e76f14e1325a1b4f3d6ba007e624
He Jie Xu <xuhj@linux.vnet.ibm.com>
2013-09-06 16:25:26 +0800

    Add V3 API sample for admin-password
    Adds api sample tests and api samples for admin-password
    
    Partially implements blueprint v3-api-unittests
    
    Change-Id: Iade39ff931471d1d68e358ac5d35a1921e85df66

---
+++ nova/tests/integrated/v3/test_admin_password.py
@@ -0,0 +1,34 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# Copyright 2012 Nebula, Inc.
+# Copyright 2013 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nova.tests.integrated.v3 import test_servers
+
+
+class AdminPasswordJsonTest(test_servers.ServersSampleBase):
+    extension_name = 'os-admin-password'
+
+    def test_server_password(self):
+        uuid = self._post_server()
+        subs = {"password": "foo"}
+        response = self._do_post('servers/%s/action' % uuid,
+                                 'admin-password-change-password',
+                                 subs)
+        self.assertEqual(response.status, 204)
+        self.assertEqual(response.read(), "")
+
+
+class AdminPasswordXmlTest(AdminPasswordJsonTest):
+    ctype = 'xml'
