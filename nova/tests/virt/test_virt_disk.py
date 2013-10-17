f59c2d64ac8f0523c587e23b2d1c59fdfe7ea131
Michael Davies <michael@the-davies.net>
2013-10-17 04:41:09 -0700

    Fail quickly if file injection for boot volume
    Previously if the user attempted file injection on a booted volume it
    would fail and retry several times, which slowed down the instance
    spawning.  Instead of attempting file injection, fail quickly, and
    log a warning
    
    Fix bug 1188543
    
    Change-Id: I264540de736c2bcb92567826fe5ba672e1244ba2

--- nova/tests/virt/test_virt_disk.py
+++ nova/tests/virt/test_virt_disk.py
@@ -14,8 +14,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import fixtures
 import os
 import sys
+
+from posix import stat_result
 
 from nova import exception
 from nova import test
@@ -33,6 +36,19 @@ class VirtDiskTest(test.NoDBTestCase):
 
     def test_inject_data(self):
 
+        orig_os_stat = os.stat
+
+        def fake_stat(arg):
+            if arg == '/some/file':  # fake success
+                return stat_result((16877, 2, 2049L,
+                                    23, 0, 0,
+                                    4096, 1381787843,
+                                    1381635971, 1381635971))
+            else:
+                return orig_os_stat(arg)
+
+        self.useFixture(fixtures.MonkeyPatch('os.stat', fake_stat))
+
         self.assertTrue(diskapi.inject_data("/some/file", use_cow=True))
 
         self.assertTrue(diskapi.inject_data("/some/file",
@@ -49,6 +65,8 @@ class VirtDiskTest(test.NoDBTestCase):
                           mandatory=('admin_password',))
         self.assertFalse(diskapi.inject_data("/some/file", admin_password="p"))
         os.name = os_name
+
+        self.assertFalse(diskapi.inject_data("/some/fail/file"))
 
     def test_inject_data_key(self):
 
