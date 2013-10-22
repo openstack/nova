e3afd49eef5bf19c6fba01762bb72134afd92956
Davanum Srinivas <dims@linux.vnet.ibm.com>
2013-10-22 10:29:06 -0400

    Fix bad typo in cloudpipe.py
    Looks like a refactoring problem. we should be raising
    exc.HTTPBadRequest where exc is in webob.
    
    Change-Id: Id6d4e1d43f1d49c05d9da1234e40a603022d35c2

--- nova/tests/api/openstack/compute/contrib/test_cloudpipe.py
+++ nova/tests/api/openstack/compute/contrib/test_cloudpipe.py
@@ -15,10 +15,12 @@
 
 from lxml import etree
 from oslo.config import cfg
+from webob import exc
 
 from nova.api.openstack.compute.contrib import cloudpipe
 from nova.api.openstack import wsgi
 from nova.compute import utils as compute_utils
+from nova import exception
 from nova.openstack.common import timeutils
 from nova import test
 from nova.tests.api.openstack import fakes
@@ -114,6 +116,17 @@ class CloudpipeTest(test.NoDBTestCase):
 
         response = {'instance_id': 7777}
         self.assertEqual(res_dict, response)
+
+    def test_cloudpipe_create_no_networks(self):
+        def launch_vpn_instance(context):
+            raise exception.NoMoreNetworks
+
+        self.stubs.Set(self.controller.cloudpipe, 'launch_vpn_instance',
+                       launch_vpn_instance)
+        body = {'cloudpipe': {'project_id': 1}}
+        req = fakes.HTTPRequest.blank('/v2/fake/os-cloudpipe')
+        self.assertRaises(exc.HTTPBadRequest,
+                          self.controller.create, req, body)
 
     def test_cloudpipe_create_already_running(self):
         def launch_vpn_instance(*args, **kwargs):
