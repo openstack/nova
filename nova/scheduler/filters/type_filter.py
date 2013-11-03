a2b454c87863fbb4cf3ddaa5a5fd22841339bc8f
Russell Bryant <rbryant@redhat.com>
2013-11-03 19:32:21 +0800

    nit: fix indentation
    These lines (indented 3 spaces instead of 4) were making me twitch, so
    fix it.
    
    Change-Id: I7009214a44109b01a99531d7d84550eeafab6326

--- nova/scheduler/filters/type_filter.py
+++ nova/scheduler/filters/type_filter.py
@@ -46,7 +46,7 @@ class AggregateTypeAffinityFilter(filters.BaseHostFilter):
     key 'instance_type' has the instance_type name as a value
     """
 
-   # Aggregate data does not change within a request
+    # Aggregate data does not change within a request
     run_filter_once_per_request = True
 
     def host_passes(self, host_state, filter_properties):
