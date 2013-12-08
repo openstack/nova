df71afc3fc6bdb1454691bf825458003b37ff67d
Matt Riedemann <mriedem@us.ibm.com>
2013-12-08 13:44:51 -0800

    Rename instance_type to flavor in hyper-v virt driver
    Renames 'instance_type' to 'flavor' in the hyper-v virt driver and
    associated tests.
    
    Part of blueprint flavor-instance-type-dedup
    
    Change-Id: I769bf86a63283152c5bbc5644e7b74cc2bb24697

--- nova/virt/hyperv/imagecache.py
+++ nova/virt/hyperv/imagecache.py
@@ -54,9 +54,9 @@ class ImageCache(object):
     def _get_root_vhd_size_gb(self, instance):
         try:
             # In case of resizes we need the old root disk size
-            old_instance_type = flavors.extract_flavor(
+            old_flavor = flavors.extract_flavor(
                 instance, prefix='old_')
-            return old_instance_type['root_gb']
+            return old_flavor['root_gb']
         except KeyError:
             return instance['root_gb']
 
