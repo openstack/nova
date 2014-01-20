8cc59d2084a8b1129929a162b753d4ca407018e7
lizheming <lizheming.li@huawei.com>
2014-01-20 11:10:37 +0800

    Make exception message more friendly
    Exceptions that get returned via REST api
    should have more user friendly message,
    users shouldn't have to know backend concept.
    more detail,see bug.
    
    Change-Id: Idc20680e46c36fa3441838177ce61b6f5e1d2979
    Closes-Bug: #1263204

--- nova/exception.py
+++ nova/exception.py
@@ -1284,7 +1284,7 @@ class CryptoCRLFileNotFound(FileNotFound):
 
 
 class InstanceRecreateNotSupported(Invalid):
-    msg_fmt = _('Instance recreate is not implemented by this virt driver.')
+    msg_fmt = _('Instance recreate is not supported.')
 
 
 class ServiceGroupUnavailable(NovaException):
