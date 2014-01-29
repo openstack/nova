4803d7c7e084e0ff13cc4054b4fa1455495bd395
Shuangtai Tian <shuangtai.tian@intel.com>
2014-01-29 10:15:17 +0800

    Remove XML support from flavor v3 API plugins
    Remove XML support from the following v3 compute API plugins:
    
     - flavor_access
     - flavor_manage
     - flavor_rxtx
     - flavors
     - flavors_extraspecs
    
    Part of blueprint remove-v3-xml-api
    
    DocImpact
    
    Change-Id: I9745285cb6053dcb634c23002f1f24412dc62021

--- nova/api/openstack/compute/plugins/v3/flavor_access.py
+++ nova/api/openstack/compute/plugins/v3/flavor_access.py
@@ -22,7 +22,6 @@ import webob
 from nova.api.openstack.compute.schemas.v3 import flavor_access_schema
 from nova.api.openstack import extensions
 from nova.api.openstack import wsgi
-from nova.api.openstack import xmlutil
 from nova.api import validation
 from nova import exception
 from nova.objects import flavor as flavor_obj
@@ -32,44 +31,6 @@ ALIAS = 'flavor-access'
 soft_authorize = extensions.soft_extension_authorizer('compute',
                                                       'v3:' + ALIAS)
 authorize = extensions.extension_authorizer('compute', 'v3:%s' % ALIAS)
-
-
-def make_flavor(elem):
-    elem.set('{%s}is_public' % FlavorAccess.namespace,
-             '%s:is_public' % FlavorAccess.alias)
-
-
-def make_flavor_access(elem):
-    elem.set('flavor_id')
-    elem.set('tenant_id')
-
-
-class FlavorTemplate(xmlutil.TemplateBuilder):
-    def construct(self):
-        root = xmlutil.TemplateElement('flavor', selector='flavor')
-        make_flavor(root)
-        alias = FlavorAccess.alias
-        namespace = FlavorAccess.namespace
-        return xmlutil.SlaveTemplate(root, 1, nsmap={alias: namespace})
-
-
-class FlavorsTemplate(xmlutil.TemplateBuilder):
-    def construct(self):
-        root = xmlutil.TemplateElement('flavors')
-        elem = xmlutil.SubTemplateElement(root, 'flavor', selector='flavors')
-        make_flavor(elem)
-        alias = FlavorAccess.alias
-        namespace = FlavorAccess.namespace
-        return xmlutil.SlaveTemplate(root, 1, nsmap={alias: namespace})
-
-
-class FlavorAccessTemplate(xmlutil.TemplateBuilder):
-    def construct(self):
-        root = xmlutil.TemplateElement('flavor_access')
-        elem = xmlutil.SubTemplateElement(root, 'access',
-                                          selector='flavor_access')
-        make_flavor_access(elem)
-        return xmlutil.MasterTemplate(root, 1)
 
 
 def _marshall_flavor_access(flavor):
@@ -88,7 +49,6 @@ class FlavorAccessController(object):
         super(FlavorAccessController, self).__init__()
 
     @extensions.expected_errors(404)
-    @wsgi.serializers(xml=FlavorAccessTemplate)
     def index(self, req, flavor_id):
         context = req.environ['nova.context']
         authorize(context)
@@ -126,8 +86,6 @@ class FlavorActionController(wsgi.Controller):
     def show(self, req, resp_obj, id):
         context = req.environ['nova.context']
         if soft_authorize(context):
-            # Attach our slave template to the response object
-            resp_obj.attach(xml=FlavorTemplate())
             db_flavor = req.get_db_flavor(id)
 
             self._extend_flavor(resp_obj.obj['flavor'], db_flavor)
@@ -136,9 +94,6 @@ class FlavorActionController(wsgi.Controller):
     def detail(self, req, resp_obj):
         context = req.environ['nova.context']
         if soft_authorize(context):
-            # Attach our slave template to the response object
-            resp_obj.attach(xml=FlavorsTemplate())
-
             flavors = list(resp_obj.obj['flavors'])
             for flavor_rval in flavors:
                 db_flavor = req.get_db_flavor(flavor_rval['id'])
@@ -148,15 +103,11 @@ class FlavorActionController(wsgi.Controller):
     def create(self, req, body, resp_obj):
         context = req.environ['nova.context']
         if soft_authorize(context):
-            # Attach our slave template to the response object
-            resp_obj.attach(xml=FlavorTemplate())
-
             db_flavor = req.get_db_flavor(resp_obj.obj['flavor']['id'])
 
             self._extend_flavor(resp_obj.obj['flavor'], db_flavor)
 
     @extensions.expected_errors((400, 403, 404, 409))
-    @wsgi.serializers(xml=FlavorAccessTemplate)
     @wsgi.action("add_tenant_access")
     @validation.schema(request_body_schema=
                        flavor_access_schema.add_tenant_access)
@@ -179,7 +130,6 @@ class FlavorActionController(wsgi.Controller):
         return _marshall_flavor_access(flavor)
 
     @extensions.expected_errors((400, 403, 404))
-    @wsgi.serializers(xml=FlavorAccessTemplate)
     @wsgi.action("remove_tenant_access")
     @validation.schema(request_body_schema=
                        flavor_access_schema.remove_tenant_access)
