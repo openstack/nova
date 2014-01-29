12ecf0bdd80ea8d9643f1f798bc0e263453b9103
Shuangtai Tian <shuangtai.tian@intel.com>
2014-01-29 10:35:40 +0800

    Remove XML support from migrations pci multiple_create v3 API plugins
    Remove XML support from the following v3 compute API plugins:
    
     - migrations
     - multiple_create
     - pci
    
    Part of blueprint remove-v3-xml-api
    
    DocImpact
    
    Change-Id: If7dfda52eea63a39fae0e546cfeaf4b1d814b7b9

--- nova/api/openstack/compute/plugins/v3/pci.py
+++ nova/api/openstack/compute/plugins/v3/pci.py
@@ -18,39 +18,12 @@
 
 from nova.api.openstack import extensions
 from nova.api.openstack import wsgi
-from nova.api.openstack import xmlutil
 from nova.openstack.common import jsonutils
 
 
 ALIAS = 'os-pci'
 instance_authorize = extensions.soft_extension_authorizer(
     'compute', 'v3:' + ALIAS + ':pci_servers')
-
-
-def make_server(elem):
-    pci_devices = xmlutil.TemplateElement('%s:pci_devices' % Pci.alias,
-                                          colon_ns=True)
-    elem.append(pci_devices)
-    device = xmlutil.SubTemplateElement(pci_devices,
-                                        '%s:pci_device' % Pci.alias,
-                                        selector='%s:pci_devices' % Pci.alias,
-                                        colon_ns=True)
-    device.set('id')
-
-
-class PciServerTemplate(xmlutil.TemplateBuilder):
-    def construct(self):
-        root = xmlutil.TemplateElement('server', selector='server')
-        make_server(root)
-        return xmlutil.SlaveTemplate(root, 1, nsmap={Pci.alias: Pci.namespace})
-
-
-class PciServersTemplate(xmlutil.TemplateBuilder):
-    def construct(self):
-        root = xmlutil.TemplateElement('servers')
-        elem = xmlutil.SubTemplateElement(root, 'server', selector='servers')
-        make_server(elem)
-        return xmlutil.SlaveTemplate(root, 1, nsmap={Pci.alias: Pci.namespace})
 
 
 class PciServerController(wsgi.Controller):
@@ -64,7 +37,6 @@ class PciServerController(wsgi.Controller):
     def show(self, req, resp_obj, id):
         context = req.environ['nova.context']
         if instance_authorize(context):
-            resp_obj.attach(xml=PciServerTemplate())
             server = resp_obj.obj['server']
             instance = req.get_db_instance(server['id'])
             self._extend_server(server, instance)
@@ -73,41 +45,10 @@ class PciServerController(wsgi.Controller):
     def detail(self, req, resp_obj):
         context = req.environ['nova.context']
         if instance_authorize(context):
-            resp_obj.attach(xml=PciServersTemplate())
             servers = list(resp_obj.obj['servers'])
             for server in servers:
                 instance = req.get_db_instance(server['id'])
                 self._extend_server(server, instance)
-
-
-def make_hypervisor(elem):
-    pci_stats = xmlutil.TemplateElement('%s:pci_stats' % Pci.alias,
-                                        colon_ns=True)
-    elem.append(pci_stats)
-    pci_stat = xmlutil.make_flat_dict('%s:pci_stat' % Pci.alias,
-                                      selector='%s:pci_stats' % Pci.alias,
-                                      colon_ns=True,
-                                      root=pci_stats,
-                                      ignore_sub_dicts=True)
-    extra = xmlutil.make_flat_dict('extra_info', selector='extra_info')
-    pci_stat.append(extra)
-    pci_stats.append(pci_stat)
-
-
-class PciHypervisorTemplate(xmlutil.TemplateBuilder):
-    def construct(self):
-        root = xmlutil.TemplateElement('hypervisor', selector='hypervisor')
-        make_hypervisor(root)
-        return xmlutil.SlaveTemplate(root, 1, nsmap={Pci.alias: Pci.namespace})
-
-
-class HypervisorDetailTemplate(xmlutil.TemplateBuilder):
-    def construct(self):
-        root = xmlutil.TemplateElement('hypervisors')
-        elem = xmlutil.SubTemplateElement(root, 'hypervisor',
-                                          selector='hypervisors')
-        make_hypervisor(elem)
-        return xmlutil.SlaveTemplate(root, 1, nsmap={Pci.alias: Pci.namespace})
 
 
 class PciHypervisorController(wsgi.Controller):
@@ -117,14 +58,12 @@ class PciHypervisorController(wsgi.Controller):
 
     @wsgi.extends
     def show(self, req, resp_obj, id):
-        resp_obj.attach(xml=PciHypervisorTemplate())
         hypervisor = resp_obj.obj['hypervisor']
         compute_node = req.get_db_compute_node(hypervisor['id'])
         self._extend_hypervisor(hypervisor, compute_node)
 
     @wsgi.extends
     def detail(self, req, resp_obj):
-        resp_obj.attach(xml=HypervisorDetailTemplate())
         hypervisors = list(resp_obj.obj['hypervisors'])
         for hypervisor in hypervisors:
             compute_node = req.get_db_compute_node(hypervisor['id'])
