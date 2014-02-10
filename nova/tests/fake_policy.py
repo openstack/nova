f5cc4f4a7a08a5447ee6120fd9a19e53db181dae
Christopher Yeoh <cyeoh@au1.ibm.com>
2014-02-09 18:07:49 -0700

    Removes os-instance-usage-audit-log from the V3 API
    The extension pre-dates ceilometer and equivalent
    functionality is now much better managed using
    ceilometer. So we're removing this before the V3 API
    is officially released.
    
    Partially implements blueprint v3-api-remove-extensions
    
    Change-Id: I9b7892346a4ad7c54a903cfcbcb97156dfc8c734

--- nova/tests/fake_policy.py
+++ nova/tests/fake_policy.py
@@ -215,7 +215,6 @@ policy_data = """
     "compute_extension:instance_actions:events": "is_admin:True",
     "compute_extension:v3:os-instance-actions:events": "is_admin:True",
     "compute_extension:instance_usage_audit_log": "",
-    "compute_extension:v3:os-instance-usage-audit-log": "",
     "compute_extension:keypairs": "",
     "compute_extension:keypairs:index": "",
     "compute_extension:keypairs:show": "",
