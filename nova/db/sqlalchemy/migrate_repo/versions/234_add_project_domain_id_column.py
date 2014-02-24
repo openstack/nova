a9b3d81815167c13dbc427a53f2f8f726aa45618
Henrique Truta <henrique@lsd.ufcg.edu.br>
2014-02-24 13:53:16 -0300

    Inclusao

--- nova/db/sqlalchemy/migrate_repo/versions/234_add_project_domain_id_column.py
+++ nova/db/sqlalchemy/migrate_repo/versions/234_add_project_domain_id_column.py
@@ -14,7 +14,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from sqlalchemy import Column, MetaData, Boolean, Table, text
+from sqlalchemy import Column, MetaData, String, Table
 
 
 def upgrade(migrate_engine):
