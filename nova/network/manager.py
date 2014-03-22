e08ce4de920b68c84ac45be8a657a95113688780
jichenjc <jichenjc@cn.ibm.com>
2014-03-23 01:49:56 +0800

    Add virtual interface before add fixed IP on nova-network
    nova allow user to add fixed IP to an instance when the instance
    is running. This action will fail due to no virtual interface
    will be created before create fixed ip.
    TypeError: 'NoneType' object is unsubscriptable will be reported.
    
    Change-Id: I5885806d1965022816633d0105606e7aaf763b3a
    Closes-Bug: #1294939

--- nova/network/manager.py
+++ nova/network/manager.py
@@ -893,6 +893,13 @@ class NetworkManager(manager.Manager):
 
                 vif = objects.VirtualInterface.get_by_instance_and_network(
                         context, instance_id, network['id'])
+                if vif is None:
+                    LOG.debug('vif for network %(network)s is used up, '
+                              'trying to create new vif',
+                              {'network': network['id']}, instance=instance)
+                    vif = self._add_virtual_interface(context,
+                        instance_id, network['id'])
+
                 fip.allocated = True
                 fip.virtual_interface_id = vif.id
                 fip.save()
@@ -1924,6 +1931,15 @@ class VlanManager(RPCAllocateFixedIP, floating_ips.FloatingIP, NetworkManager):
 
         vif = objects.VirtualInterface.get_by_instance_and_network(
             context, instance_id, network['id'])
+        if vif is None:
+            LOG.debug('vif for network %(network)s and instance '
+                      '%(instance_id)s is used up, '
+                      'trying to create new vif',
+                      {'network': network['id'],
+                       'instance_id': instance_id})
+            vif = self._add_virtual_interface(context,
+                instance_id, network['id'])
+
         fip.allocated = True
         fip.virtual_interface_id = vif.id
         fip.save()
