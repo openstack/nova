e0fa47cecc4a1c340db716032fd58c4564a186e3
Gary Kotton <gkotton@vmware.com>
2014-03-25 00:44:03 -0700

    VMware: remove double import
    "import mock" was done twice.
    
    Change-Id: Ibc38d27d7fd8b57b193cbe37d2abd89b823ed43f

--- nova/tests/virt/vmwareapi/test_vmops.py
+++ nova/tests/virt/vmwareapi/test_vmops.py
@@ -12,9 +12,8 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-import mock
-
 import contextlib
+
 import mock
 
 from nova import exception
