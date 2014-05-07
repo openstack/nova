0617601de93ed2149ac16d6c10e46e508a0977b0
Radoslav Gerganov <rgerganov@vmware.com>
2014-05-07 17:57:31 +0300

    VMware: use default values in get_info() when properties are missing
    The properties that we retrieve from VirtualMachineConfigSummary are
    optional ('summary.config.numCpu', 'summary.config.memorySizeMB') and
    if they are missing we should return default values from get_info()
    
    This patch also improves the unit test for get_info(). It removes the
    test in test_driver_api.py (test_get_info() and test_spawn() are the
    same) and adds a new test in test_vmops.py
    
    Conflicts:
    
    	nova/tests/virt/vmwareapi/test_driver_api.py
    	nova/tests/virt/vmwareapi/test_vmops.py
    
    Closes-Bug: 1317912
    Change-Id: Ibbb28985d13de371fdac8a119ad820d25acc21be
    (cherry picked from commit 2db7528ca258d7b0c1205ab2f9a76569d9f9a82d)

--- nova/tests/virt/vmwareapi/test_vmops.py
+++ nova/tests/virt/vmwareapi/test_vmops.py
@@ -17,9 +17,13 @@ import mock
 import contextlib
 import mock
 
+from nova.compute import power_state
+from nova import context
 from nova import exception
 from nova.network import model as network_model
 from nova import test
+from nova.tests import fake_instance
+import nova.tests.image.fake
 from nova.tests.virt.vmwareapi import stubs
 from nova import utils
 from nova.virt.vmwareapi import driver
@@ -38,7 +42,19 @@ class VMwareVMOpsTestCase(test.NoDBTestCase):
         self._session = driver.VMwareAPISession()
 
         self._vmops = vmops.VMwareVMOps(self._session, None, None)
-        self._instance = {'name': 'fake_name', 'uuid': 'fake_uuid'}
+        self._image_id = nova.tests.image.fake.get_valid_image_id()
+        values = {
+            'name': 'fake_name',
+            'uuid': 'fake_uuid',
+            'vcpus': 1,
+            'memory_mb': 512,
+            'image_ref': self._image_id,
+            'root_gb': 1,
+            'node': 'respool-1001(MyResPoolName)',
+        }
+        self._context = context.RequestContext('fake_user', 'fake_project')
+        self._instance = fake_instance.fake_instance_obj(self._context,
+                                                         **values)
 
         subnet_4 = network_model.Subnet(cidr='192.168.0.1/24',
                                         dns=[network_model.IP('192.168.0.1')],
@@ -277,3 +293,30 @@ class VMwareVMOpsTestCase(test.NoDBTestCase):
                 'get_dynamic_property', moref, 'Datastore', 'browser')
             self.assertIs(ds_browser, ret)
             self.assertIs(ds_browser, cache.get(moref.value))
+
+    @mock.patch('nova.virt.vmwareapi.vm_util.get_vm_ref',
+                return_value='fake_ref')
+    def test_get_info(self, mock_get_vm_ref):
+        props = ['summary.config.numCpu', 'summary.config.memorySizeMB',
+                 'runtime.powerState']
+        prop_cpu = vmwareapi_fake.Prop(props[0], 4)
+        prop_mem = vmwareapi_fake.Prop(props[1], 128)
+        prop_state = vmwareapi_fake.Prop(props[2], 'poweredOn')
+        prop_list = [prop_state, prop_mem, prop_cpu]
+        obj_content = vmwareapi_fake.ObjectContent(None, prop_list=prop_list)
+        result = vmwareapi_fake.FakeRetrieveResult()
+        result.add_object(obj_content)
+        mock_call_method = mock.Mock(return_value=result)
+        with mock.patch.object(self._session, '_call_method',
+                               mock_call_method):
+            info = self._vmops.get_info(self._instance)
+            mock_call_method.assert_called_once_with(vim_util,
+                'get_object_properties', None, 'fake_ref', 'VirtualMachine',
+                props)
+            mock_get_vm_ref.assert_called_once_with(self._session,
+                self._instance)
+            self.assertEqual(power_state.RUNNING, info['state'])
+            self.assertEqual(128 * 1024, info['max_mem'])
+            self.assertEqual(128 * 1024, info['mem'])
+            self.assertEqual(4, info['num_cpu'])
+            self.assertEqual(0, info['cpu_time'])
