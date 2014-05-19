openstack-archive/oslo-incubator

Reap child processes gracefully if greenlet thread gets killed
If ProcessLauncher is run from within a thread we should be able to
call wait after the thread is killed to reap the child processes.
Catching this exception and then proceeding to the cleanup phase
accomplishes this.

Closes-Bug: #1282206
Change-Id: I9c863864c25c0478b720fc57cf0dccbe9bc9d9f6

openstack/common/service.py
@@ -384,19 +384,22 @@ class ProcessLauncher(object):
         LOG.debug('Full set of CONF:')
         CONF.log_opt_values(LOG, std_logging.DEBUG)
 
-        while True:
-            self.handle_signal()
-            self._respawn_children()
-            if self.sigcaught:
-                signame = _signo_to_signame(self.sigcaught)
-                LOG.info(_LI('Caught %s, stopping children'), signame)
-            if not _is_sighup_and_daemon(self.sigcaught):
-                break
+        try:
+            while True:
+                self.handle_signal()
+                self._respawn_children()
+                if self.sigcaught:
+                    signame = _signo_to_signame(self.sigcaught)
+                    LOG.info(_LI('Caught %s, stopping children'), signame)
+                if not _is_sighup_and_daemon(self.sigcaught):
+                    break
 
-            for pid in self.children:
-                os.kill(pid, signal.SIGHUP)
-            self.running = True
-            self.sigcaught = None
+                for pid in self.children:
+                    os.kill(pid, signal.SIGHUP)
+                self.running = True
+                self.sigcaught = None
+        except eventlet.greenlet.GreenletExit:
+            LOG.info(_("Wait called after thread killed.  Cleaning up."))
 
         for pid in self.children:
             try:

openstack/nova

Conflicts:
	nova/openstack/common/service.py
(cherry picked from commit a6fec438abe61d40fc4fb6907ea1fefd4dd93e22)

nova/openstack/common/service.py
@@ -378,19 +378,22 @@ class ProcessLauncher(object):
         LOG.debug(_('Full set of CONF:'))
         CONF.log_opt_values(LOG, std_logging.DEBUG)
 
-        while True:
-            self.handle_signal()
-            self._respawn_children()
-            if self.sigcaught:
-                signame = _signo_to_signame(self.sigcaught)
-                LOG.info(_('Caught %s, stopping children'), signame)
-            if not _is_sighup_and_daemon(self.sigcaught):
-                break
+        try:
+            while True:
+                self.handle_signal()
+                self._respawn_children()
+                if self.sigcaught:
+                    signame = _signo_to_signame(self.sigcaught)
+                    LOG.info(_('Caught %s, stopping children'), signame)
+                if not _is_sighup_and_daemon(self.sigcaught):
+                    break
 
-            for pid in self.children:
-                os.kill(pid, signal.SIGHUP)
-            self.running = True
-            self.sigcaught = None
+                for pid in self.children:
+                    os.kill(pid, signal.SIGHUP)
+                self.running = True
+                self.sigcaught = None
+        except eventlet.greenlet.GreenletExit:
+            LOG.info(_("Wait called after thread killed.  Cleaning up."))
 
         for pid in self.children:
             try: