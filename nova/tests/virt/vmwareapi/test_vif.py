openstack/nova

commit 6fb9cc8b278ceab05e65e1c5145141203211d246

VMware: spawn refactor - phase 1 - get_vif_info
get_vif_info converts OpenStacks nova network_info
into a data structure that is used to prepare the
configuration spec for the instance that is being deployed.

partial blueprint vmware-spawn-refactor

Change-Id: I5d1535e84de44a995e1605d0d5b850c9756612de

nova/virt/vmwareapi/vif.py
@@ -32,6 +32,9 @@ vmwareapi_vif_opts = [
     cfg.StrOpt('vlan_interface',
                default='vmnic0',
                help='Physical ethernet adapter name for vlan networking'),
+    cfg.StrOpt('integration_bridge',
+               default='br-int',
+               help='Name of Integration Bridge'),
 ]
 
 CONF.register_opts(vmwareapi_vif_opts, 'vmware')
@@ -149,3 +152,20 @@ def get_network_ref(session, cluster, vif, is_neutron):
         network_ref = ensure_vlan_bridge(session, vif, cluster=cluster,
                                          create_vlan=create_vlan)
     return network_ref
+
+
+def get_vif_info(session, cluster, is_neutron, vif_model, network_info):
+    vif_infos = []
+    if not network_info:
+        return vif_infos
+    for vif in network_info:
+        mac_address = vif['address']
+        net_name = vif['network']['bridge'] or CONF.vmware.integration_bridge
+        network_ref = get_network_ref(session, cluster, vif, is_neutron)
+        vif_infos.append({'network_name': net_name,
+                          'mac_address': mac_address,
+                          'network_ref': network_ref,
+                          'iface_id': vif['id'],
+                          'vif_model': vif_model
+                         })
+    return vif_infos

nova/tests/virt/vmwareapi/test_vif.py
@@ -22,6 +22,7 @@ from nova import exception
 from nova.network import model as network_model
 from nova import test
 from nova.tests import matchers
+from nova.tests import utils
 from nova.tests.virt.vmwareapi import test_vm_util
 from nova.virt.vmwareapi import error_util
 from nova.virt.vmwareapi import fake
@@ -322,3 +323,20 @@ class VMwareVifTestCase(test.NoDBTestCase):
         ) as (_get_host, _call_method, _get_name):
             vif.get_neutron_network(self.session, 'network_name',
                                     'cluster', 'vif')
+
+    def test_get_vif_info_none(self):
+        vif_info = vif.get_vif_info('fake_session', 'fake_cluster',
+                                    'is_neutron', 'fake_model', None)
+        self.assertEqual([], vif_info)
+
+    @mock.patch.object(vif, 'get_network_ref', return_value='fake_ref')
+    def test_get_vif_info(self, mock_get_network_ref):
+        network_info = utils.get_test_network_info()
+        vif_info = vif.get_vif_info('fake_session', 'fake_cluster',
+                                    'is_neutron', 'fake_model', network_info)
+        expected = [{'iface_id': 'vif-xxx-yyy-zzz',
+                     'mac_address': 'fake',
+                     'network_name': 'fake',
+                     'network_ref': 'fake_ref',
+                     'vif_model': 'fake_model'}]
+        self.assertEqual(expected, vif_info)

nova/virt/vmwareapi/vmops.py
@@ -53,18 +53,7 @@ from nova.virt.vmwareapi import vm_util
 from nova.virt.vmwareapi import vmware_images
 
 
-vmware_vif_opts = [
-    cfg.StrOpt('integration_bridge',
-               default='br-int',
-               help='Name of Integration Bridge'),
-    ]
-
-vmware_group = cfg.OptGroup(name='vmware',
-                            title='VMware Options')
-
 CONF = cfg.CONF
-CONF.register_group(vmware_group)
-CONF.register_opts(vmware_vif_opts, vmware_group)
 CONF.import_opt('image_cache_subdirectory_name', 'nova.virt.imagecache')
 CONF.import_opt('remove_unused_base_images', 'nova.virt.imagecache')
 CONF.import_opt('vnc_enabled', 'nova.vnc')
@@ -263,27 +252,9 @@ class VMwareVMOps(object):
         res_pool_ref = vm_util.get_res_pool_ref(self._session,
                                                 self._cluster, node_mo_id)
 
-        def _get_vif_infos():
-            vif_infos = []
-            if network_info is None:
-                return vif_infos
-            for vif in network_info:
-                mac_address = vif['address']
-                network_name = vif['network']['bridge'] or \
-                               CONF.vmware.integration_bridge
-                network_ref = vmwarevif.get_network_ref(self._session,
-                                                        self._cluster,
-                                                        vif,
-                                                        utils.is_neutron())
-                vif_infos.append({'network_name': network_name,
-                                  'mac_address': mac_address,
-                                  'network_ref': network_ref,
-                                  'iface_id': vif['id'],
-                                  'vif_model': vif_model
-                                 })
-            return vif_infos
-
-        vif_infos = _get_vif_infos()
+        vif_infos = vmwarevif.get_vif_info(self._session, self._cluster,
+                                           utils.is_neutron(), vif_model,
+                                           network_info)
 
         # Get the instance name. In some cases this may differ from the 'uuid',
         # for example when the spawn of a rescue instance takes place.

commit 6003726ee95e25410092e73441a5afe610ad4a53

VMware: validate the network_info is defined
Ensure that the network info is defined correctly prior to allocating
networks.

Commit 6fb9cc8b278ceab05e65e1c5145141203211d246 caused a degradation.

Change-Id: I74eddc0ff744ee5d80df825a52527c67583bacea

nova/virt/vmwareapi/vif.py
@@ -156,7 +156,7 @@ def get_network_ref(session, cluster, vif, is_neutron):
 
 def get_vif_info(session, cluster, is_neutron, vif_model, network_info):
     vif_infos = []
-    if not network_info:
+    if network_info is None:
         return vif_infos
     for vif in network_info:
         mac_address = vif['address']

nova/tests/virt/vmwareapi/test_vif.py
@@ -329,6 +329,11 @@ class VMwareVifTestCase(test.NoDBTestCase):
                                     'is_neutron', 'fake_model', None)
         self.assertEqual([], vif_info)
 
+    def test_get_vif_info_empty_list(self):
+        vif_info = vif.get_vif_info('fake_session', 'fake_cluster',
+                                    'is_neutron', 'fake_model', [])
+        self.assertEqual([], vif_info)
+
     @mock.patch.object(vif, 'get_network_ref', return_value='fake_ref')
     def test_get_vif_info(self, mock_get_network_ref):
         network_info = utils.get_test_network_info()