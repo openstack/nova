348ef1006e9f6442b7fa252a337ebe4309a56faa
Daniel P. Berrange <berrange@redhat.com>
2014-07-14 14:25:14 +0100

    compute: Add standard constants for hypervisor virt types
    Introduce a nova/compute/hvtype.py module to serve a similar
    role to nova/compute/vm_mode.py, by providing constants for known
    hypervisor virtualization types. This is primarily used as second
    component of entries in the 'supported_instances' host state
    property. It is not to be confused with the hypervisor driver
    types.
    
    Related-bug: #1348623
    Change-Id: I553697992b010179e2bbf25fb8c0d471a15e4b3e

---
+++ nova/tests/compute/test_hvtype.py
@@ -0,0 +1,40 @@
+#    Copyright (C) 2014 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nova.compute import hvtype
+from nova import exception
+from nova import test
+
+
+class HvTypeTest(test.NoDBTestCase):
+
+    def test_valid_string(self):
+        self.assertTrue(hvtype.is_valid("vmware"))
+
+    def test_valid_constant(self):
+        self.assertTrue(hvtype.is_valid(hvtype.QEMU))
+
+    def test_valid_bogus(self):
+        self.assertFalse(hvtype.is_valid("acmehypervisor"))
+
+    def test_canonicalize_none(self):
+        self.assertIsNone(hvtype.canonicalize(None))
+
+    def test_canonicalize_case(self):
+        self.assertEqual(hvtype.QEMU, hvtype.canonicalize("QeMu"))
+
+    def test_canonicalize_invalid(self):
+        self.assertRaises(exception.InvalidHypervisorVirtType,
+                          hvtype.canonicalize,
+                          "wibble")
