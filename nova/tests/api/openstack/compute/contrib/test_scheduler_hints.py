d05cc3f78f74acd8db688867ddb262c14e50e1d0
Ghanshyam <ghanshyam.mann@nectechnologies.in>
2014-08-21 18:03:25 +0900

    Backport some v3 scheduler hints API UT to v2 API
    This patch adds below v3 scheduler hints API unittest
    into V2 API to improve V2 API unittesting.
    
    Change-Id: I5352906567c1c90c33d0709addaeb445fe1a7371

--- nova/tests/api/openstack/compute/contrib/test_scheduler_hints.py
+++ nova/tests/api/openstack/compute/contrib/test_scheduler_hints.py
@@ -13,11 +13,19 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import datetime
+
 from nova.api.openstack import compute
+from nova.api.openstack.compute import servers
+from nova.api.openstack import extensions
 import nova.compute.api
+from nova.compute import flavors
+from nova import db
 from nova.openstack.common import jsonutils
 from nova import test
 from nova.tests.api.openstack import fakes
+from nova.tests import fake_instance
+from nova.tests.image import fake
 
 
 UUID = fakes.FAKE_UUID
@@ -95,3 +103,68 @@ class SchedulerHintsTestCase(test.TestCase):
         req.body = jsonutils.dumps(body)
         res = req.get_response(self.app)
         self.assertEqual(400, res.status_int)
+
+
+class ServersControllerCreateTest(test.TestCase):
+
+    def setUp(self):
+        """Shared implementation for tests below that create instance."""
+        super(ServersControllerCreateTest, self).setUp()
+
+        self.instance_cache_num = 0
+        self.ext_mgr = extensions.ExtensionManager()
+        self.ext_mgr.extensions = {}
+        self.controller = servers.Controller(self.ext_mgr)
+
+        def instance_create(context, inst):
+            inst_type = flavors.get_flavor_by_flavor_id(3)
+            image_uuid = '76fa36fc-c930-4bf3-8c8a-ea2a2420deb6'
+            def_image_ref = 'http://localhost/images/%s' % image_uuid
+            self.instance_cache_num += 1
+            instance = fake_instance.fake_db_instance(**{
+                'id': self.instance_cache_num,
+                'display_name': inst['display_name'] or 'test',
+                'uuid': fakes.FAKE_UUID,
+                'instance_type': dict(inst_type),
+                'access_ip_v4': '1.2.3.4',
+                'access_ip_v6': 'fead::1234',
+                'image_ref': inst.get('image_ref', def_image_ref),
+                'user_id': 'fake',
+                'project_id': 'fake',
+                'reservation_id': inst['reservation_id'],
+                "created_at": datetime.datetime(2010, 10, 10, 12, 0, 0),
+                "updated_at": datetime.datetime(2010, 11, 11, 11, 0, 0),
+                "progress": 0,
+                "fixed_ips": [],
+                "task_state": "",
+                "vm_state": "",
+                "root_device_name": inst.get('root_device_name', 'vda'),
+            })
+
+            return instance
+
+        fake.stub_out_image_service(self.stubs)
+        self.stubs.Set(db, 'instance_create', instance_create)
+
+    def _test_create_extra(self, params):
+        image_uuid = 'c905cedb-7281-47e4-8a62-f26bc5fc4c77'
+        server = dict(name='server_test', imageRef=image_uuid, flavorRef=2)
+        body = dict(server=server)
+        body.update(params)
+        req = fakes.HTTPRequest.blank('/fake//servers')
+        req.method = 'POST'
+        req.body = jsonutils.dumps(body)
+        req.headers["content-type"] = "application/json"
+        server = self.controller.create(req, body=body).obj['server']
+
+    def test_create_instance_with_scheduler_hints_disabled(self):
+        hints = {'same_host': '48e6a9f6-30af-47e0-bc04-acaed113bb4e'}
+        params = {'OS-SCH-HNT:scheduler_hints': hints}
+        old_create = nova.compute.api.API.create
+
+        def create(*args, **kwargs):
+            self.assertEqual(kwargs['scheduler_hints'], {})
+            return old_create(*args, **kwargs)
+
+        self.stubs.Set(nova.compute.api.API, 'create', create)
+        self._test_create_extra(params)
