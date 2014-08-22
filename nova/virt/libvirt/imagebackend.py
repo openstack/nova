1613cd99345e51e314bf011f47654ed730138f64
Roman Podoliaka <rpodolyaka@mirantis.com>
2014-08-22 20:01:33 +0300

    Fix instance boot when Ceph is used for ephemeral storage
    is_block_dev attribute was mistakenly set to True for RBD images
    (we don't actually map RBD images to block devices in the *host*
    system, but only in *guests*, so as far as the host system is
    concerned an RBD image must not be treated like a system block
    device). This led to a situation when mkfs was called for a non
    existing file and failed when trying to create an ephemeral fs.
    
    Closes-Bug: #1362221
    
    Change-Id: I54c0d117a53bb61f278b2e137bd29a595548f5a1
    (cherry picked from commit e9eb1d69bf75c22b0ad4b50e4892a5644c78cf68)

--- nova/virt/libvirt/imagebackend.py
+++ nova/virt/libvirt/imagebackend.py
@@ -538,7 +538,7 @@ def ascii_str(s):
 
 class Rbd(Image):
     def __init__(self, instance=None, disk_name=None, path=None, **kwargs):
-        super(Rbd, self).__init__("block", "rbd", is_block_dev=True)
+        super(Rbd, self).__init__("block", "rbd", is_block_dev=False)
         if path:
             try:
                 self.rbd_name = path.split('/')[1]
