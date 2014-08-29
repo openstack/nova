openstack/nova

commit cc0be157d005c5588fe5db779fc30fefbf22b44d

Recover from REBOOT-* state on compute manager start-up
If a compute manager is stopped / fails during certain REBOOT-*
operations then the instance will be left stuck in a transitional
task_state

This change handles two possible task states REBOOT_PENDING and
REBOOT_STARTED (for both soft and hard reboots). And either clears
these states or retries the reboot depending on the instance state.

Both task states are set after the request has gotten to the compute
manager so we can handle these safely knowing the operation has
ended/failed with the restart of the compute manager.

We retry the reboot where the state is PENDING and where the state is
STARTED and the instance is not running.

Where the instance is running and the state is STARTED we simply
transition the instance to an ACTIVE state. The user can retry the
reboot if required.

Related to blueprint recover-stuck-state

Change-Id: Ib318b3d444a67616441302f3daa8de20ee946f3f

nova/compute/api.py
@@ -2050,9 +2050,8 @@ class API(base.Base):
 
     @wrap_check_policy
     @check_instance_lock
-    @check_instance_state(vm_state=[vm_states.ACTIVE, vm_states.STOPPED,
-                                    vm_states.PAUSED, vm_states.SUSPENDED,
-                                    vm_states.ERROR],
+    @check_instance_state(vm_state=set(
+                    vm_states.ALLOW_SOFT_REBOOT + vm_states.ALLOW_HARD_REBOOT),
                           task_state=[None, task_states.REBOOTING,
                                       task_states.REBOOTING_HARD,
                                       task_states.RESUMING,
@@ -2062,10 +2061,7 @@ class API(base.Base):
     def reboot(self, context, instance, reboot_type):
         """Reboot the given instance."""
         if (reboot_type == 'SOFT' and
-                (instance['vm_state'] in [vm_states.STOPPED,
-                                          vm_states.PAUSED,
-                                          vm_states.SUSPENDED,
-                                          vm_states.ERROR])):
+            (instance['vm_state'] not in vm_states.ALLOW_SOFT_REBOOT)):
             raise exception.InstanceInvalidState(
                 attr='vm_state',
                 instance_uuid=instance['uuid'],

nova/api/openstack/common.py
@@ -59,7 +59,11 @@ _STATE_MAP = {
     vm_states.ACTIVE: {
         'default': 'ACTIVE',
         task_states.REBOOTING: 'REBOOT',
+        task_states.REBOOT_PENDING: 'REBOOT',
+        task_states.REBOOT_STARTED: 'REBOOT',
         task_states.REBOOTING_HARD: 'HARD_REBOOT',
+        task_states.REBOOT_PENDING_HARD: 'HARD_REBOOT',
+        task_states.REBOOT_STARTED_HARD: 'HARD_REBOOT',
         task_states.UPDATING_PASSWORD: 'PASSWORD',
         task_states.REBUILDING: 'REBUILD',
         task_states.REBUILD_BLOCK_DEVICE_MAPPING: 'REBUILD',

nova/compute/manager.py
@@ -848,6 +848,34 @@ class ComputeManager(manager.Manager):
             finally:
                 return
 
+        try_reboot, reboot_type = self._retry_reboot(context, instance)
+        current_power_state = self._get_power_state(context, instance)
+
+        if try_reboot:
+            LOG.debug(_("Instance in transitional state (%(task_state)s) at "
+                        "start-up and power state is (%(power_state)s), "
+                        "triggering reboot"),
+                       {'task_state': instance['task_state'],
+                        'power_state': current_power_state},
+                       instance=instance)
+            self.compute_rpcapi.reboot_instance(context, instance,
+                                                block_device_info=None,
+                                                reboot_type=reboot_type)
+            return
+
+        elif (current_power_state == power_state.RUNNING and
+           instance.task_state in [task_states.REBOOT_STARTED,
+                                   task_states.REBOOT_STARTED_HARD]):
+            LOG.warning(_("Instance in transitional state "
+                          "(%(task_state)s) at start-up and power state "
+                          "is (%(power_state)s), clearing task state"),
+                        {'task_state': instance['task_state'],
+                         'power_state': current_power_state},
+                        instance=instance)
+            instance = self._instance_update(context, instance.uuid,
+                                             vm_state=vm_states.ACTIVE,
+                                             task_state=None)
+
         net_info = compute_utils.get_nw_info_for_instance(instance)
         try:
             self.driver.plug_vifs(instance, net_info)
@@ -916,6 +944,27 @@ class ComputeManager(manager.Manager):
             except NotImplementedError:
                 LOG.warning(_('Hypervisor driver does not support '
                               'firewall rules'), instance=instance)
+
+    def _retry_reboot(self, context, instance):
+        current_power_state = self._get_power_state(context, instance)
+        current_task_state = instance.task_state
+        retry_reboot = False
+        reboot_type = compute_utils.get_reboot_type(current_task_state,
+                                                    current_power_state)
+
+        pending_soft = (current_task_state == task_states.REBOOT_PENDING and
+                        instance.vm_state in vm_states.ALLOW_SOFT_REBOOT)
+        pending_hard = (current_task_state == task_states.REBOOT_PENDING_HARD
+                        and instance.vm_state in vm_states.ALLOW_HARD_REBOOT)
+        started_not_running = (current_task_state in
+                               [task_states.REBOOT_STARTED,
+                                task_states.REBOOT_STARTED_HARD] and
+                               current_power_state != power_state.RUNNING)
+
+        if pending_soft or pending_hard or started_not_running:
+            retry_reboot = True
+
+        return retry_reboot, reboot_type
 
     def handle_lifecycle_event(self, event):
         LOG.info(_("Lifecycle event %(state)d on VM %(uuid)s") %
@@ -2528,6 +2577,17 @@ class ComputeManager(manager.Manager):
     def reboot_instance(self, context, instance, block_device_info,
                         reboot_type):
         """Reboot an instance on this host."""
+        # acknowledge the request made it to the manager
+        if reboot_type == "SOFT":
+            instance.task_state = task_states.REBOOT_PENDING
+            expected_states = (task_states.REBOOTING,
+                               task_states.REBOOT_PENDING,
+                               task_states.REBOOT_STARTED)
+        else:
+            instance.task_state = task_states.REBOOT_PENDING_HARD
+            expected_states = (task_states.REBOOTING_HARD,
+                               task_states.REBOOT_PENDING_HARD,
+                               task_states.REBOOT_STARTED_HARD)
         context = context.elevated()
         LOG.audit(_("Rebooting instance"), context=context, instance=instance)
 
@@ -2541,7 +2601,7 @@ class ComputeManager(manager.Manager):
         current_power_state = self._get_power_state(context, instance)
 
         instance.power_state = current_power_state
-        instance.save()
+        instance.save(expected_task_state=expected_states)
 
         if instance['power_state'] != power_state.RUNNING:
             state = instance['power_state']
@@ -2562,7 +2622,13 @@ class ComputeManager(manager.Manager):
             else:
                 new_vm_state = vm_states.ACTIVE
             new_power_state = None
-
+            if reboot_type == "SOFT":
+                instance.task_state = task_states.REBOOT_STARTED
+                expected_state = task_states.REBOOT_PENDING
+            else:
+                instance.task_state = task_states.REBOOT_STARTED_HARD
+                expected_state = task_states.REBOOT_PENDING_HARD
+            instance.save(expected_task_state=expected_state)
             self.driver.reboot(context, instance,
                                network_info,
                                reboot_type,

nova/compute/task_states.py
@@ -55,7 +55,11 @@ RESIZE_CONFIRMING = 'resize_confirming'
 
 # possible task states during reboot()
 REBOOTING = 'rebooting'
+REBOOT_PENDING = 'reboot_pending'
+REBOOT_STARTED = 'reboot_started'
 REBOOTING_HARD = 'rebooting_hard'
+REBOOT_PENDING_HARD = 'reboot_pending_hard'
+REBOOT_STARTED_HARD = 'reboot_started_hard'
 
 # possible task states during pause()
 PAUSING = 'pausing'

nova/compute/utils.py
@@ -23,6 +23,8 @@ from oslo.config import cfg
 
 from nova import block_device
 from nova.compute import flavors
+from nova.compute import power_state
+from nova.compute import task_states
 from nova import exception
 from nova.network import model as network_model
 from nova import notifications
@@ -446,6 +448,16 @@ def usage_volume_info(vol_usage):
                 vol_usage['curr_write_bytes'])
 
     return usage_info
+
+
+def get_reboot_type(task_state, current_power_state):
+    """Checks if the current instance state requires a HARD reboot."""
+    if current_power_state != power_state.RUNNING:
+        return 'HARD'
+    soft_types = [task_states.REBOOT_STARTED, task_states.REBOOT_PENDING,
+                  task_states.REBOOTING]
+    reboot_type = 'SOFT' if task_state in soft_types else 'HARD'
+    return reboot_type
 
 
 class EventReporter(object):

nova/compute/vm_states.py
@@ -46,3 +46,7 @@ ERROR = 'error'
 SHELVED = 'shelved'  # VM is powered off, resources still on hypervisor
 SHELVED_OFFLOADED = 'shelved_offloaded'  # VM and associated resources are
 # not on hypervisor
+
+ALLOW_SOFT_REBOOT = [ACTIVE]  # states we can soft reboot from
+ALLOW_HARD_REBOOT = ALLOW_SOFT_REBOOT + [STOPPED, PAUSED, SUSPENDED, ERROR]
+# states we allow hard reboot from

nova/tests/api/openstack/compute/plugins/v3/test_servers.py
@@ -955,7 +955,10 @@ class ServersControllerTest(ControllerTest):
                          expected_attrs=[]):
             self.assertIsNotNone(search_opts)
             self.assertIn('task_state', search_opts)
-            self.assertEqual(search_opts['task_state'], [task_state])
+            self.assertEqual([task_states.REBOOT_PENDING,
+                              task_states.REBOOT_STARTED,
+                              task_states.REBOOTING],
+                             search_opts['task_state'])
             db_list = [fakes.stub_instance(100, uuid=server_uuid,
                                                 task_state=task_state)]
             return instance_obj._make_instance_list(

nova/tests/api/openstack/compute/test_servers.py
@@ -876,7 +876,10 @@ class ServersControllerTest(ControllerTest):
                          limit=None, marker=None, want_objects=False):
             self.assertIsNotNone(search_opts)
             self.assertIn('task_state', search_opts)
-            self.assertEqual(search_opts['task_state'], [task_state])
+            self.assertEqual([task_states.REBOOT_PENDING,
+                              task_states.REBOOT_STARTED,
+                              task_states.REBOOTING],
+                             search_opts['task_state'])
             db_list = [fakes.stub_instance(100, uuid=server_uuid,
                                                 task_state=task_state)]
             return instance_obj._make_instance_list(

commit 7d21ceac9e1bd14b0efb605b3ff3276b8d193c45

Allow hard reboots when still attempting a soft reboot
Commit cc0be157d005c5588fe5db779fc30fefbf22b44d introduced a
regression in the reboot code. Previously, a hard reboot could be
performed when an instance was in soft reboot.

This fixes the regression by allowing a hard reboot when the instance
is in one of the two new task states.

Closes-bug: 1362799
Change-Id: Id8aaf852b0cccb75a79794a8bf9eb94e21550b81

nova/compute/api.py
@@ -2059,6 +2059,8 @@ class API(base.Base):
     @check_instance_state(vm_state=set(
                     vm_states.ALLOW_SOFT_REBOOT + vm_states.ALLOW_HARD_REBOOT),
                           task_state=[None, task_states.REBOOTING,
+                                      task_states.REBOOT_PENDING,
+                                      task_states.REBOOT_STARTED,
                                       task_states.REBOOTING_HARD,
                                       task_states.RESUMING,
                                       task_states.UNPAUSING,
@@ -2075,7 +2077,8 @@ class API(base.Base):
                 method='reboot')
         if ((reboot_type == 'SOFT' and
                 instance['task_state'] in
-                (task_states.REBOOTING, task_states.REBOOTING_HARD)) or
+                (task_states.REBOOTING, task_states.REBOOTING_HARD,
+                 task_states.REBOOT_PENDING, task_states.REBOOT_STARTED)) or
             (reboot_type == 'HARD' and
                 instance['task_state'] == task_states.REBOOTING_HARD)):
             raise exception.InstanceInvalidState(
@@ -2086,7 +2089,9 @@ class API(base.Base):
         state = {'SOFT': task_states.REBOOTING,
                  'HARD': task_states.REBOOTING_HARD}[reboot_type]
         instance.task_state = state
-        instance.save(expected_task_state=[None, task_states.REBOOTING])
+        instance.save(expected_task_state=[None, task_states.REBOOTING,
+                                           task_states.REBOOT_PENDING,
+                                           task_states.REBOOT_STARTED])
 
         self._record_action_start(context, instance, instance_actions.REBOOT)
 

nova/tests/compute/test_compute_api.py
@@ -419,7 +419,9 @@ class _ComputeAPIUnitTestMixIn(object):
         self.mox.StubOutWithMock(self.compute_api, '_record_action_start')
         self.mox.StubOutWithMock(self.compute_api, 'update')
         self.mox.StubOutWithMock(inst, 'save')
-        inst.save(expected_task_state=[None, task_states.REBOOTING])
+        inst.save(expected_task_state=[None, task_states.REBOOTING,
+                                       task_states.REBOOT_PENDING,
+                                       task_states.REBOOT_STARTED])
         self.compute_api._record_action_start(self.context, inst,
                                               instance_actions.REBOOT)
 
@@ -454,6 +456,14 @@ class _ComputeAPIUnitTestMixIn(object):
         self._test_reboot_type(vm_states.ACTIVE, 'HARD',
                                task_state=task_states.REBOOTING)
 
+    def test_reboot_hard_reboot_started(self):
+        self._test_reboot_type(vm_states.ACTIVE, 'HARD',
+                               task_state=task_states.REBOOT_STARTED)
+
+    def test_reboot_hard_reboot_pending(self):
+        self._test_reboot_type(vm_states.ACTIVE, 'HARD',
+                               task_state=task_states.REBOOT_PENDING)
+
     def test_reboot_hard_rescued(self):
         self._test_reboot_type_fails('HARD', vm_state=vm_states.RESCUED)
 
@@ -482,6 +492,14 @@ class _ComputeAPIUnitTestMixIn(object):
     def test_reboot_soft_rebooting_hard(self):
         self._test_reboot_type_fails('SOFT',
                                      task_state=task_states.REBOOTING_HARD)
+
+    def test_reboot_soft_reboot_started(self):
+        self._test_reboot_type_fails('SOFT',
+                                     task_state=task_states.REBOOT_STARTED)
+
+    def test_reboot_soft_reboot_pending(self):
+        self._test_reboot_type_fails('SOFT',
+                                     task_state=task_states.REBOOT_PENDING)
 
     def test_reboot_soft_rescued(self):
         self._test_reboot_type_fails('SOFT', vm_state=vm_states.RESCUED)