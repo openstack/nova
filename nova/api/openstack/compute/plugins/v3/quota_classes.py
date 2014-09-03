c82f5886abd4073460c35bb023d85e4313c47d87
Chris Yeoh <cyeoh@au1.ibm.com>
2014-09-03 17:29:42 +0930

    Port v2 quota_classes extension to work in v2.1(v3) framework
    Port v2 quota_classes extension and adapts it to the v2.1/v3 API
    framework. API behaviour is identical with the exception that
    there is no support for XML. Also
    
    - unittest code modified to share existing testing with both
      v2/v2.1
    - Adds expected error decorators for API methods
    - Adds API samples
    
    Partially implements blueprint v2-on-v3-api
    
    Change-Id: I372e9940f499d3e2cf621a58eafa9502d4e14cea

---
+++ nova/api/openstack/compute/plugins/v3/quota_classes.py
@@ -0,0 +1,131 @@
+# Copyright 2012 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import webob
+
+from nova.api.openstack import extensions
+from nova.api.openstack import wsgi
+import nova.context
+from nova import db
+from nova import exception
+from nova.i18n import _
+from nova import quota
+from nova import utils
+
+
+QUOTAS = quota.QUOTAS
+ALIAS = "os-quota-class-sets"
+
+# Quotas that are only enabled by specific extensions
+EXTENDED_QUOTAS = {'server_groups': 'os-server-group-quotas',
+                   'server_group_members': 'os-server-group-quotas'}
+
+
+authorize = extensions.extension_authorizer('compute', 'v3:' + ALIAS)
+
+
+class QuotaClassSetsController(wsgi.Controller):
+
+    supported_quotas = []
+
+    def __init__(self, **kwargs):
+        self.supported_quotas = QUOTAS.resources
+        extension_info = kwargs.pop('extension_info').get_extensions()
+        for resource, extension in EXTENDED_QUOTAS.items():
+            if extension not in extension_info:
+                self.supported_quotas.remove(resource)
+
+    def _format_quota_set(self, quota_class, quota_set):
+        """Convert the quota object to a result dict."""
+
+        if quota_class:
+            result = dict(id=str(quota_class))
+        else:
+            result = {}
+
+        for resource in self.supported_quotas:
+            if resource in quota_set:
+                result[resource] = quota_set[resource]
+
+        return dict(quota_class_set=result)
+
+    @extensions.expected_errors(403)
+    def show(self, req, id):
+        context = req.environ['nova.context']
+        authorize(context)
+        try:
+            nova.context.authorize_quota_class_context(context, id)
+            values = QUOTAS.get_class_quotas(context, id)
+            return self._format_quota_set(id, values)
+        except exception.Forbidden:
+            raise webob.exc.HTTPForbidden()
+
+    @extensions.expected_errors((400, 403))
+    def update(self, req, id, body):
+        context = req.environ['nova.context']
+        authorize(context)
+        quota_class = id
+        bad_keys = []
+
+        if not self.is_valid_body(body, 'quota_class_set'):
+            msg = _("quota_class_set not specified")
+            raise webob.exc.HTTPBadRequest(explanation=msg)
+        quota_class_set = body['quota_class_set']
+        for key in quota_class_set.keys():
+            if key not in self.supported_quotas:
+                bad_keys.append(key)
+                continue
+            try:
+                value = utils.validate_integer(
+                        body['quota_class_set'][key], key)
+            except exception.InvalidInput as e:
+                raise webob.exc.HTTPBadRequest(
+                    explanation=e.format_message())
+
+        if bad_keys:
+            msg = _("Bad key(s) %s in quota_set") % ",".join(bad_keys)
+            raise webob.exc.HTTPBadRequest(explanation=msg)
+
+        for key in quota_class_set.keys():
+            value = utils.validate_integer(
+                        body['quota_class_set'][key], key)
+            try:
+                db.quota_class_update(context, quota_class, key, value)
+            except exception.QuotaClassNotFound:
+                db.quota_class_create(context, quota_class, key, value)
+            except exception.AdminRequired:
+                raise webob.exc.HTTPForbidden()
+
+        values = QUOTAS.get_class_quotas(context, quota_class)
+        return self._format_quota_set(None, values)
+
+
+class QuotaClasses(extensions.V3APIExtensionBase):
+    """Quota classes management support."""
+
+    name = "QuotaClasses"
+    alias = ALIAS
+    version = 1
+
+    def get_resources(self):
+        resources = []
+        res = extensions.ResourceExtension(
+            ALIAS,
+            QuotaClassSetsController(extension_info=self.extension_info))
+        resources.append(res)
+        return resources
+
+    def get_controller_extensions(self):
+        return []
