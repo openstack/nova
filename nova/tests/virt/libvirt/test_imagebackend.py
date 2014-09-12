7a94100c715ce009ced04edb1500bf455a62362f
Daniel P. Berrange <berrange@redhat.com>
2014-09-12 12:16:32 +0100

    libvirt: convert encrypted LVM test to avoid DB usage
    The EncryptedLvmTestCase test case was depending on the DB setup
    for no purpose. This masked a bug whereby the test was relying
    on the implicit import of nova.keymgr.conf_key_mgr config opts.
    Explicitly import the config opt and remove DB setup. This
    improves performance from 1.8s to 0.6s
    
    Partial-bug: #1369516
    Change-Id: Icd5c27b3ac6cab5a014fae65904bbbefb5343282

--- nova/tests/virt/libvirt/test_imagebackend.py
+++ nova/tests/virt/libvirt/test_imagebackend.py
@@ -35,6 +35,7 @@ from nova.virt.libvirt import imagebackend
 from nova.virt.libvirt import rbd_utils
 
 CONF = cfg.CONF
+CONF.import_opt('fixed_key', 'nova.keymgr.conf_key_mgr', group='keymgr')
 
 
 class _ImageTestCase(object):
@@ -661,7 +662,7 @@ class LvmTestCase(_ImageTestCase, test.NoDBTestCase):
         self.assertEqual(fake_processutils.fake_execute_get_log(), [])
 
 
-class EncryptedLvmTestCase(_ImageTestCase, test.TestCase):
+class EncryptedLvmTestCase(_ImageTestCase, test.NoDBTestCase):
     VG = 'FakeVG'
     TEMPLATE_SIZE = 512
     SIZE = 1024
