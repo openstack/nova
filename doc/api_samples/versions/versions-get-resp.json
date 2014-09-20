6abb88befe2914040e3307b0bc5dfd13683db8b2
jichenjc <jichenjc@cn.ibm.com>
2014-09-21 03:16:32 +0800

    Check flavor type before add tenant access
    Currently we allow tenant access for public flavor, however,
    we can't query it after that because flavor is public.
    This patch adds check for add access function to raise exception
    if the flavor is public.
    
    Also, a nit change is use methods introduced in
    793bcc07b9ae41b3de2c146cf69117632c88e27b to get flavor.
    
    APIImpact: Adds new 2.7 API microversion due to new error condition
               in flavor access API
    Implements blueprint check-flavor-type-before-add-tenant
    Closes-Bug: #1361476
    
    Change-Id: I461175e9969a0dd5b2b7ef75ea7d9f36f3a306d0

--- doc/api_samples/versions/versions-get-resp.json
+++ doc/api_samples/versions/versions-get-resp.json
@@ -22,7 +22,7 @@
                 }
             ],
             "status": "CURRENT",
-            "version": "2.6",
+            "version": "2.7",
             "min_version": "2.1",
             "updated": "2013-07-23T11:33:21Z"
         }
--- nova/tests/unit/api/openstack/compute/test_versions.py
+++ nova/tests/unit/api/openstack/compute/test_versions.py
@@ -65,7 +65,7 @@ EXP_VERSIONS = {
     "v2.1": {
         "id": "v2.1",
         "status": "CURRENT",
-        "version": "2.6",
+        "version": "2.7",
         "min_version": "2.1",
         "updated": "2013-07-23T11:33:21Z",
         "links": [
@@ -114,7 +114,7 @@ class VersionsTestV20(test.NoDBTestCase):
             {
                 "id": "v2.1",
                 "status": "CURRENT",
-                "version": "2.6",
+                "version": "2.7",
                 "min_version": "2.1",
                 "updated": "2013-07-23T11:33:21Z",
                 "links": [
