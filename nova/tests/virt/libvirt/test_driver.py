openstack/nova

commit f0ff4d51057080e769407e873e5ed212f15b773d

Support configuring libvirt watchdog from flavors
The previous watchdog-related patch only enabled the watchdog
configuration to be set from the image metadata.  Now, the watchdog
can be configured from both the flavor extra_specs as well as the
image metadata, with the latter overriding the former.

Implements: blueprint libvirt-watchdog
DocImpact

Change-Id: I626eb8749c1b9a531e49f2471734d903694c5ee8

nova/virt/libvirt/driver.py
@@ -3349,7 +3349,8 @@ class LibvirtDriver(driver.ComputeDriver):
                 raise exception.PciDeviceUnsupportedHypervisor(
                     type=CONF.libvirt.virt_type)
 
-        watchdog_action = 'disabled'
+        watchdog_action = flavor.extra_specs.get('hw_watchdog_action',
+                                                 'disabled')
         if (image_meta is not None and
                 image_meta.get('properties', {}).get('hw_watchdog_action')):
             watchdog_action = image_meta['properties']['hw_watchdog_action']

nova/tests/virt/libvirt/test_libvirt.py
@@ -1291,6 +1291,94 @@ class LibvirtConnTestCase(test.TestCase):
 
         self.assertEqual("none", cfg.devices[7].action)
 
+    def test_get_guest_config_with_watchdog_action_through_flavor(self):
+        self.flags(virt_type='kvm', group='libvirt')
+
+        conn = libvirt_driver.LibvirtDriver(fake.FakeVirtAPI(), True)
+
+        flavor = db.flavor_get(self.context,
+                               self.test_instance['instance_type_id'])
+        db.flavor_extra_specs_update_or_create(
+                self.context,
+                flavor['flavorid'],
+                {'hw_watchdog_action': 'none'})
+
+        instance_ref = db.instance_create(self.context, self.test_instance)
+
+        disk_info = blockinfo.get_disk_info(CONF.libvirt.virt_type,
+                                            instance_ref)
+
+        cfg = conn.get_guest_config(instance_ref, [], {}, disk_info)
+
+        db.flavor_extra_specs_delete(self.context,
+                                     flavor['flavorid'],
+                                     'hw_watchdog_action')
+
+        self.assertEqual(8, len(cfg.devices))
+        self.assertIsInstance(cfg.devices[0],
+                              vconfig.LibvirtConfigGuestDisk)
+        self.assertIsInstance(cfg.devices[1],
+                              vconfig.LibvirtConfigGuestDisk)
+        self.assertIsInstance(cfg.devices[2],
+                              vconfig.LibvirtConfigGuestSerial)
+        self.assertIsInstance(cfg.devices[3],
+                              vconfig.LibvirtConfigGuestSerial)
+        self.assertIsInstance(cfg.devices[4],
+                              vconfig.LibvirtConfigGuestInput)
+        self.assertIsInstance(cfg.devices[5],
+                              vconfig.LibvirtConfigGuestGraphics)
+        self.assertIsInstance(cfg.devices[6],
+                              vconfig.LibvirtConfigGuestVideo)
+        self.assertIsInstance(cfg.devices[7],
+                              vconfig.LibvirtConfigGuestWatchdog)
+
+        self.assertEqual("none", cfg.devices[7].action)
+
+    def test_get_guest_config_with_watchdog_action_meta_overrides_flavor(self):
+        self.flags(virt_type='kvm', group='libvirt')
+
+        conn = libvirt_driver.LibvirtDriver(fake.FakeVirtAPI(), True)
+
+        flavor = db.flavor_get(self.context,
+                               self.test_instance['instance_type_id'])
+        db.flavor_extra_specs_update_or_create(
+                self.context,
+                flavor['flavorid'],
+                {'hw_watchdog_action': 'none'})
+
+        instance_ref = db.instance_create(self.context, self.test_instance)
+
+        disk_info = blockinfo.get_disk_info(CONF.libvirt.virt_type,
+                                            instance_ref)
+
+        image_meta = {"properties": {"hw_watchdog_action": "pause"}}
+
+        cfg = conn.get_guest_config(instance_ref, [], image_meta, disk_info)
+
+        db.flavor_extra_specs_delete(self.context,
+                                     flavor['flavorid'],
+                                     'hw_watchdog_action')
+
+        self.assertEqual(8, len(cfg.devices))
+        self.assertIsInstance(cfg.devices[0],
+                              vconfig.LibvirtConfigGuestDisk)
+        self.assertIsInstance(cfg.devices[1],
+                              vconfig.LibvirtConfigGuestDisk)
+        self.assertIsInstance(cfg.devices[2],
+                              vconfig.LibvirtConfigGuestSerial)
+        self.assertIsInstance(cfg.devices[3],
+                              vconfig.LibvirtConfigGuestSerial)
+        self.assertIsInstance(cfg.devices[4],
+                              vconfig.LibvirtConfigGuestInput)
+        self.assertIsInstance(cfg.devices[5],
+                              vconfig.LibvirtConfigGuestGraphics)
+        self.assertIsInstance(cfg.devices[6],
+                              vconfig.LibvirtConfigGuestVideo)
+        self.assertIsInstance(cfg.devices[7],
+                              vconfig.LibvirtConfigGuestWatchdog)
+
+        self.assertEqual("pause", cfg.devices[7].action)
+
     def test_unsupported_video_driver_through_image_meta(self):
         self.flags(virt_type='kvm', group='libvirt')
 

commit 79bfb1bf343484e98aa36dcc663a5370baf4cab7

Fix libvirt watchdog support
Using the flavor extra_specs property "hw_watchdog_action" was broken.
Scheduling of a new instance always failed with NoValidHostFound error
because of ComputeCapabilitiesFilter, which treated this property as a
host capability to be checked.

Commit f0ff4d51057080e769407e873e5ed212f15b773d caused the problem.

To fix this watchdog_action property is put into 'hw:' scope, so
that it will be ignored by ComputeCapabilitiesFilter in scheduler and
handled in libvirt driver. The doc must be fixed accordingly.

Now driver accepts both 'hw_watchdog_action' and
'hw:watchdog_action', tests were edited for these cases.
Were added TODO items to delete the compat code in the next release.

DocImpact
Closes-Bug: #1367344

Change-Id: Ic5344ec34a130ee5a0ed2c7348af0b9d79e3508e

nova/virt/libvirt/driver.py
@@ -4075,8 +4075,16 @@ class LibvirtDriver(driver.ComputeDriver):
                 raise exception.PciDeviceUnsupportedHypervisor(
                     type=CONF.libvirt.virt_type)
 
-        watchdog_action = flavor.extra_specs.get('hw_watchdog_action',
-                                                 'disabled')
+        if 'hw_watchdog_action' in flavor.extra_specs:
+            LOG.warn(_LW('Old property name "hw_watchdog_action" is now '
+                         'deprecated and will be removed in the next release. '
+                         'Use updated property name '
+                         '"hw:watchdog_action" instead'))
+        # TODO(pkholkin): accepting old property name 'hw_watchdog_action'
+        #                should be removed in the next release
+        watchdog_action = (flavor.extra_specs.get('hw_watchdog_action') or
+                           flavor.extra_specs.get('hw:watchdog_action')
+                           or 'disabled')
         if (image_meta is not None and
                 image_meta.get('properties', {}).get('hw_watchdog_action')):
             watchdog_action = image_meta['properties']['hw_watchdog_action']

nova/tests/virt/libvirt/test_driver.py
@@ -2318,15 +2318,15 @@ class LibvirtConnTestCase(test.NoDBTestCase):
         self.assertEqual("none", cfg.devices[7].action)
 
     @mock.patch.object(objects.Flavor, 'get_by_id')
-    def test_get_guest_config_with_watchdog_action_flavor(self,
-                                                          mock_flavor):
+    def _test_get_guest_config_with_watchdog_action_flavor(self, mock_flavor,
+            hw_watchdog_action="hw:watchdog_action"):
         self.flags(virt_type='kvm', group='libvirt')
 
         conn = libvirt_driver.LibvirtDriver(fake.FakeVirtAPI(), True)
 
         instance_ref = objects.Instance(**self.test_instance)
         flavor = instance_ref.get_flavor()
-        flavor.extra_specs = {'hw_watchdog_action': 'none'}
+        flavor.extra_specs = {hw_watchdog_action: 'none'}
         mock_flavor.return_value = flavor
 
         disk_info = blockinfo.get_disk_info(CONF.libvirt.virt_type,
@@ -2355,6 +2355,16 @@ class LibvirtConnTestCase(test.NoDBTestCase):
                               vconfig.LibvirtConfigMemoryBalloon)
 
         self.assertEqual("none", cfg.devices[7].action)
+
+    def test_get_guest_config_with_watchdog_action_through_flavor(self):
+        self._test_get_guest_config_with_watchdog_action_flavor()
+
+    # TODO(pkholkin): the test accepting old property name 'hw_watchdog_action'
+    #                should be removed in the next release
+    def test_get_guest_config_with_watchdog_action_through_flavor_no_scope(
+            self):
+        self._test_get_guest_config_with_watchdog_action_flavor(
+            hw_watchdog_action="hw_watchdog_action")
 
     @mock.patch.object(objects.Flavor, 'get_by_id')
     def test_get_guest_config_with_watchdog_overrides_flavor(self,