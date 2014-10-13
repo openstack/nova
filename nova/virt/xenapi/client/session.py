41c453314e87e5ecc9ca8d142be41cdfb9abed5c
Bob Ball <bob.ball@citrix.com>
2014-10-13 12:43:35 +0100

    XenAPI: Inform XAPI who is connecting to it
    XenServer supports an 'originator' being passed to the login function
    This originator is very useful in identifying how XAPI is being used
    and, when there is a problem, which software package issued which
    commands
    
    Change-Id: I003979bb3796e870890e220cf4b046e51581a373

--- nova/virt/xenapi/client/session.py
+++ nova/virt/xenapi/client/session.py
@@ -30,6 +30,7 @@ from nova import objects
 from nova.openstack.common import log as logging
 from nova.openstack.common import versionutils
 from nova import utils
+from nova import version
 from nova.virt.xenapi.client import objects as cli_objects
 from nova.virt.xenapi import pool
 from nova.virt.xenapi import pool_states
@@ -74,6 +75,11 @@ class XenAPISession(object):
     PLUGIN_REQUIRED_VERSION = '1.2'
 
     def __init__(self, url, user, pw):
+        version_string = version.version_string_with_package()
+        self.nova_version = _('%(vendor)s %(product)s %(version)s') % \
+                              {'vendor': version.vendor_string(),
+                               'product': version.product_string(),
+                               'version': version_string}
         import XenAPI
         self.XenAPI = XenAPI
         self._sessions = queue.Queue()
@@ -105,14 +111,16 @@ class XenAPISession(object):
         try:
             session = self._create_session(url)
             with timeout.Timeout(CONF.xenserver.login_timeout, exception):
-                session.login_with_password(user, pw)
+                session.login_with_password(user, pw,
+                                            self.nova_version, 'OpenStack')
         except self.XenAPI.Failure as e:
             # if user and pw of the master are different, we're doomed!
             if e.details[0] == 'HOST_IS_SLAVE':
                 master = e.details[1]
                 url = pool.swap_xapi_host(url, master)
                 session = self.XenAPI.Session(url)
-                session.login_with_password(user, pw)
+                session.login_with_password(user, pw,
+                                            self.nova_version, 'OpenStack')
                 self.is_slave = True
             else:
                 raise
@@ -123,7 +131,8 @@ class XenAPISession(object):
         for i in xrange(CONF.xenserver.connection_concurrent - 1):
             session = self._create_session(url)
             with timeout.Timeout(CONF.xenserver.login_timeout, exception):
-                session.login_with_password(user, pw)
+                session.login_with_password(user, pw,
+                                            self.nova_version, 'OpenStack')
             self._sessions.put(session)
 
     def _get_host_uuid(self):
