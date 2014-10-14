9bf5c425eda5ecc5d46f0c0867347e92d64017dd
Eugeniya Kudryashova <ekudryashova@mirantis.com>
2014-10-14 17:29:30 +0300

    Use sizelimit from oslo.middleware
    Module sizelimit in nova and oslo.middleware is the same, so
    switch nova to use RequestBodySizalimiter from oslo.middleware
    lib
    
    Replace RequestBodySizalimiter and LimitingReader by temporal
    aliases of this classes in oslo.middleware due to failures in grenade
    jobs. Remove tests for this module
    
    Change-Id: I6296215857c2b16035c9081e13ac4345ed5c420a

--- nova/api/sizelimit.py
+++ nova/api/sizelimit.py
@@ -15,68 +15,10 @@
 Request Body limiting middleware.
 
 """
-
-from oslo.config import cfg
-import webob.dec
-import webob.exc
-
-from nova.i18n import _
-from nova import wsgi
+from oslo.middleware import sizelimit
 
 
-# default request size is 112k
-max_request_body_size_opt = cfg.IntOpt('osapi_max_request_body_size',
-                                       default=114688,
-                                       help='The maximum body size '
-                                            'per each osapi request(bytes)')
-
-CONF = cfg.CONF
-CONF.register_opt(max_request_body_size_opt)
-
-
-class LimitingReader(object):
-    """Reader to limit the size of an incoming request."""
-    def __init__(self, data, limit):
-        """Initialize a new `LimitingReader`.
-
-           :param data: underlying data object
-           :param limit: maximum number of bytes the reader should allow
-        """
-        self.data = data
-        self.limit = limit
-        self.bytes_read = 0
-
-    def __iter__(self):
-        for chunk in self.data:
-            self.bytes_read += len(chunk)
-            if self.bytes_read > self.limit:
-                msg = _("Request is too large.")
-                raise webob.exc.HTTPRequestEntityTooLarge(explanation=msg)
-            else:
-                yield chunk
-
-    def read(self, i=None):
-        result = self.data.read(i)
-        self.bytes_read += len(result)
-        if self.bytes_read > self.limit:
-            msg = _("Request is too large.")
-            raise webob.exc.HTTPRequestEntityTooLarge(explanation=msg)
-        return result
-
-
-class RequestBodySizeLimiter(wsgi.Middleware):
-    """Limit the size of incoming requests."""
-
-    def __init__(self, *args, **kwargs):
-        super(RequestBodySizeLimiter, self).__init__(*args, **kwargs)
-
-    @webob.dec.wsgify(RequestClass=wsgi.Request)
-    def __call__(self, req):
-        if req.content_length > CONF.osapi_max_request_body_size:
-            msg = _("Request is too large.")
-            raise webob.exc.HTTPRequestEntityTooLarge(explanation=msg)
-        if req.content_length is None and req.is_body_readable:
-            limiter = LimitingReader(req.body_file,
-                                     CONF.osapi_max_request_body_size)
-            req.body_file = limiter
-        return self.application
+# TODO(ekudryashova): Remove below aliases when grenade jobs
+# will start upgrading from juno
+LimitingReader = sizelimit.LimitingReader
+RequestBodySizeLimiter = sizelimit.RequestBodySizeLimiter
