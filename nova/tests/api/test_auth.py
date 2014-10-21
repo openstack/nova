7f7cfc2dc6baa53adbea431aba840cd88eea8040
Joe Gordon <joe.gordon0@gmail.com>
2014-10-21 14:51:56 +0000

    Revert "Replace outdated oslo-incubator middleware"
    This is breaking the Juno -> master grenade path. As we are using the old
    api-paste.ini file which requires the oslo-incubator copy of request_id
    
    This breakage didn't happen from Icehouse -> master since the icehouse
    api-paste.ini didn't have request_id as a middleware. request_id is only
    used as middleware in the V21/V3 API and not the V2 API.
    
    Closes-Bug: #1383757
    This reverts commit 641de5666f38532252e91735a1cd2611d96c489a.
    
    Change-Id: I1ca6bddf9d9370529e999efcc7eae60622e19e6f

--- nova/tests/api/test_auth.py
+++ nova/tests/api/test_auth.py
@@ -13,13 +13,13 @@
 #    under the License.
 
 from oslo.config import cfg
-from oslo.middleware import request_id
 from oslo.serialization import jsonutils
 import webob
 import webob.exc
 
 import nova.api.auth
 from nova.i18n import _
+from nova.openstack.common.middleware import request_id
 from nova import test
 
 CONF = cfg.CONF
