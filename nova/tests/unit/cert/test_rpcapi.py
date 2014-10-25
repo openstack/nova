b2a2e1b8fc0005820e0cd99ec2e484d8c865b33d
Russell Bryant <rbryant@redhat.com>
2014-10-25 15:40:16 -0400

    Remove havana compat from nova.cert.rpcapi
    Since we only support upgrading from N-1 to N, there's no need to keep
    around this older upgrade related code.
    
    Change-Id: Ie13f98da84f3d631efea2670fbc926b69b405284

--- nova/tests/unit/cert/test_rpcapi.py
+++ nova/tests/unit/cert/test_rpcapi.py
@@ -37,7 +37,6 @@ class CertRpcAPITestCase(test.NoDBTestCase):
         self.assertEqual(rpcapi.client.target.topic, CONF.cert_topic)
 
         orig_prepare = rpcapi.client.prepare
-        expected_version = kwargs.pop('version', rpcapi.client.target.version)
 
         with contextlib.nested(
             mock.patch.object(rpcapi.client, 'call'),
@@ -49,75 +48,37 @@ class CertRpcAPITestCase(test.NoDBTestCase):
             prepare_mock.return_value = rpcapi.client
             rpc_mock.return_value = 'foo'
             csv_mock.side_effect = (
-                lambda v: orig_prepare(version=v).can_send_version())
+                lambda v: orig_prepare().can_send_version())
 
             retval = getattr(rpcapi, method)(ctxt, **kwargs)
             self.assertEqual(retval, rpc_mock.return_value)
 
-            prepare_mock.assert_called_once_with(version=expected_version)
+            prepare_mock.assert_called_once_with()
             rpc_mock.assert_called_once_with(ctxt, method, **kwargs)
 
     def test_revoke_certs_by_user(self):
         self._test_cert_api('revoke_certs_by_user', user_id='fake_user_id')
 
-        # NOTE(russellb) Havana compat
-        self.flags(cert='havana', group='upgrade_levels')
-        self._test_cert_api('revoke_certs_by_user', user_id='fake_user_id',
-                version='1.0')
-
     def test_revoke_certs_by_project(self):
         self._test_cert_api('revoke_certs_by_project',
                             project_id='fake_project_id')
-
-        # NOTE(russellb) Havana compat
-        self.flags(cert='havana', group='upgrade_levels')
-        self._test_cert_api('revoke_certs_by_project',
-                            project_id='fake_project_id', version='1.0')
 
     def test_revoke_certs_by_user_and_project(self):
         self._test_cert_api('revoke_certs_by_user_and_project',
                             user_id='fake_user_id',
                             project_id='fake_project_id')
 
-        # NOTE(russellb) Havana compat
-        self.flags(cert='havana', group='upgrade_levels')
-        self._test_cert_api('revoke_certs_by_user_and_project',
-                            user_id='fake_user_id',
-                            project_id='fake_project_id', version='1.0')
-
     def test_generate_x509_cert(self):
         self._test_cert_api('generate_x509_cert',
                             user_id='fake_user_id',
                             project_id='fake_project_id')
 
-        # NOTE(russellb) Havana compat
-        self.flags(cert='havana', group='upgrade_levels')
-        self._test_cert_api('generate_x509_cert',
-                            user_id='fake_user_id',
-                            project_id='fake_project_id', version='1.0')
-
     def test_fetch_ca(self):
         self._test_cert_api('fetch_ca', project_id='fake_project_id')
-
-        # NOTE(russellb) Havana compat
-        self.flags(cert='havana', group='upgrade_levels')
-        self._test_cert_api('fetch_ca', project_id='fake_project_id',
-                version='1.0')
 
     def test_fetch_crl(self):
         self._test_cert_api('fetch_crl', project_id='fake_project_id')
 
-        # NOTE(russellb) Havana compat
-        self.flags(cert='havana', group='upgrade_levels')
-        self._test_cert_api('fetch_crl', project_id='fake_project_id',
-                version='1.0')
-
     def test_decrypt_text(self):
         self._test_cert_api('decrypt_text',
                             project_id='fake_project_id', text='blah')
-
-        # NOTE(russellb) Havana compat
-        self.flags(cert='havana', group='upgrade_levels')
-        self._test_cert_api('decrypt_text',
-                            project_id='fake_project_id', text='blah',
-                            version='1.0')
