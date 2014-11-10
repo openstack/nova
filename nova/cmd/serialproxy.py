d2cbb9adeaddefc00db430644e0d51b11ffb7600
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@redhat.com>
2014-11-10 04:15:46 -0500

    console: introduce baseproxy and update consoles cmd
    Introduces a new module baseproxy that provides the base
    code shared between novncproxy, serialproxy, spicehtml5proxy
    Updates them to use this new module.
    
    Change-Id: I2d46b926f1c895aba412d84b4ee059fda3df9011

--- nova/cmd/serialproxy.py
+++ nova/cmd/serialproxy.py
@@ -18,16 +18,9 @@ Serial consoles. Leverages websockify.py by Joel Martin.
 Based on nova-novncproxy.
 """
 
-import os
-import sys
-
 from oslo.config import cfg
 
-from nova import config
-from nova.console import websocketproxy
-from nova.openstack.common import log as logging
-from nova.openstack.common.report import guru_meditation_report as gmr
-from nova import version
+from nova.cmd import baseproxy
 
 
 opts = [
@@ -41,41 +34,12 @@ opts = [
 
 CONF = cfg.CONF
 CONF.register_cli_opts(opts, group="serial_console")
-CONF.import_opt('debug', 'nova.openstack.common.log')
-CONF.import_opt('record', 'nova.cmd.novnc')
-CONF.import_opt('daemon', 'nova.cmd.novnc')
-CONF.import_opt('ssl_only', 'nova.cmd.novnc')
-CONF.import_opt('source_is_ipv6', 'nova.cmd.novnc')
-CONF.import_opt('cert', 'nova.cmd.novnc')
-CONF.import_opt('key', 'nova.cmd.novnc')
-
-
-def exit_with_error(msg, errno=-1):
-    print(msg) and sys.exit(errno)
 
 
 def main():
-    # Setup flags
-    config.parse_args(sys.argv)
+    # set default web flag option
+    CONF.set_default('web', None)
 
-    if CONF.ssl_only and not os.path.exists(CONF.cert):
-        exit_with_error("SSL only and %s not found" % CONF.cert)
-
-    logging.setup("nova")
-    gmr.TextGuruMeditation.setup_autorun(version)
-
-    # Create and start the NovaWebSockets proxy
-    server = websocketproxy.NovaWebSocketProxy(
-        listen_host=CONF.serial_console.serialproxy_host,
-        listen_port=CONF.serial_console.serialproxy_port,
-        source_is_ipv6=CONF.source_is_ipv6,
-        verbose=CONF.verbose,
-        cert=CONF.cert,
-        key=CONF.key,
-        ssl_only=CONF.ssl_only,
-        daemon=CONF.daemon,
-        record=CONF.record,
-        traffic=CONF.verbose and not CONF.daemon,
-        file_only=True,
-        RequestHandlerClass=websocketproxy.NovaProxyRequestHandler)
-    server.start_server()
+    baseproxy.proxy(
+        host=CONF.serial_console.serialproxy_host,
+        port=CONF.serial_console.serialproxy_port)
