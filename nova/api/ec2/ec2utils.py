30dde3a2e1673c111bc9a3b251f93676447e3530
Hans Lindgren <hanlind@kth.se>
2014-11-10 10:29:42 +0100

    Make ec2/cloud.py use get_instance_availability_zone() helper
    Commit 488fcb4ad345bfb4fd614a140b317b606e008872 added a helper
    for getting the AZ for a instance. This patch makes use of that
    helper method.
    
    This also removes the now unused get_availability_zone_by_host()
    method from ec2utils and adds a reset_cache() call to the class
    setup of CloudTestCase in test_cloud.py to reset the cache now
    used between tests.
    
    Change-Id: I2dd6a54fcc7cefe17816d0fb2875d8da0ec57eba

--- nova/api/ec2/ec2utils.py
+++ nova/api/ec2/ec2utils.py
@@ -19,7 +19,6 @@ import re
 
 from oslo.utils import timeutils
 
-from nova import availability_zones
 from nova import context
 from nova import exception
 from nova.i18n import _
@@ -172,11 +171,6 @@ def get_ip_info_for_instance(context, instance):
     if not nw_info:
         nw_info = []
     return get_ip_info_for_instance_from_nw_info(nw_info)
-
-
-def get_availability_zone_by_host(host, conductor_api=None):
-    return availability_zones.get_host_availability_zone(
-        context.get_admin_context(), host, conductor_api)
 
 
 def id_to_ec2_id(instance_id, template='i-%08x'):
