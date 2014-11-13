openstack/nova

commit 73d4a7c1b4681accd993c1109a9eff837ff612f4

vmwareapi oslo.vmware library integration
These are the minimal changes necessary to integrate the vmwareapi
driver with the oslo.vmware code. Much of the change is mechanical
switching to call the library code, and removing obseleted code in the
driver where appropriate.

Effort was made to minimize the amount of actual driver code changes in
this round by keeping a small amount of shim code. Existing unit tests
were modified as appropriate. Additional patches will be introduced
shortly to include additional conversions and tests.

Summary of changes:

- use common/vmware/exceptions.py
- use oslo.vmware vim.py for the Vim implemenation
- use oslo.vmware api.py for api invocation/session
- updated existing unit tests
- nova retains its own version of get_{soap/wsdl}_url which using conf
  settings for default and overrides.

blueprint use-oslo-vmware
Co-authored-by: Radoslav Gerganov <rgerganov@vmware.com>
Co-authored-by: Davanum Srinivas <dims@linux.vnet.ibm.com>
Change-Id: Idab7778d4c7e5e7099d842debbcbc009cf9127b6

nova/virt/vmwareapi/error_util.py
@@ -14,132 +14,19 @@
 #    under the License.
 
 """
-Exception classes and SOAP response error checking module.
+Exception classes specific for the VMware driver.
 """
-import six
+from oslo.vmware import exceptions as vexc
 
-from nova import exception
 from nova.i18n import _
-from nova.openstack.common import log as logging
 
-LOG = logging.getLogger(__name__)
-
-ALREADY_EXISTS = 'AlreadyExists'
-CANNOT_DELETE_FILE = 'CannotDeleteFile'
-FILE_ALREADY_EXISTS = 'FileAlreadyExists'
-FILE_FAULT = 'FileFault'
-FILE_LOCKED = 'FileLocked'
-FILE_NOT_FOUND = 'FileNotFound'
-INVALID_POWER_STATE = 'InvalidPowerState'
-INVALID_PROPERTY = 'InvalidProperty'
-NO_PERMISSION = 'NoPermission'
-NOT_AUTHENTICATED = 'NotAuthenticated'
-TASK_IN_PROGRESS = 'TaskInProgress'
+# Most VMware-specific exception classes are now centrally defined in
+# oslo.vmware.
+# Note(vui):
+# - map back to NovaException?
 
 
-class VimException(Exception):
-    """The VIM Exception class."""
-
-    def __init__(self, exception_summary, excep):
-        Exception.__init__(self)
-        if isinstance(exception_summary, list):
-            # we need this to protect against developers using
-            # this method like VimFaultException
-            raise ValueError(_("exception_summary must not be a list"))
-
-        self.exception_summary = six.text_type(exception_summary)
-        self.exception_obj = excep
-
-    def __str__(self):
-        return (self.exception_summary + ": " +
-                six.text_type(self.exception_obj))
-
-
-class SessionOverLoadException(VimException):
-    """Session Overload Exception."""
-    pass
-
-
-class SessionConnectionException(VimException):
-    """Session Connection Exception."""
-    pass
-
-
-class VimAttributeError(VimException):
-    """VI Attribute Error."""
-    pass
-
-
-class VimFaultException(Exception):
-    """The VIM Fault exception class."""
-
-    def __init__(self, fault_list, fault_string, details=None):
-        Exception.__init__(self)
-        if not isinstance(fault_list, list):
-            raise ValueError(_("fault_list must be a list"))
-        self.fault_list = fault_list
-        self.fault_string = fault_string
-        self.details = details
-
-    def __str__(self):
-        if self.details:
-            return '%s %s' % (self.fault_string, self.details)
-        return self.fault_string
-
-
-class FaultCheckers(object):
-    """Methods for fault checking of SOAP response. Per Method error handlers
-    for which we desire error checking are defined. SOAP faults are
-    embedded in the SOAP messages as properties and not as SOAP faults.
-    """
-
-    @staticmethod
-    def retrievepropertiesex_fault_checker(resp_obj):
-        """Checks the RetrievePropertiesEx response for errors. Certain faults
-        are sent as part of the SOAP body as property of missingSet.
-        For example NotAuthenticated fault.
-        """
-        fault_list = []
-        details = {}
-        if not resp_obj:
-            # This is the case when the session has timed out. ESX SOAP server
-            # sends an empty RetrievePropertiesResponse. Normally missingSet in
-            # the returnval field has the specifics about the error, but that's
-            # not the case with a timed out idle session. It is as bad as a
-            # terminated session for we cannot use the session. So setting
-            # fault to NotAuthenticated fault.
-            fault_list = [NOT_AUTHENTICATED]
-        else:
-            for obj_cont in resp_obj.objects:
-                if hasattr(obj_cont, "missingSet"):
-                    for missing_elem in obj_cont.missingSet:
-                        fault_type = missing_elem.fault.fault
-                        # Fault needs to be added to the type of fault for
-                        # uniformity in error checking as SOAP faults define
-                        fault_list.append(fault_type.__class__.__name__)
-                        if fault_type.__class__.__name__ == NO_PERMISSION:
-                            details['object'] = fault_type.object.value
-                            details['privilegeId'] = fault_type.privilegeId
-        if fault_list:
-            exc_msg_list = ', '.join(fault_list)
-            fault_string = _("Error(s) %s occurred in the call to "
-                             "RetrievePropertiesEx") % exc_msg_list
-            raise VimFaultException(fault_list, fault_string, details)
-
-
-class VMwareDriverException(exception.NovaException):
-    """Base class for all exceptions raised by the VMware Driver.
-
-    All exceptions raised by the VMwareAPI drivers should raise
-    an exception descended from this class as a root. This will
-    allow the driver to potentially trap problems related to its
-    own internal configuration before halting the nova-compute
-    node.
-    """
-    msg_fmt = _("VMware Driver fault.")
-
-
-class VMwareDriverConfigurationException(VMwareDriverException):
+class VMwareDriverConfigurationException(vexc.VMwareDriverException):
     """Base class for all configuration exceptions.
     """
     msg_fmt = _("VMware Driver configuration fault.")
@@ -149,90 +36,9 @@ class UseLinkedCloneConfigurationFault(VMwareDriverConfigurationException):
     msg_fmt = _("No default value for use_linked_clone found.")
 
 
-class MissingParameter(VMwareDriverException):
-    msg_fmt = _("Missing parameter : %(param)s")
-
-
-class NoRootDiskDefined(VMwareDriverException):
+class NoRootDiskDefined(vexc.VMwareDriverException):
     msg_fmt = _("No root disk defined.")
 
 
-class AlreadyExistsException(VMwareDriverException):
-    msg_fmt = _("Resource already exists.")
-    code = 409
-
-
-class CannotDeleteFileException(VMwareDriverException):
-    msg_fmt = _("Cannot delete file.")
-    code = 403
-
-
-class FileAlreadyExistsException(VMwareDriverException):
-    msg_fmt = _("File already exists.")
-    code = 409
-
-
-class FileFaultException(VMwareDriverException):
-    msg_fmt = _("File fault.")
-    code = 409
-
-
-class FileLockedException(VMwareDriverException):
-    msg_fmt = _("File locked.")
-    code = 403
-
-
-class FileNotFoundException(VMwareDriverException):
-    msg_fmt = _("File not found.")
-    code = 404
-
-
-class InvalidPropertyException(VMwareDriverException):
-    msg_fmt = _("Invalid property.")
-    code = 400
-
-
-class NoPermissionException(VMwareDriverException):
-    msg_fmt = _("No Permission.")
-    code = 403
-
-
-class NotAuthenticatedException(VMwareDriverException):
-    msg_fmt = _("Not Authenticated.")
-    code = 403
-
-
-class InvalidPowerStateException(VMwareDriverException):
-    msg_fmt = _("Invalid Power State.")
-    code = 409
-
-
-class TaskInProgress(VMwareDriverException):
+class TaskInProgress(vexc.VMwareDriverException):
     msg_fmt = _("Virtual machine is busy.")
-
-
-# Populate the fault registry with the exceptions that have
-# special treatment.
-_fault_classes_registry = {
-    ALREADY_EXISTS: AlreadyExistsException,
-    CANNOT_DELETE_FILE: CannotDeleteFileException,
-    FILE_ALREADY_EXISTS: FileAlreadyExistsException,
-    FILE_FAULT: FileFaultException,
-    FILE_LOCKED: FileLockedException,
-    FILE_NOT_FOUND: FileNotFoundException,
-    INVALID_POWER_STATE: InvalidPowerStateException,
-    INVALID_PROPERTY: InvalidPropertyException,
-    NO_PERMISSION: NoPermissionException,
-    NOT_AUTHENTICATED: NotAuthenticatedException,
-    TASK_IN_PROGRESS: TaskInProgress,
-}
-
-
-def get_fault_class(name):
-    """Get a named subclass of VMwareDriverException."""
-    name = str(name)
-    fault_class = _fault_classes_registry.get(name)
-    if not fault_class:
-        LOG.warning(_('Fault %s not matched.'), name)
-        fault_class = VMwareDriverException
-    return fault_class

nova/virt/vmwareapi/vmops.py
@@ -22,8 +22,11 @@ Class for VM tasks like spawn, snapshot, suspend, resume etc.
 import collections
 import copy
 import os
+import time
 
+import decorator
 from oslo.config import cfg
+from oslo.vmware import exceptions as vexc
 
 from nova.api.metadata import base as instance_metadata
 from nova import compute
@@ -48,7 +51,6 @@ from nova.virt.vmwareapi import ds_util
 from nova.virt.vmwareapi import error_util
 from nova.virt.vmwareapi import imagecache
 from nova.virt.vmwareapi import vif as vmwarevif
-from nova.virt.vmwareapi import vim
 from nova.virt.vmwareapi import vim_util
 from nova.virt.vmwareapi import vm_util
 from nova.virt.vmwareapi import vmware_images
@@ -110,6 +112,29 @@ class VirtualMachineInstanceConfigInfo(object):
         return self.cache_image_folder.join(cached_image_file_name)
 
 
+# Note(vui): See https://bugs.launchpad.net/nova/+bug/1363349
+# for cases where mocking time.sleep() can have unintended effects on code
+# not under test. For now, unblock the affected test cases by providing
+# a wrapper function to work around needing to mock time.sleep()
+def _time_sleep_wrapper(delay):
+    time.sleep(delay)
+
+
+@decorator.decorator
+def retry_if_task_in_progress(f, *args, **kwargs):
+    retries = max(CONF.vmware.api_retry_count, 1)
+    delay = 1
+    for attempt in range(1, retries + 1):
+        if attempt != 1:
+            _time_sleep_wrapper(delay)
+            delay = min(2 * delay, 60)
+        try:
+            f(*args, **kwargs)
+            return
+        except error_util.TaskInProgress:
+            pass
+
+
 class VMwareVMOps(object):
     """Management class for VM-related tasks."""
 
@@ -150,7 +175,7 @@ class VMwareVMOps(object):
         return lst_vm_names
 
     def _extend_virtual_disk(self, instance, requested_size, name, dc_ref):
-        service_content = self._session._get_vim().get_service_content()
+        service_content = self._session._get_vim().service_content
         LOG.debug("Extending root virtual disk to %s", requested_size)
         vmdk_extend_task = self._session._call_method(
                 self._session._get_vim(),
@@ -177,10 +202,10 @@ class VMwareVMOps(object):
     def _delete_datastore_file(self, instance, datastore_path, dc_ref):
         try:
             ds_util.file_delete(self._session, datastore_path, dc_ref)
-        except (error_util.CannotDeleteFileException,
-                error_util.FileFaultException,
-                error_util.FileLockedException,
-                error_util.FileNotFoundException):
+        except (vexc.CannotDeleteFileException,
+                vexc.FileFaultException,
+                vexc.FileLockedException,
+                vexc.FileNotFoundException):
             LOG.debug("Unable to delete %(ds)s. There may be more than "
                       "one process or thread trying to delete the file",
                       {'ds': datastore_path},
@@ -427,7 +452,7 @@ class VMwareVMOps(object):
 
                 vmware_images.fetch_image(context,
                                           instance,
-                                          self._session._host_ip,
+                                          self._session._host,
                                           dc_info.name,
                                           datastore.name,
                                           upload_rel_path,
@@ -453,7 +478,7 @@ class VMwareVMOps(object):
                 try:
                     ds_util.file_move(self._session, dc_info.ref,
                                       src_folder, dest_folder)
-                except error_util.FileAlreadyExistsException:
+                except vexc.FileAlreadyExistsException:
                     # File move has failed. This may be due to the fact that a
                     # process or thread has already completed the opertaion.
                     # In the event of a FileAlreadyExists we continue,
@@ -563,7 +588,7 @@ class VMwareVMOps(object):
                                         ds_util.file_delete(self._session,
                                                             root_vmdk_path,
                                                             dc_info.ref)
-                                    except error_util.FileNotFoundException:
+                                    except vexc.FileNotFoundException:
                                         # File was never created: cleanup not
                                         # required
                                         pass
@@ -623,7 +648,7 @@ class VMwareVMOps(object):
                         upload_folder)
                     vmware_images.upload_iso_to_datastore(
                         tmp_file, instance,
-                        host=self._session._host_ip,
+                        host=self._session._host,
                         data_center_name=dc_name,
                         datastore_name=data_store_name,
                         cookies=cookies,
@@ -681,7 +706,7 @@ class VMwareVMOps(object):
         snapshot = task_info.result
         return snapshot
 
-    @vim.retry_if_task_in_progress
+    @retry_if_task_in_progress
     def _delete_vm_snapshot(self, instance, vm_ref, snapshot):
         LOG.debug("Deleting Snapshot of the VM instance", instance=instance)
         delete_snapshot_task = self._session._call_method(
@@ -708,7 +733,7 @@ class VMwareVMOps(object):
         """
         vm_ref = vm_util.get_vm_ref(self._session, instance)
         client_factory = self._session._get_vim().client.factory
-        service_content = self._session._get_vim().get_service_content()
+        service_content = self._session._get_vim().service_content
 
         def _get_vm_and_vmdk_attribs():
             # Get the vmdk file name that the VM is pointing to
@@ -800,7 +825,7 @@ class VMwareVMOps(object):
                 disk_type=constants.DEFAULT_DISK_TYPE,
                 adapter_type=adapter_type,
                 image_version=1,
-                host=self._session._host_ip,
+                host=self._session._host,
                 data_center_name=dc_info.name,
                 datastore_name=datastore_name,
                 cookies=cookies,
@@ -1268,7 +1293,7 @@ class VMwareVMOps(object):
         data = {}
         # All of values received are objects. Convert them to dictionaries
         for value in query.values():
-            prop_dict = vim.object_to_dict(value, list_depth=1)
+            prop_dict = vim_util.object_to_dict(value, list_depth=1)
             data.update(prop_dict)
         return data
 
@@ -1436,7 +1461,7 @@ class VMwareVMOps(object):
         try:
             ds_util.mkdir(self._session, path, dc_info.ref)
             LOG.debug("Folder %s created.", path)
-        except error_util.FileAlreadyExistsException:
+        except vexc.FileAlreadyExistsException:
             # NOTE(hartsocks): if the folder already exists, that
             # just means the folder was prepped by another process.
             pass

nova/tests/virt/vmwareapi/fake.py
@@ -22,6 +22,8 @@ A fake VMware VI API implementation.
 import collections
 import pprint
 
+from oslo.vmware import exceptions as vexc
+
 from nova import exception
 from nova.i18n import _
 from nova.openstack.common import jsonutils
@@ -30,7 +32,6 @@ from nova.openstack.common import units
 from nova.openstack.common import uuidutils
 from nova.virt.vmwareapi import constants
 from nova.virt.vmwareapi import ds_util
-from nova.virt.vmwareapi import error_util
 
 _CLASSES = ['Datacenter', 'Datastore', 'ResourcePool', 'VirtualMachine',
             'Network', 'HostSystem', 'HostNetworkSystem', 'Task', 'session',
@@ -40,6 +41,7 @@ _FAKE_FILE_SIZE = 1024
 
 _db_content = {}
 _array_types = {}
+_vim_map = {}
 
 LOG = logging.getLogger(__name__)
 
@@ -296,7 +298,7 @@ class FileAlreadyExists(DataObject):
 
     def __init__(self):
         super(FileAlreadyExists, self).__init__()
-        self.__name__ = error_util.FILE_ALREADY_EXISTS
+        self.__name__ = vexc.FILE_ALREADY_EXISTS
 
 
 class FileNotFound(DataObject):
@@ -304,7 +306,7 @@ class FileNotFound(DataObject):
 
     def __init__(self):
         super(FileNotFound, self).__init__()
-        self.__name__ = error_util.FILE_NOT_FOUND
+        self.__name__ = vexc.FILE_NOT_FOUND
 
 
 class FileFault(DataObject):
@@ -312,7 +314,7 @@ class FileFault(DataObject):
 
     def __init__(self):
         super(FileFault, self).__init__()
-        self.__name__ = error_util.FILE_FAULT
+        self.__name__ = vexc.FILE_FAULT
 
 
 class CannotDeleteFile(DataObject):
@@ -320,7 +322,7 @@ class CannotDeleteFile(DataObject):
 
     def __init__(self):
         super(CannotDeleteFile, self).__init__()
-        self.__name__ = error_util.CANNOT_DELETE_FILE
+        self.__name__ = vexc.CANNOT_DELETE_FILE
 
 
 class FileLocked(DataObject):
@@ -328,7 +330,7 @@ class FileLocked(DataObject):
 
     def __init__(self):
         super(FileLocked, self).__init__()
-        self.__name__ = error_util.FILE_LOCKED
+        self.__name__ = vexc.FILE_LOCKED
 
 
 class VirtualDisk(DataObject):
@@ -965,7 +967,7 @@ def _remove_file(file_path):
     # Check if the remove is for a single file object or for a folder
     if file_path.find(".vmdk") != -1:
         if file_path not in _db_content.get("files"):
-            raise error_util.FileNotFoundException(file_path)
+            raise vexc.FileNotFoundException(file_path)
         _db_content.get("files").remove(file_path)
     else:
         # Removes the files in the folder and the folder too from the db
@@ -997,6 +999,13 @@ def get_file(file_path):
 def fake_upload_image(context, image, instance, **kwargs):
     """Fakes the upload of an image."""
     pass
+
+
+def fake_fetch_image(context, instance, host, dc_name, ds_name, file_path,
+                     cookies=None):
+    """Fakes the fetch of an image."""
+    ds_file_path = "[" + ds_name + "] " + file_path
+    _add_file(ds_file_path)
 
 
 def _get_vm_mdo(vm_ref):
@@ -1079,6 +1088,13 @@ class FakeObjectRetrievalSession(FakeSession):
         return self.ret[self.ind - 1]
 
 
+def get_fake_vim_object(vmware_api_session):
+    key = vmware_api_session.__repr__()
+    if key not in _vim_map:
+        _vim_map[key] = FakeVim()
+    return _vim_map[key]
+
+
 class FakeVim(object):
     """Fake VIM Class."""
 
@@ -1112,7 +1128,8 @@ class FakeVim(object):
 
         self._service_content = service_content
 
-    def get_service_content(self):
+    @property
+    def service_content(self):
         return self._service_content
 
     def __repr__(self):
@@ -1142,8 +1159,8 @@ class FakeVim(object):
         if (self._session is None or self._session not in
                  _db_content['session']):
             LOG.debug("Session is faulty")
-            raise error_util.VimFaultException(
-                               [error_util.NOT_AUTHENTICATED],
+            raise vexc.VimFaultException(
+                               [vexc.NOT_AUTHENTICATED],
                                _("Session Invalid"))
 
     def _session_is_active(self, *args, **kwargs):
@@ -1360,7 +1377,7 @@ class FakeVim(object):
         if _db_content.get("files", None) is None:
             raise exception.NoFilesFound()
         if get_file(ds_path):
-            raise error_util.FileAlreadyExistsException()
+            raise vexc.FileAlreadyExistsException()
         _db_content["files"].append('%s/' % ds_path)
 
     def _set_power_state(self, method, vm_ref, pwr_state="poweredOn"):

nova/tests/virt/vmwareapi/stubs.py
@@ -20,18 +20,25 @@ Stubouts for the test suite
 import contextlib
 
 import mock
+from oslo.vmware import exceptions as vexc
 
 from nova import db
 from nova.tests import test_flavors
 from nova.tests.virt.vmwareapi import fake
 from nova.virt.vmwareapi import driver
-from nova.virt.vmwareapi import error_util
 from nova.virt.vmwareapi import network_util
+from nova.virt.vmwareapi import vmware_images
 
 
 def fake_get_vim_object(arg):
     """Stubs out the VMwareAPISession's get_vim_object method."""
     return fake.FakeVim()
+
+
+@property
+def fake_vim_prop(arg):
+    """Stubs out the VMwareAPISession's vim property access method."""
+    return fake.get_fake_vim_object(arg)
 
 
 def fake_is_vim_object(arg, module):
@@ -40,26 +47,27 @@ def fake_is_vim_object(arg, module):
 
 
 def fake_temp_method_exception():
-    raise error_util.VimFaultException(
-            [error_util.NOT_AUTHENTICATED],
+    raise vexc.VimFaultException(
+            [vexc.NOT_AUTHENTICATED],
             "Session Empty/Not Authenticated")
 
 
 def fake_temp_session_exception():
-    raise error_util.SessionConnectionException("it's a fake!",
+    raise vexc.VimConnectionException("it's a fake!",
             "Session Exception")
 
 
 def fake_session_file_exception():
-    fault_list = [error_util.FILE_ALREADY_EXISTS]
-    raise error_util.VimFaultException(fault_list, 'fake')
+    fault_list = [vexc.FILE_ALREADY_EXISTS]
+    raise vexc.VimFaultException(fault_list,
+                                 Exception('fake'))
 
 
 def fake_session_permission_exception():
-    fault_list = [error_util.NO_PERMISSION]
+    fault_list = [vexc.NO_PERMISSION]
     fault_string = 'Permission to perform this operation was denied.'
     details = {'privilegeId': 'Resource.AssignVMToPool', 'object': 'domain-c7'}
-    raise error_util.VimFaultException(fault_list, fault_string, details)
+    raise vexc.VimFaultException(fault_list, fault_string, details=details)
 
 
 def _fake_flavor_get(context, id):
@@ -78,8 +86,9 @@ def set_stubs(stubs):
     """Set the stubs."""
     stubs.Set(network_util, 'get_network_with_the_name',
               fake.fake_get_network)
-    stubs.Set(driver.VMwareAPISession, "_get_vim_object",
-              fake_get_vim_object)
+    stubs.Set(vmware_images, 'upload_image', fake.fake_upload_image)
+    stubs.Set(vmware_images, 'fetch_image', fake.fake_fetch_image)
+    stubs.Set(driver.VMwareAPISession, "vim", fake_vim_prop)
     stubs.Set(driver.VMwareAPISession, "_is_vim_object",
               fake_is_vim_object)
     stubs.Set(db, 'flavor_get', _fake_flavor_get)

nova/tests/virt/vmwareapi/test_configdrive.py
@@ -13,10 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import contextlib
-
 import fixtures
-import mock
 import mox
 
 from nova import context
@@ -29,7 +26,6 @@ from nova.tests.virt.vmwareapi import fake as vmwareapi_fake
 from nova.tests.virt.vmwareapi import stubs
 from nova.virt import fake
 from nova.virt.vmwareapi import driver
-from nova.virt.vmwareapi import read_write_util
 from nova.virt.vmwareapi import vm_util
 from nova.virt.vmwareapi import vmops
 from nova.virt.vmwareapi import vmware_images
@@ -123,42 +119,11 @@ class ConfigDriveTestCase(test.NoDBTestCase):
                   block_device_info=None):
 
         injected_files = injected_files or []
-        read_file_handle = mock.MagicMock()
-        write_file_handle = mock.MagicMock()
-        self.image_ref = self.test_instance.image_ref
-
-        def fake_read_handle(read_iter):
-            return read_file_handle
-
-        def fake_write_handle(host, dc_name, ds_name, cookies,
-                 file_path, file_size, scheme="https"):
-            self.assertEqual('dc1', dc_name)
-            self.assertEqual('ds1', ds_name)
-            self.assertEqual('Fake-CookieJar', cookies)
-            split_file_path = file_path.split('/')
-            self.assertEqual('vmware_temp', split_file_path[0])
-            self.assertEqual(self.image_ref, split_file_path[2])
-            self.assertEqual(('%s-flat.vmdk' % self.image_ref),
-                             split_file_path[3])
-            self.assertEqual(int(self.image['size']), file_size)
-
-            return write_file_handle
-
-        with contextlib.nested(
-             mock.patch.object(read_write_util, 'VMwareHTTPWriteFile',
-                               side_effect=fake_write_handle),
-             mock.patch.object(read_write_util, 'GlanceFileRead',
-                                side_effect=fake_read_handle),
-             mock.patch.object(vmware_images, 'start_transfer')
-        ) as (fake_http_write, fake_glance_read, fake_start_transfer):
-            self.conn.spawn(self.context, self.test_instance, self.image,
+        self.conn.spawn(self.context, self.test_instance, self.image,
                         injected_files=injected_files,
                         admin_password=admin_password,
                         network_info=self.network_info,
                         block_device_info=block_device_info)
-            fake_start_transfer.assert_called_once_with(self.context,
-                read_file_handle, self.image['size'],
-                write_file_handle=write_file_handle)
 
     def test_create_vm_with_config_drive_verify_method_invocation(self):
         self.test_instance.config_drive = 'True'

nova/tests/virt/vmwareapi/test_ds_util.py
@@ -16,6 +16,7 @@ import contextlib
 import re
 
 import mock
+from oslo.vmware import exceptions as vexc
 
 from nova import exception
 from nova.i18n import _
@@ -23,7 +24,6 @@ from nova.openstack.common import units
 from nova import test
 from nova.tests.virt.vmwareapi import fake
 from nova.virt.vmwareapi import ds_util
-from nova.virt.vmwareapi import error_util
 
 
 class DsUtilTestCase(test.NoDBTestCase):
@@ -146,7 +146,7 @@ class DsUtilTestCase(test.NoDBTestCase):
 
         def fake_wait_for_task(task_ref):
             if task_ref == 'fake_exists_task':
-                raise error_util.FileNotFoundException()
+                raise vexc.FileNotFoundException()
 
             # Should never get here
             self.fail()

commit af4723842a5ac0c5da409cd3338541cf10a243b1

VMware: fix regression for 'TaskInProgress'
Commit 73d4a7c1b4681accd993c1109a9eff837ff612f4 caused a regression
with the snapshot deletion. That is, the snapshot deletion will retry
if the resource is busy. The fact that the exception was not supported
in olso caused the problem.

Version 0.6.0 supports this exception.

Change-Id: I12fcc7d039a123e681c51978b9cf632af8f9644b
Closes-bug: #1369463

nova/virt/vmwareapi/error_util.py
@@ -30,10 +30,6 @@ class NoRootDiskDefined(vexc.VMwareDriverException):
     msg_fmt = _("No root disk defined.")
 
 
-class TaskInProgress(vexc.VMwareDriverException):
-    msg_fmt = _("Virtual machine is busy.")
-
-
 class PbmDefaultPolicyUnspecified(vexc.VMwareDriverConfigurationException):
     msg_fmt = _("Default PBM policy is required if PBM is enabled.")
 

nova/virt/vmwareapi/vmops.py
@@ -132,7 +132,7 @@ def retry_if_task_in_progress(f, *args, **kwargs):
         try:
             f(*args, **kwargs)
             return
-        except error_util.TaskInProgress:
+        except vexc.TaskInProgress:
             pass
 
 

nova/tests/unit/virt/vmwareapi/test_driver_api.py
@@ -1244,7 +1244,7 @@ class VMwareAPIVMTestCase(test.NoDBTestCase):
                               side_effect=exception),
             mock.patch.object(vmops, '_time_sleep_wrapper')
         ) as (_fake_wait, _fake_sleep):
-            if exception != error_util.TaskInProgress:
+            if exception != vexc.TaskInProgress:
                 self.assertRaises(exception,
                                   self.conn._vmops._delete_vm_snapshot,
                                   self.instance, fake_vm, snapshot_ref)
@@ -1260,7 +1260,7 @@ class VMwareAPIVMTestCase(test.NoDBTestCase):
 
     def test_snapshot_delete_vm_snapshot_exception_retry(self):
         self.flags(api_retry_count=5, group='vmware')
-        self._snapshot_delete_vm_snapshot_exception(error_util.TaskInProgress,
+        self._snapshot_delete_vm_snapshot_exception(vexc.TaskInProgress,
                                                     5)
 
     def test_reboot(self):