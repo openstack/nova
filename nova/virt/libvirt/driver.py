6ad7e17e19b9809306b0b96ae7aa9cdfda91fcbb
Daniel Genin <Daniel.Genin@jhuapl.edu>
2014-11-17 15:16:14 -0500

    libvirt: Fixes live migration for volume backed instances
    Live migration fails for volume backed instances in LibvirtDriver
    because _is_shared_block_storage() incorrectly identifies volume
    backed disks as local. This is fixed by passing the block_device_info
    parameter to get_instance_disk_info, which allows for correct
    filtering of volume backed disks.
    
    Change-Id: I1437b2a7d5a62615b0099114ed1b5b1110f56de2
    Closes-bug: 1392773

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -5158,7 +5158,8 @@ class LibvirtDriver(driver.ComputeDriver):
                     dest_check_data['filename'])})
 
         dest_check_data.update({'is_shared_block_storage':
-                self._is_shared_block_storage(instance, dest_check_data)})
+                self._is_shared_block_storage(instance, dest_check_data,
+                                              block_device_info)})
 
         if dest_check_data['block_migration']:
             if (dest_check_data['is_shared_block_storage'] or
@@ -5186,7 +5187,8 @@ class LibvirtDriver(driver.ComputeDriver):
 
         return dest_check_data
 
-    def _is_shared_block_storage(self, instance, dest_check_data):
+    def _is_shared_block_storage(self, instance, dest_check_data,
+                                 block_device_info=None):
         """Check if all block storage of an instance can be shared
         between source and destination of a live migration.
 
@@ -5200,6 +5202,7 @@ class LibvirtDriver(driver.ComputeDriver):
         """
         if (CONF.libvirt.images_type == dest_check_data.get('image_type') and
                 self.image_backend.backend().is_shared_block_storage()):
+            # NOTE(dgenin): currently true only for RBD image backend
             return True
 
         if (dest_check_data.get('is_shared_instance_path') and
@@ -5210,7 +5213,8 @@ class LibvirtDriver(driver.ComputeDriver):
 
         if (dest_check_data.get('is_volume_backed') and
                 not bool(jsonutils.loads(
-                    self.get_instance_disk_info(instance['name'])))):
+                    self.get_instance_disk_info(instance['name'],
+                                                block_device_info)))):
             # pylint: disable E1120
             return True
 
