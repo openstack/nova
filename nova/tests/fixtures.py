f96ec4411ce89606cf52211061003c14306dcfa1
Sean Dague <sean@dague.net>
2014-12-09 08:35:14 -0500

    extract fixtures from nova.test to nova.test.fixtures
    Start extracting all the setup code in note.test into fixtures which
    will let us decompose test setup (and have test base classes that
    don't setup everything).
    
    This creates new OutputStreamCapture fixture and StandardLogging
    fixture and unit tests for both of them plus tests for the exiting
    Conf fixture.
    
    H305/306 have to be turned off because hacking import rules are broken
    and continue to believe that fixtures is a nova module not the
    absolute one.
    
    Change-Id: I97fdacdf5a6ad2957b0efa1e21ae084d4eb04ab9

---
+++ nova/tests/fixtures.py
@@ -0,0 +1,130 @@
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Fixtures for Nova tests."""
+from __future__ import absolute_import
+
+import logging
+import os
+
+import fixtures
+
+_TRUE_VALUES = ('True', 'true', '1', 'yes')
+
+
+class NullHandler(logging.Handler):
+    """custom default NullHandler to attempt to format the record.
+
+    Used in conjunction with
+    log_fixture.get_logging_handle_error_fixture to detect formatting errors in
+    debug level logs without saving the logs.
+    """
+    def handle(self, record):
+        self.format(record)
+
+    def emit(self, record):
+        pass
+
+    def createLock(self):
+        self.lock = None
+
+
+class StandardLogging(fixtures.Fixture):
+    """Setup Logging redirection for tests.
+
+    There are a number of things we want to handle with logging in tests:
+
+    * Redirect the logging to somewhere that we can test or dump it later.
+
+    * Ensure that as many DEBUG messages as possible are actually
+       executed, to ensure they are actually syntactically valid (they
+       often have not been).
+
+    * Ensure that we create useful output for tests that doesn't
+      overwhelm the testing system (which means we can't capture the
+      100 MB of debug logging on every run).
+
+    To do this we create a logger fixture at the root level, which
+    defaults to INFO and create a Null Logger at DEBUG which lets
+    us execute log messages at DEBUG but not keep the output.
+
+    To support local debugging OS_DEBUG=True can be set in the
+    environment, which will print out the full debug logging.
+
+    There are also a set of overrides for particularly verbose
+    modules to be even less than INFO.
+
+    """
+
+    def setUp(self):
+        super(StandardLogging, self).setUp()
+
+        # set root logger to debug
+        root = logging.getLogger()
+        root.setLevel(logging.DEBUG)
+
+        # supports collecting debug level for local runs
+        if os.environ.get('OS_DEBUG') in _TRUE_VALUES:
+            level = logging.DEBUG
+        else:
+            level = logging.INFO
+
+        # Collect logs
+        fs = '%(asctime)s %(levelname)s [%(name)s] %(message)s'
+        self.logger = self.useFixture(
+            fixtures.FakeLogger(format=fs, level=None))
+        # TODO(sdague): why can't we send level through the fake
+        # logger? Tests prove that it breaks, but it's worth getting
+        # to the bottom of.
+        root.handlers[0].setLevel(level)
+
+        if level > logging.DEBUG:
+            # Just attempt to format debug level logs, but don't save them
+            handler = NullHandler()
+            self.useFixture(fixtures.LogHandler(handler, nuke_handlers=False))
+            handler.setLevel(logging.DEBUG)
+
+            # Don't log every single DB migration step
+            logging.getLogger(
+                'migrate.versioning.api').setLevel(logging.WARNING)
+
+
+class OutputStreamCapture(fixtures.Fixture):
+    """Capture output streams during tests.
+
+    This fixture captures errant printing to stderr / stdout during
+    the tests and lets us see those streams at the end of the test
+    runs instead. Useful to see what was happening during failed
+    tests.
+    """
+    def setUp(self):
+        super(OutputStreamCapture, self).setUp()
+        if os.environ.get('OS_STDOUT_CAPTURE') in _TRUE_VALUES:
+            self.out = self.useFixture(fixtures.StringStream('stdout'))
+            self.useFixture(
+                fixtures.MonkeyPatch('sys.stdout', self.out.stream))
+        if os.environ.get('OS_STDERR_CAPTURE') in _TRUE_VALUES:
+            self.err = self.useFixture(fixtures.StringStream('stderr'))
+            self.useFixture(
+                fixtures.MonkeyPatch('sys.stderr', self.err.stream))
+
+    @property
+    def stderr(self):
+        return self.err._details["stderr"].as_text()
+
+    @property
+    def stdout(self):
+        return self.out._details["stdout"].as_text()
