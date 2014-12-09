2e8e74ffebb36ea4003c0b525634b9e2586c242a
Sean Dague <sean@dague.net>
2014-12-09 08:53:48 -0500

    move all conf overrides to conf_fixture
    Some config overrides were in conf_fixture, and some were living in
    test.py:setUp. If we are globally overriding conf variables we should
    do it in one place, the fixture.
    
    Change-Id: Ief66e2245179a37eae60bafaf9e8f452d89319a7

--- nova/test.py
+++ nova/test.py
@@ -317,10 +317,6 @@ class TestCase(testtools.TestCase):
         self.addCleanup(self._clear_attrs)
         self.useFixture(fixtures.EnvironmentVariable('http_proxy'))
         self.policy = self.useFixture(policy_fixture.PolicyFixture())
-        CONF.set_override('fatal_exception_format_errors', True)
-        CONF.set_override('enabled', True, 'osapi_v3')
-        CONF.set_override('force_dhcp_release', False)
-        CONF.set_override('periodic_enable', False)
 
     def _restore_obj_registry(self):
         objects_base.NovaObject._obj_classes = self._base_test_obj_backup
