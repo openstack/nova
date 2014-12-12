f3b9fefd77033f7af90096973d833df507297c3c
ghanshyam <ghanshyam.mann@nectechnologies.in>
2014-12-12 15:37:42 +0900

    Add API schema for v2.1 block_device_mapping_v1
    By defining the API schema, it is possible to separate the validation
    code from the API method. The API method can be more simple.
    In addition, a response of API validation error can be consistent for
    the whole Nova API.
    
    This patch adds the schema validation for block_device_mapping_v1
    by sharing the schema file with block_device_mapping.
    
    Partially implements blueprint v2-on-v3-api
    
    Change-Id: Ib8857d9f725e277f27ccfab83335142b05e2cdf1

--- nova/api/openstack/compute/schemas/v3/block_device_mapping.py
+++ nova/api/openstack/compute/schemas/v3/block_device_mapping.py
@@ -12,75 +12,60 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import copy
+
+from nova.api.openstack.compute.schemas.v3 import block_device_mapping_v1
 from nova.api.validation import parameter_types
+
+
+block_device_mapping_new_item = {
+    # defined in nova/block_device.py:from_api()
+    # NOTE: Client can specify the Id with the combination of
+    # source_type and uuid, or a single attribute like volume_id/
+    # image_id/snapshot_id.
+    'source_type': {
+        'type': 'string',
+        'enum': ['volume', 'image', 'snapshot', 'blank'],
+    },
+    'uuid': {
+        'type': 'string', 'minLength': 1, 'maxLength': 255,
+        'pattern': '^[a-zA-Z0-9._-]*$',
+    },
+    'image_id': parameter_types.image_id,
+    # Defined as varchar(255) in column "destination_type" in table
+    # "block_device_mapping"
+    'destination_type': {
+        'type': 'string', 'maxLength': 255,
+    },
+    # Defined as varchar(255) in column "guest_format" in table
+    # "block_device_mapping"
+    'guest_format': {
+        'type': 'string', 'maxLength': 255,
+    },
+    # Defined as varchar(255) in column "device_type" in table
+    # "block_device_mapping"
+    'device_type': {
+        'type': 'string', 'maxLength': 255,
+    },
+    # Defined as varchar(255) in column "disk_bus" in table
+    # "block_device_mapping"
+    'disk_bus': {
+        'type': 'string', 'maxLength': 255,
+    },
+    # Defined as integer in nova/block_device.py:from_api()
+    'boot_index': {
+        'type': ['integer', 'string'],
+        'pattern': '^-?[0-9]+$',
+    },
+}
+
+block_device_mapping = copy.deepcopy(
+                        block_device_mapping_v1.legacy_block_device_mapping)
+block_device_mapping['properties'].update(block_device_mapping_new_item)
 
 server_create = {
     'block_device_mapping_v2': {
         'type': 'array',
-        'items': [{
-            'type': 'object',
-            'properties': {
-                'virtual_name': {
-                    'type': 'string', 'maxLength': 255,
-                },
-                # defined in nova/block_device.py:from_api()
-                # NOTE: Client can specify the Id with the combination of
-                # source_type and uuid, or a single attribute like volume_id/
-                # image_id/snapshot_id.
-                'source_type': {
-                    'type': 'string',
-                    'enum': ['volume', 'image', 'snapshot', 'blank'],
-                },
-                'uuid': {
-                    'type': 'string', 'minLength': 1, 'maxLength': 255,
-                    'pattern': '^[a-zA-Z0-9._-]*$',
-                },
-                'volume_id': parameter_types.volume_id,
-                'image_id': parameter_types.image_id,
-                'snapshot_id': parameter_types.image_id,
-
-                'volume_size': parameter_types.non_negative_integer,
-                # Defined as varchar(255) in column "destination_type" in table
-                # "block_device_mapping"
-                'destination_type': {
-                    'type': 'string', 'maxLength': 255,
-                },
-                # Defined as varchar(255) in column "guest_format" in table
-                # "block_device_mapping"
-                'guest_format': {
-                    'type': 'string', 'maxLength': 255,
-                },
-                # Defined as varchar(255) in column "device_type" in table
-                # "block_device_mapping"
-                'device_type': {
-                    'type': 'string', 'maxLength': 255,
-                },
-                # Defined as varchar(255) in column "disk_bus" in table
-                # "block_device_mapping"
-                'disk_bus': {
-                    'type': 'string', 'maxLength': 255,
-                },
-                # Defined as integer in nova/block_device.py:from_api()
-                'boot_index': {
-                    'type': ['integer', 'string'],
-                    'pattern': '^-?[0-9]+$',
-                },
-                # Do not allow empty device names and number values and
-                # containing spaces(defined in nova/block_device.py:from_api())
-                'device_name': {
-                    'type': 'string', 'minLength': 1, 'maxLength': 255,
-                    'pattern': '^[a-zA-Z0-9._-r/]*$',
-                },
-                # Defined as boolean in nova/block_device.py:from_api()
-                'delete_on_termination': parameter_types.boolean,
-                'no_device': {},
-                # Defined as mediumtext in column "connection_info" in table
-                # "block_device_mapping"
-                'connection_info': {
-                    'type': 'string', 'maxLength': 16777215
-                },
-            },
-            'additionalProperties': False
-        }]
+        'items': [block_device_mapping]
     }
 }
