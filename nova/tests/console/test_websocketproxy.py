676ba7bbc788a528b0fe4c87c1c4bf94b4bb6eb1
Dave McCowan <dmccowan@cisco.com>
2015-02-24 21:35:48 -0500

    Websocket Proxy should verify Origin header
    If the Origin HTTP header passed in the WebSocket handshake does
    not match the host, this could indicate an attempt at a
    cross-site attack.  This commit adds a check to verify
    the origin matches the host.
    
    Change-Id: Ica6ec23d6f69a236657d5ba0c3f51b693c633649
    Closes-Bug: 1409142

--- nova/tests/console/test_websocketproxy.py
+++ nova/tests/console/test_websocketproxy.py
@@ -16,9 +16,13 @@
 
 
 import mock
+from oslo.config import cfg
 
 from nova.console import websocketproxy
+from nova import exception
 from nova import test
+
+CONF = cfg.CONF
 
 
 class NovaProxyRequestHandlerBaseTestCase(test.TestCase):
@@ -31,15 +35,82 @@ class NovaProxyRequestHandlerBaseTestCase(test.TestCase):
         self.wh.msg = mock.MagicMock()
         self.wh.do_proxy = mock.MagicMock()
         self.wh.headers = mock.MagicMock()
+        CONF.set_override('novncproxy_base_url',
+                          'https://example.net:6080/vnc_auto.html')
+        CONF.set_override('html5proxy_base_url',
+                          'https://example.net:6080/vnc_auto.html',
+                          'spice')
+
+    def _fake_getheader(self, header):
+        if header == 'cookie':
+            return 'token="123-456-789"'
+        elif header == 'Origin':
+            return 'https://example.net:6080'
+        elif header == 'Host':
+            return 'example.net:6080'
+        else:
+            return
+
+    def _fake_getheader_bad_token(self, header):
+        if header == 'cookie':
+            return 'token="XXX"'
+        elif header == 'Origin':
+            return 'https://example.net:6080'
+        elif header == 'Host':
+            return 'example.net:6080'
+        else:
+            return
+
+    def _fake_getheader_bad_origin(self, header):
+        if header == 'cookie':
+            return 'token="123-456-789"'
+        elif header == 'Origin':
+            return 'https://bad-origin-example.net:6080'
+        elif header == 'Host':
+            return 'example.net:6080'
+        else:
+            return
+
+    def _fake_getheader_blank_origin(self, header):
+        if header == 'cookie':
+            return 'token="123-456-789"'
+        elif header == 'Origin':
+            return ''
+        elif header == 'Host':
+            return 'example.net:6080'
+        else:
+            return
+
+    def _fake_getheader_no_origin(self, header):
+        if header == 'cookie':
+            return 'token="123-456-789"'
+        elif header == 'Origin':
+            return None
+        elif header == 'Host':
+            return 'any-example.net:6080'
+        else:
+            return
+
+    def _fake_getheader_http(self, header):
+        if header == 'cookie':
+            return 'token="123-456-789"'
+        elif header == 'Origin':
+            return 'http://example.net:6080'
+        elif header == 'Host':
+            return 'example.net:6080'
+        else:
+            return
 
     @mock.patch('nova.consoleauth.rpcapi.ConsoleAuthAPI.check_token')
     def test_new_websocket_client(self, check_token):
         check_token.return_value = {
             'host': 'node1',
-            'port': '10000'
+            'port': '10000',
+            'console_type': 'novnc'
         }
         self.wh.socket.return_value = '<socket>'
         self.wh.path = "ws://127.0.0.1/?token=123-456-789"
+        self.wh.headers.getheader = self._fake_getheader
 
         self.wh.new_websocket_client()
 
@@ -52,6 +123,7 @@ class NovaProxyRequestHandlerBaseTestCase(test.TestCase):
         check_token.return_value = False
 
         self.wh.path = "ws://127.0.0.1/?token=XXX"
+        self.wh.headers.getheader = self._fake_getheader
 
         self.assertRaises(Exception, self.wh.new_websocket_client)  # noqa
         check_token.assert_called_with(mock.ANY, token="XXX")
@@ -60,11 +132,12 @@ class NovaProxyRequestHandlerBaseTestCase(test.TestCase):
     def test_new_websocket_client_novnc(self, check_token):
         check_token.return_value = {
             'host': 'node1',
-            'port': '10000'
+            'port': '10000',
+            'console_type': 'novnc'
         }
         self.wh.socket.return_value = '<socket>'
         self.wh.path = "http://127.0.0.1/"
-        self.wh.headers.getheader.return_value = "token=123-456-789"
+        self.wh.headers.getheader = self._fake_getheader
 
         self.wh.new_websocket_client()
 
@@ -77,7 +150,111 @@ class NovaProxyRequestHandlerBaseTestCase(test.TestCase):
         check_token.return_value = False
 
         self.wh.path = "http://127.0.0.1/"
-        self.wh.headers.getheader.return_value = "token=XXX"
+        self.wh.headers.getheader = self._fake_getheader_bad_token
 
         self.assertRaises(Exception, self.wh.new_websocket_client)  # noqa
         check_token.assert_called_with(mock.ANY, token="XXX")
+
+    @mock.patch('nova.consoleauth.rpcapi.ConsoleAuthAPI.check_token')
+    def test_new_websocket_client_novnc_bad_origin_header(self, check_token):
+        check_token.return_value = {
+            'host': 'node1',
+            'port': '10000',
+            'console_type': 'novnc'
+        }
+
+        self.wh.path = "http://127.0.0.1/"
+        self.wh.headers.getheader = self._fake_getheader_bad_origin
+
+        self.assertRaises(exception.ValidationError,
+                          self.wh.new_websocket_client)
+
+    @mock.patch('nova.consoleauth.rpcapi.ConsoleAuthAPI.check_token')
+    def test_new_websocket_client_novnc_blank_origin_header(self, check_token):
+        check_token.return_value = {
+            'host': 'node1',
+            'port': '10000',
+            'console_type': 'novnc'
+        }
+
+        self.wh.path = "http://127.0.0.1/"
+        self.wh.headers.getheader = self._fake_getheader_blank_origin
+
+        self.assertRaises(exception.ValidationError,
+                          self.wh.new_websocket_client)
+
+    @mock.patch('nova.consoleauth.rpcapi.ConsoleAuthAPI.check_token')
+    def test_new_websocket_client_novnc_no_origin_header(self, check_token):
+        check_token.return_value = {
+            'host': 'node1',
+            'port': '10000',
+            'console_type': 'novnc'
+        }
+        self.wh.socket.return_value = '<socket>'
+        self.wh.path = "http://127.0.0.1/"
+        self.wh.headers.getheader = self._fake_getheader_no_origin
+
+        self.wh.new_websocket_client()
+
+        check_token.assert_called_with(mock.ANY, token="123-456-789")
+        self.wh.socket.assert_called_with('node1', 10000, connect=True)
+        self.wh.do_proxy.assert_called_with('<socket>')
+
+    @mock.patch('nova.consoleauth.rpcapi.ConsoleAuthAPI.check_token')
+    def test_new_websocket_client_novnc_bad_origin_proto_vnc(self,
+                                                             check_token):
+        check_token.return_value = {
+            'host': 'node1',
+            'port': '10000',
+            'console_type': 'novnc'
+        }
+
+        self.wh.path = "http://127.0.0.1/"
+        self.wh.headers.getheader = self._fake_getheader_http
+
+        self.assertRaises(exception.ValidationError,
+                          self.wh.new_websocket_client)
+
+    @mock.patch('nova.consoleauth.rpcapi.ConsoleAuthAPI.check_token')
+    def test_new_websocket_client_novnc_bad_origin_proto_spice(self,
+                                                               check_token):
+        check_token.return_value = {
+            'host': 'node1',
+            'port': '10000',
+            'console_type': 'spice-html5'
+        }
+
+        self.wh.path = "http://127.0.0.1/"
+        self.wh.headers.getheader = self._fake_getheader_http
+
+        self.assertRaises(exception.ValidationError,
+                          self.wh.new_websocket_client)
+
+    @mock.patch('nova.consoleauth.rpcapi.ConsoleAuthAPI.check_token')
+    def test_new_websocket_client_novnc_bad_origin_proto_serial(self,
+                                                                check_token):
+        check_token.return_value = {
+            'host': 'node1',
+            'port': '10000',
+            'console_type': 'serial'
+        }
+
+        self.wh.path = "http://127.0.0.1/"
+        self.wh.headers.getheader = self._fake_getheader_http
+
+        self.assertRaises(exception.ValidationError,
+                          self.wh.new_websocket_client)
+
+    @mock.patch('nova.consoleauth.rpcapi.ConsoleAuthAPI.check_token')
+    def test_new_websocket_client_novnc_bad_console_type(self, check_token):
+        check_token.return_value = {
+            'host': 'node1',
+            'port': '10000',
+            'console_type': 'bad-console-type'
+        }
+
+        self.wh.path = "http://127.0.0.1/"
+        self.wh.headers.getheader = self._fake_getheader
+
+        self.assertRaises(exception.ValidationError,
+                          self.wh.new_websocket_client)
