0dc7b09514990e3165df48e2ec8f9a8ef1005886
ghanshyam <ghanshyam.mann@nectechnologies.in>
2015-03-25 17:07:35 +0900

    Share V2 and V2.1 hosts functional tests
    Currently v2 and v2.1 have separate functional tests and their
    corresponding sample files. As v2 and v2.1 are supposed to be identical,
    there is overhead to maintain two set of functional tests and sample files.
    We can have one set of tests which can run for both v2 and v2.1.
    
    This commit merges all hosts functional tests.
    
    Change-Id: Ibd53a7ddd4792942fe2dc45121dcc25fc9864903

--- nova/tests/functional/v3/test_hosts.py
+++ nova/tests/functional/v3/test_hosts.py
@@ -13,12 +13,29 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from oslo_config import cfg
+
 from nova.tests.functional.v3 import api_sample_base
+
+CONF = cfg.CONF
+CONF.import_opt('osapi_compute_extension',
+                'nova.api.openstack.compute.extensions')
 
 
 class HostsSampleJsonTest(api_sample_base.ApiSampleTestBaseV3):
     ADMIN_API = True
     extension_name = "os-hosts"
+    # TODO(gmann): Overriding '_api_version' till all functional tests
+    # are merged between v2 and v2.1. After that base class variable
+    # itself can be changed to 'v2'
+    _api_version = 'v2'
+
+    def _get_flags(self):
+        f = super(HostsSampleJsonTest, self)._get_flags()
+        f['osapi_compute_extension'] = CONF.osapi_compute_extension[:]
+        f['osapi_compute_extension'].append(
+            'nova.api.openstack.compute.contrib.hosts.Hosts')
+        return f
 
     def test_host_startup(self):
         response = self._do_get('os-hosts/%s/startup' % self.compute.host)
