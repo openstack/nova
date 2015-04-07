79c47a0919f2a353b863d58d55d97c40fe25b555
ghanshyam <ghanshyam.mann@nectechnologies.in>
2015-04-07 10:25:22 +0900

    Merge availability_zone tests between v2 and v2.1
    Currently v2 and v2.1 have separate functional tests and their
    corresponding sample files. As v2 and v2.1 are supposed to be identical,
    there is overhead to maintain two set of functional tests and sample files.
    We can have one set of tests which can run for both v2 and v2.1.
    
    This commit merges availability_zone functional tests.
    
    In V2.1 os-access-ip is separate plugin where in v2 it was mixed in server
    plugin itself. So os-access-ip extension is enabled to run
    merged functional tests for both v2 and v2.1.
    Also enabling some required extension for v2 tests.
    
    Change-Id: I82e7011da212cb4699c4c19b95523a54a6c2f259

--- nova/tests/functional/v3/test_availability_zone.py
+++ nova/tests/functional/v3/test_availability_zone.py
@@ -18,11 +18,26 @@ from nova.tests.functional.v3 import test_servers
 
 CONF = cfg.CONF
 CONF.import_opt('manager', 'nova.cells.opts', group='cells')
+CONF.import_opt('osapi_compute_extension',
+                'nova.api.openstack.compute.extensions')
 
 
 class AvailabilityZoneJsonTest(test_servers.ServersSampleBase):
     ADMIN_API = True
     extension_name = "os-availability-zone"
+    extra_extensions_to_load = ["os-access-ips"]
+    # TODO(gmann): Overriding '_api_version' till all functional tests
+    # are merged between v2 and v2.1. After that base class variable
+    # itself can be changed to 'v2'
+    _api_version = 'v2'
+
+    def _get_flags(self):
+        f = super(AvailabilityZoneJsonTest, self)._get_flags()
+        f['osapi_compute_extension'] = CONF.osapi_compute_extension[:]
+        f['osapi_compute_extension'].append(
+            'nova.api.openstack.compute.contrib.availability_zone.'
+            'Availability_zone')
+        return f
 
     def _setup_services(self):
         self.conductor = self.start_service('conductor',
