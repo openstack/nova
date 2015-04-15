0cf44ff628cc9ed1e3092ac1c7746b696ac3b6aa
Timofey Durakov <tdurakov@mirantis.com>
2015-04-15 17:29:00 +0300

    Fixed nova-network dhcp-hostsfile update during live-migration
    During live migration _post_live_migration and
    post_live_migration_at_destination_method are executed
    simultaneously, because second one is called over rpc.cast
    In _post_live_migration method there was setup_network_on_host
    call with teardown=True, which expects new host in instances
    table db field. This update could be happened later, as it
    executes on destination node in second method. To guarantee
    execution order  setup_network_on_host call, which cleans
    dhcp-hostfile is moved to destination node.
    Closes-Bug: #1444497
    
    (cherry picked from commit d15c57a499fd7e87b3752ab007f344e055714f05)
    
    Change-Id: I55f0c0148c937601e78f0beecc21b30a1164a690

--- nova/tests/unit/compute/test_compute.py
+++ nova/tests/unit/compute/test_compute.py
@@ -5574,9 +5574,6 @@ class ComputeTestCase(BaseTestCase):
 
         self.mox.StubOutWithMock(self.compute.network_api,
                                  'setup_networks_on_host')
-        self.compute.network_api.setup_networks_on_host(c, instance,
-                                                        instance['host'],
-                                                        teardown=True)
         self.mox.StubOutWithMock(self.compute.instance_events,
                                  'clear_events_for_instance')
         self.compute.instance_events.clear_events_for_instance(
@@ -5636,9 +5633,6 @@ class ComputeTestCase(BaseTestCase):
 
         self.mox.StubOutWithMock(self.compute.network_api,
                                  'setup_networks_on_host')
-        self.compute.network_api.setup_networks_on_host(c, instance,
-                                                        self.compute.host,
-                                                        teardown=True)
         self.mox.StubOutWithMock(self.compute.instance_events,
                                  'clear_events_for_instance')
         self.compute.instance_events.clear_events_for_instance(
@@ -5705,8 +5699,6 @@ class ComputeTestCase(BaseTestCase):
                 mock.call(c, instance, False, dest)])
             post_live_migration_at_source.assert_has_calls(
                 [mock.call(c, instance, [])])
-            setup_networks_on_host.assert_has_calls([
-                mock.call(c, instance, self.compute.host, teardown=True)])
             clear_events.assert_called_once_with(instance)
             update_available_resource.assert_has_calls([mock.call(c)])
 
@@ -5791,6 +5783,8 @@ class ComputeTestCase(BaseTestCase):
                                       self.instance).AndReturn(10001)
 
     def _finish_post_live_migration_at_destination(self):
+        self.compute.network_api.setup_networks_on_host(self.admin_ctxt,
+                mox.IgnoreArg(), mox.IgnoreArg(), teardown=True)
         self.compute.network_api.setup_networks_on_host(self.admin_ctxt,
                 mox.IgnoreArg(), self.compute.host)
 
