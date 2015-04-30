6463b38c0b1d330000a17b6abb074570b5061b8e
Dan Smith <dansmith@redhat.com>
2015-04-30 13:23:07 -0700

    Don't wait for an event on a resize-revert
    Since we never unplugged, the event ain't comin'.
    
    Sadly, the test that covers this was pretty wrong on multiple fronts,
    which this patch fixes up.
    
    Closes-Bug: #1450624
    Change-Id: Id515137747a4b76e9b7057c95f80c8ae74017519
    (cherry picked from commit 4814e9126ec19a1edfa9c696138c4b3fec71aa0d)

--- nova/tests/unit/virt/libvirt/test_driver.py
+++ nova/tests/unit/virt/libvirt/test_driver.py
@@ -11711,10 +11711,13 @@ class LibvirtDriverTestCase(test.NoDBTestCase):
         def fake_plug_vifs(instance, network_info):
             pass
 
-        def fake_create_domain(xml, instance=None, launch_flags=0,
-                               power_on=True):
+        def fake_create_domain(context, xml, instance, network_info,
+                               disk_info, block_device_info=None,
+                               power_on=None,
+                               vifs_already_plugged=None):
             self.fake_create_domain_called = True
             self.assertEqual(powered_on, power_on)
+            self.assertTrue(vifs_already_plugged)
             return mock.MagicMock()
 
         def fake_enable_hairpin(instance):
@@ -11736,7 +11739,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase):
         self.stubs.Set(utils, 'execute', fake_execute)
         fw = base_firewall.NoopFirewallDriver()
         self.stubs.Set(self.drvr, 'firewall_driver', fw)
-        self.stubs.Set(self.drvr, '_create_domain',
+        self.stubs.Set(self.drvr, '_create_domain_and_network',
                        fake_create_domain)
         self.stubs.Set(self.drvr, '_enable_hairpin',
                        fake_enable_hairpin)
