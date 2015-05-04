c259eb9e247119b2f9f6a2620f14fbf7caebc56c
Davanum Srinivas <davanum@gmail.com>
2015-05-04 10:47:30 -0400

    Remove usage of WritableLogger from oslo_log
    With Eventlet 0.17.2, we can directly pass the logger
    instance. This allows us to deprecate/remove the WritableLogger
    class from oslo.log.
    
    Closes-Bug: #1440773
    Change-Id: I3985b30b499a1e676d39c2a6cd3a31ac13c0ab47

--- nova/wsgi.py
+++ nova/wsgi.py
@@ -29,7 +29,6 @@ import eventlet.wsgi
 import greenlet
 from oslo_config import cfg
 from oslo_log import log as logging
-from oslo_log import loggers
 from oslo_utils import excutils
 from paste import deploy
 import routes.middleware
@@ -116,7 +115,6 @@ class Server(object):
         self.pool_size = pool_size or self.default_pool_size
         self._pool = eventlet.GreenPool(self.pool_size)
         self._logger = logging.getLogger("nova.%s.wsgi.server" % self.name)
-        self._wsgi_logger = loggers.WritableLogger(self._logger)
         self._use_ssl = use_ssl
         self._max_url_len = max_url_len
         self.client_socket_timeout = CONF.client_socket_timeout or None
@@ -222,7 +220,7 @@ class Server(object):
             'site': self.app,
             'protocol': self._protocol,
             'custom_pool': self._pool,
-            'log': self._wsgi_logger,
+            'log': self._logger,
             'log_format': CONF.wsgi_log_format,
             'debug': False,
             'keepalive': CONF.wsgi_keep_alive,
