2dd6abca758a18af887756ea7b6d8aba03b6c1a3
Gary Kotton <gkotton@vmware.com>
2015-05-05 03:34:21 -0700

    Fix import order
    Commit 117351d6c46bd6d00cc92fcd234d67a913833cbc added urllib to the
    incorrect import section.
    
    TrivialFix
    
    Change-Id: Ia9e019aa20f6e83b5789c8f8f7fe48f2b0817a7f

--- nova/tests/unit/virt/vmwareapi/test_read_write_util.py
+++ nova/tests/unit/virt/vmwareapi/test_read_write_util.py
@@ -13,12 +13,13 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import urllib
+
 import mock
 from oslo_config import cfg
 
 from nova import test
 from nova.virt.vmwareapi import read_write_util
-import urllib
 
 CONF = cfg.CONF
 
