8c69469e54b6d39088865f2e84ddc5de238d4627
Eric Harney <eharney@redhat.com>
2015-05-05 11:29:48 -0400

    Fix missing format arg in compute manager
    Error message is not printing correctly due
    to broken format string.
    
    Change-Id: Ic153e11e8847418576fde8578f17b842b5fedc46
    Related-Bug: #1451891

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -6295,7 +6295,7 @@ class ComputeManager(manager.Manager):
                 continue
             except Exception as e:
                 LOG.error(_LE("Error updating resources for node "
-                              "%(node)%s: %(e)s"),
+                              "%(node)s: %(e)s"),
                           {'node': nodename, 'e': e})
             new_resource_tracker_dict[nodename] = rt
 
