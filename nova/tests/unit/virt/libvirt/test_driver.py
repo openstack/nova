c66c8e172ed00d67c3172572aa60c3ece83beeea
Taylor Peoples <tpeoples@us.ibm.com>
2015-05-10 06:23:26 +0200

    libvirt: safe_decode xml for i18n logging
    The xml argument passed to _create_domain can be a utf-8 encoded string
    which causes a UnicodeDecodeError when it is substituted into the _LE
    unicode translated message. Safely decoding the xml argument before
    attempting to substitute it into the error message avoids the
    UnicodeDecodeError.
    
    (cherry picked from commit 96a2283c1a07f0298c57f57d8c4112c1c33b6128)
    
    Conflicts:
    
    	nova/virt/libvirt/driver.py
    
    Closes-Bug: #1453274
    Change-Id: I4cf1836f4ca9097f7c6d98c5212a14d24111fe67

--- nova/tests/unit/virt/libvirt/test_driver.py
+++ nova/tests/unit/virt/libvirt/test_driver.py
@@ -10017,12 +10017,17 @@ Active:          8381604 kB
             self.assertEqual(fake_xml, xml)
             raise fakelibvirt.libvirtError('virDomainDefineXML() failed')
 
+        def fake_safe_decode(text, *args, **kwargs):
+            return text + 'safe decoded'
+
         self.log_error_called = False
 
         def fake_error(msg, *args):
             self.log_error_called = True
             self.assertIn(fake_xml, msg % args)
+            self.assertIn('safe decoded', msg % args)
 
+        self.stubs.Set(encodeutils, 'safe_decode', fake_safe_decode)
         self.stubs.Set(nova.virt.libvirt.driver.LOG, 'error', fake_error)
 
         self.create_fake_libvirt_mock(defineXML=fake_defineXML)
