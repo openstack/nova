b13726bcf6b9e6a006ec9bfcde051331741ded5a
Chris Friesen <chris.friesen@windriver.com>
2015-05-13 11:15:25 -0600

    Fix race between resource audit and cpu pinning
    This fixes a race between the claimed CPUs of an instance being
    persisted to the DB, and the resource audit scanning the DB for
    instances and subtracting pinned CPUs from the list of available CPUs.
    
    The problem only shows up when the following sequence happens:
    1) instance A (with dedicated cpus) boots on a compute node
    2) resource audit runs on that compute node
    3) instance B (with dedicated cpus) boots on the same compute node
    
    The bug is that the claimed numa topology isn't updated until
    after we release COMPUTE_RESOURCES_SEMAPHORE, so when the resource
    audit retrieves the list of instances the numa_topology hasn't
    been updated yet for the most recent one.
    
    The fix is to persist the claimed numa topology before releasing
    COMPUTE_RESOURCES_SEMAPHORE.
    
    Closes-Bug: #1454451
    Co-Authored-By: Dan Smith <dansmith@redhat.com>
    (cherry picked from commit 2427d288bc017a5b91430ffe16419d47703d2060)
    
    Conflicts:
    	nova/compute/manager.py
    	nova/tests/unit/compute/test_resource_tracker.py
    	nova/tests/unit/compute/test_tracker.py
    
    Change-Id: I553f2e43a68577c83d890c3671380af68f9e725a

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -1532,7 +1532,7 @@ class ComputeManager(manager.Manager):
         rt = self._get_resource_tracker(node)
         try:
             limits = filter_properties.get('limits', {})
-            with rt.instance_claim(context, instance, limits) as inst_claim:
+            with rt.instance_claim(context, instance, limits):
                 # NOTE(russellb) It's important that this validation be done
                 # *after* the resource tracker instance claim, as that is where
                 # the host is set on the instance.
@@ -1552,7 +1552,6 @@ class ComputeManager(manager.Manager):
 
                 instance.vm_state = vm_states.BUILDING
                 instance.task_state = task_states.BLOCK_DEVICE_MAPPING
-                instance.numa_topology = inst_claim.claimed_numa_topology
                 instance.save()
 
                 block_device_info = self._prep_block_device(
@@ -2296,7 +2295,7 @@ class ComputeManager(manager.Manager):
                 extra_usage_info={'image_name': image_name})
         try:
             rt = self._get_resource_tracker(node)
-            with rt.instance_claim(context, instance, limits) as inst_claim:
+            with rt.instance_claim(context, instance, limits):
                 # NOTE(russellb) It's important that this validation be done
                 # *after* the resource tracker instance claim, as that is where
                 # the host is set on the instance.
@@ -2307,7 +2306,6 @@ class ComputeManager(manager.Manager):
                         block_device_mapping) as resources:
                     instance.vm_state = vm_states.BUILDING
                     instance.task_state = task_states.SPAWNING
-                    instance.numa_topology = inst_claim.claimed_numa_topology
                     # NOTE(JoshNang) This also saves the changes to the
                     # instance from _allocate_network_async, as they aren't
                     # saved in that function to prevent races.
