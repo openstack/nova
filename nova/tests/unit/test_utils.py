7f293ec49ce87ec022798003470f5903dae603fa
Qin Zhao <chaochin@gmail.com>
2015-05-15 01:54:21 +0800

    Ensure to store context in thread local after spawn/spawn_n
    https://review.openstack.org/#/c/171299/ introduces a wrapper for utils.spawn_n
    to update context in thread local store. However, there are other routines in
    driver code which calls greenthread.spawn or greenthread.spawn_n, so that they
    will not update context in thread local store. The commit adds utils.spawn as a
    new wrapper, and also make those codes call spawn/spawn_n of utils, in order to
    ensure the context of logging is correct.
    
    Change-Id: I3623e60c49e442e2431cf017540422aa59bc285a
    Related-Bug: 1404268

--- nova/tests/unit/test_utils.py
+++ nova/tests/unit/test_utils.py
@@ -1032,6 +1032,7 @@ class SpawnNTestCase(test.NoDBTestCase):
     def setUp(self):
         super(SpawnNTestCase, self).setUp()
         self.useFixture(context_fixture.ClearRequestContext())
+        self.spawn_name = 'spawn_n'
 
     def test_spawn_n_no_context(self):
         self.assertIsNone(common_context.get_current())
@@ -1044,8 +1045,8 @@ class SpawnNTestCase(test.NoDBTestCase):
         def fake(arg):
             pass
 
-        with mock.patch.object(eventlet, 'spawn_n', _fake_spawn):
-            utils.spawn_n(fake, 'test')
+        with mock.patch.object(eventlet, self.spawn_name, _fake_spawn):
+            getattr(utils, self.spawn_name)(fake, 'test')
         self.assertIsNone(common_context.get_current())
 
     def test_spawn_n_context(self):
@@ -1061,8 +1062,8 @@ class SpawnNTestCase(test.NoDBTestCase):
         def fake(context, kwarg1=None):
             pass
 
-        with mock.patch.object(eventlet, 'spawn_n', _fake_spawn):
-            utils.spawn_n(fake, ctxt, kwarg1='test')
+        with mock.patch.object(eventlet, self.spawn_name, _fake_spawn):
+            getattr(utils, self.spawn_name)(fake, ctxt, kwarg1='test')
         self.assertEqual(ctxt, common_context.get_current())
 
     def test_spawn_n_context_different_from_passed(self):
@@ -1081,6 +1082,12 @@ class SpawnNTestCase(test.NoDBTestCase):
         def fake(context, kwarg1=None):
             pass
 
-        with mock.patch.object(eventlet, 'spawn_n', _fake_spawn):
-            utils.spawn_n(fake, ctxt_passed, kwarg1='test')
+        with mock.patch.object(eventlet, self.spawn_name, _fake_spawn):
+            getattr(utils, self.spawn_name)(fake, ctxt_passed, kwarg1='test')
         self.assertEqual(ctxt, common_context.get_current())
+
+
+class SpawnTestCase(SpawnNTestCase):
+    def setUp(self):
+        super(SpawnTestCase, self).setUp()
+        self.spawn_name = 'spawn'
