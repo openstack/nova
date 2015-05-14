800a5098ba3a104f0e2b01c8c9d60d95a907f050
Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>
2015-05-14 03:04:06 +0100

    Added new scheduler filter: AggregateTypeExtraSpecsAffinityFilter
    The flavor_extra_spec metadata pair will be consumed by the
    AggregateTypeExtraSpecsAffinityFilter to allow operators to define a
    set of extra specs key value pairs that are required to schedule to
    the aggregate, e.g.:
      standard memory backing aggregate:
        flavor_extra_spec: "hw:mem_page_size=small,hw:mem_page_size=any"
      high bandwidth memory backing aggregate:
        flavor_extra_spec: "hw:mem_page_size=2M,hw:mem_page_size=1G," \
                           "hw:mem_page_size=large"
    
    DocImpact
    Implements: blueprint aggregate-extra-specs-filter
    Change-Id: Id3a9918cf9f83b2a9b1dfbcd91803b5b1b2bcc78

--- nova/scheduler/filters/type_filter.py
+++ nova/scheduler/filters/type_filter.py
@@ -13,9 +13,16 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import collections
+import six
 
+from nova.i18n import _LI
 from nova.scheduler import filters
 from nova.scheduler.filters import utils
+from oslo_log import log as logging
+
+
+LOG = logging.getLogger(__name__)
 
 
 class TypeAffinityFilter(filters.BaseHostFilter):
@@ -59,3 +66,136 @@ class AggregateTypeAffinityFilter(filters.BaseHostFilter):
                     [x.strip() for x in val.split(',')]):
                 return True
         return not aggregate_vals
+
+
+class AggregateTypeExtraSpecsAffinityFilter(filters.BaseHostFilter):
+    """AggregateTypeExtraSpecsAffinityFilter filters only hosts aggregated
+    inside host aggregates containing "flavor_extra_spec" metadata. Keys inside
+    this variable must match with the instance extra specs.
+
+    To use this filter, the name of this class should be added to the variable
+    ``scheduler_default_filters``, in ``nova.conf``:
+
+    |   [DEFAULT]
+    |   scheduler_default_filters=<list of other filters>, \
+                                  AggregateTypeExtraSpecsAffinityFilter
+
+    The content of the list will be formatted as follows. The entries in the
+    list will be separated by commas without white space. Each entry will
+    comprise of an instance type extra spec key followed by and equals "="
+    followed by a value: <key>=<value>.
+
+    Eg.
+        Host Aggregate metadata:
+            'flavor_extra_spec=hw:mem_page_size=any,hw:mem_page_size=~,' \
+            'hw:mem_page_size=small'
+        Flavor extra specs:
+            'hw:mem_page_size=small'
+
+    Valid sentinel values are:
+
+    | * (asterisk): may be used to specify that any value is valid.
+    | ~ (tilde): may be used to specify that a key may optionally be omitted.
+    | ! (exclamation): may be used to specify that the key must not be present.
+    """
+
+    def _parse_extra_spec_key_pairs(self, aggregate_extra_spec):
+        """Parse and group all key/data from aggregate_extra_spec.
+
+        :param aggregate_extra_spec: string containing a list of required
+                                     instance flavor extra specs, separated by
+                                     commas, with format "key=value"
+        :type aggregate_extra_spec: unicode.
+        :return: dictionary with the values parsed and grouped by keys.
+        :type: dict.
+        """
+        extra_specs = collections.defaultdict(set)
+        kv_list = str(aggregate_extra_spec).split(',')
+
+        # Create a new set entry in a dict for every new key, update the key
+        # value (set object) for every other value.
+        for kv_element in kv_list:
+            key, value = kv_element.split('=', 1)
+            extra_specs[key].add(value)
+            if '=' in value:
+                LOG.info(_LI("Value string has an '=' char: key = '%(key)s', "
+                             "value = '%(value)s'. Check if it's malformed"),
+                         {'value': value, 'key': key})
+        return extra_specs
+
+    def _instance_is_allowed_in_aggregate(self,
+                                          aggregate_extra_spec,
+                                          instance_extra_specs):
+        """Loop over all aggregate_extra_spec elements parsed and execute the
+        appropriate filter action.
+
+        :param aggregate_extra_spec: dictionary with the values parsed and
+                                     grouped by keys.
+        :type aggregate_extra_spec: dict.
+        :param instance_extra_specs: dictionary containing the extra specs of
+                                     the instance to be filtered.
+        :type instance_extra_specs: dict.
+        :return: True if all parameters executed correctly; False is there
+                 is any error.
+        :type: boolean.
+        """
+        for key, value in six.iteritems(aggregate_extra_spec):
+            if '*' in value:
+                if key not in instance_extra_specs:
+                    LOG.debug("'flavor_extra_spec' key: %(key)s "
+                              "is not present", {'key': key})
+                    return False
+                else:
+                    continue
+
+            if '!' in value:
+                if key in instance_extra_specs:
+                    LOG.debug("'flavor_extra_spec' key: %(key)s "
+                              "is present", {'key': key})
+                    return False
+                else:
+                    continue
+
+            if '~' in value:
+                if key not in instance_extra_specs:
+                    continue
+                else:
+                    value.discard('~')
+
+            for element in value:
+                match = [char for char in ["*", "!", "~"] if char in element]
+                if match:
+                    LOG.info(_LI("Value string has '%(chars)s' char(s): "
+                                 "key = '%(key)s', value = '%(value)s'. "
+                                 "Check if it's malformed"),
+                             {'value': element, 'chars': match, 'key': key})
+                if key not in instance_extra_specs:
+                    LOG.debug("'flavor_extra_spec' key: %(key)s "
+                              "is not present", {'key': key})
+                    return False
+                if instance_extra_specs[key] not in value:
+                    LOG.debug("The following 'flavor_extra_spec' "
+                              "key=value: %(key)s=%(value)s doesn't "
+                              "match", {'key': key, 'value': value})
+                    return False
+
+        return True
+
+    def host_passes(self, host_state, spec_obj):
+        instance_type = spec_obj.flavor
+        # If 'extra_specs' is not present or extra_specs are empty then we
+        # need not proceed further
+        if (not instance_type.obj_attr_is_set('extra_specs')
+                or not instance_type.extra_specs):
+            return True
+
+        aggregate_extra_spec_list = utils.aggregate_values_from_key(host_state,
+            'flavor_extra_spec')
+
+        for aggregate_extra_spec in aggregate_extra_spec_list:
+            aggregate_extra_spec = self._parse_extra_spec_key_pairs(
+                aggregate_extra_spec)
+            if not self._instance_is_allowed_in_aggregate(aggregate_extra_spec,
+                    instance_type.extra_specs):
+                return False
+        return True
