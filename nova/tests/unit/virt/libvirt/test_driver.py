openstack/nova

commit e9639913294036ff94f354e9c8ea18cd816243ab

libvirt: update volume delete snapshot to use Guest
This commit updates the method _volume_delete_snapshot
to make it pass a Guest object and then use a
BlockDevice object to deal with block device operations

Change-Id: Ibd2296109696cbd62292b5c68e038e2b58e7519c

nova/virt/libvirt/driver.py
@@ -1820,11 +1820,6 @@ class LibvirtDriver(driver.ComputeDriver):
 
         try:
             guest = self._host.get_guest(instance)
-
-            # TODO(sahid): We are converting all calls from a
-            # virDomain object to use nova.virt.libvirt.Guest.
-            # We should be able to remove virt_dom at the end.
-            virt_dom = guest._domain
         except exception.InstanceNotFound:
             raise exception.InstanceNotRunning(instance_id=instance.uuid)
 
@@ -1908,12 +1903,10 @@ class LibvirtDriver(driver.ComputeDriver):
             # Merge the most recent snapshot into the active image
 
             rebase_disk = my_dev
-            rebase_flags = 0
             rebase_base = delete_info['file_to_merge']  # often None
             if active_protocol is not None:
                 rebase_base = _get_snap_dev(delete_info['file_to_merge'],
                                             active_disk_object.backing_store)
-            rebase_bw = 0
 
             # NOTE(deepakcs): libvirt added support for _RELATIVE in v1.2.7,
             # and when available this flag _must_ be used to ensure backing
@@ -1922,26 +1915,27 @@ class LibvirtDriver(driver.ComputeDriver):
             # If _RELATIVE flag not found, continue with old behaviour
             # (relative backing path seems to work for this case)
             try:
-                if rebase_base is not None:
-                    rebase_flags |= libvirt.VIR_DOMAIN_BLOCK_REBASE_RELATIVE
+                libvirt.VIR_DOMAIN_BLOCK_REBASE_RELATIVE
+                relative = True
             except AttributeError:
                 LOG.warn(_LW("Relative blockrebase support was not detected. "
                              "Continuing with old behaviour."))
+                relative = False
 
-            LOG.debug('disk: %(disk)s, base: %(base)s, '
-                      'bw: %(bw)s, flags: %(flags)s',
-                      {'disk': rebase_disk,
-                       'base': rebase_base,
-                       'bw': rebase_bw,
-                       'flags': rebase_flags})
+            LOG.debug(
+                'disk: %(disk)s, base: %(base)s, '
+                'bw: %(bw)s, relative: %(relative)s',
+                {'disk': rebase_disk,
+                 'base': rebase_base,
+                 'bw': libvirt_guest.BlockDevice.REBASE_DEFAULT_BANDWIDTH,
+                 'relative': str(relative)})
 
-            result = virt_dom.blockRebase(rebase_disk, rebase_base,
-                                          rebase_bw, rebase_flags)
-
+            dev = guest.get_block_device(rebase_disk)
+            result = dev.rebase(rebase_base, relative=relative)
             if result == 0:
                 LOG.debug('blockRebase started successfully')
 
-            while self._wait_for_block_job(virt_dom, my_dev,
+            while self._wait_for_block_job(guest._domain, my_dev,
                                            abort_on_error=True):
                 LOG.debug('waiting for blockRebase job completion')
                 time.sleep(0.5)
@@ -1951,7 +1945,6 @@ class LibvirtDriver(driver.ComputeDriver):
             my_snap_base = None
             my_snap_top = None
             commit_disk = my_dev
-            commit_flags = 0
 
             # NOTE(deepakcs): libvirt added support for _RELATIVE in v1.2.7,
             # and when available this flag _must_ be used to ensure backing
@@ -1961,7 +1954,7 @@ class LibvirtDriver(driver.ComputeDriver):
             # path may not be maintained and Cinder flow is broken if allowed
             # to continue.
             try:
-                commit_flags |= libvirt.VIR_DOMAIN_BLOCK_COMMIT_RELATIVE
+                libvirt.VIR_DOMAIN_BLOCK_COMMIT_RELATIVE
             except AttributeError:
                 ver = '.'.join(
                     [str(x) for x in
@@ -1980,7 +1973,6 @@ class LibvirtDriver(driver.ComputeDriver):
 
             commit_base = my_snap_base or delete_info['merge_target_file']
             commit_top = my_snap_top or delete_info['file_to_merge']
-            bandwidth = 0
 
             LOG.debug('will call blockCommit with commit_disk=%(commit_disk)s '
                       'commit_base=%(commit_base)s '
@@ -1989,13 +1981,13 @@ class LibvirtDriver(driver.ComputeDriver):
                          'commit_base': commit_base,
                          'commit_top': commit_top})
 
-            result = virt_dom.blockCommit(commit_disk, commit_base, commit_top,
-                                          bandwidth, commit_flags)
+            dev = guest.get_block_device(commit_disk)
+            result = dev.commit(commit_base, commit_top, relative=True)
 
             if result == 0:
                 LOG.debug('blockCommit started successfully')
 
-            while self._wait_for_block_job(virt_dom, my_dev,
+            while self._wait_for_block_job(guest._domain, my_dev,
                                            abort_on_error=True):
                 LOG.debug('waiting for blockCommit job completion')
                 time.sleep(0.5)

nova/tests/unit/virt/libvirt/test_driver.py
@@ -13266,7 +13266,7 @@ class LibvirtVolumeSnapshotTestCase(test.NoDBTestCase):
         self.drvr._host.get_domain(instance).AndReturn(domain)
         self.drvr._host.has_min_version(mox.IgnoreArg()).AndReturn(True)
 
-        domain.blockRebase('vda', 'snap.img', 0, 0)
+        domain.blockRebase('vda', 'snap.img', 0, flags=0)
 
         domain.blockJobInfo('vda', 0).AndReturn({'cur': 1, 'end': 1000})
         domain.blockJobInfo('vda', 0).AndReturn({'cur': 1000, 'end': 1000})
@@ -13288,20 +13288,21 @@ class LibvirtVolumeSnapshotTestCase(test.NoDBTestCase):
         snapshot_id = 'snapshot-1234'
 
         domain = FakeVirtDomain(fake_xml=self.dom_xml)
+        guest = libvirt_guest.Guest(domain)
         self.mox.StubOutWithMock(domain, 'XMLDesc')
         domain.XMLDesc(flags=0).AndReturn(self.dom_xml)
 
-        self.mox.StubOutWithMock(self.drvr._host, 'get_domain')
+        self.mox.StubOutWithMock(self.drvr._host, 'get_guest')
         self.mox.StubOutWithMock(self.drvr._host, 'has_min_version')
         self.mox.StubOutWithMock(domain, 'blockRebase')
         self.mox.StubOutWithMock(domain, 'blockCommit')
         self.mox.StubOutWithMock(domain, 'blockJobInfo')
 
-        self.drvr._host.get_domain(instance).AndReturn(domain)
+        self.drvr._host.get_guest(instance).AndReturn(guest)
         self.drvr._host.has_min_version(mox.IgnoreArg()).AndReturn(True)
 
         domain.blockRebase('vda', 'snap.img', 0,
-                           fakelibvirt.VIR_DOMAIN_BLOCK_REBASE_RELATIVE)
+                           flags=fakelibvirt.VIR_DOMAIN_BLOCK_REBASE_RELATIVE)
 
         domain.blockJobInfo('vda', 0).AndReturn({'cur': 1, 'end': 1000})
         domain.blockJobInfo('vda', 0).AndReturn({'cur': 1000, 'end': 1000})
@@ -13370,7 +13371,7 @@ class LibvirtVolumeSnapshotTestCase(test.NoDBTestCase):
         self.drvr._host.has_min_version(mox.IgnoreArg()).AndReturn(True)
 
         domain.blockCommit('vda', 'other-snap.img', 'snap.img', 0,
-                           fakelibvirt.VIR_DOMAIN_BLOCK_COMMIT_RELATIVE)
+                           flags=fakelibvirt.VIR_DOMAIN_BLOCK_COMMIT_RELATIVE)
 
         domain.blockJobInfo('vda', 0).AndReturn({'cur': 1, 'end': 1000})
         domain.blockJobInfo('vda', 0).AndReturn({})
@@ -13499,7 +13500,7 @@ class LibvirtVolumeSnapshotTestCase(test.NoDBTestCase):
         self.drvr._host.get_domain(instance).AndReturn(domain)
         self.drvr._host.has_min_version(mox.IgnoreArg()).AndReturn(True)
 
-        domain.blockRebase('vdb', 'vdb[1]', 0, 0)
+        domain.blockRebase('vdb', 'vdb[1]', 0, flags=0)
 
         domain.blockJobInfo('vdb', 0).AndReturn({'cur': 1, 'end': 1000})
         domain.blockJobInfo('vdb', 0).AndReturn({'cur': 1000, 'end': 1000})
@@ -13540,7 +13541,7 @@ class LibvirtVolumeSnapshotTestCase(test.NoDBTestCase):
         self.drvr._host.has_min_version(mox.IgnoreArg()).AndReturn(True)
 
         domain.blockRebase('vdb', 'vdb[1]', 0,
-                           fakelibvirt.VIR_DOMAIN_BLOCK_REBASE_RELATIVE)
+                           flags=fakelibvirt.VIR_DOMAIN_BLOCK_REBASE_RELATIVE)
 
         domain.blockJobInfo('vdb', 0).AndReturn({'cur': 1, 'end': 1000})
         domain.blockJobInfo('vdb', 0).AndReturn({'cur': 1000, 'end': 1000})
@@ -13622,7 +13623,7 @@ class LibvirtVolumeSnapshotTestCase(test.NoDBTestCase):
         self.drvr._host.has_min_version(mox.IgnoreArg()).AndReturn(True)
 
         domain.blockCommit('vdb', 'vdb[0]', 'vdb[1]', 0,
-                           fakelibvirt.VIR_DOMAIN_BLOCK_COMMIT_RELATIVE)
+                           flags=fakelibvirt.VIR_DOMAIN_BLOCK_COMMIT_RELATIVE)
 
         domain.blockJobInfo('vdb', 0).AndReturn({'cur': 1, 'end': 1000})
         domain.blockJobInfo('vdb', 0).AndReturn({'cur': 1000, 'end': 1000})

commit 45d73c49bd2f37ff0091f10a928b2ad548256bae

libvirt: do relative block rebase only with non-null base
You can't use VIR_DOMAIN_BLOCK_REBASE_RELATIVE with null
base, libvirt reports error in this case, so check if
base is non-null before setting this flag (relative argument
of BlockDevice.rebase method).

The error was introduced by commit
e9639913294036ff94f354e9c8ea18cd816243ab

Change-Id: Ife50f211fd54190a2665ad090f6208048b841690
Closes-Bug: #1475202

nova/virt/libvirt/driver.py
@@ -1887,7 +1887,7 @@ class LibvirtDriver(driver.ComputeDriver):
             # (relative backing path seems to work for this case)
             try:
                 libvirt.VIR_DOMAIN_BLOCK_REBASE_RELATIVE
-                relative = True
+                relative = rebase_base is not None
             except AttributeError:
                 LOG.warn(_LW("Relative blockrebase support was not detected. "
                              "Continuing with old behaviour."))

nova/tests/unit/virt/libvirt/fakelibvirt.py
@@ -108,6 +108,7 @@ VIR_FROM_NWFILTER = 330
 VIR_FROM_REMOTE = 340
 VIR_FROM_RPC = 345
 VIR_FROM_NODEDEV = 666
+VIR_ERR_INVALID_ARG = 8
 VIR_ERR_NO_SUPPORT = 3
 VIR_ERR_XML_DETAIL = 350
 VIR_ERR_NO_DOMAIN = 420
@@ -739,7 +740,14 @@ class Domain(object):
     def blockResize(self, disk, size):
         pass
 
-    def blockRebase(self, disk, base, flags):
+    def blockRebase(self, disk, base, bandwidth=0, flags=0):
+        if (not base) and (flags and VIR_DOMAIN_BLOCK_REBASE_RELATIVE):
+            raise make_libvirtError(
+                    libvirtError,
+                    'flag VIR_DOMAIN_BLOCK_REBASE_RELATIVE is '
+                    'valid only with non-null base',
+                    error_code=VIR_ERR_INVALID_ARG,
+                    error_domain=VIR_FROM_QEMU)
         return 0
 
     def blockCommit(self, disk, base, top, flags):

nova/tests/unit/virt/libvirt/test_driver.py
@@ -13118,6 +13118,10 @@ class LibvirtVolumeSnapshotTestCase(test.NoDBTestCase):
                               'file_to_merge': 'snap.img',
                               'merge_target_file': 'other-snap.img'}
 
+        self.delete_info_3 = {'type': 'qcow2',
+                              'file_to_merge': None,
+                              'merge_target_file': None}
+
         self.delete_info_netdisk = {'type': 'qcow2',
                                     'file_to_merge': 'snap.img',
                                     'merge_target_file': 'root.img'}
@@ -13479,6 +13483,38 @@ class LibvirtVolumeSnapshotTestCase(test.NoDBTestCase):
                                           snapshot_id, self.delete_info_2)
 
         self.mox.VerifyAll()
+
+    def test_volume_snapshot_delete_nonrelative_null_base(self):
+        # Deleting newest and last snapshot of a volume
+        # with blockRebase. So base of the new image will be null.
+
+        instance = objects.Instance(**self.inst)
+        snapshot_id = 'snapshot-1234'
+
+        domain = FakeVirtDomain(fake_xml=self.dom_xml)
+        guest = libvirt_guest.Guest(domain)
+
+        with contextlib.nested(
+            mock.patch.object(domain, 'XMLDesc', return_value=self.dom_xml),
+            mock.patch.object(self.drvr._host, 'get_guest',
+                              return_value=guest),
+            mock.patch.object(self.drvr._host, 'has_min_version',
+                              return_value=True),
+            mock.patch.object(domain, 'blockRebase'),
+            mock.patch.object(domain, 'blockJobInfo',
+                              return_value={'cur': 1000, 'end': 1000})
+        ) as (mock_xmldesc, mock_get_guest, mock_has_min_version,
+              mock_rebase, mock_job_info):
+
+            self.drvr._volume_snapshot_delete(self.c, instance,
+                                              self.volume_uuid, snapshot_id,
+                                              self.delete_info_3)
+
+            mock_xmldesc.assert_called_once_with(flags=0)
+            mock_get_guest.assert_called_once_with(instance)
+            mock_has_min_version.assert_called_once_with((1, 1, 1,))
+            mock_rebase.assert_called_once_with('vda', None, 0, flags=0)
+            mock_job_info.assert_called_once_with('vda', flags=0)
 
     def test_volume_snapshot_delete_outer_success(self):
         instance = objects.Instance(**self.inst)