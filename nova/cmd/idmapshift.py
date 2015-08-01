925211a01059f3078a8c524c1935f0ee307babae
Steph Zylstra <steph@stephaniezylstra.com>
2015-08-01 16:41:36 +1000

    Spelling/grammar

--- nova/cmd/idmapshift.py
+++ nova/cmd/idmapshift.py
@@ -17,7 +17,7 @@ IDMapShift
 ##########
 
 IDMapShift is a tool that properly sets the ownership of a filesystem for use
-with linux user namespaces.
+with Linux user namespaces.
 
 =====
 Usage
@@ -25,7 +25,7 @@ Usage
 
     nova-idmapshift -i -u 0:10000:2000 -g 0:10000:2000 path
 
-This command will idempotently shift `path` to proper ownership using
+This command will idempotently shift `path` to proper ownership, using
 the provided uid and gid mappings.
 
 =========
@@ -61,13 +61,13 @@ Returns 1 when filesystem appears unshifted
 Purpose
 =======
 
-When using user namespaces with linux containers, the filesystem of the
-container must be owned by the targeted user and group ids being applied
+When using user namespaces with Linux containers, the filesystem of the
+container must be owned by the targeted user and group IDs being applied
 to that container. Otherwise, processes inside the container won't be able
 to access the filesystem.
 
-For example, when using the id map string '0:10000:2000', this means that
-user ids inside the container between 0 and 1999 will map to user ids on
+For example, when using the ID map string '0:10000:2000', this means that
+user IDs inside the container between 0 and 1999 will map to user IDs on
 the host between 10000 and 11999. Root (0) becomes 10000, user 1 becomes
 10001, user 50 becomes 10050 and user 1999 becomes 11999. This means that
 files that are owned by root need to actually be owned by user 10000, and
