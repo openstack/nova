openstack/nova

commit 9a09674220a071e51fdca7911b52c0027c01ff64

libvirt: enable virtio-net multiqueue
Add supoort to enable the virtio-net multiqueue feature for guest
instances.

With virtio-net multiqueue enabled network performance can be scaled
accross number of vCPUs, by transfering the packets through multiple virtqueue
pairs at a time.

When the feature is enabled, the Libvirt driver will set the number of queues
equal to the number of guest VCPUs.

To enable this feature, the following should be set in the Glace image
properties:

    hw_vif_multiqueue_enabled=true

DocImpact
Partially Implements blueprint libvirt-virtio-net-multiqueue

Change-Id: I8505cb71de2cf9d771fa1fe9eddce3d39158e74f

nova/virt/libvirt/driver.py
@@ -398,6 +398,9 @@ MIN_LIBVIRT_PARALLELS_VERSION = (1, 2, 12)
 # Ability to set the user guest password with Qemu
 MIN_LIBVIRT_SET_ADMIN_PASSWD = (1, 2, 16)
 
+# vhostuser queues support
+MIN_LIBVIRT_VHOSTUSER_MQ = (1, 2, 17)
+
 
 class LibvirtDriver(driver.ComputeDriver):
     capabilities = {
@@ -425,7 +428,10 @@ class LibvirtDriver(driver.ComputeDriver):
             self.virtapi,
             host=self._host)
 
-        self.vif_driver = libvirt_vif.LibvirtGenericVIFDriver()
+        support_vhostuser_mq = self._host.has_min_version(
+                                          MIN_LIBVIRT_VHOSTUSER_MQ)
+        self.vif_driver = libvirt_vif.LibvirtGenericVIFDriver(
+                                    support_vhostuser_mq=support_vhostuser_mq)
 
         self.volume_drivers = driver.driver_dict_from_config(
             self._get_volume_drivers(), self)

nova/virt/libvirt/vif.py
@@ -29,6 +29,7 @@ from nova.i18n import _
 from nova.i18n import _LE
 from nova.network import linux_net
 from nova.network import model as network_model
+from nova import objects
 from nova import utils
 from nova.virt.libvirt import config as vconfig
 from nova.virt.libvirt import designer
@@ -83,6 +84,9 @@ def is_vif_model_valid_for_virt(virt_type, vif_model):
 class LibvirtGenericVIFDriver(object):
     """Generic VIF driver for libvirt networking."""
 
+    def __init__(self, support_vhostuser_mq=False):
+        self.support_vhostuser_mq = support_vhostuser_mq
+
     def _normalize_vif_type(self, vif_type):
         return vif_type.replace('2.1q', '2q')
 
@@ -101,6 +105,7 @@ class LibvirtGenericVIFDriver(object):
         # Default to letting libvirt / the hypervisor choose the model
         model = None
         driver = None
+        vhost_queues = None
 
         # If the user has specified a 'vif_model' against the
         # image then honour that model
@@ -127,11 +132,31 @@ class LibvirtGenericVIFDriver(object):
                                            model):
             raise exception.UnsupportedHardware(model=model,
                                                 virt=virt_type)
+        if (virt_type == 'kvm' and
+            model == network_model.VIF_MODEL_VIRTIO):
+            vhost_drv, vhost_queues = self._get_virtio_mq_settings(image_meta,
+                                                                   inst_type)
+            driver = vhost_drv or driver
 
         designer.set_vif_guest_frontend_config(
-            conf, vif['address'], model, driver)
+            conf, vif['address'], model, driver, vhost_queues)
 
         return conf
+
+    def _get_virtio_mq_settings(self, image_meta, flavor):
+        """A methods to set the number of virtio queues,
+           if it has been requested in extra specs.
+        """
+        driver = None
+        vhost_queues = None
+        if not isinstance(image_meta, objects.ImageMeta):
+            image_meta = objects.ImageMeta.from_dict(image_meta)
+        img_props = image_meta.properties
+        if img_props.get('hw_vif_multiqueue_enabled'):
+            driver = 'vhost'
+            vhost_queues = flavor.vcpus
+
+        return (driver, vhost_queues)
 
     def get_bridge_name(self, vif):
         return vif['network']['bridge']
@@ -383,6 +408,13 @@ class LibvirtGenericVIFDriver(object):
             raise exception.VifDetailsMissingVhostuserSockPath(
                                                         vif_id=vif['id'])
         designer.set_vif_host_backend_vhostuser_config(conf, mode, sock_path)
+        # (vladikr) Not setting up driver and queues for vhostuser
+        # as queues are not supported in Libvirt until version 1.2.17
+        if not self.support_vhostuser_mq:
+            LOG.debug('Queues are not a vhostuser supported feature.')
+            conf.driver_name = None
+            conf.vhost_queues = None
+
         return conf
 
     def get_config_ib_hostdev(self, instance, vif, image_meta,

commit e7396e7c6601cd9ab2aaddf82311353a5b4fefe3

libvirt: don't open connection in driver constructor
Calling host.has_min_version triggers opening of a new
libvirt connection. This should not ever be done in the
constructor of LibvirtDriver class, as the 'host_init'
method must be allowed to run first during normal Nova
compute service startup.

This mistake was introduced in

  commit 9a09674220a071e51fdca7911b52c0027c01ff64
  Author: Vladik Romanovsky <vromanso@redhat.com>
  Date:   Wed Oct 15 14:42:37 2014 -0400

    libvirt: enable virtio-net multiqueue

and is thought to also be causing instability in the
unit tests in the gate.

Avoid the problem by passing the Host object instance
into the VIF driver get_config method, instead of
trying to pass a flag into its constructor.

Closes-Bug: #1482230
Change-Id: Ic940d6ffc5b7d664540363b8bcbea23c7bc7d9dd

nova/virt/libvirt/driver.py
@@ -399,9 +399,6 @@ MIN_LIBVIRT_PARALLELS_VERSION = (1, 2, 12)
 # Ability to set the user guest password with Qemu
 MIN_LIBVIRT_SET_ADMIN_PASSWD = (1, 2, 16)
 
-# vhostuser queues support
-MIN_LIBVIRT_VHOSTUSER_MQ = (1, 2, 17)
-
 
 class LibvirtDriver(driver.ComputeDriver):
     capabilities = {
@@ -429,10 +426,7 @@ class LibvirtDriver(driver.ComputeDriver):
             self.virtapi,
             host=self._host)
 
-        support_vhostuser_mq = self._host.has_min_version(
-                                          MIN_LIBVIRT_VHOSTUSER_MQ)
-        self.vif_driver = libvirt_vif.LibvirtGenericVIFDriver(
-                                    support_vhostuser_mq=support_vhostuser_mq)
+        self.vif_driver = libvirt_vif.LibvirtGenericVIFDriver()
 
         self.volume_drivers = driver.driver_dict_from_config(
             self._get_volume_drivers(), self)
@@ -1228,7 +1222,8 @@ class LibvirtDriver(driver.ComputeDriver):
         self.firewall_driver.setup_basic_filtering(instance, [vif])
         cfg = self.vif_driver.get_config(instance, vif, image_meta,
                                          instance.flavor,
-                                         CONF.libvirt.virt_type)
+                                         CONF.libvirt.virt_type,
+                                         self._host)
         try:
             state = self._get_power_state(guest._domain)
             live = state in (power_state.RUNNING, power_state.PAUSED)
@@ -1243,7 +1238,7 @@ class LibvirtDriver(driver.ComputeDriver):
     def detach_interface(self, instance, vif):
         guest = self._host.get_guest(instance)
         cfg = self.vif_driver.get_config(instance, vif, None, instance.flavor,
-                                         CONF.libvirt.virt_type)
+                                         CONF.libvirt.virt_type, self._host)
         try:
             self.vif_driver.unplug(instance, vif)
             state = self._get_power_state(guest._domain)
@@ -3026,7 +3021,8 @@ class LibvirtDriver(driver.ComputeDriver):
                                                      vif,
                                                      image_meta,
                                                      instance.flavor,
-                                                     CONF.libvirt.virt_type)
+                                                     CONF.libvirt.virt_type,
+                                                     self._host)
                     LOG.debug('Attaching SR-IOV port %(port)s to %(dom)s',
                               {'port': vif, 'dom': guest.id},
                               instance=instance)
@@ -3056,7 +3052,8 @@ class LibvirtDriver(driver.ComputeDriver):
                                                      vif,
                                                      image_meta,
                                                      instance.flavor,
-                                                     CONF.libvirt.virt_type)
+                                                     CONF.libvirt.virt_type,
+                                                     self._host)
                     guest.detach_device(cfg, live=True)
 
     def _set_host_enabled(self, enabled,
@@ -4107,7 +4104,7 @@ class LibvirtDriver(driver.ComputeDriver):
         for vif in network_info:
             config = self.vif_driver.get_config(
                 instance, vif, image_meta,
-                flavor, virt_type)
+                flavor, virt_type, self._host)
             guest.add_device(config)
 
         consolepty = self._create_consoles(virt_type, guest, instance, flavor,

nova/virt/libvirt/host.py
@@ -551,6 +551,7 @@ class Host(object):
         libvirt.registerErrorHandler(self._libvirt_error_handler, None)
         libvirt.virEventRegisterDefaultImpl()
         self._init_events()
+        self._initialized = True
 
     def _version_check(self, lv_ver=None, hv_ver=None, hv_type=None,
                        op=operator.lt):

nova/virt/libvirt/vif.py
@@ -48,6 +48,9 @@ CONF.import_opt('use_ipv6', 'nova.netconf')
 
 DEV_PREFIX_ETH = 'eth'
 
+# vhostuser queues support
+MIN_LIBVIRT_VHOSTUSER_MQ = (1, 2, 17)
+
 
 def is_vif_model_valid_for_virt(virt_type, vif_model):
     valid_models = {
@@ -83,9 +86,6 @@ def is_vif_model_valid_for_virt(virt_type, vif_model):
 
 class LibvirtGenericVIFDriver(object):
     """Generic VIF driver for libvirt networking."""
-
-    def __init__(self, support_vhostuser_mq=False):
-        self.support_vhostuser_mq = support_vhostuser_mq
 
     def _normalize_vif_type(self, vif_type):
         return vif_type.replace('2.1q', '2q')
@@ -179,7 +179,7 @@ class LibvirtGenericVIFDriver(object):
         return False
 
     def get_config_bridge(self, instance, vif, image_meta,
-                          inst_type, virt_type):
+                          inst_type, virt_type, host):
         """Get VIF configurations for bridge type."""
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
@@ -197,7 +197,7 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_ovs_bridge(self, instance, vif, image_meta,
-                              inst_type, virt_type):
+                              inst_type, virt_type, host):
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
 
@@ -211,37 +211,40 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_ovs_hybrid(self, instance, vif, image_meta,
-                              inst_type, virt_type):
+                              inst_type, virt_type, host):
         newvif = copy.deepcopy(vif)
         newvif['network']['bridge'] = self.get_br_name(vif['id'])
         return self.get_config_bridge(instance, newvif, image_meta,
-                                      inst_type, virt_type)
+                                      inst_type, virt_type, host)
 
     def get_config_ovs(self, instance, vif, image_meta,
-                       inst_type, virt_type):
+                       inst_type, virt_type, host):
         if self.get_firewall_required(vif) or vif.is_hybrid_plug_enabled():
             return self.get_config_ovs_hybrid(instance, vif,
                                               image_meta,
                                               inst_type,
-                                              virt_type)
+                                              virt_type,
+                                              host)
         else:
             return self.get_config_ovs_bridge(instance, vif,
                                               image_meta,
                                               inst_type,
-                                              virt_type)
+                                              virt_type,
+                                              host)
 
     def get_config_ivs_hybrid(self, instance, vif, image_meta,
-                              inst_type, virt_type):
+                              inst_type, virt_type, host):
         newvif = copy.deepcopy(vif)
         newvif['network']['bridge'] = self.get_br_name(vif['id'])
         return self.get_config_bridge(instance,
                                       newvif,
                                       image_meta,
                                       inst_type,
-                                      virt_type)
+                                      virt_type,
+                                      host)
 
     def get_config_ivs_ethernet(self, instance, vif, image_meta,
-                                inst_type, virt_type):
+                                inst_type, virt_type, host):
         conf = self.get_base_config(instance,
                                     vif,
                                     image_meta,
@@ -254,20 +257,22 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_ivs(self, instance, vif, image_meta,
-                       inst_type, virt_type):
+                       inst_type, virt_type, host):
         if self.get_firewall_required(vif) or vif.is_hybrid_plug_enabled():
             return self.get_config_ivs_hybrid(instance, vif,
                                               image_meta,
                                               inst_type,
-                                              virt_type)
+                                              virt_type,
+                                              host)
         else:
             return self.get_config_ivs_ethernet(instance, vif,
                                                 image_meta,
                                                 inst_type,
-                                                virt_type)
+                                                virt_type,
+                                                host)
 
     def get_config_802qbg(self, instance, vif, image_meta,
-                          inst_type, virt_type):
+                          inst_type, virt_type, host):
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
 
@@ -284,7 +289,7 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_802qbh(self, instance, vif, image_meta,
-                          inst_type, virt_type):
+                          inst_type, virt_type, host):
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
 
@@ -303,7 +308,7 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_hw_veb(self, instance, vif, image_meta,
-                            inst_type, virt_type):
+                            inst_type, virt_type, host):
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
 
@@ -322,7 +327,7 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_macvtap(self, instance, vif, image_meta,
-                           inst_type, virt_type):
+                           inst_type, virt_type, host):
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
 
@@ -353,7 +358,7 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_iovisor(self, instance, vif, image_meta,
-                           inst_type, virt_type):
+                           inst_type, virt_type, host):
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
 
@@ -365,7 +370,7 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_midonet(self, instance, vif, image_meta,
-                           inst_type, virt_type):
+                           inst_type, virt_type, host):
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
 
@@ -375,7 +380,7 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_tap(self, instance, vif, image_meta,
-                       inst_type, virt_type):
+                       inst_type, virt_type, host):
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
 
@@ -385,7 +390,7 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_mlnx_direct(self, instance, vif, image_meta,
-                               inst_type, virt_type):
+                               inst_type, virt_type, host):
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
 
@@ -397,7 +402,7 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_vhostuser(self, instance, vif, image_meta,
-                              inst_type, virt_type):
+                              inst_type, virt_type, host):
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
         vif_details = vif['details']
@@ -410,7 +415,7 @@ class LibvirtGenericVIFDriver(object):
         designer.set_vif_host_backend_vhostuser_config(conf, mode, sock_path)
         # (vladikr) Not setting up driver and queues for vhostuser
         # as queues are not supported in Libvirt until version 1.2.17
-        if not self.support_vhostuser_mq:
+        if not host.has_min_version(MIN_LIBVIRT_VHOSTUSER_MQ):
             LOG.debug('Queues are not a vhostuser supported feature.')
             conf.driver_name = None
             conf.vhost_queues = None
@@ -418,14 +423,14 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config_ib_hostdev(self, instance, vif, image_meta,
-                              inst_type, virt_type):
+                              inst_type, virt_type, host):
         conf = vconfig.LibvirtConfigGuestHostdevPCI()
         pci_slot = vif['profile']['pci_slot']
         designer.set_vif_host_backend_ib_hostdev_config(conf, pci_slot)
         return conf
 
     def get_config_vrouter(self, instance, vif, image_meta,
-                           inst_type, virt_type):
+                           inst_type, virt_type, host):
         conf = self.get_base_config(instance, vif, image_meta,
                                     inst_type, virt_type)
         dev = self.get_vif_devname(vif)
@@ -435,7 +440,7 @@ class LibvirtGenericVIFDriver(object):
         return conf
 
     def get_config(self, instance, vif, image_meta,
-                   inst_type, virt_type):
+                   inst_type, virt_type, host):
         vif_type = vif['type']
 
         LOG.debug('vif_type=%(vif_type)s instance=%(instance)s '
@@ -453,7 +458,7 @@ class LibvirtGenericVIFDriver(object):
             raise exception.NovaException(
                 _("Unexpected vif_type=%s") % vif_type)
         return func(instance, vif, image_meta,
-                    inst_type, virt_type)
+                    inst_type, virt_type, host)
 
     def plug_bridge(self, instance, vif):
         """Ensure that the bridge exists, and add VIF to it."""

nova/tests/unit/virt/libvirt/test_driver.py
@@ -536,7 +536,6 @@ class LibvirtConnTestCase(test.NoDBTestCase):
                        imagebackend.Image._get_driver_format)
 
         self.useFixture(fakelibvirt.FakeLibvirtFixture())
-        mock.patch.object(host.Host, "has_min_version", return_value=True)
         self.test_instance = _create_test_instance()
         self.image_service = nova.tests.unit.image.fake.stub_out_image_service(
                 self.stubs)
@@ -5028,8 +5027,7 @@ class LibvirtConnTestCase(test.NoDBTestCase):
 
     @mock.patch.object(libvirt_driver.LibvirtDriver,
                        "_get_host_sysinfo_serial_hardware",)
-    @mock.patch.object(host.Host, "has_min_version", return_value=True)
-    def _check_xml_and_uri(self, instance, mock_ver, mock_serial,
+    def _check_xml_and_uri(self, instance, mock_serial,
                            expect_ramdisk=False, expect_kernel=False,
                            rescue=None, expect_xen_hvm=False, xen_only=False):
         mock_serial.return_value = "cef19ce0-0ca2-11df-855d-b19fbce37686"
@@ -7843,10 +7841,9 @@ class LibvirtConnTestCase(test.NoDBTestCase):
         ip = drvr.get_host_ip_addr()
         self.assertEqual(ip, CONF.my_ip)
 
-    @mock.patch.object(host.Host, "has_min_version", return_value=True)
     @mock.patch.object(libvirt_driver.LOG, 'warn')
     @mock.patch('nova.compute.utils.get_machine_ips')
-    def test_get_host_ip_addr_failure(self, mock_ips, mock_log, mock_ver):
+    def test_get_host_ip_addr_failure(self, mock_ips, mock_log):
         mock_ips.return_value = ['8.8.8.8', '75.75.75.75']
         drvr = libvirt_driver.LibvirtDriver(fake.FakeVirtAPI(), False)
         drvr.get_host_ip_addr()
@@ -7856,8 +7853,7 @@ class LibvirtConnTestCase(test.NoDBTestCase):
                                          {'ifaces': '8.8.8.8, 75.75.75.75',
                                           'my_ip': mock.ANY})
 
-    @mock.patch.object(host.Host, "has_min_version", return_value=True)
-    def test_conn_event_handler(self, mock_ver):
+    def test_conn_event_handler(self):
         self.mox.UnsetStubs()
         drvr = libvirt_driver.LibvirtDriver(fake.FakeVirtAPI(), True)
         service_mock = mock.MagicMock()
@@ -7881,8 +7877,7 @@ class LibvirtConnTestCase(test.NoDBTestCase):
                               "wibble")
             self.assertTrue(service_mock.disabled)
 
-    @mock.patch.object(host.Host, "has_min_version", return_value=True)
-    def test_command_with_broken_connection(self, mock_ver):
+    def test_command_with_broken_connection(self):
         self.mox.UnsetStubs()
         drvr = libvirt_driver.LibvirtDriver(fake.FakeVirtAPI(), True)
         service_mock = mock.MagicMock()
@@ -7908,8 +7903,7 @@ class LibvirtConnTestCase(test.NoDBTestCase):
                               drvr.get_num_instances)
             self.assertTrue(service_mock.disabled)
 
-    @mock.patch.object(host.Host, "has_min_version", return_value=True)
-    def test_service_resume_after_broken_connection(self, mver):
+    def test_service_resume_after_broken_connection(self):
         self.mox.UnsetStubs()
         drvr = libvirt_driver.LibvirtDriver(fake.FakeVirtAPI(), False)
         service_mock = mock.MagicMock()
@@ -10463,12 +10457,14 @@ class LibvirtConnTestCase(test.NoDBTestCase):
         expected = drvr.vif_driver.get_config(instance, network_info[0],
                                               fake_image_meta,
                                               instance.get_flavor(),
-                                              CONF.libvirt.virt_type)
+                                              CONF.libvirt.virt_type,
+                                              drvr._host)
         self.mox.StubOutWithMock(drvr.vif_driver, 'get_config')
         drvr.vif_driver.get_config(instance, network_info[0],
                                    fake_image_meta,
                                    mox.IsA(objects.Flavor),
-                                   CONF.libvirt.virt_type).\
+                                   CONF.libvirt.virt_type,
+                                   drvr._host).\
                                    AndReturn(expected)
 
         self.mox.ReplayAll()
@@ -12534,7 +12530,7 @@ class LibvirtDriverTestCase(test.NoDBTestCase):
             fake_image_meta = None
         expected = self.drvr.vif_driver.get_config(
             instance, network_info[0], fake_image_meta, instance.flavor,
-            CONF.libvirt.virt_type)
+            CONF.libvirt.virt_type, self.drvr._host)
 
         self.mox.StubOutWithMock(self.drvr.vif_driver,
                                  'get_config')
@@ -12542,7 +12538,8 @@ class LibvirtDriverTestCase(test.NoDBTestCase):
             instance, network_info[0],
             fake_image_meta,
             mox.IsA(objects.Flavor),
-            CONF.libvirt.virt_type).AndReturn(expected)
+            CONF.libvirt.virt_type,
+            self.drvr._host).AndReturn(expected)
         domain.info().AndReturn([power_state])
         if method == 'attach_interface':
             domain.attachDeviceFlags(expected.to_xml(), flags=expected_flags)
@@ -13193,8 +13190,7 @@ class LibvirtNonblockingTestCase(test.NoDBTestCase):
         drvr.set_host_enabled = mock.Mock()
         jsonutils.to_primitive(drvr._conn, convert_instances=True)
 
-    @mock.patch.object(host.Host, 'has_min_version', return_value=True)
-    def test_tpool_execute_calls_libvirt(self, mock_ver):
+    def test_tpool_execute_calls_libvirt(self):
         conn = fakelibvirt.virConnect()
         conn.is_expected = True
 

nova/tests/unit/virt/libvirt/test_vif.py
@@ -28,6 +28,7 @@ from nova.pci import utils as pci_utils
 from nova import test
 from nova import utils
 from nova.virt.libvirt import config as vconfig
+from nova.virt.libvirt import host
 from nova.virt.libvirt import vif
 
 CONF = cfg.CONF
@@ -448,8 +449,10 @@ class LibvirtVifTestCase(test.NoDBTestCase):
                                 id=2, disabled=False, rxtx_factor=1.0)
 
         conf = self._get_conf()
+        hostimpl = host.Host("qemu:///system")
         nic = driver.get_config(self.instance, vif, image_meta,
-                                flavor, CONF.libvirt.virt_type)
+                                flavor, CONF.libvirt.virt_type,
+                                hostimpl)
         conf.add_device(nic)
         return conf.to_xml()
 