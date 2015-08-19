388ef726a048bf5fde49a0088f930ffea8e1b525
ghanshyam <ghanshyam.mann@nectechnologies.in>
2015-08-19 17:30:59 +0900

    Move "versions" functional tests in v2.1 tests
    This commit moves "versions" functional tests in v2.1 tests which are
    under /v3 directory and run those for v2 and v2.1.
    
    There was no tests for v2.1 version so this patch adds
    tests for v2.1 version request.
    
    Partially implements blueprint test-collapse-v2-and-v21
    
    Change-Id: Ibd19c5f96679864db4c4c83b8981bac839feafd2

---
+++ nova/tests/functional/api_sample_tests/test_versions.py
@@ -0,0 +1,37 @@
+# Copyright 2012 Nebula, Inc.
+# Copyright 2013 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nova.tests.functional.api_sample_tests import api_sample_base
+
+
+class VersionsSampleJsonTest(api_sample_base.ApiSampleTestBaseV3):
+    sample_dir = 'versions'
+    scenarios = [('', {'_test': ''})]
+    _api_version = 'v2'
+
+    def test_versions_get(self):
+        response = self._do_get('', strip_version=True)
+        subs = self._get_regexes()
+        self._verify_response('versions-get-resp', subs, response, 200)
+
+    def test_versions_get_v2(self):
+        response = self._do_get('/v2', strip_version=True)
+        subs = self._get_regexes()
+        self._verify_response('v2-version-get-resp', subs, response, 200)
+
+    def test_versions_get_v21(self):
+        response = self._do_get('/v2.1', strip_version=True)
+        subs = self._get_regexes()
+        self._verify_response('v21-version-get-resp', subs, response, 200)
