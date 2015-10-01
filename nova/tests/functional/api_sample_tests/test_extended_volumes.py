bbd6fbfbf53f4d0c30af317f8ef0a6564ff8edc0
Dan Smith <dansmith@redhat.com>
2015-10-01 15:23:15 -0700

    Remove orphaned code related to extended_volumes
    This was orphaned code in some other patch a ways back and
    is no longer necessary, but confusing in terms of relevance to
    the existing code.
    
    Change-Id: I62320663628bf05654ef56209628814917db26c1
    Co-Authored-By: Diana Clarke <diana.joan.clarke@gmail.com>

--- nova/tests/functional/api_sample_tests/test_extended_volumes.py
+++ nova/tests/functional/api_sample_tests/test_extended_volumes.py
@@ -15,12 +15,9 @@
 
 from oslo_config import cfg
 
-from nova.compute import api as compute_api
 from nova import db
 from nova.tests.functional.api_sample_tests import test_servers
 from nova.tests.unit.api.openstack import fakes
-from nova.tests.unit import fake_block_device
-from nova.tests.unit import fake_instance
 
 CONF = cfg.CONF
 CONF.import_opt('osapi_compute_extension',
@@ -46,36 +43,6 @@ class ExtendedVolumesSampleJsonTests(test_servers.ServersSampleBase):
             'nova.api.openstack.compute.contrib.extended_ips.'
                       'Extended_ips')
         return f
-
-    def _stub_compute_api_get_instance_bdms(self, server_id):
-
-        def fake_bdms_get_all_by_instance(context, instance_uuid,
-                                          use_slave=False):
-            bdms = [
-                fake_block_device.FakeDbBlockDeviceDict(
-                {'id': 1, 'volume_id': 'a26887c6-c47b-4654-abb5-dfadf7d3f803',
-                'instance_uuid': server_id, 'source_type': 'volume',
-                'destination_type': 'volume', 'device_name': '/dev/sdd'}),
-                fake_block_device.FakeDbBlockDeviceDict(
-                {'id': 2, 'volume_id': 'a26887c6-c47b-4654-abb5-dfadf7d3f804',
-                'instance_uuid': server_id, 'source_type': 'volume',
-                'destination_type': 'volume', 'device_name': '/dev/sdc'})
-            ]
-            return bdms
-
-        self.stubs.Set(db, 'block_device_mapping_get_all_by_instance',
-                       fake_bdms_get_all_by_instance)
-
-    def _stub_compute_api_get(self):
-        def fake_compute_api_get(self, context, instance_id, **kwargs):
-            want_objects = kwargs.get('want_objects')
-            if want_objects:
-                return fake_instance.fake_instance_obj(
-                        context, **{'uuid': instance_id})
-            else:
-                return {'uuid': instance_id}
-
-        self.stubs.Set(compute_api.API, 'get', fake_compute_api_get)
 
     def test_show(self):
         uuid = self._post_server()
