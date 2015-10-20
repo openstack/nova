14eb1d7483afce9ca1e5d15913fb69bab0b08e7c
ghanshyam <ghanshyam.mann@nectechnologies.in>
2015-10-20 14:34:43 +0900

    Enable all extension for image API sample tests
    As extensions options has been deprecated, API sample tests
    should run against all extension enable.
    
    This commit enables all extensions for Image API tests and remove its specific
    extensions tests and sample files.
    Also adds some basic hack to merge all APIs tests for all extension.
    
    Partially implements blueprint api-sample-tests-with-all-extensions
    
    Change-Id: Ib603fbccfdc09b25d4c69c872d79e4cafc30fff5

--- nova/tests/functional/api_samples_test_base.py
+++ nova/tests/functional/api_samples_test_base.py
@@ -51,7 +51,12 @@ class ApiSampleTestBase(integrated_helpers._IntegratedTestBase):
     def _get_sample_path(cls, name, dirname, suffix='', api_version=None):
         parts = [dirname]
         parts.append('api_samples')
-        if cls.all_extensions:
+        # TODO(gmann): Once all tests gets merged for all extension
+        # then we need to have a simple logic here to select sample file
+        # directory which will be based on cls.sample_dir and api_version.
+        # All other things will go away from here. Currently hacking this
+        # till we merge every extensions tests.
+        if cls.all_extensions and not cls.sample_dir:
             parts.append('all_extensions')
         # Note(gmann): if _use_common_server_api_samples is set to True
         # then common server sample files present in 'servers' directory
