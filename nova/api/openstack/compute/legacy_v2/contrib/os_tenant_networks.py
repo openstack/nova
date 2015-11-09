d8474e044e820f29f70641b8fd5fd590750441a3
ChangBo Guo(gcb) <eric.guo@easystack.cn>
2015-11-09 19:56:48 +0800

    use_neutron_default_nets: StrOpt ->BoolOpt
    Config option use_neutron_default_nets is StrOpt with
    value 'True' or 'False'.  But current method _test_network_index
    uses it as boolean type, this lead to can't pass when comparing it
    with string type 'True', it doesn't test properly. This commit
    makes use_neutron_default_nets as BoolOpt.
    
    DocImpact: This option is now a BoolOpt and documentation should
    be updated accordingly.
    
    Change-Id: I19a57db073359a9e58a16cd0de39d39aa95d2aa5

--- nova/api/openstack/compute/legacy_v2/contrib/os_tenant_networks.py
+++ nova/api/openstack/compute/legacy_v2/contrib/os_tenant_networks.py
@@ -53,7 +53,7 @@ class NetworkController(object):
 
     def _refresh_default_networks(self):
         self._default_networks = []
-        if CONF.use_neutron_default_nets == "True":
+        if CONF.use_neutron_default_nets:
             try:
                 self._default_networks = self._get_default_networks()
             except Exception:
