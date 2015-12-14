2dc5d9f8632c83c9bc7ee6871c6631edf69a5570
Sean Dague <sean@dague.net>
2015-12-14 15:14:10 -0500

    deprecate run_tests.sh
    This removes the run_tests.sh script, replacing it with documentation
    on how to use tox itself for testing nova things, as well as links to
    learn more about both that, and how to not use tox at all if you
    really care.
    
    Why should we do this?
    
    New people coming to the project see a file called run_tests.sh and
    use it, because why would you have a file like that in the top of your
    directory if that wasn't what you were supposed to use. However it's
    not what we want anyone using for upstream contributions. One of the
    critical differences between it and tox is that it lacks ``python
    hashseed randomization``. This runs every test run with a different
    forced hashseed, that exposes bugs in tests early. Test contributors
    need to be running that on their tests before submission to shake out
    these errors early.
    
    In addition, because we have multiple ways in tree to run tests,
    helping people debug their test failures means we have to start at a
    step -1 of "how are you running tests". This adds load for helping new
    folks.
    
    What about people that want to run tests with specific system or
    virtual env python setups for pre deployment testing?
    
    Good news! Our tests comply to python standard testing interfaces, so
    are runnable by any test runner (we recommend ``testr`` because that's
    what tox calls under the covers). You can just run ``testr run`` at
    the top level from your virtuenv or system of choice. And life is
    good. The constraints we need / want for inbound tests don't apply in
    these cases.
    
    This removes a bit of confusion for new contributors, ensures that new
    test contributions receive correct local testing before they are run
    in our gate, and only require a very minor change in behavior for
    downstream test environments.
    
    Change-Id: I05d85a15c0db2a573abb82cb2f7dc3eeaeac0b41

--- run_tests.sh
+++ run_tests.sh
@@ -2,251 +2,41 @@
 
 set -eu
 
-function usage {
-  echo "Usage: $0 [OPTION]..."
-  echo "Run Nova's test suite(s)"
-  echo ""
-  echo "  -V, --virtual-env           Always use virtualenv.  Install automatically if not present"
-  echo "  -N, --no-virtual-env        Don't use virtualenv.  Run tests in local environment"
-  echo "  -s, --no-site-packages      Isolate the virtualenv from the global Python environment"
-  echo "  -f, --force                 Force a clean re-build of the virtual environment. Useful when dependencies have been added."
-  echo "  -u, --update                Update the virtual environment with any newer package versions"
-  echo "  -p, --pep8                  Just run PEP8 and HACKING compliance check"
-  echo "  -8, --pep8-only-changed     Just run PEP8 and HACKING compliance check on files changed since HEAD~1"
-  echo "  -P, --no-pep8               Don't run static code checks"
-  echo "  -c, --coverage              Generate coverage report"
-  echo "  -d, --debug                 Run tests with testtools instead of testr. This allows you to use the debugger."
-  echo "  -h, --help                  Print this usage message"
-  echo "  --hide-elapsed              Don't print the elapsed time for each test along with slow test list"
-  echo "  --virtual-env-path <path>   Location of the virtualenv directory"
-  echo "                               Default: \$(pwd)"
-  echo "  --virtual-env-name <name>   Name of the virtualenv directory"
-  echo "                               Default: .venv"
-  echo "  --tools-path <dir>          Location of the tools directory"
-  echo "                               Default: \$(pwd)"
-  echo "  --concurrency <concurrency> How many processes to use when running the tests. A value of 0 autodetects concurrency from your CPU count"
-  echo "                               Default: 0"
-  echo ""
-  echo "Note: with no options specified, the script will try to run the tests in a virtual environment,"
-  echo "      If no virtualenv is found, the script will ask if you would like to create one.  If you "
-  echo "      prefer to run tests NOT in a virtual environment, simply pass the -N option."
-  exit
-}
+cat <<EOF
+run_tests.sh is deprecated and this script will be removed
+before the Mitaka release. All tests should be run through
+tox.
 
-function process_options {
-  i=1
-  while [ $i -le $# ]; do
-    case "${!i}" in
-      -h|--help) usage;;
-      -V|--virtual-env) always_venv=1; never_venv=0;;
-      -N|--no-virtual-env) always_venv=0; never_venv=1;;
-      -s|--no-site-packages) no_site_packages=1;;
-      -f|--force) force=1;;
-      -u|--update) update=1;;
-      -p|--pep8) just_pep8=1;;
-      -8|--pep8-only-changed) just_pep8_changed=1;;
-      -P|--no-pep8) no_pep8=1;;
-      -c|--coverage) coverage=1;;
-      -d|--debug) debug=1;;
-      --virtual-env-path)
-        (( i++ ))
-        venv_path=${!i}
-        ;;
-      --virtual-env-name)
-        (( i++ ))
-        venv_dir=${!i}
-        ;;
-      --tools-path)
-        (( i++ ))
-        tools_path=${!i}
-        ;;
-      --concurrency)
-        (( i++ ))
-        concurrency=${!i}
-        ;;
-      -*) testropts="$testropts ${!i}";;
-      *) testrargs="$testrargs ${!i}"
-    esac
-    (( i++ ))
-  done
-}
+To run style checks:
 
-tool_path=${tools_path:-$(pwd)}
-venv_path=${venv_path:-$(pwd)}
-venv_dir=${venv_name:-.venv}
-with_venv=tools/with_venv.sh
-always_venv=0
-never_venv=0
-force=0
-no_site_packages=0
-installvenvopts=
-testrargs=
-testropts=
-wrapper=""
-just_pep8=0
-just_pep8_changed=0
-no_pep8=0
-coverage=0
-debug=0
-update=0
-concurrency=0
+ tox -e pep8
 
-LANG=en_US.UTF-8
-LANGUAGE=en_US:en
-LC_ALL=C
+To run python 2.7 unit tests
 
-process_options $@
-# Make our paths available to other scripts we call
-export venv_path
-export venv_dir
-export venv_name
-export tools_dir
-export venv=${venv_path}/${venv_dir}
+ tox -e py27
 
-if [ $no_site_packages -eq 1 ]; then
-  installvenvopts="--no-site-packages"
-fi
+To run functional tests
 
-function run_tests {
-  # Cleanup *pyc
-  ${wrapper} find . -type f -name "*.pyc" -delete
+ tox -e functional
 
-  if [ $debug -eq 1 ]; then
-    if [ "$testropts" = "" ] && [ "$testrargs" = "" ]; then
-      # Default to running all tests if specific test is not
-      # provided.
-      testrargs="discover ./nova/tests/unit"
-    fi
-    ${wrapper} python -m testtools.run $testropts $testrargs
+To run a subset of any of these tests:
 
-    # Short circuit because all of the testr and coverage stuff
-    # below does not make sense when running testtools.run for
-    # debugging purposes.
-    return $?
-  fi
+ tox -e py27 someregex
 
-  if [ $coverage -eq 1 ]; then
-    TESTRTESTS="$TESTRTESTS --coverage"
-  else
-    TESTRTESTS="$TESTRTESTS"
-  fi
+ i.e.: tox -e py27 test_servers
 
-  # Just run the test suites in current environment
-  set +e
-  testrargs=`echo "$testrargs" | sed -e's/^\s*\(.*\)\s*$/\1/'`
-  TESTRTESTS="$TESTRTESTS --testr-args='--subunit --concurrency $concurrency $testropts $testrargs'"
-  if [ setup.cfg -nt nova.egg-info/entry_points.txt ]
-  then
-    ${wrapper} python setup.py egg_info
-  fi
-  echo "Running \`${wrapper} $TESTRTESTS\`"
-  if ${wrapper} which subunit-2to1 >/dev/null 2>&1
-  then
-    # subunit-2to1 is present, testr subunit stream should be in version 2
-    # format. Convert to version one before colorizing.
-    bash -c "${wrapper} $TESTRTESTS | ${wrapper} subunit-2to1 | ${wrapper} tools/colorizer.py"
-  else
-    bash -c "${wrapper} $TESTRTESTS | ${wrapper} tools/colorizer.py"
-  fi
-  RESULT=$?
-  set -e
+Additional tox targets are available in tox.ini. For more information
+see:
+http://docs.openstack.org/infra/manual/python.html#running-python-unit-tests
 
-  copy_subunit_log
+NOTE: if you really really don't want to use tox to run tests, you
+can instead use:
 
-  if [ $coverage -eq 1 ]; then
-    echo "Generating coverage report in covhtml/"
-    # Don't compute coverage for common code, which is tested elsewhere
-    ${wrapper} coverage combine
-    ${wrapper} coverage html --include='nova/*' --omit='nova/openstack/common/*' -d covhtml -i
-  fi
+ testr run
 
-  return $RESULT
-}
+Documentation on using testr directly can be found at
+http://testrepository.readthedocs.org/en/latest/MANUAL.html
 
-function copy_subunit_log {
-  LOGNAME=`cat .testrepository/next-stream`
-  LOGNAME=$(($LOGNAME - 1))
-  LOGNAME=".testrepository/${LOGNAME}"
-  cp $LOGNAME subunit.log
-}
+EOF
 
-function warn_on_flake8_without_venv {
-  if [ $never_venv -eq 1 ]; then
-    echo "**WARNING**:"
-    echo "Running flake8 without virtual env may miss OpenStack HACKING detection"
-  fi
-}
-
-function run_pep8 {
-  echo "Running flake8 ..."
-  warn_on_flake8_without_venv
-  bash -c "${wrapper} flake8"
-}
-
-
-TESTRTESTS="python setup.py testr"
-
-if [ $never_venv -eq 0 ]
-then
-  # Remove the virtual environment if --force used
-  if [ $force -eq 1 ]; then
-    echo "Cleaning virtualenv..."
-    rm -rf ${venv}
-  fi
-  if [ $update -eq 1 ]; then
-      echo "Updating virtualenv..."
-      python tools/install_venv.py $installvenvopts
-  fi
-  if [ -e ${venv} ]; then
-    wrapper="${with_venv}"
-  else
-    if [ $always_venv -eq 1 ]; then
-      # Automatically install the virtualenv
-      python tools/install_venv.py $installvenvopts
-      wrapper="${with_venv}"
-    else
-      echo -e "No virtual environment found...create one? (Y/n) \c"
-      read use_ve
-      if [ "x$use_ve" = "xY" -o "x$use_ve" = "x" -o "x$use_ve" = "xy" ]; then
-        # Install the virtualenv and run the test suite in it
-        python tools/install_venv.py $installvenvopts
-        wrapper=${with_venv}
-      fi
-    fi
-  fi
-fi
-
-# Delete old coverage data from previous runs
-if [ $coverage -eq 1 ]; then
-    ${wrapper} coverage erase
-fi
-
-if [ $just_pep8 -eq 1 ]; then
-    run_pep8
-    exit
-fi
-
-if [ $just_pep8_changed -eq 1 ]; then
-    # NOTE(gilliard) We want use flake8 to check the entirety of every file that has
-    # a change in it. Unfortunately the --filenames argument to flake8 only accepts
-    # file *names* and there are no files named (eg) "nova/compute/manager.py".  The
-    # --diff argument behaves surprisingly as well, because although you feed it a
-    # diff, it actually checks the file on disk anyway.
-    files=$(git diff --name-only HEAD~1 | tr '\n' ' ')
-    echo "Running flake8 on ${files}"
-    warn_on_flake8_without_venv
-    bash -c "diff -u --from-file /dev/null ${files} | ${wrapper} flake8 --diff"
-    exit
-fi
-
-run_tests
-
-# NOTE(sirp): we only want to run pep8 when we're running the full-test suite,
-# not when we're running tests individually. To handle this, we need to
-# distinguish between options (testropts), which begin with a '-', and
-# arguments (testrargs).
-if [ -z "$testrargs" ]; then
-  if [ $no_pep8 -eq 0 ]; then
-    run_pep8
-  fi
-fi
+exit 1
