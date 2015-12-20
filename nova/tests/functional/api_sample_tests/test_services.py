5d394c710cf1ec860fc8cdd5cb5569d057d8a04f
Hans Lindgren <hanlind@kth.se>
2015-12-20 10:43:46 +0100

    Use TimeFixture from oslo_utils in functional tests
    Use TimeFixture to override time like we do in other places instead of
    stubbing out utcnow().
    
    Change-Id: I3da31259912bca0d823cd0606a3e32a04f1563de

--- nova/tests/functional/api_sample_tests/test_services.py
+++ nova/tests/functional/api_sample_tests/test_services.py
@@ -14,7 +14,7 @@
 #    under the License.
 
 from oslo_config import cfg
-from oslo_utils import timeutils
+from oslo_utils import fixture as utils_fixture
 
 from nova.tests.functional.api_sample_tests import api_sample_base
 from nova.tests.unit.api.openstack.compute import test_services
@@ -45,14 +45,11 @@ class ServicesJsonTest(api_sample_base.ApiSampleTestBaseV21):
         super(ServicesJsonTest, self).setUp()
         self.stub_out("nova.db.service_get_all",
                       test_services.fake_db_api_service_get_all)
-        self.stub_out("oslo_utils.timeutils.utcnow", test_services.fake_utcnow)
-        self.stub_out("oslo_utils.timeutils.utcnow_ts",
-                      test_services.fake_utcnow_ts)
         self.stub_out("nova.db.service_get_by_host_and_binary",
                       test_services.fake_service_get_by_host_binary)
         self.stub_out("nova.db.service_update",
                       test_services.fake_service_update)
-        self.addCleanup(timeutils.clear_time_override)
+        self.useFixture(utils_fixture.TimeFixture(test_services.fake_utcnow()))
 
     def test_services_list(self):
         """Return a list of all agent builds."""
