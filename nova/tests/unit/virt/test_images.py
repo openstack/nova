9a4ecfd96dad32fd4726c46dc6d89e956f1f2a29
Chris Dent <cdent@anticdent.org>
2016-01-11 12:05:00 +0000

    Propagate qemu-img errors to compute manager
    When qemu-img is called with oslo_concurrency.process_utils.execute
    the ProcessExecutionError was raised when qemu-img either fails to
    execute or has a non-zero exit code. This error did not propagate
    up to the compute manager with any meaningful information meaning
    that if an instance build fails the error message is the generic
    "There are not enough hosts available".
    
    This change captures ProcessExecutionError and re-raises the
    exception as either InvalidDiskInfo (in qemu_img_info) or
    ImageUnacceptable (in convert_image and fetch_to_raw) and makes the
    manager accept this as a cause for a BuildAbortException on the
    logic that if the image is bad, things are dire, let's bail.
    
    Based on the code in qemu_img_info it appears there was a
    misunderstanding of how process_utils.execute behaves so it seems
    likely this problem is present elsewhere in the code. This change
    attempts to only address the issue as it shows up on the new
    instance path described in the related bug.
    
    Change-Id: I4fa1c258db58c70dfbf0178b7bb13978fda3a11f
    Closes-Bug: #1436166

--- nova/tests/unit/virt/test_images.py
+++ nova/tests/unit/virt/test_images.py
@@ -31,7 +31,7 @@ class QemuTestCase(test.NoDBTestCase):
 
     @mock.patch.object(os.path, 'exists', return_value=True)
     def test_qemu_info_with_errors(self, path_exists):
-        self.assertRaises(processutils.ProcessExecutionError,
+        self.assertRaises(exception.InvalidDiskInfo,
                           images.qemu_img_info,
                           '/fake/path')
 
@@ -43,3 +43,26 @@ class QemuTestCase(test.NoDBTestCase):
         image_info = images.qemu_img_info('/fake/path')
         self.assertTrue(image_info)
         self.assertTrue(str(image_info))
+
+    @mock.patch.object(utils, 'execute',
+                       side_effect=processutils.ProcessExecutionError)
+    def test_convert_image_with_errors(self, mocked_execute):
+        self.assertRaises(exception.ImageUnacceptable,
+                          images.convert_image,
+                          '/path/that/does/not/exist',
+                          '/other/path/that/does/not/exist',
+                          'qcow2',
+                          'raw')
+
+    @mock.patch.object(images, 'convert_image',
+                       side_effect=exception.ImageUnacceptable)
+    @mock.patch.object(images, 'qemu_img_info')
+    @mock.patch.object(images, 'fetch')
+    def test_fetch_to_raw_errors(self, convert_image, qemu_img_info, fetch):
+        qemu_img_info.backing_file = None
+        qemu_img_info.file_format = 'qcow2'
+        qemu_img_info.virtual_size = 20
+        self.assertRaisesRegex(exception.ImageUnacceptable,
+                               'Image href123 is unacceptable.*',
+                               images.fetch_to_raw,
+                               None, 'href123', '/no/path', None, None)
