7451862d75fabdbead567db9c18aa02c45026162
Esra Celik <celik.esra@tubitak.gov.tr>
2016-01-13 14:17:39 +0200

    Improve the help text for configdrive options
    Implements blueprint centralize-config-options-newton
    Change-Id: If36261eae448aec2d017829276c0c072e37773f3

--- nova/conf/configdrive.py
+++ nova/conf/configdrive.py
@@ -19,18 +19,88 @@ config_drive_format_opt = cfg.StrOpt('config_drive_format',
         choices=('iso9660', 'vfat'),
         help="""
 Configuration drive format
+
+Configuration drive format that will contain metadata attached to the
+instance when it boots.
+
+Possible values:
+
+* iso9660: A file system image standard that is widely supported across
+  operating systems. NOTE: Mind the libvirt bug
+  (https://bugs.launchpad.net/nova/+bug/1246201) - If your hypervisor
+  driver is libvirt, and you want live migrate to work without shared storage,
+  then use VFAT.
+* vfat: For legacy reasons, you can configure the configuration drive to
+  use VFAT format instead of ISO 9660.
+
+Related options:
+
+* This option is meaningful when one of the following alternatives occur:
+  1. force_config_drive option set to 'true'
+  2. the REST API call to create the instance contains an enable flag for
+     config drive option
+  3. the image used to create the instance requires a config drive,
+     this is defined by img_config_drive property for that image.
+* A compute node running Hyper-V hypervisor can be configured to attach
+  configuration drive as a CD drive. To attach the configuration drive as a CD
+  drive, set config_drive_cdrom option at hyperv section, to true.
 """)
 
 force_config_drive_opt = cfg.BoolOpt('force_config_drive',
         default=False,
         help="""
 Force injection to take place on a config drive
+
+When this option is set to true configuration drive functionality will be
+forced enabled by default, otherwise user can still enable configuration
+drives via the REST API or image metadata properties.
+
+Possible values:
+
+* True: Force to use of configuration drive regardless the user's input in the
+        REST API call.
+* False: Do not force use of configuration drive. Config drives can still be
+         enabled via the REST API or image metadata properties.
+
+Related options:
+
+* Use the 'mkisofs_cmd' flag to set the path where you install the
+  genisoimage program. If genisoimage is in same path as the
+  nova-compute service, you do not need to set this flag.
+* To use configuration drive with Hyper-V, you must set the
+  'mkisofs_cmd' value to the full path to an mkisofs.exe installation.
+  Additionally, you must set the qemu_img_cmd value in the hyperv
+  configuration section to the full path to an qemu-img command
+  installation.
 """)
+
 
 mkisofs_cmd_opt = cfg.StrOpt('mkisofs_cmd',
         default='genisoimage',
         help="""
-Name and optionally path of the tool used for ISO image creation
+Name or path of the tool used for ISO image creation
+
+Use the mkisofs_cmd flag to set the path where you install the genisoimage
+program. If genisoimage is on the system path, you do not need to change
+the default value.
+
+To use configuration drive with Hyper-V, you must set the mkisofs_cmd value
+to the full path to an mkisofs.exe installation. Additionally, you must set
+the qemu_img_cmd value in the hyperv configuration section to the full path
+to an qemu-img command installation.
+
+Possible values:
+
+* Name of the ISO image creator program, in case it is in the same directory
+  as the nova-compute service
+* Path to ISO image creator program
+
+Related options:
+
+* This option is meaningful when config drives are enabled.
+* To use configuration drive with Hyper-V, you must set the qemu_img_cmd
+  value in the hyperv configuration section to the full path to an qemu-img
+  command installation.
 """)
 
 ALL_OPTS = [config_drive_format_opt,
