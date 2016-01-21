56773f04c70760f137b5270d56da927159fde698
Eli Qiao <liyong.qiao@intel.com>
2016-01-21 17:19:54 +0800

    API: Remove unexpected from errors get_console_output
    No HTTPBadRequest(400) error will be raised from get_console_output.
    
    Closes-Bug: #1537629
    Change-Id: I5c652697ac0e7ccdb6af33a35074e26fb3f3891f

--- nova/api/openstack/compute/console_output.py
+++ nova/api/openstack/compute/console_output.py
@@ -35,7 +35,7 @@ class ConsoleOutputController(wsgi.Controller):
         super(ConsoleOutputController, self).__init__(*args, **kwargs)
         self.compute_api = compute.API(skip_policy_check=True)
 
-    @extensions.expected_errors((400, 404, 409, 501))
+    @extensions.expected_errors((404, 409, 501))
     @wsgi.action('os-getConsoleOutput')
     @validation.schema(console_output.get_console_output)
     def get_console_output(self, req, id, body):
