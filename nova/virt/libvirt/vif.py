98464d54d0fcdba452191bc0291d59957c9cdae6
Sean Mooney <sean.k.mooney@intel.com>
2016-01-22 17:00:36 +0000

    stop setting mtu when plugging vhost-user ports
    vhost-user is a userspace protocol to establish connectivity
    between a virto-net frontend typically qemu and a
    userspace virtio backend such as ovs with dpdk.
    
    vhost-user interfaces exist only in userspace from the host perspective
    and are not represented in the linux networking stack as kernel netdevs.
    As a result attempting to set the mtu on a vhost-user interface
    using ifconfig or ip link will fail with a device not found error.
    
    - this change removes a call to _set_device_mtu when plugging
      vhost-user interfaces.
    - this change prevents the device not found error from occurring
      which stopped vms booting with vhost-user interfaces
      due to an uncaught exception resulting in a failure to set the
      interface type in ovs.
    - this change make creating vhost-user interface
      an atomic action.
    
    This latent bug is only triggered when the mtu value is set to a
    value other than 0 which was the default proir to mitaka.
    
    Conflicts:
    	nova/network/model.py
    	nova/tests/unit/virt/libvirt/test_vif.py
    	nova/virt/libvirt/vif.py
    
    Change-Id: I2e17723d5052d57cd1557bd8a173c06ea0dcb2d4
    Closes-Bug: #1533876
    (cherry picked from commit adf7ba61dd73fe4bfffa20295be9a4b1006a1fe6)

--- nova/virt/libvirt/vif.py
+++ nova/virt/libvirt/vif.py
@@ -675,10 +675,11 @@ class LibvirtGenericVIFDriver(object):
             port_name = os.path.basename(
                     vif['details'][network_model.VIF_DETAILS_VHOSTUSER_SOCKET])
             mtu = vif['network'].get_meta('mtu')
-            linux_net.create_ovs_vif_port(self.get_bridge_name(vif),
-                                          port_name, iface_id, vif['address'],
-                                          instance.uuid, mtu)
-            linux_net.ovs_set_vhostuser_port_type(port_name)
+            linux_net.create_ovs_vif_port(
+                self.get_bridge_name(vif),
+                port_name, iface_id, vif['address'],
+                instance.uuid, mtu,
+                interface_type=network_model.OVS_VHOSTUSER_INTERFACE_TYPE)
 
     def plug_vrouter(self, instance, vif):
         """Plug into Contrail's network port
