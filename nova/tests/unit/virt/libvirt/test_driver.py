83574aa27364e538dc4b96a9c90adae75b14ecf3
Aysy Anne Duarte <aysyanne@linux.vnet.ibm.com>
2016-01-26 15:43:36 +0000

    Add ppc64le architecture to some libvirt unit tests
    Some unit tests are failing when running on ppc64le.
    It is necessary to add the option ppc64le inside the
    architecture check for some test_blockinfo and test_driver unit tests.
    
    Closes-Bug: #1523734
    
    Change-Id: I16eafee7bd24febf58201d984cdcbfdef8b97ef3

--- nova/tests/unit/virt/libvirt/test_driver.py
+++ nova/tests/unit/virt/libvirt/test_driver.py
@@ -3070,7 +3070,7 @@ class LibvirtConnTestCase(test.NoDBTestCase):
         # device (hdd). Since power only support scsi, the last device
         # is sdz
 
-        expect = {"ppc": "sdz", "ppc64": "sdz"}
+        expect = {"ppc": "sdz", "ppc64": "sdz", "ppc64le": "sdz"}
         disk = expect.get(blockinfo.libvirt_utils.get_arch({}), "hdd")
         self.assertIsInstance(cfg.devices[2],
                               vconfig.LibvirtConfigGuestDisk)
@@ -5288,7 +5288,8 @@ class LibvirtConnTestCase(test.NoDBTestCase):
         # all ide calls to scsi
 
         expected = {arch.PPC: ("cdrom", "scsi", "sda"),
-                    arch.PPC64: ("cdrom", "scsi", "sda")}
+                    arch.PPC64: ("cdrom", "scsi", "sda"),
+                    arch.PPC64LE: ("cdrom", "scsi", "sda")}
 
         expec_val = expected.get(blockinfo.libvirt_utils.get_arch({}),
                                   ("cdrom", "ide", "hda"))
@@ -5304,7 +5305,8 @@ class LibvirtConnTestCase(test.NoDBTestCase):
         # all ide calls to scsi
 
         expected = {arch.PPC: ("cdrom", "scsi", "sda"),
-                    arch.PPC64: ("cdrom", "scsi", "sda")}
+                    arch.PPC64: ("cdrom", "scsi", "sda"),
+                    arch.PPC64LE: ("cdrom", "scsi", "sda")}
 
         swap = {'device_name': '/dev/vdc',
                 'swap_size': 1}
