4c08d37705908d50d44a388d33ccf41b93820cbe
Kevin_Zheng <zhengzhenyu@huawei.com>
2016-01-27 17:01:55 +0800

    config options: centralize section "cloudpipe"
    The config options of the "nova.conf" section "cloudpipe"
    got moved to the new central location "nova/conf/cloudpipe.py".
    
    Change-Id: I5547c13427de44e6dba8a1c29c8e96cc892af07c
    Implements: blueprint centralize-config-options-newton

--- nova/cloudpipe/pipelib.py
+++ nova/cloudpipe/pipelib.py
@@ -24,7 +24,6 @@ import os
 import string
 import zipfile
 
-from oslo_config import cfg
 from oslo_log import log as logging
 from oslo_utils import fileutils
 
@@ -34,34 +33,10 @@ import nova.conf
 from nova import crypto
 from nova import db
 from nova import exception
-from nova.i18n import _
-from nova import paths
 from nova import utils
 
 
-cloudpipe_opts = [
-    cfg.StrOpt('vpn_image_id',
-               default='0',
-               help='Image ID used when starting up a cloudpipe vpn server'),
-    cfg.StrOpt('vpn_flavor',
-               default='m1.tiny',
-               help=_('Flavor for vpn instances')),
-    cfg.StrOpt('boot_script_template',
-               default=paths.basedir_def('nova/cloudpipe/bootscript.template'),
-               help=_('Template for cloudpipe instance boot script')),
-    cfg.StrOpt('dmz_net',
-               default='10.0.0.0',
-               help=_('Network to push into openvpn config')),
-    cfg.StrOpt('dmz_mask',
-               default='255.255.255.0',
-               help=_('Netmask to push into openvpn config')),
-    cfg.StrOpt('vpn_key_suffix',
-               default='-vpn',
-               help='Suffix to add to project name for vpn key and secgroups'),
-    ]
-
 CONF = nova.conf.CONF
-CONF.register_opts(cloudpipe_opts)
 CONF.import_opt('keys_path', 'nova.crypto')
 
 LOG = logging.getLogger(__name__)
