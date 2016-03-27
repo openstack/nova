d07a129bb5144a0c6fc2cf617dbf3ef30501814e
Hironori Shiina <shiina.hironori@jp.fujitsu.com>
2016-03-27 20:32:30 +0900

    Remove mox from tests/unit/objects/test_migration.py
    This patch replaces mox with mock in
    tests/unit/objects/test_migration.py.
    
    Part of blueprint remove-mox-newton
    
    Change-Id: I9c61c4a5aab0bfb9ee95fc06f7f10ba60fa62152

--- nova/tests/unit/objects/test_migration.py
+++ nova/tests/unit/objects/test_migration.py
@@ -60,27 +60,26 @@ def fake_db_migration(**updates):
 
 
 class _TestMigrationObject(object):
-    def test_get_by_id(self):
+    @mock.patch.object(db, 'migration_get')
+    def test_get_by_id(self, mock_get):
         ctxt = context.get_admin_context()
         fake_migration = fake_db_migration()
-        self.mox.StubOutWithMock(db, 'migration_get')
-        db.migration_get(ctxt, fake_migration['id']).AndReturn(fake_migration)
-        self.mox.ReplayAll()
+        mock_get.return_value = fake_migration
         mig = migration.Migration.get_by_id(ctxt, fake_migration['id'])
         self.compare_obj(mig, fake_migration)
+        mock_get.assert_called_once_with(ctxt, fake_migration['id'])
 
-    def test_get_by_instance_and_status(self):
+    @mock.patch.object(db, 'migration_get_by_instance_and_status')
+    def test_get_by_instance_and_status(self, mock_get):
         ctxt = context.get_admin_context()
         fake_migration = fake_db_migration()
-        self.mox.StubOutWithMock(db, 'migration_get_by_instance_and_status')
-        db.migration_get_by_instance_and_status(ctxt,
-                                                fake_migration['id'],
-                                                'migrating'
-                                                ).AndReturn(fake_migration)
-        self.mox.ReplayAll()
+        mock_get.return_value = fake_migration
         mig = migration.Migration.get_by_instance_and_status(
             ctxt, fake_migration['id'], 'migrating')
         self.compare_obj(mig, fake_migration)
+        mock_get.assert_called_once_with(ctxt,
+                                         fake_migration['id'],
+                                         'migrating')
 
     @mock.patch('nova.db.migration_get_in_progress_by_instance')
     def test_get_in_progress_by_instance(self, m_get_mig):
@@ -96,72 +95,70 @@ class _TestMigrationObject(object):
         for index, db_migration in enumerate(db_migrations):
             self.compare_obj(migrations[index], db_migration)
 
-    def test_create(self):
+    @mock.patch.object(db, 'migration_create')
+    def test_create(self, mock_create):
         ctxt = context.get_admin_context()
         fake_migration = fake_db_migration()
-        self.mox.StubOutWithMock(db, 'migration_create')
-        db.migration_create(ctxt, {'source_compute': 'foo',
-                                   'migration_type': 'resize'}
-                            ).AndReturn(fake_migration)
-        self.mox.ReplayAll()
+        mock_create.return_value = fake_migration
         mig = migration.Migration(context=ctxt)
         mig.source_compute = 'foo'
         mig.migration_type = 'resize'
         mig.create()
         self.assertEqual(fake_migration['dest_compute'], mig.dest_compute)
+        mock_create.assert_called_once_with(ctxt,
+                                            {'source_compute': 'foo',
+                                             'migration_type': 'resize'})
 
-    def test_recreate_fails(self):
+    @mock.patch.object(db, 'migration_create')
+    def test_recreate_fails(self, mock_create):
         ctxt = context.get_admin_context()
         fake_migration = fake_db_migration()
-        self.mox.StubOutWithMock(db, 'migration_create')
-        db.migration_create(ctxt, {'source_compute': 'foo',
-                                   'migration_type': 'resize'}
-                            ).AndReturn(fake_migration)
-        self.mox.ReplayAll()
+        mock_create.return_value = fake_migration
         mig = migration.Migration(context=ctxt)
         mig.source_compute = 'foo'
         mig.migration_type = 'resize'
         mig.create()
         self.assertRaises(exception.ObjectActionError, mig.create)
+        mock_create.assert_called_once_with(ctxt,
+                                            {'source_compute': 'foo',
+                                             'migration_type': 'resize'})
 
     def test_create_fails_migration_type(self):
         ctxt = context.get_admin_context()
-        self.mox.StubOutWithMock(db, 'migration_create')
-        self.mox.ReplayAll()
         mig = migration.Migration(context=ctxt,
                                   old_instance_type_id=42,
                                   new_instance_type_id=84)
         mig.source_compute = 'foo'
         self.assertRaises(exception.ObjectActionError, mig.create)
 
-    def test_save(self):
+    @mock.patch.object(db, 'migration_update')
+    def test_save(self, mock_update):
         ctxt = context.get_admin_context()
         fake_migration = fake_db_migration()
-        self.mox.StubOutWithMock(db, 'migration_update')
-        db.migration_update(ctxt, 123, {'source_compute': 'foo'}
-                            ).AndReturn(fake_migration)
-        self.mox.ReplayAll()
+        mock_update.return_value = fake_migration
         mig = migration.Migration(context=ctxt)
         mig.id = 123
         mig.source_compute = 'foo'
         mig.save()
         self.assertEqual(fake_migration['dest_compute'], mig.dest_compute)
+        mock_update.assert_called_once_with(ctxt, 123,
+                                            {'source_compute': 'foo'})
 
-    def test_instance(self):
+    @mock.patch.object(db, 'instance_get_by_uuid')
+    def test_instance(self, mock_get):
         ctxt = context.get_admin_context()
         fake_migration = fake_db_migration()
         fake_inst = fake_instance.fake_db_instance()
-        self.mox.StubOutWithMock(db, 'instance_get_by_uuid')
-        db.instance_get_by_uuid(ctxt, fake_migration['instance_uuid'],
-                                columns_to_join=['info_cache',
-                                                 'security_groups']
-                                ).AndReturn(fake_inst)
+        mock_get.return_value = fake_inst
         mig = migration.Migration._from_db_object(ctxt,
                                                   migration.Migration(),
                                                   fake_migration)
         mig._context = ctxt
-        self.mox.ReplayAll()
         self.assertEqual(mig.instance.host, fake_inst['host'])
+        mock_get.assert_called_once_with(ctxt,
+                                         fake_migration['instance_uuid'],
+                                         columns_to_join=['info_cache',
+                                                          'security_groups'])
 
     def test_instance_setter(self):
         migration = objects.Migration(instance_uuid=uuidsentinel.instance)
@@ -173,51 +170,46 @@ class _TestMigrationObject(object):
         self.assertEqual(inst, migration._cached_instance)
         self.assertEqual(inst, migration.instance)
 
-    def test_get_unconfirmed_by_dest_compute(self):
+    @mock.patch.object(db, 'migration_get_unconfirmed_by_dest_compute')
+    def test_get_unconfirmed_by_dest_compute(self, mock_get):
         ctxt = context.get_admin_context()
         fake_migration = fake_db_migration()
         db_migrations = [fake_migration, dict(fake_migration, id=456)]
-        self.mox.StubOutWithMock(
-            db, 'migration_get_unconfirmed_by_dest_compute')
-        db.migration_get_unconfirmed_by_dest_compute(
-            ctxt, 'window', 'foo').AndReturn(db_migrations)
-        self.mox.ReplayAll()
+        mock_get.return_value = db_migrations
         migrations = (
             migration.MigrationList.get_unconfirmed_by_dest_compute(
                 ctxt, 'window', 'foo', use_slave=False))
         self.assertEqual(2, len(migrations))
         for index, db_migration in enumerate(db_migrations):
             self.compare_obj(migrations[index], db_migration)
+        mock_get.assert_called_once_with(ctxt, 'window', 'foo')
 
-    def test_get_in_progress_by_host_and_node(self):
+    @mock.patch.object(db, 'migration_get_in_progress_by_host_and_node')
+    def test_get_in_progress_by_host_and_node(self, mock_get):
         ctxt = context.get_admin_context()
         fake_migration = fake_db_migration()
         db_migrations = [fake_migration, dict(fake_migration, id=456)]
-        self.mox.StubOutWithMock(
-            db, 'migration_get_in_progress_by_host_and_node')
-        db.migration_get_in_progress_by_host_and_node(
-            ctxt, 'host', 'node').AndReturn(db_migrations)
-        self.mox.ReplayAll()
+        mock_get.return_value = db_migrations
         migrations = (
             migration.MigrationList.get_in_progress_by_host_and_node(
                 ctxt, 'host', 'node'))
         self.assertEqual(2, len(migrations))
         for index, db_migration in enumerate(db_migrations):
             self.compare_obj(migrations[index], db_migration)
+        mock_get.assert_called_once_with(ctxt, 'host', 'node')
 
-    def test_get_by_filters(self):
+    @mock.patch.object(db, 'migration_get_all_by_filters')
+    def test_get_by_filters(self, mock_get):
         ctxt = context.get_admin_context()
         fake_migration = fake_db_migration()
         db_migrations = [fake_migration, dict(fake_migration, id=456)]
-        self.mox.StubOutWithMock(
-            db, 'migration_get_all_by_filters')
         filters = {'foo': 'bar'}
-        db.migration_get_all_by_filters(ctxt, filters).AndReturn(db_migrations)
-        self.mox.ReplayAll()
+        mock_get.return_value = db_migrations
         migrations = migration.MigrationList.get_by_filters(ctxt, filters)
         self.assertEqual(2, len(migrations))
         for index, db_migration in enumerate(db_migrations):
             self.compare_obj(migrations[index], db_migration)
+        mock_get.assert_called_once_with(ctxt, filters)
 
     def test_migrate_old_resize_record(self):
         db_migration = dict(fake_db_migration(), migration_type=None)
