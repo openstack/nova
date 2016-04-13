82a23c682e39730372d1b776b9daafae7119cf5a
Thomas Bechtold <tbechtold@suse.com>
2016-04-13 09:02:46 +0200

    Fix doc build if git is absent
    When building packages if git is absent, then we should not set
    html_last_updated_fmt. It can still be set via the -D switch
    when building with sphinx-build.
    
    Change-Id: I5da86b7a163c0e795cd34abf79b0980a8552f79b
    Closes-Bug: #1552251

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -27,6 +27,7 @@
 import os
 import subprocess
 import sys
+import warnings
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -145,8 +146,12 @@ html_static_path = ['_static']
 # html_last_updated_fmt = '%b %d, %Y'
 git_cmd = ["git", "log", "--pretty=format:'%ad, commit %h'", "--date=local",
     "-n1"]
-html_last_updated_fmt = subprocess.Popen(
-    git_cmd, stdout=subprocess.PIPE).communicate()[0]
+try:
+    html_last_updated_fmt = subprocess.Popen(
+        git_cmd, stdout=subprocess.PIPE).communicate()[0]
+except Exception:
+    warnings.warn('Cannot get last updated time from git repository. '
+                  'Not setting "html_last_updated_fmt".')
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,6 +15,7 @@
 import subprocess
 import sys
 import os
+import warnings
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -194,8 +195,13 @@ html_static_path = ['_static']
 #html_last_updated_fmt = '%b %d, %Y'
 git_cmd = ["git", "log", "--pretty=format:'%ad, commit %h'", "--date=local",
     "-n1"]
-html_last_updated_fmt = subprocess.Popen(
-    git_cmd, stdout=subprocess.PIPE).communicate()[0]
+try:
+    html_last_updated_fmt = subprocess.Popen(
+        git_cmd, stdout=subprocess.PIPE).communicate()[0]
+except Exception:
+    warnings.warn('Cannot get last updated time from git repository. '
+                  'Not setting "html_last_updated_fmt".')
+
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
