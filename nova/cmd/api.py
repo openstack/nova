e80a87352431ee6f05729be32296e90b2d9c13c7
Dan Smith <dansmith@redhat.com>
2016-04-22 14:22:08 -0700

    Fail to start nova-api if no APIs were able to be started
    This helps to make it clear that either you specified no API services,
    or you specified all bad ones, or that you fat-fingered the thing
    entirely. If we can't start any, we shouldn't start the process acting
    like we did and then wait for you to figure out the hard way that we
    aren't actually listening for any requests.
    
    Change-Id: I864486a58cf3fde35647ae3b8d2ad917c809ea54
    Related-Bug: #1572495
    (cherry picked from commit 9b2329d10021a7fa5f43b9fd8be3c833dd7212d0)

--- nova/cmd/api.py
+++ nova/cmd/api.py
@@ -29,7 +29,7 @@ import six
 
 from nova import config
 from nova import exception
-from nova.i18n import _LW
+from nova.i18n import _LE, _LW
 from nova import objects
 from nova import service
 from nova import utils
@@ -50,14 +50,21 @@ def main():
     gmr.TextGuruMeditation.setup_autorun(version)
 
     launcher = service.process_launcher()
+    started = 0
     for api in CONF.enabled_apis:
         should_use_ssl = api in CONF.enabled_ssl_apis
         try:
             server = service.WSGIService(api, use_ssl=should_use_ssl)
             launcher.launch_service(server, workers=server.workers or 1)
+            started += 1
         except exception.PasteAppNotFound as ex:
             log.warning(
                 _LW("%s. ``enabled_apis`` includes bad values. "
                     "Fix to remove this warning."), six.text_type(ex))
+
+    if started == 0:
+        log.error(_LE('No APIs were started. '
+                      'Check the enabled_apis config option.'))
+        sys.exit(1)
 
     launcher.wait()
