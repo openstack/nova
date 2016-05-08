120d77e076273e4757b225e89b73a78e79c3a42e
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2016-05-08 11:27:57 +0900

    Remove APIRouter of legacy v2 API code
    This patch removes the APIRouter because there is not any code
    which uses the APIRouter. In addition, this patch update the
    corresponding reno for explaining actual way for api-paste.ini.
    
    Partially implements blueprint remove-legacy-v2-api-code
    
    Change-Id: I136eb6ce7478f9ff692ebcbe1b14aa76222826bf

--- nova/api/openstack/__init__.py
+++ nova/api/openstack/__init__.py
@@ -29,7 +29,6 @@ from nova.api.openstack import extensions
 from nova.api.openstack import wsgi
 import nova.conf
 from nova import exception
-from nova.i18n import _
 from nova.i18n import _LC
 from nova.i18n import _LE
 from nova.i18n import _LI
@@ -219,85 +218,6 @@ class PlainMapper(APIMapper):
         routes.Mapper.resource(self, member_name,
                                      collection_name,
                                      **kwargs)
-
-
-class APIRouter(base_wsgi.Router):
-    """Routes requests on the OpenStack API to the appropriate controller
-    and method.
-    """
-    ExtensionManager = None  # override in subclasses
-
-    @classmethod
-    def factory(cls, global_config, **local_config):
-        """Simple paste factory, :class:`nova.wsgi.Router` doesn't have one."""
-        return cls()
-
-    def __init__(self, ext_mgr=None, init_only=None):
-        if ext_mgr is None:
-            if self.ExtensionManager:
-                ext_mgr = self.ExtensionManager()
-            else:
-                raise Exception(_("Must specify an ExtensionManager class"))
-
-        mapper = ProjectMapper()
-        self.resources = {}
-        self._setup_routes(mapper, ext_mgr, init_only)
-        self._setup_ext_routes(mapper, ext_mgr, init_only)
-        self._setup_extensions(ext_mgr)
-        super(APIRouter, self).__init__(mapper)
-
-    def _setup_ext_routes(self, mapper, ext_mgr, init_only):
-        for resource in ext_mgr.get_resources():
-            LOG.debug('Extending resource: %s',
-                      resource.collection)
-
-            if init_only is not None and resource.collection not in init_only:
-                continue
-
-            inherits = None
-            if resource.inherits:
-                inherits = self.resources.get(resource.inherits)
-                if not resource.controller:
-                    resource.controller = inherits.controller
-            wsgi_resource = wsgi.Resource(resource.controller,
-                                          inherits=inherits)
-            self.resources[resource.collection] = wsgi_resource
-            kargs = dict(
-                controller=wsgi_resource,
-                collection=resource.collection_actions,
-                member=resource.member_actions)
-
-            if resource.parent:
-                kargs['parent_resource'] = resource.parent
-
-            mapper.resource(resource.collection, resource.collection, **kargs)
-
-            if resource.custom_routes_fn:
-                resource.custom_routes_fn(mapper, wsgi_resource)
-
-    def _setup_extensions(self, ext_mgr):
-        for extension in ext_mgr.get_controller_extensions():
-            collection = extension.collection
-            controller = extension.controller
-
-            msg_format_dict = {'collection': collection,
-                               'ext_name': extension.extension.name}
-            if collection not in self.resources:
-                LOG.warning(_LW('Extension %(ext_name)s: Cannot extend '
-                                'resource %(collection)s: No such resource'),
-                            msg_format_dict)
-                continue
-
-            LOG.debug('Extension %(ext_name)s extended resource: '
-                      '%(collection)s',
-                      msg_format_dict)
-
-            resource = self.resources[collection]
-            resource.register_actions(controller)
-            resource.register_extensions(controller)
-
-    def _setup_routes(self, mapper, ext_mgr, init_only):
-        raise NotImplementedError()
 
 
 class APIRouterV21(base_wsgi.Router):
