08f7f7a89366487bf2782bdc3ca5b4dab9a4487f
Markus Zoeller <mzoeller@de.ibm.com>
2016-05-27 18:11:25 +0200

    add "needs:*" tags to the config option modules
    To make it trackable which fixes the config options need, we add tag
    at the beginning of the files. If the fix is done, the tag will be
    removed. We discussed this on the ML with [1].
    
    [1] http://lists.openstack.org/pipermail/openstack-dev/2016-May/095538.html
    
    Implements blueprint centralize-config-options-newton
    
    Change-Id: Ibcd89bc550c6f534b5fa297df74bd59eb481e921

--- nova/conf/base.py
+++ nova/conf/base.py
@@ -1,3 +1,10 @@
+# needs:fix_opt_description
+# needs:check_deprecation_status
+# needs:check_opt_group_and_type
+# needs:fix_opt_description_indentation
+# needs:fix_opt_registration_consistency
+
+
 # Copyright 2010 United States Government as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 # Copyright 2011 Justin Santa Barbara
