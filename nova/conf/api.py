6a6db22332c856d3b88764447e000f4097eec488
Maciej Szankin <maciej.szankin@intel.com>
2016-06-21 09:39:48 -0500

    Improve consistency in API
    Removed sections that are no longer required [1].
    Updated header flags
    Improved indentation and lists
    
    [1] https://wiki.openstack.org/wiki/ConfigOptionsConsistency
    
    Blueprint centralize-config-options-newton
    
    Change-Id: I7116f3ea89a4c38fad6a339bbfc6a3b8af2e0d60

--- nova/conf/api.py
+++ nova/conf/api.py
@@ -1,8 +1,4 @@
-# needs:fix_opt_description
-# needs:check_deprecation_status
 # needs:check_opt_group_and_type
-# needs:fix_opt_description_indentation
-# needs:fix_opt_registration_consistency
 
 
 # Copyright 2015 OpenStack Foundation
@@ -22,7 +18,6 @@
 
 from oslo_config import cfg
 
-
 auth_opts = [
     cfg.StrOpt("auth_strategy",
             default="keystone",
@@ -32,18 +27,6 @@ This determines the strategy to use for authentication: keystone or noauth2.
 'noauth2' is designed for testing only, as it does no actual credential
 checking. 'noauth2' provides administrative credentials only if 'admin' is
 specified as the username.
-
-* Possible values:
-
-    Either 'keystone' (default) or 'noauth2'.
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
 """),
     cfg.BoolOpt("use_forwarded_for",
             default=False,
@@ -52,18 +35,6 @@ When True, the 'X-Forwarded-For' header is treated as the canonical remote
 address. When False (the default), the 'remote_address' header is used.
 
 You should only enable this if you have an HTML sanitizing proxy.
-
-* Possible values:
-
-    True, False (default)
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
 """),
 ]
 
@@ -76,32 +47,22 @@ When gathering the existing metadata for a config drive, the EC2-style metadata
 is returned for all versions that don't appear in this option. As of the
 Liberty release, the available versions are:
 
-    1.0
-    2007-01-19
-    2007-03-01
-    2007-08-29
-    2007-10-10
-    2007-12-15
-    2008-02-01
-    2008-09-01
-    2009-04-04
+* 1.0
+* 2007-01-19
+* 2007-03-01
+* 2007-08-29
+* 2007-10-10
+* 2007-12-15
+* 2008-02-01
+* 2008-09-01
+* 2009-04-04
 
 The option is in the format of a single string, with each version separated by
 a space.
 
-* Possible values:
+Possible values:
 
-    Any string that represents zero or more versions, separated by spaces. The
-    default is "1.0 2007-01-19 2007-03-01 2007-08-29 2007-10-10 2007-12-15
-    2008-02-01 2008-09-01".
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
+* Any string that represents zero or more versions, separated by spaces.
 """),
     cfg.StrOpt("vendordata_driver",
             default="nova.api.metadata.vendordata_json.JsonFileVendorData",
@@ -111,18 +72,9 @@ When returning instance metadata, this is the class that is used
 for getting vendor metadata when that class isn't specified in the individual
 request. The value should be the full dot-separated path to the class to use.
 
-* Possible values:
+Possible values:
 
-    Any valid dot-separated class path that can be imported. The default is
-    'nova.api.metadata.vendordata_json.JsonFileVendorData'.
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
+* Any valid dot-separated class path that can be imported.
 """),
     cfg.ListOpt('vendordata_providers',
                 default=[],
@@ -237,28 +189,18 @@ Maximum wait time for an external REST service to return data once connected.
 """),
     cfg.IntOpt("metadata_cache_expiration",
             default=15,
+            min=0,
             help="""
 This option is the time (in seconds) to cache metadata. When set to 0, metadata
 caching is disabled entirely; this is generally not recommended for performance
 reasons. Increasing this setting should improve response times of the metadata
 API when under heavy load. Higher values may increase memory usage, and result
 in longer times for host metadata changes to take effect.
-
-* Possible values:
-
-    Zero or any positive integer. The default is 15.
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
 """),
 ]
 
-file_opt = cfg.StrOpt("vendordata_jsonfile_path",
+file_opts = [
+    cfg.StrOpt("vendordata_jsonfile_path",
         help="""
 Cloud providers may store custom data in vendor data file that will then be
 available to the instances via the metadata service, and to the rendering of
@@ -266,38 +208,20 @@ config-drive. The default class for this, JsonFileVendorData, loads this
 information from a JSON file, whose path is configured by this option. If there
 is no path set by this option, the class returns an empty dictionary.
 
-* Possible values:
+Possible values:
 
-    Any string representing the path to the data file, or an empty string
+* Any string representing the path to the data file, or an empty string
     (default).
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
 """)
+]
 
 osapi_opts = [
     cfg.IntOpt("osapi_max_limit",
             default=1000,
+            min=0,
             help="""
 As a query can potentially return many thousands of items, you can limit the
 maximum number of items in a single response by setting this option.
-
-* Possible values:
-
-    Any positive integer. Default is 1000.
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
 """),
     cfg.StrOpt("osapi_compute_link_prefix",
             help="""
@@ -305,114 +229,67 @@ This string is prepended to the normal URL that is returned in links to the
 OpenStack Compute API. If it is empty (the default), the URLs are returned
 unchanged.
 
-* Possible values:
+Possible values:
 
-    Any string, including an empty string (the default).
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
+* Any string, including an empty string (the default).
 """),
     cfg.StrOpt("osapi_glance_link_prefix",
             help="""
 This string is prepended to the normal URL that is returned in links to Glance
 resources. If it is empty (the default), the URLs are returned unchanged.
 
-* Possible values:
+Possible values:
 
-    Any string, including an empty string (the default).
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
+* Any string, including an empty string (the default).
 """),
 ]
 
-allow_instance_snapshots_opt = cfg.BoolOpt("allow_instance_snapshots",
+allow_instance_snapshots_opts = [
+    cfg.BoolOpt("allow_instance_snapshots",
         default=True,
         help="""
 Operators can turn off the ability for a user to take snapshots of their
 instances by setting this option to False. When disabled, any attempt to take a
 snapshot will result in a HTTP 400 response ("Bad Request").
-
-* Possible values:
-
-    True (default), False
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
 """)
+]
 
 # NOTE(edleafe): I would like to import the value directly from
 # nova.compute.vm_states, but that creates a circular import. Since this value
 # is not likely to be changed, I'm copy/pasting it here.
 BUILDING = "building"  # VM only exists in DB
-osapi_hide_opt = cfg.ListOpt('osapi_hide_server_address_states',
+osapi_hide_opts = [
+    cfg.ListOpt("osapi_hide_server_address_states",
         default=[BUILDING],
         help="""
 This option is a list of all instance states for which network address
 information should not be returned from the API.
 
-* Possible values:
+Possible values:
 
-    A list of strings, where each string is a valid VM state, as defined in
-    nova/compute/vm_states.py. As of the Newton release, they are:
+  A list of strings, where each string is a valid VM state, as defined in
+  nova/compute/vm_states.py. As of the Newton release, they are:
 
-    * "active"
-    * "building"
-    * "paused"
-    * "suspended"
-    * "stopped"
-    * "rescued"
-    * "resized"
-    * "soft-delete"
-    * "deleted"
-    * "error"
-    * "shelved"
-    * "shelved_offloaded"
-
-    The default value is ["building"].
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
+* "active"
+* "building"
+* "paused"
+* "suspended"
+* "stopped"
+* "rescued"
+* "resized"
+* "soft-delete"
+* "deleted"
+* "error"
+* "shelved"
+* "shelved_offloaded"
 """)
+]
 
-fping_path_opt = cfg.StrOpt("fping_path",
+fping_path_opts = [
+    cfg.StrOpt("fping_path",
         default="/usr/sbin/fping",
-        help="""
-The full path to the fping binary.
-
-* Possible values:
-
-    Any string representing a valid file path. The default is
-    "/usr/sbin/fping".
-
-* Services that use this:
-
-    ``nova-api``
-
-* Related options:
-
-    None
-""")
+        help="The full path to the fping binary.")
+]
 
 os_network_opts = [
     cfg.BoolOpt("enable_network_quota",
@@ -424,109 +301,66 @@ and nova-network is itself deprecated.""",
             help="""
 This option is used to enable or disable quota checking for tenant networks.
 
-* Services that use this:
+Related options:
 
-    ``nova-api``
-
-Possible values:
-
-    True, False (default)
-
-* Related options:
-
-    quota_networks
+* quota_networks
 """),
-    cfg.BoolOpt('use_neutron_default_nets',
+    cfg.BoolOpt("use_neutron_default_nets",
             default=False,
             help="""
 When True, the TenantNetworkController will query the Neutron API to get the
 default networks to use.
 
-* Services that use this:
+Related options:
 
-    ``nova-api``
-
-Possible values:
-
-    True, False (default)
-
-* Related options:
-
-    neutron_default_tenant_id
+* neutron_default_tenant_id
 """),
-    cfg.StrOpt('neutron_default_tenant_id',
+    cfg.StrOpt("neutron_default_tenant_id",
             default="default",
             help="""
-When getting the default network from the Neutron API, this is the tenant ID
-(also referred in some places as the 'project ID') to use. The default is the
-string 'default'.
+Tenant ID for getting the default network from Neutron API (also referred in
+some places as the 'project ID') to use.
 
-* Services that use this:
+Related options:
 
-    ``nova-api``
-
-Possible values:
-
-    A string representing a valid tenant ID
-
-* Related options:
-
-    use_neutron_default_nets
+* use_neutron_default_nets
 """),
-    cfg.IntOpt('quota_networks',
+    cfg.IntOpt("quota_networks",
             deprecated_for_removal=True,
             deprecated_reason="""
 CRUD operations on tenant networks are only available when using nova-network
 and nova-network is itself deprecated.""",
             default=3,
+            min=0,
             help="""
 This option controls the number of private networks that can be created per
 project (or per tenant).
 
-* Services that use this:
+Related options:
 
-    ``nova-api``
-
-Possible values:
-
-    Any positive integer. The default is 3.
-
-* Related options:
-
-    enable_network_quota
+* enable_network_quota
 """),
 ]
 
-enable_inst_pw_opt = cfg.BoolOpt('enable_instance_password',
+enable_inst_pw_opts = [
+    cfg.BoolOpt("enable_instance_password",
         default=True,
         help="""
 Enables returning of the instance password by the relevant server API calls
 such as create, rebuild, evacuate, or rescue. If the hypervisor does not
 support password injection, then the password returned will not be correct, so
 if your hypervisor does not support password injection, set this to False.
-
-* Services that use this:
-
-    ``nova-api``
-
-Possible values:
-
-    True (default), False
-
-* Related options:
-
-    None
 """)
-
+]
 ALL_OPTS = (auth_opts +
             metadata_opts +
-            [file_opt] +
+            file_opts +
             osapi_opts +
-            [allow_instance_snapshots_opt] +
-            [osapi_hide_opt] +
-            [fping_path_opt] +
+            allow_instance_snapshots_opts +
+            osapi_hide_opts +
+            fping_path_opts +
             os_network_opts +
-            [enable_inst_pw_opt])
+            enable_inst_pw_opts)
 
 
 def register_opts(conf):
@@ -534,4 +368,5 @@ def register_opts(conf):
 
 
 def list_opts():
+    # TODO(macsz) add opt group
     return {"DEFAULT": ALL_OPTS}
