4e750dfcc6013e980b3d3698115c6b6b8ad3c96b
Eli Qiao <liyong.qiao@intel.com>
2016-06-30 10:40:17 +0800

    Mapping power_state from integer to string
    To improve user experience, mapping an integer code of power state to
    real string.
    
    Closes-Bug: #1587780
    Change-Id: I4fbeab0f31492a8aa153044f14f588656f3e332f

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -4002,9 +4002,9 @@ class ComputeManager(manager.Manager):
             return self.driver.get_diagnostics(instance)
         else:
             raise exception.InstanceInvalidState(
-                attr='power_state',
+                attr='power state',
                 instance_uuid=instance.uuid,
-                state=instance.power_state,
+                state=power_state.STATE_MAP[instance.power_state],
                 method='get_diagnostics')
 
     @object_compat
@@ -4020,9 +4020,9 @@ class ComputeManager(manager.Manager):
             return diags.serialize()
         else:
             raise exception.InstanceInvalidState(
-                attr='power_state',
+                attr='power state',
                 instance_uuid=instance.uuid,
-                state=instance.power_state,
+                state=power_state.STATE_MAP[instance.power_state],
                 method='get_diagnostics')
 
     @wrap_exception()
