a83613946d5849bfcc65e40efe3902e8754a22a9
ghanshyam <ghanshyam.mann@nectechnologies.in>
2016-09-12 15:03:21 +0900

    Use tempest tox with regex first
    This commit add regex first while running tempest tox
    
    There is changes in way tempest tox runs tests by using the tempest run
    command directly instead of some bash script.
    
    With tempest run you specify a regex with a '--regex' parameter and to
    keep backwards compatibility this is added to the tempest tox
    definitions.
    
    But in Nova tests scripts it is being used after concurrency and ends up
    having a call equivalent to:
    
      tempest run --regex --concurrency=2 $tempest_regex
    
    which obviously is incorrect. Simply switching the arg order should work here.
    
    Change-Id: If0064b9e8358332972ef4a1eee8f150e66f8c50f
    Needed-by: I3684fce66a799579fa68af119652cafef25a9f03

--- nova/tests/live_migration/hooks/utils.sh
+++ nova/tests/live_migration/hooks/utils.sh
@@ -3,7 +3,7 @@
 function run_tempest {
     local message=$1
     local tempest_regex=$2
-    sudo -H -u tempest tox -eall -- --concurrency=$TEMPEST_CONCURRENCY $tempest_regex
+    sudo -H -u tempest tox -eall -- $tempest_regex --concurrency=$TEMPEST_CONCURRENCY
     exitcode=$?
     if [[ $exitcode -ne 0 ]]; then
       die $LINENO "$message failure"
