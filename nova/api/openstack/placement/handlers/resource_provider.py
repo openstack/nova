f87af91d90f8f1322e92f9bc4d198a83162bb118
Pushkar Umaranikar <pushkar.umaranikar@intel.com>
2016-10-17 20:42:43 +0000

    Placement api: 404 response do not indicate what was not found
    Add informative messages to the 404 error response from
    nova/objects/resource_provider.py
    
    For the method _update_inventory_in_db() from resource_provider.py,
    gabbi test validation is not added since it is not being used and
    from the placement API you create an inventory but then call
    set_inventory or add_inventory on a resource_provider.
    
    Similar change, i.e. adding informative messages to 404 error can be
    done for api tests from inventory.yaml and usage.yaml
    
    It includes adding informative logging in code for 404 exceptions and
    adding gabbi tests for those.
    This can be done in follow up patch.
    
    Change-Id: If53f84ac5f7521e9926b97bdcce3cf77ec5b4ffd
    Closes-Bug: #1634115

--- nova/api/openstack/placement/handlers/resource_provider.py
+++ nova/api/openstack/placement/handlers/resource_provider.py
@@ -114,15 +114,18 @@ def delete_resource_provider(req):
     uuid = util.wsgi_path_item(req.environ, 'uuid')
     context = req.environ['placement.context']
     # The containing application will catch a not found here.
-    resource_provider = objects.ResourceProvider.get_by_uuid(
-        context, uuid)
     try:
+        resource_provider = objects.ResourceProvider.get_by_uuid(
+            context, uuid)
         resource_provider.destroy()
     except exception.ResourceProviderInUse as exc:
         raise webob.exc.HTTPConflict(
             _('Unable to delete resource provider %(rp_uuid)s: %(error)s') %
             {'rp_uuid': uuid, 'error': exc},
             json_formatter=util.json_error_formatter)
+    except exception.NotFound as exc:
+        raise webob.exc.HTTPNotFound(
+            _("No resource provider with uuid %s found for delete") % uuid)
     req.response.status = 204
     req.response.content_type = None
     return req.response
