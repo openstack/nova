c3353b8132881b72908273f1fc123c033843f0c3
Stephen Finucane <sfinucan@redhat.com>
2016-11-03 17:38:59 +0000

    conf: Remove extraneous whitespace
    Per the conventions seen in other files.
    
    Change-Id: I84b15f0eeddfd4d654aea9b5386ae3d90faafabf
    Implements: bp centralize-config-options-ocata

--- nova/conf/database.py
+++ nova/conf/database.py
@@ -54,53 +54,42 @@ api_db_opts = [
     cfg.StrOpt('connection',
                secret=True,
                help=''),
-
     cfg.BoolOpt('sqlite_synchronous',
                 default=True,
                 help=''),
-
     cfg.StrOpt('slave_connection',
                secret=True,
                help=''),
-
     cfg.StrOpt('mysql_sql_mode',
                default='TRADITIONAL',
                help=''),
-
     cfg.IntOpt('idle_timeout',
                default=3600,
                help=''),
-
     # TODO(markus_z): We should probably default this to 5 to not rely on the
     # SQLAlchemy default. Otherwise we wouldn't provide a stable default.
     cfg.IntOpt('max_pool_size',
                help=''),
-
     cfg.IntOpt('max_retries',
                default=10,
                help=''),
-
     # TODO(markus_z): This should have a minimum attribute of 0
     cfg.IntOpt('retry_interval',
                default=10,
                help=''),
-
     # TODO(markus_z): We should probably default this to 10 to not rely on the
     # SQLAlchemy default. Otherwise we wouldn't provide a stable default.
     cfg.IntOpt('max_overflow',
                help=''),
-
     # TODO(markus_z): This should probably make use of the "choices" attribute.
     # "oslo.db" uses only the values [<0, 0, 50, 100] see module
     # /oslo_db/sqlalchemy/engines.py method "_setup_logging"
     cfg.IntOpt('connection_debug',
                default=0,
                help=''),
-
     cfg.BoolOpt('connection_trace',
                 default=False,
                 help=''),
-
     # TODO(markus_z): We should probably default this to 30 to not rely on the
     # SQLAlchemy default. Otherwise we wouldn't provide a stable default.
     cfg.IntOpt('pool_timeout',
