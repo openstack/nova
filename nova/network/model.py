openstack/nova

commit 61fc1b9ee11e416aecbf3a29e1d150a53fc890e8

Change SpawnIsSynchronous fixture return
In normal usage utils.spawn returns an eventlet.greenthread.GreenThread
object which has a few methods defined on it. Currently the wait() and
link() methods are used in Nova. However the SpawnIsSynchronous fixture
was returning the results of wait() directly which led to an interface
mismatch. Normally utils.spawn_n returns a greenlet object which does
not allow checking the return but in testing we have never blocked that
capability before. This could be enhanced later to be strict for spawn_n
if we choose, but it is handy to have raised exceptions pass through in
testing.

SpawnIsSynchronous now returns a GreenThread object lookalike which has
wait() and link() methods that work as expected. This allows for the
removal of the wait() method from network/model.py:NetworkInfo.

A few mocks which emulated the improper behavior were also updated.

Change-Id: I1ded81f93235e217822e385ddd983b24c6359a53

nova/network/model.py
@@ -464,17 +464,6 @@ class NetworkInfo(list):
     def json(self):
         return jsonutils.dumps(self)
 
-    def wait(self, do_raise=True):
-        """Return this NetworkInfo
-
-        This is useful to avoid type checking when it's not clear if you have a
-        NetworkInfo or NetworkInfoAsyncWrapper. It's also useful when
-        utils.spawn in NetworkInfoAsyncWrapper is patched to return a
-        NetworkInfo rather than a GreenThread and wait() should return the same
-        thing for both cases.
-        """
-        return self
-
 
 class NetworkInfoAsyncWrapper(NetworkInfo):
     """Wrapper around NetworkInfo that allows retrieving NetworkInfo

nova/tests/unit/compute/test_compute_mgr.py
@@ -3068,6 +3068,8 @@ class ComputeManagerBuildInstanceTestCase(test.NoDBTestCase):
                                             'hosts': [[self.compute.host,
                                                        'fake-node']]}}
 
+        self.useFixture(fixtures.SpawnIsSynchronousFixture())
+
         def fake_network_info():
             return network_model.NetworkInfo([{'address': '1.2.3.4'}])
 
@@ -3080,8 +3082,6 @@ class ComputeManagerBuildInstanceTestCase(test.NoDBTestCase):
         fake_rt = fake_resource_tracker.FakeResourceTracker(self.compute.host,
                     self.compute.driver, self.node)
         self.compute._resource_tracker_dict[self.node] = fake_rt
-
-        self.useFixture(fixtures.SpawnIsSynchronousFixture())
 
     def _do_build_instance_update(self, reschedule_update=False):
         self.mox.StubOutWithMock(self.instance, 'save')
@@ -3652,7 +3652,7 @@ class ComputeManagerBuildInstanceTestCase(test.NoDBTestCase):
                     side_effect=[self.instance, self.instance, self.instance]),
                 mock.patch.object(self.compute,
                     '_build_networks_for_instance',
-                    return_value=network_model.NetworkInfo()),
+                    return_value=self.network_info),
                 mock.patch.object(self.compute,
                     '_notify_about_instance_usage'),
                 mock.patch.object(self.compute,
@@ -3696,7 +3696,7 @@ class ComputeManagerBuildInstanceTestCase(test.NoDBTestCase):
 
             _shutdown_instance.assert_called_once_with(self.context,
                     self.instance, self.block_device_mapping,
-                    self.requested_networks, try_deallocate_networks=True)
+                    self.requested_networks, try_deallocate_networks=False)
 
     def test_reschedule_on_resources_unavailable(self):
         reason = 'resource unavailable'
@@ -3851,7 +3851,7 @@ class ComputeManagerBuildInstanceTestCase(test.NoDBTestCase):
         self.mox.StubOutWithMock(self.compute, '_shutdown_instance')
         self.compute._build_networks_for_instance(self.context, self.instance,
                 self.requested_networks, self.security_groups).AndReturn(
-                        network_model.NetworkInfo([{'address': '1.2.3.4'}]))
+                        self.network_info)
         self.compute._shutdown_instance(self.context, self.instance,
                 self.block_device_mapping, self.requested_networks,
                 try_deallocate_networks=False)

nova/tests/fixtures.py
@@ -460,15 +460,41 @@ class IndirectionAPIFixture(fixtures.Fixture):
         self.addCleanup(self.cleanup)
 
 
+class _FakeGreenThread(object):
+    def __init__(self, func, *args, **kwargs):
+        self._result = func(*args, **kwargs)
+
+    def cancel(self, *args, **kwargs):
+        # This method doesn't make sense for a synchronous call, it's just
+        # defined to satisfy the interface.
+        pass
+
+    def kill(self, *args, **kwargs):
+        # This method doesn't make sense for a synchronous call, it's just
+        # defined to satisfy the interface.
+        pass
+
+    def link(self, func, *args, **kwargs):
+        func(self, *args, **kwargs)
+
+    def unlink(self, func, *args, **kwargs):
+        # This method doesn't make sense for a synchronous call, it's just
+        # defined to satisfy the interface.
+        pass
+
+    def wait(self):
+        return self._result
+
+
 class SpawnIsSynchronousFixture(fixtures.Fixture):
     """Patch and restore the spawn_n utility method to be synchronous"""
 
     def setUp(self):
         super(SpawnIsSynchronousFixture, self).setUp()
         self.useFixture(fixtures.MonkeyPatch(
-            'nova.utils.spawn_n', lambda f, *a, **k: f(*a, **k)))
+            'nova.utils.spawn_n', _FakeGreenThread))
         self.useFixture(fixtures.MonkeyPatch(
-            'nova.utils.spawn', lambda f, *a, **k: f(*a, **k)))
+            'nova.utils.spawn', _FakeGreenThread))
 
 
 class BannedDBSchemaOperations(fixtures.Fixture):

nova/tests/unit/test_fixtures.py
@@ -342,6 +342,46 @@ class TestSpawnIsSynchronousFixture(testtools.TestCase):
         utils.spawn_n(tester.function, 'foo', bar='bar')
         tester.function.assert_called_once_with('foo', bar='bar')
 
+    def test_spawn_return_has_wait(self):
+        self.useFixture(fixtures.SpawnIsSynchronousFixture())
+        gt = utils.spawn(lambda x: '%s' % x, 'foo')
+        foo = gt.wait()
+        self.assertEqual('foo', foo)
+
+    def test_spawn_n_return_has_wait(self):
+        self.useFixture(fixtures.SpawnIsSynchronousFixture())
+        gt = utils.spawn_n(lambda x: '%s' % x, 'foo')
+        foo = gt.wait()
+        self.assertEqual('foo', foo)
+
+    def test_spawn_has_link(self):
+        self.useFixture(fixtures.SpawnIsSynchronousFixture())
+        gt = utils.spawn(mock.MagicMock)
+        passed_arg = 'test'
+        call_count = []
+
+        def fake(thread, param):
+            self.assertEqual(gt, thread)
+            self.assertEqual(passed_arg, param)
+            call_count.append(1)
+
+        gt.link(fake, passed_arg)
+        self.assertEqual(1, len(call_count))
+
+    def test_spawn_n_has_link(self):
+        self.useFixture(fixtures.SpawnIsSynchronousFixture())
+        gt = utils.spawn_n(mock.MagicMock)
+        passed_arg = 'test'
+        call_count = []
+
+        def fake(thread, param):
+            self.assertEqual(gt, thread)
+            self.assertEqual(passed_arg, param)
+            call_count.append(1)
+
+        gt.link(fake, passed_arg)
+        self.assertEqual(1, len(call_count))
+
 
 class TestBannedDBSchemaOperations(testtools.TestCase):
     def test_column(self):

commit 1b351ef5356e0472aef51221ff02376dd8b42954

Add a no-op wait method to NetworkInfo
The normal deploy flow uses a NetworkInfoAsyncWrapper for network
allocation, and because of that many places have to call that class's
wait method to make sure it has completed. During a reschedule where
the network was allocated by a previous build attempt, a NetworkInfo
instance is retrieved instead, which does not have a wait method. This
then results in an exception complaining the missing method when it is
called. This fix addresses that by adding a no-op wait method to the
NetworkInfo class. Alternatively could have used isinstance or hasattr
to avoid making wait calls on NetworkInfo, but that could be
problematic to maintain as more places need to make wait calls in the
future and may not know to make the isinstance/hasattr check.

This fixes a regression issue caused by
61fc1b9ee11e416aecbf3a29e1d150a53fc890e8 ,
which reverted the previous fix made under
24a04c405ab2c98e52ea1edf8775489907526c6d

Change-Id: Id7a71b2eb46ea7df19e7da0afbc0eafa87cac965
Closes-Bug: 1636109

nova/network/model.py
@@ -467,6 +467,15 @@ class NetworkInfo(list):
             network_info = jsonutils.loads(network_info)
         return cls([VIF.hydrate(vif) for vif in network_info])
 
+    def wait(self, do_raise=True):
+        """Wait for asynchronous call to finish."""
+        # There is no asynchronous call for this class, so this is a no-op
+        # here, but subclasses may override to provide asynchronous
+        # capabilities. Must be defined here in the parent class so that code
+        # which works with both parent and subclass types can reference this
+        # method.
+        pass
+
     def json(self):
         return jsonutils.dumps(self)
 
@@ -529,7 +538,7 @@ class NetworkInfoAsyncWrapper(NetworkInfo):
         return self._sync_wrapper(fn, *args, **kwargs)
 
     def wait(self, do_raise=True):
-        """Wait for async call to finish."""
+        """Wait for asynchronous call to finish."""
         if self._gt is not None:
             try:
                 # NOTE(comstud): This looks funky, but this object is

nova/tests/unit/compute/test_compute_mgr.py
@@ -4378,11 +4378,12 @@ class ComputeManagerBuildInstanceTestCase(test.NoDBTestCase):
                 system_metadata={},
                 expected_attrs=['system_metadata'])
 
-        self.compute._build_networks_for_instance(self.context, instance,
-                self.requested_networks, self.security_groups)
+        nw_info_obj = self.compute._build_networks_for_instance(self.context,
+                instance, self.requested_networks, self.security_groups)
 
         mock_allocate.assert_called_once_with(self.context, instance,
                 self.requested_networks, None, self.security_groups, None)
+        self.assertTrue(hasattr(nw_info_obj, 'wait'), "wait must be there")
 
     @mock.patch.object(manager.ComputeManager, '_allocate_network')
     @mock.patch.object(network_api.API, 'get_instance_nw_info')
@@ -4391,11 +4392,12 @@ class ComputeManagerBuildInstanceTestCase(test.NoDBTestCase):
                 system_metadata=dict(network_allocated='False'),
                 expected_attrs=['system_metadata'])
 
-        self.compute._build_networks_for_instance(self.context, instance,
-                self.requested_networks, self.security_groups)
+        nw_info_obj = self.compute._build_networks_for_instance(self.context,
+                instance, self.requested_networks, self.security_groups)
 
         mock_allocate.assert_called_once_with(self.context, instance,
                 self.requested_networks, None, self.security_groups, None)
+        self.assertTrue(hasattr(nw_info_obj, 'wait'), "wait must be there")
 
     @mock.patch.object(network_api.API, 'setup_instance_network_on_host')
     @mock.patch.object(manager.ComputeManager, '_allocate_network')
@@ -4409,8 +4411,9 @@ class ComputeManagerBuildInstanceTestCase(test.NoDBTestCase):
         def fake_network_info():
             return network_model.NetworkInfo([{'address': '123.123.123.123'}])
 
-        mock_get.return_value = network_model.NetworkInfoAsyncWrapper(
-                                                            fake_network_info)
+        # this should be a NetworkInfo, not NetworkInfoAsyncWrapper, to match
+        # what get_instance_nw_info really returns
+        mock_get.return_value = fake_network_info()
 
         self.compute._build_networks_for_instance(self.context, instance,
                 self.requested_networks, self.security_groups)