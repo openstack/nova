37ec9b99c7ca85467f5d274505e86436d86657dc
Sylvain Bauza <sbauza@redhat.com>
2016-11-28 18:36:53 +0100

    Default deleted if the instance from BuildRequest is not having it
    We should already have the rehydrated instance having the deleted field being
    set. That field is also a BooleanField type, so we should never try to cast it
    from an integer (even if we support it). Instead, we need to make sure that if
    the field is not set for any reason (mostly because an Instance wasn't persisted
    before we persisted the BuildRequest object that was nesting it), then we would
    default the deleted field to False.
    
    Change-Id: I828191f750a5978fed65a86e797388d11042b4a0
    Related-Bug: #1644513

--- nova/tests/unit/objects/test_build_request.py
+++ nova/tests/unit/objects/test_build_request.py
@@ -12,6 +12,7 @@
 
 import mock
 from oslo_serialization import jsonutils
+from oslo_versionedobjects import base as o_vo_base
 
 from nova import exception
 from nova import objects
@@ -146,6 +147,17 @@ class _TestBuildRequestObject(object):
             self.assertIn(field, instance.obj_what_changed())
             self.assertEqual(getattr(build_request.instance, field),
                              getattr(instance, field))
+
+    def test_from_db_object_set_deleted(self):
+        # Assert that if we persisted an instance not yet having the deleted
+        # field being set, we still return a value for that field.
+        fake_req = fake_build_request.fake_db_req()
+        with mock.patch.object(o_vo_base.VersionedObject,
+                               'obj_set_defaults') as mock_obj_set_defaults:
+            build_request = objects.BuildRequest._from_db_object(
+                self.context, objects.BuildRequest(), fake_req)
+        mock_obj_set_defaults.assert_called_once_with('deleted')
+        self.assertFalse(build_request.instance.deleted)
 
 
 class TestBuildRequestObject(test_objects._LocalTest,
