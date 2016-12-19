0e90396666eb8c5ce699b43aa36ca2a3fa47108b
Maciej Szankin <maciej.szankin@intel.com>
2016-12-19 12:48:13 -0600

    conf: refactor conf_fixture.py
    Tidied up the conf_fixture.py file. Sorted all defaults by group and conf
    name.
    
    Change-Id: Ifa25daa28a2c2a606efa83905e4bf0720dd31fac
    Implements: blueprint centralize-config-options-ocata

--- nova/tests/unit/conf_fixture.py
+++ nova/tests/unit/conf_fixture.py
@@ -31,39 +31,52 @@ class ConfFixture(config_fixture.Config):
     """Fixture to manage global conf settings."""
     def setUp(self):
         super(ConfFixture, self).setUp()
-        self.conf.set_default('api_paste_config',
-                              paths.state_path_def('etc/nova/api-paste.ini'),
-                              group='wsgi')
-        self.conf.set_default('host', 'fake-mini')
+
+        # default group
         self.conf.set_default('compute_driver', 'fake.SmallFakeDriver')
         self.conf.set_default('fake_network', True)
         self.conf.set_default('flat_network_bridge', 'br100')
         self.conf.set_default('floating_ip_dns_manager',
                               'nova.tests.unit.utils.dns_manager')
+        self.conf.set_default('force_dhcp_release', False)
+        self.conf.set_default('host', 'fake-mini')
         self.conf.set_default('instance_dns_manager',
                               'nova.tests.unit.utils.dns_manager')
         self.conf.set_default('network_size', 8)
         self.conf.set_default('num_networks', 2)
-        self.conf.set_default('use_ipv6', True)
-        self.conf.set_default('vlan_interface', 'eth0')
-        self.conf.set_default('auth_strategy', 'noauth2', group='api')
-        config.parse_args([], default_config_files=[], configure_db=False,
-                          init_rpc=False)
-        self.conf.set_default('connection', "sqlite://", group='database')
-        self.conf.set_default('connection', "sqlite://", group='api_database')
-        self.conf.set_default('sqlite_synchronous', False, group='database')
-        self.conf.set_default('sqlite_synchronous', False,
-                group='api_database')
+        self.conf.set_default('periodic_enable', False)
         # TODO(sdague): this makes our project_id match 'fake' as well.
         # We should fix the tests to use real
         # UUIDs then drop this work around.
         self.conf.set_default('project_id_regex',
                               '[0-9a-fk\-]+', 'osapi_v21')
-        self.conf.set_default('force_dhcp_release', False)
-        self.conf.set_default('periodic_enable', False)
+        self.conf.set_default('use_ipv6', True)
+        self.conf.set_default('vlan_interface', 'eth0')
+
+        # api group
+        self.conf.set_default('auth_strategy', 'noauth2', group='api')
+
+        # api_database group
+        self.conf.set_default('connection', "sqlite://", group='api_database')
+        self.conf.set_default('sqlite_synchronous', False,
+                              group='api_database')
+
+        # database group
+        self.conf.set_default('connection', "sqlite://", group='database')
+        self.conf.set_default('sqlite_synchronous', False, group='database')
+
+        # key_manager group
         self.conf.set_default('api_class',
                               'nova.keymgr.conf_key_mgr.ConfKeyManager',
                               group='key_manager')
+
+        # wsgi group
+        self.conf.set_default('api_paste_config',
+                              paths.state_path_def('etc/nova/api-paste.ini'),
+                              group='wsgi')
+
+        config.parse_args([], default_config_files=[], configure_db=False,
+                          init_rpc=False)
         policy_opts.set_defaults(self.conf)
         self.addCleanup(utils.cleanup_dns_managers)
         self.addCleanup(ipv6.api.reset_backend)
