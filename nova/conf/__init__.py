c2511e4d08529095e3ca161d87065178b7352fdc
Stephen Finucane <sfinucan@redhat.com>
2016-12-19 09:50:22 +0000

    conf: Remove 'virt' file
    We have "compute" options and "virt" options but there isn't really a
    difference between the two. Move two to "network" and the remainder to
    "compute", allowing us to drop "virt".
    
    There are no actual changes to the opts, besides a typo fix for the
    'reserved_huge_pages' opt.
    
    Change-Id: Ibe7ba5716c6bfb096aabb85c73e5167f337a7327
    Implements: blueprint centralize-config-options-ocata

--- nova/conf/__init__.py
+++ nova/conf/__init__.py
@@ -64,7 +64,6 @@ from nova.conf import servicegroup
 from nova.conf import spice
 from nova.conf import ssl
 from nova.conf import upgrade_levels
-from nova.conf import virt
 from nova.conf import vmware
 from nova.conf import vnc
 from nova.conf import workarounds
@@ -118,7 +117,6 @@ servicegroup.register_opts(CONF)
 spice.register_opts(CONF)
 ssl.register_opts(CONF)
 upgrade_levels.register_opts(CONF)
-virt.register_opts(CONF)
 vmware.register_opts(CONF)
 vnc.register_opts(CONF)
 workarounds.register_opts(CONF)
