d5c29e1be5ff12045c6dd324ca73509a5213adc3
Jay Jahns <jjahns@vmware.com>
2016-12-22 04:43:17 -0800

    Add connection pool size to vSphere settings
    In larger environments, the default value of 10 for the HTTP connection
    pool is insufficient as Nova will begin to warn user that the pool
    is full and begin discarding connections.
    
    This change adds the ability to configure the connection_pool_size in
    Nova.  The default is 10 and the minimum value is also 10.
    
    Closes-Bug: #1494688
    Co-Authored-By: rgerganov@vmware.com
    Change-Id: I2990899b467ac86664e1b2859f8a190aba7c2905

--- nova/conf/vmware.py
+++ nova/conf/vmware.py
@@ -191,6 +191,16 @@ operation from the cache location to the hypervisor file directory
 in the shared datastore. If set to true, the above copy operation
 is avoided as it creates copy of the virtual machine that shares
 virtual disks with its parent VM.
+"""),
+    cfg.IntOpt('connection_pool_size',
+               min=10,
+               default=10,
+               help="""
+This option sets the http connection pool size
+
+The connection pool size is the maximum number of connections from nova to
+vSphere.  It should only be increased if there are warnings indicating that
+the connection pool is full, otherwise, the default should suffice.
 """)
 ]
 
