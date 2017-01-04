98b9001a7b64c25b692a5579233d2bfb62fb4226
Stephen Finucane <sfinucan@redhat.com>
2017-01-04 10:45:08 +0000

    tools: Remove 'with_venv'
    Nothing appears to use this any more. No point in keeping it around.
    
    Change-Id: I7150c78f55fd2eb639ed85c868d38461219fda12

--- tools/with_venv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tools_path=${tools_path:-$(dirname $0)}
-venv_path=${venv_path:-${tools_path}}
-venv_dir=${venv_name:-/../.venv}
-TOOLS=${tools_path}
-VENV=${venv:-${venv_path}/${venv_dir}}
-source ${VENV}/bin/activate && "$@"
