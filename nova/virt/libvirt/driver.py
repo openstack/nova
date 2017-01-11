df334b4f414b8c341709df37ca10065d3b50fcef
Pawel Koniszewski <pawel.koniszewski@intel.com>
2017-01-11 17:43:40 +0100

    Add new configuration option live_migration_scheme
    This change adds a new configuration option to change scheme that is used
    for live migrations. The change also deprecates old live_migration_uri
    in favor of two other flags:
    
    * live_migraton_inbound_addr to configure live migration target URI
    * live_migration_scheme to change scheme used for live migration
    
    The new configuration option does not break backward compatibility as
    it does not override scheme set in live_migration_uri.
    
    Change-Id: I38b3d1d42b2ffc4a2f6d7398061829d3c745b9a1

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -684,15 +684,25 @@ class LibvirtDriver(driver.ComputeDriver):
         # Only Xen and QEMU support live migration, see
         # https://libvirt.org/migration.html#scenarios for reference
         uris = {
-            'kvm': 'qemu+tcp://%s/system',
-            'qemu': 'qemu+tcp://%s/system',
+            'kvm': 'qemu+%s://%s/system',
+            'qemu': 'qemu+%s://%s/system',
             'xen': 'xenmigr://%s/system',
         }
         virt_type = CONF.libvirt.virt_type
-        uri = CONF.libvirt.live_migration_uri or uris.get(virt_type)
+        # TODO(pkoniszewski): Remove fetching live_migration_uri in Pike
+        uri = CONF.libvirt.live_migration_uri
+        if uri:
+            return uri % dest
+
+        uri = uris.get(virt_type)
         if uri is None:
             raise exception.LiveMigrationURINotAvailable(virt_type=virt_type)
-        return uri % dest
+
+        str_format = (dest,)
+        if virt_type in ('kvm', 'qemu'):
+            scheme = CONF.libvirt.live_migration_scheme or 'tcp'
+            str_format = (scheme, dest)
+        return uris.get(virt_type) % str_format
 
     @staticmethod
     def _migrate_uri(dest):
