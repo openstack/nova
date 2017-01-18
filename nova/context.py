377f4004970e6f5b26a146b24bb108e04fe5c0ae
scottda <scott.dangelo@ibm.com>
2017-01-18 09:10:21 -0700

    Fix Nova to allow using cinder v3 endpoint
    Nova creates a limited service catalog using just the endpoints
    required for Nova. This needs to include the new Cinder v3.
    
    Change-Id: I06f9f2885bb1748897332cb15d4aafe4a99aa9dc
    Closes-Bug: #1657496

--- nova/context.py
+++ nova/context.py
@@ -102,8 +102,8 @@ class RequestContext(context.RequestContext):
         if service_catalog:
             # Only include required parts of service_catalog
             self.service_catalog = [s for s in service_catalog
-                if s.get('type') in ('volume', 'volumev2', 'key-manager',
-                                     'placement')]
+                if s.get('type') in ('volume', 'volumev2', 'volumev3',
+                                     'key-manager', 'placement')]
         else:
             # if list is empty or none
             self.service_catalog = []
