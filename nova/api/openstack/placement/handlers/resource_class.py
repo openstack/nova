95e1da06e7d407cbdb427b19946021c8a95324d0
Pushkar Umaranikar <pushkar.umaranikar@intel.com>
2017-02-07 22:36:06 +0000

    Placement api: set custom json_error_formatter in resource_class
    Change decorator for resource based actions in resource_class
    handler to wsgi_wrapper.PlacementWsgify
    
    This is a newly introduced wrapper class around webob.dec.wsgify
    to set json formatter in case of webob exceptions.
    
    Change-Id: Ic330d23b2eda3b3f299291b9cdc4489d02481786
    Partial-Bug: #1635182

--- nova/api/openstack/placement/handlers/resource_class.py
+++ nova/api/openstack/placement/handlers/resource_class.py
@@ -19,6 +19,7 @@ import webob
 
 from nova.api.openstack.placement import microversion
 from nova.api.openstack.placement import util
+from nova.api.openstack.placement import wsgi_wrapper
 from nova import exception
 from nova.i18n import _
 from nova import objects
@@ -63,7 +64,7 @@ def _serialize_resource_classes(environ, rcs):
     return {"resource_classes": output}
 
 
-@webob.dec.wsgify
+@wsgi_wrapper.PlacementWsgify
 @microversion.version_handler(1.2)
 @util.require_content('application/json')
 def create_resource_class(req):
@@ -81,15 +82,13 @@ def create_resource_class(req):
     except exception.ResourceClassExists:
         raise webob.exc.HTTPConflict(
             _('Conflicting resource class already exists: %(name)s') %
-            {'name': data['name']},
-            json_formatter=util.json_error_formatter)
+            {'name': data['name']})
     except exception.MaxDBRetriesExceeded:
         raise webob.exc.HTTPConflict(
             _('Max retries of DB transaction exceeded attempting '
               'to create resource class: %(name)s, please'
               'try again.') %
-            {'name': data['name']},
-            json_formatter=util.json_error_formatter)
+            {'name': data['name']})
 
     req.response.location = util.resource_class_url(req.environ, rc)
     req.response.status = 201
@@ -97,7 +96,7 @@ def create_resource_class(req):
     return req.response
 
 
-@webob.dec.wsgify
+@wsgi_wrapper.PlacementWsgify
 @microversion.version_handler(1.2)
 def delete_resource_class(req):
     """DELETE to destroy a single resource class.
@@ -113,19 +112,17 @@ def delete_resource_class(req):
     except exception.ResourceClassCannotDeleteStandard as exc:
         raise webob.exc.HTTPBadRequest(
             _('Cannot delete standard resource class %(rp_name)s: %(error)s') %
-            {'rp_name': name, 'error': exc},
-            json_formatter=util.json_error_formatter)
+            {'rp_name': name, 'error': exc})
     except exception.ResourceClassInUse as exc:
         raise webob.exc.HTTPConflict(
             _('Unable to delete resource class %(rp_name)s: %(error)s') %
-            {'rp_name': name, 'error': exc},
-            json_formatter=util.json_error_formatter)
+            {'rp_name': name, 'error': exc})
     req.response.status = 204
     req.response.content_type = None
     return req.response
 
 
-@webob.dec.wsgify
+@wsgi_wrapper.PlacementWsgify
 @microversion.version_handler(1.2)
 @util.check_accept('application/json')
 def get_resource_class(req):
@@ -146,7 +143,7 @@ def get_resource_class(req):
     return req.response
 
 
-@webob.dec.wsgify
+@wsgi_wrapper.PlacementWsgify
 @microversion.version_handler(1.2)
 @util.check_accept('application/json')
 def list_resource_classes(req):
@@ -166,7 +163,7 @@ def list_resource_classes(req):
     return response
 
 
-@webob.dec.wsgify
+@wsgi_wrapper.PlacementWsgify
 @microversion.version_handler(1.2)
 @util.require_content('application/json')
 def update_resource_class(req):
@@ -190,13 +187,11 @@ def update_resource_class(req):
     except exception.ResourceClassExists:
         raise webob.exc.HTTPConflict(
             _('Resource class already exists: %(name)s') %
-            {'name': rc.name},
-            json_formatter=util.json_error_formatter)
+            {'name': rc.name})
     except exception.ResourceClassCannotUpdateStandard:
         raise webob.exc.HTTPBadRequest(
             _('Cannot update standard resource class %(rp_name)s') %
-            {'rp_name': name},
-            json_formatter=util.json_error_formatter)
+            {'rp_name': name})
 
     req.response.body = encodeutils.to_utf8(jsonutils.dumps(
         _serialize_resource_class(req.environ, rc))
