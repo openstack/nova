1f48d3d83b4d5f6f9cd96ee06d2fc005635c1ff9
Sivasathurappan Radhakrishnan <siva.radhakrishnan@intel.com>
2017-02-16 12:51:45 +0000

    Port binding based on events during live migration
    Currently port binding call is made at destination compute in post live
    migration phase. This may cause network outage during post-copy as the
    virtual CPUs are paused immediately at source and unpaused at
    destination by transferring a minimum set of pages.
    
    The following domain life cycle events are emitted in this order during
    post-copy:
    
    * VIR_DOMAIN_EVENT_STARTED(destination)
    * VIR_DOMAIN_EVENT_SUSPENDED_POSTCOPY(source)--migration
      entered post-copy mode
    * VIR_DOMAIN_EVENT_RESUMED_POSTCOPY(destination)--guest
      is running on the destinaton host while some if its
      memory pages still remain on sourcehost.
    * VIR_DOMAIN_EVENT_RESUMED_MIGRATED(destination)
    * VIR_DOMAIN_EVENT_STOPPED_MIGRATED(source)--migration
      finished successfully and the destination host holds
      a complete guest state.
    
    In this change, dest host port binding activation is done when the
    following events are emitted at source for post-copy and pre-copy:
    
    * VIR_DOMAIN_EVENT_SUSPENDED_POSTCOPY
    * VIR_DOMAIN_EVENT_SUSPENDED_MIGRATED
    
    This reduces the network outage during live migration as network switch
    is done right before VM resumes at destination.
    
    Co-Authored-By: Matt Riedemann <mriedem.os@gmail.com>
    
    Change-Id: Ic5cab99944df9e501ba2032eb96911c36304494d
    Closes-Bug: #1605016

--- nova/tests/unit/virt/libvirt/test_host.py
+++ nova/tests/unit/virt/libvirt/test_host.py
@@ -192,6 +192,46 @@ class HostTestCase(test.NoDBTestCase):
         self.assertEqual(got_events[0].transition,
                          event.EVENT_LIFECYCLE_STOPPED)
 
+    def test_event_lifecycle_callback_suspended_old_libvirt(self):
+        """Tests the suspended lifecycle event with libvirt before post-copy
+        """
+        hostimpl = mock.MagicMock()
+        conn = mock.MagicMock()
+        fake_dom_xml = """
+                <domain type='kvm'>
+                  <uuid>cef19ce0-0ca2-11df-855d-b19fbce37686</uuid>
+                </domain>
+            """
+        dom = fakelibvirt.Domain(conn, fake_dom_xml, running=True)
+        VIR_DOMAIN_EVENT_SUSPENDED_PAUSED = 0
+        host.Host._event_lifecycle_callback(
+            conn, dom, fakelibvirt.VIR_DOMAIN_EVENT_SUSPENDED,
+            detail=VIR_DOMAIN_EVENT_SUSPENDED_PAUSED, opaque=hostimpl)
+        expected_event = hostimpl._queue_event.call_args[0][0]
+        self.assertEqual(event.EVENT_LIFECYCLE_PAUSED,
+                         expected_event.transition)
+
+    def test_event_lifecycle_callback_suspended_postcopy(self):
+        """Tests the suspended lifecycle event with libvirt with post-copy"""
+        hostimpl = mock.MagicMock()
+        conn = mock.MagicMock()
+        fake_dom_xml = """
+                <domain type='kvm'>
+                  <uuid>cef19ce0-0ca2-11df-855d-b19fbce37686</uuid>
+                </domain>
+            """
+        dom = fakelibvirt.Domain(conn, fake_dom_xml, running=True)
+        VIR_DOMAIN_EVENT_SUSPENDED_POSTCOPY = 7
+        with mock.patch.object(host.libvirt,
+                               'VIR_DOMAIN_EVENT_SUSPENDED_POSTCOPY', new=7,
+                               create=True):
+            host.Host._event_lifecycle_callback(
+                conn, dom, fakelibvirt.VIR_DOMAIN_EVENT_SUSPENDED,
+                detail=VIR_DOMAIN_EVENT_SUSPENDED_POSTCOPY, opaque=hostimpl)
+        expected_event = hostimpl._queue_event.call_args[0][0]
+        self.assertEqual(event.EVENT_LIFECYCLE_POSTCOPY_STARTED,
+                         expected_event.transition)
+
     def test_event_emit_delayed_call_delayed(self):
         ev = event.LifecycleEvent(
             "cef19ce0-0ca2-11df-855d-b19fbce37686",
