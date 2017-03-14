59da6bc990bf5a839f1e88d72b9cff103c50b179
Anusha Unnam <anusha.unnam@intel.com>
2017-03-14 00:03:07 +0000

    Add description to policies to availability_zone
    The 'discoverable' policy is not documented as it will be removed
    in a future change.
    
    blueprint policy-docs
    
    Change-Id: I25eb7b34490f7a113bcaf6e95ee65986943e418d

--- nova/policies/availability_zone.py
+++ nova/policies/availability_zone.py
@@ -22,15 +22,29 @@ POLICY_ROOT = 'os_compute_api:os-availability-zone:%s'
 
 
 availability_zone_policies = [
-    policy.RuleDefault(
-        name=POLICY_ROOT % 'list',
-        check_str=base.RULE_ADMIN_OR_OWNER),
+    base.create_rule_default(
+        POLICY_ROOT % 'list',
+        base.RULE_ADMIN_OR_OWNER,
+        "Lists availability zone information without host information",
+        [
+            {
+                'method': 'GET',
+                'path': 'os-availability-zone'
+            }
+        ]),
     policy.RuleDefault(
         name=POLICY_ROOT % 'discoverable',
         check_str=base.RULE_ANY),
-    policy.RuleDefault(
-        name=POLICY_ROOT % 'detail',
-        check_str=base.RULE_ADMIN_API),
+    base.create_rule_default(
+        POLICY_ROOT % 'detail',
+        base.RULE_ADMIN_API,
+        "Lists detailed availability zone information with host information",
+        [
+            {
+                'method': 'GET',
+                'path': '/os-availability-zone/detail'
+            }
+        ])
 ]
 
 
