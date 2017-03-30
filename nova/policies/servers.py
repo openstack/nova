d284f0e388af8eaf1d77a82c1752c23d6481a2b0
Anusha Unnam <anusha.unnam@intel.com>
2017-03-30 20:16:26 +0000

    Add description to policies in servers.py
    The 'discoverable' policy is not documented as it will be removed in a
    future change.
    
    blueprint policy-docs
    
    Change-Id: I17b6ca6e17c777ae7d337bf70ec4774ffe5187a8

--- nova/policies/servers.py
+++ nova/policies/servers.py
@@ -20,31 +20,232 @@ RULE_AOO = base.RULE_ADMIN_OR_OWNER
 SERVERS = 'os_compute_api:servers:%s'
 
 rules = [
-    policy.RuleDefault(SERVERS % 'index', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'detail', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'detail:get_all_tenants',
-                       base.RULE_ADMIN_API),
-    policy.RuleDefault(SERVERS % 'index:get_all_tenants', base.RULE_ADMIN_API),
-    policy.RuleDefault(SERVERS % 'show', RULE_AOO),
+    base.create_rule_default(
+        SERVERS % 'index',
+        RULE_AOO,
+        "List all servers",
+        [
+            {
+                'method': 'GET',
+                'path': '/servers'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'detail',
+        RULE_AOO,
+        "List all servers with detailed information",
+        [
+            {
+                'method': 'GET',
+                'path': '/servers/detail'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'index:get_all_tenants',
+        base.RULE_ADMIN_API,
+        "List all servers for all projects",
+        [
+            {
+                'method': 'GET',
+                'path': '/servers'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'detail:get_all_tenants',
+        base.RULE_ADMIN_API,
+        "List all servers with detailed information for all projects",
+        [
+            {
+                'method': 'GET',
+                'path': '/servers/detail'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'show',
+        RULE_AOO,
+        "Show a server",
+        [
+            {
+                'method': 'GET',
+                'path': '/servers/{server_id}'
+            }
+        ]),
     # the details in host_status are pretty sensitive, only admins
     # should do that by default.
-    policy.RuleDefault(SERVERS % 'show:host_status', base.RULE_ADMIN_API),
-    policy.RuleDefault(SERVERS % 'create', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'create:forced_host', base.RULE_ADMIN_API),
-    policy.RuleDefault(SERVERS % 'create:attach_volume', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'create:attach_network', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'delete', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'update', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'confirm_resize', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'revert_resize', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'reboot', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'resize', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'rebuild', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'create_image', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'create_image:allow_volume_backed', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'start', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'stop', RULE_AOO),
-    policy.RuleDefault(SERVERS % 'trigger_crash_dump', RULE_AOO),
+    base.create_rule_default(
+        SERVERS % 'show:host_status',
+        base.RULE_ADMIN_API,
+        "Show a server with additional host status information",
+        [
+            {
+                'method': 'GET',
+                'path': '/servers/{server_id}'
+            },
+            {
+                'method': 'GET',
+                'path': '/servers/detail'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'create',
+        RULE_AOO,
+        "Create a server",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'create:forced_host',
+        base.RULE_ADMIN_API,
+        "Create a server on the specified host",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'create:attach_volume',
+        RULE_AOO,
+        "Create a server with the requested volume attached to it",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'create:attach_network',
+        RULE_AOO,
+        "Create a server with the requested network attached to it",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'delete',
+        RULE_AOO,
+        "Delete a server",
+        [
+            {
+                'method': 'DELETE',
+                'path': '/servers/{server_id}'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'update',
+        RULE_AOO,
+        "Update a server",
+        [
+            {
+                'method': 'PUT',
+                'path': '/servers/{server_id}'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'confirm_resize',
+        RULE_AOO,
+        "Confirm a server resize",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers/{server_id}/action (confirmResize)'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'revert_resize',
+        RULE_AOO,
+        "Revert a server resize",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers/{server_id}/action (revertResize)'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'reboot',
+        RULE_AOO,
+        "Reboot a server",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers/{server_id}/action (reboot)'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'resize',
+        RULE_AOO,
+        "Resize a server",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers/{server_id}/action (resize)'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'rebuild',
+        RULE_AOO,
+        "Rebuild a server",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers/{server_id}/action (rebuild)'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'create_image',
+        RULE_AOO,
+        "Create an image from a server",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers/{server_id}/action (createImage)'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'create_image:allow_volume_backed',
+        RULE_AOO,
+        "Create an image from a volume backed server",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers/{server_id}/action (createImage)'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'start',
+        RULE_AOO,
+        "Start a server",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers/{server_id}/action (os-start)'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'stop',
+        RULE_AOO,
+        "Stop a server",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers/{server_id}/action (os-stop)'
+            }
+        ]),
+    base.create_rule_default(
+        SERVERS % 'trigger_crash_dump',
+        RULE_AOO,
+        "Trigger crash dump in a server",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers/{server_id}/action (trigger_crash_dump)'
+            }
+        ]),
     policy.RuleDefault(SERVERS % 'discoverable', base.RULE_ANY),
 ]
 
