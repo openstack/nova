4ac0e53eadd174f744b56527ffb455e10683f362
Anusha Unnam <anusha.unnam@intel.com>
2017-03-31 17:04:06 +0000

    Add description to policies in server_external_events.py
    The 'discoverable' policy is not documented as it will be removed in a
    future change.
    
    blueprint policy-docs
    
    Change-Id: Ib4fd37bc962782e8bbea5de3cc88edf4114855c6

--- nova/policies/server_external_events.py
+++ nova/policies/server_external_events.py
@@ -22,9 +22,16 @@ POLICY_ROOT = 'os_compute_api:os-server-external-events:%s'
 
 
 server_external_events_policies = [
-    policy.RuleDefault(
-        name=POLICY_ROOT % 'create',
-        check_str=base.RULE_ADMIN_API),
+    base.create_rule_default(
+        POLICY_ROOT % 'create',
+        base.RULE_ADMIN_API,
+        "Creates one or more external events",
+        [
+            {
+                'method': 'POST',
+                'path': '/os-server-external-events'
+            }
+        ]),
     policy.RuleDefault(
         name=POLICY_ROOT % 'discoverable',
         check_str=base.RULE_ANY),
