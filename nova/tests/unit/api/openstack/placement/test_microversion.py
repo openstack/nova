577d96c682e1706cc1fb1e1b6f0a728f0d80ebd1
He Jie Xu <hejie.xu@intel.com>
2017-04-19 20:39:20 +0800

    Add test ensure all the microversions are sequential in placement API
    It is easy to make mistake when added multiple versioned methods and it
    generates a gap between mulitple versioned methods. This test ensures no
    gap between each versioned method.
    
    Change-Id: Ib91eba000ecd4ce28f51e44cc74be32fc2200a2f

--- nova/tests/unit/api/openstack/placement/test_microversion.py
+++ nova/tests/unit/api/openstack/placement/test_microversion.py
@@ -156,3 +156,18 @@ class TestMicroversionUtility(test.NoDBTestCase):
         self.assertRaises(KeyError,
              microversion.raise_http_status_code_if_not_version,
              self.req, 999, '1.5')
+
+
+class MicroversionSequentialTest(test.NoDBTestCase):
+
+    def test_microversion_sequential(self):
+        for method_name, method_list in microversion.VERSIONED_METHODS.items():
+            previous_min_version = method_list[0][0]
+            for method in method_list[1:]:
+                previous_min_version = microversion.parse_version_string(
+                    '%s.%s' % (previous_min_version.major,
+                               previous_min_version.minor - 1))
+                self.assertEqual(previous_min_version, method[1],
+                    "The microversions aren't sequential in the mehtod %s" %
+                    method_name)
+                previous_min_version = method[0]
