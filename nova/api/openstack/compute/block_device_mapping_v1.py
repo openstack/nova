7ef83e2a77a408f35f0a9c4f2e9a912986f54f45
jichenjc <jichenjc@cn.ibm.com>
2017-07-06 10:58:33 +0800

    Remove nova.api.extensions.server.extensions usage
    nova.api.extensions.server.extensions is not used anymore and
    we don't need to keep those incorrect comments anymore.
    
    Change-Id: I2f02ac8b5bc4bd8d2baffacb32b5ceda9f68168d

--- nova/api/openstack/compute/block_device_mapping_v1.py
+++ nova/api/openstack/compute/block_device_mapping_v1.py
@@ -42,8 +42,6 @@ class BlockDeviceMappingV1(extensions.V21APIExtensionBase):
         return []
 
 
-# use nova.api.extensions.server.extensions entry point to modify
-# server create kwargs
 # NOTE(gmann): This function is not supposed to use 'body_deprecated_param'
 # parameter as this is placed to handle scheduler_hint extension for V2.1.
 def server_create(server_dict, create_kwargs, body_deprecated_param):
