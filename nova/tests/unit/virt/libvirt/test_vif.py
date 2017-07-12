6ccff15928ce40d1d5b653d62d9e516911bda69a
Stephen Finucane <sfinucan@redhat.com>
2017-07-12 17:27:41 +0100

    tests: Remove useless test
    OVS VIFs, whether "hybrid" or not, are now handled by os-vif and the
    configuration of nova-network's firewall will therefore have no impact
    on them.
    
    While the aim of these test is lost to me, changing the nova-network
    option does not cause the test to fail. It is therefore useless and can
    be removed, along with a variable that only it uses now.
    
    Change-Id: I94b7d048831744f178884c23ff2f0361c3beadc7

--- nova/tests/unit/virt/libvirt/test_vif.py
+++ nova/tests/unit/virt/libvirt/test_vif.py
@@ -151,15 +151,6 @@ class LibvirtVifTestCase(test.NoDBTestCase):
         details={'port_filter': False},
         ovs_interfaceid='aaa-bbb-ccc')
 
-    vif_ovs_hybrid = network_model.VIF(id='vif-xxx-yyy-zzz',
-        address='ca:fe:de:ad:be:ef',
-        network=network_ovs,
-        type=network_model.VIF_TYPE_OVS,
-        details={'ovs_hybrid_plug': True,
-                 'port_filter': True},
-        devname='tap-xxx-yyy-zzz',
-        ovs_interfaceid='aaa-bbb-ccc')
-
     vif_ovs_filter_cap = network_model.VIF(id='vif-xxx-yyy-zzz',
         address='ca:fe:de:ad:be:ef',
         network=network_ovs,
@@ -1188,16 +1179,6 @@ class LibvirtVifTestCase(test.NoDBTestCase):
         node = self._get_node(xml)
         self._assertTypeAndMacEquals(node, "ethernet", "target", "dev",
                                      self.vif_ivs_filter_cap, br_want, 0)
-
-    def test_hybrid_plug_without_nova_firewall(self):
-        d = vif.LibvirtGenericVIFDriver()
-        br_want = "qbr" + self.vif_ovs_hybrid['id']
-        br_want = br_want[:network_model.NIC_NAME_LEN]
-        self.flags(firewall_driver="nova.virt.firewall.NoopFirewallDriver")
-        xml = self._get_instance_xml(d, self.vif_ovs_hybrid)
-        node = self._get_node(xml)
-        self._assertTypeAndMacEquals(node, "bridge", "source", "bridge",
-                                     self.vif_ovs_hybrid, br_want, 0)
 
     def test_direct_plug_with_port_filter_cap_no_nova_firewall(self):
         d = vif.LibvirtGenericVIFDriver()
