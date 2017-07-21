c27b76379e16d0781be5daf05c448fd8fa699bb6
He Jie Xu <hejie.xu@intel.com>
2017-07-21 17:14:41 +0800

    Remove the useless extension block_device_mapping_v1 object
    BlockDeviceMappingV1 isn't used by anywhere, so remove it.
    
    Partial-implement-blueprint api-no-more-extensions-pike
    
    Change-Id: I3658c4ea8f6b74dbf60b778a840f09e3dd2f19de

--- nova/api/openstack/compute/block_device_mapping_v1.py
+++ nova/api/openstack/compute/block_device_mapping_v1.py
@@ -20,26 +20,10 @@ from webob import exc
 
 from nova.api.openstack.compute.schemas import block_device_mapping_v1 as \
     schema_block_device_mapping
-from nova.api.openstack import extensions
 from nova.i18n import _
 
-ALIAS = "os-block-device-mapping-v1"
 ATTRIBUTE_NAME = "block_device_mapping"
 ATTRIBUTE_NAME_V2 = "block_device_mapping_v2"
-
-
-class BlockDeviceMappingV1(extensions.V21APIExtensionBase):
-    """Block device mapping boot support."""
-
-    name = "BlockDeviceMappingV1"
-    alias = ALIAS
-    version = 1
-
-    def get_resources(self):
-        return []
-
-    def get_controller_extensions(self):
-        return []
 
 
 # NOTE(gmann): This function is not supposed to use 'body_deprecated_param'
