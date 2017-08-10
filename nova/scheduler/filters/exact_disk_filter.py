7e3c4dbf3a1273fdfce7667d986975cc7f13c7a3
Dmitry Tantsur <divius.inside@gmail.com>
2017-08-10 16:36:06 +0200

    Deprecate bare metal filters
    This change deprecates the Exact filters, and two options commonly used
    to enable them. Custom resource classes should be used instead.
    
    Change-Id: I843353427c90142a366ae9ca63ee4298b4f3ecd4
    (cherry picked from commit b4295ef0496849c1bc43481d31f57d79afaf6b7b)

--- nova/scheduler/filters/exact_disk_filter.py
+++ nova/scheduler/filters/exact_disk_filter.py
@@ -23,6 +23,11 @@ LOG = logging.getLogger(__name__)
 class ExactDiskFilter(filters.BaseHostFilter):
     """Exact Disk Filter."""
 
+    def __init__(self, *args, **kwargs):
+        super(ExactDiskFilter, self).__init__(*args, **kwargs)
+        LOG.warning('ExactDiskFilter is deprecated in Pike and will be '
+                    'removed in a subsequent release.')
+
     def host_passes(self, host_state, spec_obj):
         """Return True if host has the exact amount of disk available."""
         requested_disk = (1024 * (spec_obj.root_gb +
