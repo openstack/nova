4b596bd6dafd103368e81769ff236eb5dc8f36ec
Michael Still <mikal@stillhq.com>
2017-08-20 16:07:56 +1000

    Replace dd with shred for zeroing lvm volumes.
    Per the previous code comment, we can now use shred to zero
    volumes efficiently on modern distros. Let's do that.
    
    A thread on openstack-dev (http://lists.openstack.org/pipermail/openstack-dev/2017-August/121301.html)
    confirms that all our supported distros now use a new enough
    version of shred.
    
    Change-Id: I1f0ba72ddeb26eb51bf5e7c3db017b108e0494e9

--- nova/virt/libvirt/storage/lvm.py
+++ nova/virt/libvirt/storage/lvm.py
@@ -164,37 +164,6 @@ def get_volume_size(path):
     return int(out)
 
 
-def _zero_volume(path, volume_size):
-    """Write zeros over the specified path
-
-    :param path: logical volume path
-    :param size: number of zeros to write
-    """
-    bs = units.Mi
-    direct_flags = ('oflag=direct',)
-    sync_flags = ()
-    remaining_bytes = volume_size
-
-    # The loop efficiently writes zeros using dd,
-    # and caters for versions of dd that don't have
-    # the easier to use iflag=count_bytes option.
-    while remaining_bytes:
-        zero_blocks = remaining_bytes // bs
-        seek_blocks = (volume_size - remaining_bytes) // bs
-        zero_cmd = ('dd', 'bs=%s' % bs,
-                    'if=/dev/zero', 'of=%s' % path,
-                    'seek=%s' % seek_blocks, 'count=%s' % zero_blocks)
-        zero_cmd += direct_flags
-        zero_cmd += sync_flags
-        if zero_blocks:
-            utils.execute(*zero_cmd, run_as_root=True)
-        remaining_bytes %= bs
-        bs //= units.Ki  # Limit to 3 iterations
-        # Use O_DIRECT with initial block size and fdatasync otherwise
-        direct_flags = ()
-        sync_flags = ('conv=fdatasync',)
-
-
 def clear_volume(path):
     """Obfuscate the logical volume.
 
@@ -216,13 +185,13 @@ def clear_volume(path):
     if volume_clear_size != 0 and volume_clear_size < volume_size:
         volume_size = volume_clear_size
 
+    cmd = ['shred']
     if volume_clear == 'zero':
-        # NOTE(p-draigbrady): we could use shred to do the zeroing
-        # with -n0 -z, however only versions >= 8.22 perform as well as dd
-        _zero_volume(path, volume_size)
-    elif volume_clear == 'shred':
-        utils.execute('shred', '-n3', '-s%d' % volume_size, path,
-                      run_as_root=True)
+        cmd.extend(['-n0', '-z'])
+    else:
+        cmd.extend(['-n3'])
+    cmd.extend(['-s%d' % volume_size, path])
+    utils.execute(*cmd, run_as_root=True)
 
 
 def remove_volumes(paths):
