497e0321f1ab4412a1940e2d885768fec5c23b46
jichenjc <jichenjc@cn.ibm.com>
2017-12-01 18:55:06 +0800

    deprecate fping_path config option
    /os-fping was deprecated at API version 2.36, ``fping_path`` was
    only used by this API so it should be able to be deprecated.
    
    Change-Id: I7d3faae0013315d595386ff262cadf8b18f70c68

--- nova/conf/api.py
+++ nova/conf/api.py
@@ -320,6 +320,13 @@ fping_path_opts = [
     cfg.StrOpt("fping_path",
         default="/usr/sbin/fping",
         deprecated_group="DEFAULT",
+        deprecated_for_removal=True,
+        deprecated_since="18.0.0",
+        deprecated_reason="""
+This option only used in /os-fping API and the API itself was deprecated
+at version 2.36 (Newton release), also, the API itself is based on nova-network
+and nova-network is deprecated as well.
+""",
         help="The full path to the fping binary.")
 ]
 
