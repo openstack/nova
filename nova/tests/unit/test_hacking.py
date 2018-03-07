fbb69db7c7a40ba351606efd070605475ecb9c10
Takashi NATSUME <natsume.takashi@lab.ntt.co.jp>
2018-03-07 14:45:42 +0900

    Removed unnecessary parantheses in yield statements
    The 'yield' statement is not a function.
    So it must always be followed by a space when yielding a value.
    
    Change-Id: Ie2aa1c4822d8adf721ba71d467b63209bede6fb7

--- nova/tests/unit/test_hacking.py
+++ nova/tests/unit/test_hacking.py
@@ -802,3 +802,25 @@ class HackingTestCase(test.NoDBTestCase):
                   import ab.cd.efg as d.efg
                """
         self._assert_has_no_errors(code, checks.no_redundant_import_alias)
+
+    def test_yield_followed_by_space(self):
+        code = """
+                  yield(x, y)
+                  yield{"type": "test"}
+                  yield[a, b, c]
+                  yield"test"
+                  yield'test'
+               """
+        errors = [(x + 1, 0, 'N360') for x in range(5)]
+        self._assert_has_errors(code, checks.yield_followed_by_space,
+                                expected_errors=errors)
+        code = """
+                  yield x
+                  yield (x, y)
+                  yield {"type": "test"}
+                  yield [a, b, c]
+                  yield "test"
+                  yield 'test'
+                  yieldx_func(a, b)
+               """
+        self._assert_has_no_errors(code, checks.yield_followed_by_space)
