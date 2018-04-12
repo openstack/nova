cba1a3e2c1b161204a3662a0d9fbf33da38aa7d3
Matt Riedemann <mriedem.os@gmail.com>
2018-04-11 21:24:43 -0400

    Delete allocations from API if nova-compute is down
    When performing a "local delete" of an instance, we
    need to delete the allocations that the instance has
    against any resource providers in Placement.
    
    It should be noted that without this change, restarting
    the nova-compute service will delete the allocations
    for its compute node (assuming the compute node UUID
    is the same as before the instance was deleted). That
    is shown in the existing functional test modified here.
    
    The more important reason for this change is that in
    order to fix bug 1756179, we need to make sure the
    resource provider allocations for a given compute node
    are gone by the time the compute service is deleted.
    
    This adds a new functional test and a release note for
    the new behavior and need to configure nova-api for
    talking to placement, which is idempotent if
    not configured thanks to the @safe_connect decorator
    used in SchedulerReportClient.
    
    Closes-Bug: #1679750
    Related-Bug: #1756179
    
    Change-Id: If507e23f0b7e5fa417041c3870d77786498f741d
    (cherry picked from commit ea9d0af31395fbe1686fa681cd91226ee580796e)

--- nova/tests/functional/regressions/test_bug_1679750.py
+++ nova/tests/functional/regressions/test_bug_1679750.py
@@ -10,6 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from nova.scheduler.client import report as reportclient
 from nova import test
 from nova.tests import fixtures as nova_fixtures
 from nova.tests.functional import integrated_helpers
@@ -55,12 +56,87 @@ class TestLocalDeleteAllocations(test.TestCase,
         resp = self.placement_api.get(fmt % {'uuid': rp_uuid})
         return resp.body['usages']
 
-    def test_local_delete_removes_allocations(self):
+    # NOTE(mriedem): It would be preferable to use the PlacementFixture as
+    # a context manager but that causes some issues when trying to delete the
+    # server in test_local_delete_removes_allocations_after_compute_restart.
+    def _stub_compute_api_to_not_configure_placement(self):
+        """Prior to the compute API deleting allocations in the "local delete"
+        case, nova.conf for nova-api might not be configured for talking to
+        the placement service, so we can mock that behavior by stubbing out
+        the placement client in the compute API to no-op as if safe_connect
+        failed and returned None to the caller.
+        """
+        orig_delete_alloc = (
+            reportclient.SchedulerReportClient.delete_allocation_for_instance)
+        self.call_count = 0
+
+        def fake_delete_allocation_for_instance(*args, **kwargs):
+            # The first call will be from the API, so ignore that one and
+            # return None like the @safe_connect decorator would if nova-api
+            # wasn't configured to talk to placement.
+            if self.call_count:
+                orig_delete_alloc(*args, **kwargs)
+            else:
+                self.call_count += 1
+
+        self.stub_out('nova.scheduler.client.report.SchedulerReportClient.'
+                      'delete_allocation_for_instance',
+                      fake_delete_allocation_for_instance)
+
+    def test_local_delete_removes_allocations_after_compute_restart(self):
         """Tests that allocations are removed after a local delete.
 
         This tests the scenario where a server is local deleted (because the
         compute host is down) and we want to make sure that its allocations
-        have been cleaned up.
+        have been cleaned up once the nova-compute service restarts.
+        """
+        self._stub_compute_api_to_not_configure_placement()
+        # Get allocations, make sure they are 0.
+        resp = self.placement_api.get('/resource_providers')
+        rp_uuid = resp.body['resource_providers'][0]['uuid']
+        usages_before = self._get_usages(rp_uuid)
+        for usage in usages_before.values():
+            self.assertEqual(0, usage)
+
+        # Create a server.
+        server = self._build_minimal_create_server_request(self.api,
+            'local-delete-test', self.image_id, self.flavor_id, 'none')
+        server = self.admin_api.post_server({'server': server})
+        server = self._wait_for_state_change(self.api, server, 'ACTIVE')
+
+        # Assert usages are non zero now.
+        usages_during = self._get_usages(rp_uuid)
+        for usage in usages_during.values():
+            self.assertNotEqual(0, usage)
+
+        # Force-down compute to trigger local delete.
+        self.compute.stop()
+        compute_service_id = self.admin_api.get_services(
+            host=self.compute.host, binary='nova-compute')[0]['id']
+        self.admin_api.put_service(compute_service_id, {'forced_down': True})
+
+        # Delete the server (will be a local delete because compute is down).
+        self.api.delete_server(server['id'])
+
+        # Assert usages are still non-zero.
+        usages_during = self._get_usages(rp_uuid)
+        for usage in usages_during.values():
+            self.assertNotEqual(0, usage)
+
+        # Start the compute service again. Before it comes up, it will call the
+        # update_available_resource code in the ResourceTracker which is what
+        # "heals" the allocations for the deleted instance.
+        self.compute.start()
+
+        # Get the allocations again to check against the original.
+        usages_after = self._get_usages(rp_uuid)
+
+        # They should match.
+        self.assertEqual(usages_before, usages_after)
+
+    def test_local_delete_removes_allocations_from_api(self):
+        """Tests that the compute API deletes allocations when the compute
+        service on which the instance was running is down.
         """
         # Get allocations, make sure they are 0.
         resp = self.placement_api.get('/resource_providers')
@@ -89,12 +165,7 @@ class TestLocalDeleteAllocations(test.TestCase,
         # Delete the server (will be a local delete because compute is down).
         self.api.delete_server(server['id'])
 
-        # Start the compute service again. Before it comes up, it will call the
-        # update_available_resource code in the ResourceTracker which is what
-        # "heals" the allocations for the deleted instance.
-        self.compute.start()
-
-        # Get the allocations again to check against the original.
+        # Get the allocations again to make sure they were deleted.
         usages_after = self._get_usages(rp_uuid)
 
         # They should match.
