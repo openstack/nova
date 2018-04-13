7bcd581c78bb5916bf4b52e213322e7b56283572
Matt Riedemann <mriedem.os@gmail.com>
2018-04-13 13:44:33 -0400

    Add policy rule to block image-backed servers with 0 root disk flavor
    This adds a new policy rule which defaults to behave in a
    backward compatible way, but will allow operators to enforce
    that servers created with a zero disk flavor must also be
    volume-backed servers.
    
    Allowing users to upload their own images and create image-backed
    servers on local disk with zero root disk size flavors can be
    potentially hazardous if the size of the image is unexpectedly
    large, since it can consume the local disk (or shared storage pool).
    
    It should be noted that disabling the new policy rule will
    result in a non-backward compatible API behavior change and no
    microversion is being introduced for this because enforcement via
    a new microversion would not close the security gap on any previous
    microversions.
    
    Related compute API reference and user documentation is updated
    to mention the policy rule along with a release note since
    this is tied to a security bug, which will be backported to stable
    branches.
    
    Conflicts:
          nova/policies/servers.py
          nova/tests/unit/test_policy.py
    
    NOTE(mriedem): The conflict is due to not having change
    Iedd3fea0e86648fae364f075915555dcb2c4f199 in Queens for trusted
    certs.
    
    Change-Id: Id67e1285a0522474844de130c9263e11868f67fb
    Closes-Bug: #1739646
    (cherry picked from commit 763fd62464e9a0753e061171cc1fd826055bbc01)

--- nova/policies/servers.py
+++ nova/policies/servers.py
@@ -19,6 +19,7 @@ from nova.policies import base
 RULE_AOO = base.RULE_ADMIN_OR_OWNER
 SERVERS = 'os_compute_api:servers:%s'
 NETWORK_ATTACH_EXTERNAL = 'network:attach_external_network'
+ZERO_DISK_FLAVOR = SERVERS % 'create:zero_disk_flavor'
 
 rules = [
     policy.DocumentedRuleDefault(
@@ -121,6 +122,34 @@ rules = [
         SERVERS % 'create:attach_network',
         RULE_AOO,
         "Create a server with the requested network attached to it",
+        [
+            {
+                'method': 'POST',
+                'path': '/servers'
+            }
+        ]),
+    policy.DocumentedRuleDefault(
+        ZERO_DISK_FLAVOR,
+        # TODO(mriedem): Default to RULE_ADMIN_API in Stein.
+        RULE_AOO,
+        """
+This rule controls the compute API validation behavior of creating a server
+with a flavor that has 0 disk, indicating the server should be volume-backed.
+
+For a flavor with disk=0, the root disk will be set to exactly the size of the
+image used to deploy the instance. However, in this case the filter_scheduler
+cannot select the compute host based on the virtual image size. Therefore, 0
+should only be used for volume booted instances or for testing purposes.
+
+WARNING: It is a potential security exposure to enable this policy rule
+if users can upload their own images since repeated attempts to
+create a disk=0 flavor instance with a large image can exhaust
+the local disk of the compute (or shared storage cluster). See bug
+https://bugs.launchpad.net/nova/+bug/1739646 for details.
+
+This rule defaults to ``rule:admin_or_owner`` for backward compatibility but
+will be changed to default to ``rule:admin_api`` in a subsequent release.
+""",
         [
             {
                 'method': 'POST',
