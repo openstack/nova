7aeec0e501816b8b74e5422d81d9628c2e71af16
Matt Riedemann <mriedem.os@gmail.com>
2018-08-14 11:05:06 +0800

    Delete instance_id_mappings record in instance_destroy
    The instance_create DB API creates an instance_id_mappings record
    but instance_destroy was not cleaning it up when the instance is
    deleted; this adds that delete code. Otherwise those records
    never get moved to shadow tables so you can't archive and purge them.
    
    Depends-On: https://review.opendev.org/684769
    
    Change-Id: Idfe52d3c2f987b9aac551f013a0990423d87fad3
    Closes-Bug: #1786298
    (cherry picked from commit 8a6b57cf741e17d3de54ed8c85d9fb69c43e7432)
    (cherry picked from commit 6186996c8093e723bdaa0e06664143685050312d)
    (cherry picked from commit 1b6f48354b7eacecd7a146fc05a12537b4f22e18)

--- nova/db/sqlalchemy/api.py
+++ nova/db/sqlalchemy/api.py
@@ -1893,6 +1893,8 @@ def instance_destroy(context, instance_uuid, constraint=None):
     model_query(context, models.Migration).\
             filter_by(instance_uuid=instance_uuid).\
             soft_delete()
+    model_query(context, models.InstanceIdMapping).filter_by(
+        uuid=instance_uuid).soft_delete()
     # NOTE(snikitin): We can't use model_query here, because there is no
     # column 'deleted' in 'tags' table.
     context.session.query(models.Tag).filter_by(
