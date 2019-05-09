b5142d807bc023bcf1a470cdd4307bfffe404f76
Dan Smith <dansmith@redhat.com>
2019-05-09 09:24:41 -0700

    Add extra logging to request filters
    This adds a timing decorator to request_filter.py which logs the
    filters that we ran and how long they took. This requires a small
    calling convention change for these request filters to return
    True if they are enabled and/or did anything.
    
    This also sprinkles some debug logging into all the existing filters
    to provide contextual information about what they are doing to
    make debugging scheduling decisions easier.
    
    Change-Id: Ibf62c5398a95c75fde815309e7a4f82f1bd9649c

--- nova/scheduler/request_filter.py
+++ nova/scheduler/request_filter.py
@@ -10,8 +10,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import functools
+
 import os_traits
 from oslo_log import log as logging
+from oslo_utils import timeutils
 
 import nova.conf
 from nova import exception
@@ -24,6 +27,25 @@ LOG = logging.getLogger(__name__)
 TENANT_METADATA_KEY = 'filter_tenant_id'
 
 
+def trace_request_filter(fn):
+    @functools.wraps(fn)
+    def wrapper(ctxt, request_spec):
+        timer = timeutils.StopWatch()
+        ran = False
+        with timer:
+            try:
+                ran = fn(ctxt, request_spec)
+            finally:
+                if ran:
+                    # Only log info if the filter was enabled and not
+                    # excluded for some reason
+                    LOG.debug('Request filter %r took %.1f seconds',
+                        fn.__name__, timer.elapsed())
+        return ran
+    return wrapper
+
+
+@trace_request_filter
 def require_tenant_aggregate(ctxt, request_spec):
     """Require hosts in an aggregate based on tenant id.
 
@@ -37,7 +59,7 @@ def require_tenant_aggregate(ctxt, request_spec):
     enabled = CONF.scheduler.limit_tenants_to_placement_aggregate
     agg_required = CONF.scheduler.placement_aggregate_required_for_tenants
     if not enabled:
-        return
+        return False
 
     aggregates = objects.AggregateList.get_by_metadata(
         ctxt, value=request_spec.project_id)
@@ -54,13 +76,20 @@ def require_tenant_aggregate(ctxt, request_spec):
             request_spec.requested_destination = objects.Destination()
         destination = request_spec.requested_destination
         destination.require_aggregates(aggregate_uuids_for_tenant)
+        LOG.debug('require_tenant_aggregate request filter added '
+                  'aggregates %s for tenant %r',
+                  ','.join(aggregate_uuids_for_tenant),
+                  request_spec.project_id)
     elif agg_required:
         LOG.warning('Tenant %(tenant)s has no available aggregates',
                     {'tenant': request_spec.project_id})
         raise exception.RequestFilterFailed(
             reason=_('No hosts available for tenant'))
 
+    return True
 
+
+@trace_request_filter
 def map_az_to_placement_aggregate(ctxt, request_spec):
     """Map requested nova availability zones to placement aggregates.
 
@@ -68,11 +97,11 @@ def map_az_to_placement_aggregate(ctxt, request_spec):
     matches the desired AZ of the user's request.
     """
     if not CONF.scheduler.query_placement_for_availability_zone:
-        return
+        return False
 
     az_hint = request_spec.availability_zone
     if not az_hint:
-        return
+        return False
 
     aggregates = objects.AggregateList.get_by_metadata(ctxt,
                                                        key='availability_zone',
@@ -81,10 +110,17 @@ def map_az_to_placement_aggregate(ctxt, request_spec):
         if ('requested_destination' not in request_spec or
                 request_spec.requested_destination is None):
             request_spec.requested_destination = objects.Destination()
-        request_spec.requested_destination.require_aggregates(
-            [agg.uuid for agg in aggregates])
+        agg_uuids = [agg.uuid for agg in aggregates]
+        request_spec.requested_destination.require_aggregates(agg_uuids)
+        LOG.debug('map_az_to_placement_aggregate request filter added '
+                  'aggregates %s for az %r',
+                  ','.join(agg_uuids),
+                  az_hint)
+
+    return True
 
 
+@trace_request_filter
 def require_image_type_support(ctxt, request_spec):
     """Request type-specific trait on candidates.
 
@@ -92,25 +128,30 @@ def require_image_type_support(ctxt, request_spec):
     disk_format of the image provided.
     """
     if not CONF.scheduler.query_placement_for_image_type_support:
-        return
+        return False
 
     if request_spec.is_bfv:
         # We are booting from volume, and thus compute node image
         # disk_format support does not matter.
-        return
+        return False
 
     disk_format = request_spec.image.disk_format
     trait_name = 'COMPUTE_IMAGE_TYPE_%s' % disk_format.upper()
     if not hasattr(os_traits, trait_name):
         LOG.error(('Computed trait name %r is not valid; '
                    'is os-traits up to date?'), trait_name)
-        return
+        return False
 
     # NOTE(danms): We are using the transient flavor in the request spec
     # to add the trait that we need. We make sure that we reset the dirty-ness
     # of this field to avoid persisting it.
     request_spec.flavor.extra_specs['trait:%s' % trait_name] = 'required'
     request_spec.obj_reset_changes(fields=['flavor'], recursive=True)
+
+    LOG.debug('require_image_type_support request filter added required '
+              'trait %s', trait_name)
+
+    return True
 
 
 ALL_REQUEST_FILTERS = [
