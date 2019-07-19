65d2e455e323a627ef228ed57a1f0c86d8252665
Artom Lifshitz <alifshit@redhat.com>
2019-07-19 11:35:24 -0400

    libvirt: move checking CONF.my_ip to init_host()
    Migrations use the libvirt driver's get_host_ip_addr() method to
    determine the dest_host field of the migration object.
    get_host_ip_addr() checks whether CONF.my_ip is actually assigned to
    one of the host's interfaces. It does so by calling
    get_machine_ips(), which iterates over all of the host's interfaces.
    If the host has many interfaces, this can take a long time, and
    introduces needless delays in processing the migration.
    get_machine_ips() is only used to print a warning, so this patch moves
    the get_machine_ips() call to a single method in init_host(). This
    way, a warning is still emitted at compute service startup, and
    migration progress is not needlessly slowed down.
    
    NOTE(artom) While the following paragraph still applies, the poison
    patch will not be backported. Stubbing out use of
    netifaces.interfaces() is still a good thing to do, however.
    
    This patch also has a chicken and egg problem with the patch on top of
    it, which poisons use of netifaces.interfaces() in tests. While this
    patch fixes all the tests that break with that poison, it starts
    breaking different tests because of the move of get_machine_ips() into
    init_host(). Therefore, while not directly related to the bug, this
    patch also preventatively mocks or stubs out any use of
    get_machine_ips() that will get poisoned with the subsequent patch.
    
    Closes-bug: 1837075
    Change-Id: I58a4038b04d5a9c28927d914e71609e4deea3d9f
    (cherry picked from commit 30d8159d4ee51a26a03de1cb134ea64c6c07ffb2)
    (cherry picked from commit 560317c766afc4a4c4c5017ecfc9ce432fe63ea7)

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -489,6 +489,8 @@ class LibvirtDriver(driver.ComputeDriver):
 
         self._check_file_backed_memory_support()
 
+        self._check_my_ip()
+
         if (CONF.libvirt.virt_type == 'lxc' and
                 not (CONF.libvirt.uid_maps and CONF.libvirt.gid_maps)):
             LOG.warning("Running libvirt-lxc without user namespaces is "
@@ -622,6 +624,13 @@ class LibvirtDriver(driver.ComputeDriver):
                 raise exception.InternalError(
                     'Running Nova with file_backed_memory requires '
                     'ram_allocation_ratio configured to 1.0')
+
+    def _check_my_ip(self):
+        ips = compute_utils.get_machine_ips()
+        if CONF.my_ip not in ips:
+            LOG.warning('my_ip address (%(my_ip)s) was not found on '
+                        'any of the interfaces: %(ifaces)s',
+                        {'my_ip': CONF.my_ip, 'ifaces': ", ".join(ips)})
 
     def _prepare_migration_flags(self):
         migration_flags = 0
@@ -3221,11 +3230,6 @@ class LibvirtDriver(driver.ComputeDriver):
         return self._get_console_output_file(instance, console_log)
 
     def get_host_ip_addr(self):
-        ips = compute_utils.get_machine_ips()
-        if CONF.my_ip not in ips:
-            LOG.warning('my_ip address (%(my_ip)s) was not found on '
-                        'any of the interfaces: %(ifaces)s',
-                        {'my_ip': CONF.my_ip, 'ifaces': ", ".join(ips)})
         return CONF.my_ip
 
     def get_vnc_console(self, context, instance):
