cf7d28eb6ea47818e9f3584f65ec025f5a46326b
Takashi NATSUME <natsume.takashi@lab.ntt.co.jp>
2019-08-07 14:38:45 +0900

    Fix non-existent method of Mock
    There is no method called_once_with() in Mock object.
    Use assert_called_once_with() or assert_has_calls() instead.
    
    Change-Id: I9f73fcbe7c3dfd64e75ac8224c13934b03443cd5
    Closes-Bug: #1544522

--- nova/tests/unit/compute/test_compute_api.py
+++ nova/tests/unit/compute/test_compute_api.py
@@ -5130,7 +5130,8 @@ class _ComputeAPIUnitTestMixIn(object):
         mock_rec_action.assert_called_once_with(self.context,
                                     instance,
                                     instance_actions.LIVE_MIGRATION_CANCEL)
-        mock_lm_abort.called_once_with(self.context, instance, migration.id)
+        mock_lm_abort.assert_called_once_with(self.context, instance,
+                                              migration.id)
 
     @mock.patch('nova.compute.api.API._record_action_start')
     @mock.patch.object(compute_rpcapi.ComputeAPI, 'live_migration_abort')
@@ -5157,7 +5158,8 @@ class _ComputeAPIUnitTestMixIn(object):
                                                 support_abort_in_queue=True)
             mock_rec_action.assert_called_once_with(
                 self.context, instance, instance_actions.LIVE_MIGRATION_CANCEL)
-            mock_lm_abort.called_once_with(self.context, instance, migration)
+            mock_lm_abort.assert_called_once_with(self.context, instance,
+                                                  migration.id)
             mock_get_migration.reset_mock()
             mock_rec_action.reset_mock()
             mock_lm_abort.reset_mock()
