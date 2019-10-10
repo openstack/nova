3062a1199d9bc3aa1c663a94582db2da5b36c54e
Stephen Finucane <sfinucan@redhat.com>
2019-10-10 15:13:06 +0100

    Remove 'os-consoles' API
    This exposes the 'nova-console' service via a REST API, a service that
    can only be used with XVP VNC consoles, which in turn require the
    'nova-xvpvncproxy' service. We we would like to remove the
    'nova-console' and 'nova-xvpvncproxy' services, so start here.
    
    Part of blueprint remove-xvpvncproxy
    
    Change-Id: I2ee3b8c44e5d85e9b3c811ed3c5e6cddc563054f
    Signed-off-by: Stephen Finucane <sfinucan@redhat.com>

--- nova/api/openstack/compute/consoles.py
+++ nova/api/openstack/compute/consoles.py
@@ -16,89 +16,27 @@
 from webob import exc
 
 from nova.api.openstack import wsgi
-from nova.console import api as console_api
-from nova import exception
-from nova.policies import consoles as consoles_policies
-
-
-def _translate_keys(cons):
-    """Coerces a console instance into proper dictionary format."""
-    pool = cons['pool']
-    info = {'id': cons['id'],
-            'console_type': pool['console_type']}
-    return dict(console=info)
-
-
-def _translate_detail_keys(cons):
-    """Coerces a console instance into proper dictionary format with detail."""
-    pool = cons['pool']
-    info = {'id': cons['id'],
-            'console_type': pool['console_type'],
-            'password': cons['password'],
-            'instance_name': cons['instance_name'],
-            'port': cons['port'],
-            'host': pool['public_hostname']}
-    return dict(console=info)
 
 
 class ConsolesController(wsgi.Controller):
-    """The Consoles controller for the OpenStack API."""
+    """(Removed) The Consoles controller for the OpenStack API.
 
-    def __init__(self):
-        super(ConsolesController, self).__init__()
-        self.console_api = console_api.API()
+    This was removed during the Ussuri release along with the nova-console
+    service.
+    """
 
-    @wsgi.expected_errors(())
+    @wsgi.expected_errors(410)
     def index(self, req, server_id):
-        """Returns a list of consoles for this instance."""
-        context = req.environ['nova.context']
-        context.can(consoles_policies.POLICY_ROOT % 'index')
+        raise exc.HTTPGone()
 
-        consoles = self.console_api.get_consoles(
-                req.environ['nova.context'], server_id)
-        return dict(consoles=[_translate_keys(console)
-                              for console in consoles])
-
-    # NOTE(gmann): Here should be 201 instead of 200 by v2.1
-    # +microversions because the console has been created
-    # completely when returning a response.
-    @wsgi.expected_errors(404)
+    @wsgi.expected_errors(410)
     def create(self, req, server_id, body):
-        """Creates a new console."""
-        context = req.environ['nova.context']
-        context.can(consoles_policies.POLICY_ROOT % 'create')
+        raise exc.HTTPGone()
 
-        try:
-            self.console_api.create_console(
-                req.environ['nova.context'], server_id)
-        except exception.InstanceNotFound as e:
-            raise exc.HTTPNotFound(explanation=e.format_message())
-
-    @wsgi.expected_errors(404)
+    @wsgi.expected_errors(410)
     def show(self, req, server_id, id):
-        """Shows in-depth information on a specific console."""
-        context = req.environ['nova.context']
-        context.can(consoles_policies.POLICY_ROOT % 'show')
+        raise exc.HTTPGone()
 
-        try:
-            console = self.console_api.get_console(
-                                        req.environ['nova.context'],
-                                        server_id,
-                                        int(id))
-        except exception.ConsoleNotFound as e:
-            raise exc.HTTPNotFound(explanation=e.format_message())
-        return _translate_detail_keys(console)
-
-    @wsgi.response(202)
-    @wsgi.expected_errors(404)
+    @wsgi.expected_errors(410)
     def delete(self, req, server_id, id):
-        """Deletes a console."""
-        context = req.environ['nova.context']
-        context.can(consoles_policies.POLICY_ROOT % 'delete')
-
-        try:
-            self.console_api.delete_console(req.environ['nova.context'],
-                                            server_id,
-                                            int(id))
-        except exception.ConsoleNotFound as e:
-            raise exc.HTTPNotFound(explanation=e.format_message())
+        raise exc.HTTPGone()
--- nova/tests/functional/api_sample_tests/test_consoles.py
+++ nova/tests/functional/api_sample_tests/test_consoles.py
@@ -13,47 +13,25 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import fixtures
-
-from nova.console import manager as console_manager  # noqa - only for cfg
 from nova.tests.functional.api_sample_tests import test_servers
-from nova.tests.unit import fake_xvp_console_proxy
+
+FAKE_UUID = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
 
 
 class ConsolesSamplesJsonTest(test_servers.ServersSampleBase):
-    sample_dir = "consoles"
-
-    def setUp(self):
-        super(ConsolesSamplesJsonTest, self).setUp()
-        self.flags(console_public_hostname='fake', group='xenserver')
-        self.flags(console_host='fake')
-        self.useFixture(fixtures.MonkeyPatch(
-            'nova.console.manager.xvp.XVPConsoleProxy',
-            fake_xvp_console_proxy.FakeConsoleProxy))
-        self.console = self.start_service('console', host='fake')
-
-    def _create_consoles(self, server_uuid):
-        response = self._do_post('servers/%s/consoles' % server_uuid)
-        self.assertEqual(response.status_code, 200)
 
     def test_create_consoles(self):
-        uuid = self._post_server()
-        self._create_consoles(uuid)
+        self.api.api_post('servers/%s/consoles' % FAKE_UUID, {},
+                          check_response_status=[410])
 
     def test_list_consoles(self):
-        uuid = self._post_server()
-        self._create_consoles(uuid)
-        response = self._do_get('servers/%s/consoles' % uuid)
-        self._verify_response('consoles-list-get-resp', {}, response, 200)
+        self.api.api_get('servers/%s/consoles' % FAKE_UUID,
+                         check_response_status=[410])
 
     def test_console_get(self):
-        uuid = self._post_server()
-        self._create_consoles(uuid)
-        response = self._do_get('servers/%s/consoles/1' % uuid)
-        self._verify_response('consoles-get-resp', {}, response, 200)
+        self.api.api_get('servers/%s/consoles/1' % FAKE_UUID,
+                         check_response_status=[410])
 
     def test_console_delete(self):
-        uuid = self._post_server()
-        self._create_consoles(uuid)
-        response = self._do_delete('servers/%s/consoles/1' % uuid)
-        self.assertEqual(202, response.status_code)
+        self.api.api_delete('servers/%s/consoles/1' % FAKE_UUID,
+                            check_response_status=[410])
