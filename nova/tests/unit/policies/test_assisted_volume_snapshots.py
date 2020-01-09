3b729c448ebc849205e4c7cc7ff29757479dca54
Ghanshyam Mann <gmann@ghanshyammann.com>
2020-01-09 20:57:07 +0000

    Introduce scope_types in os-assisted_volume_snapshots policy
    oslo.policy introduced the scope_type feature which can
    control the access level at system-level and project-level.
     - https://docs.openstack.org/oslo.policy/latest/user/usage.html#setting-scope
     - http://specs.openstack.org/openstack/keystone-specs/specs/keystone/queens/system-scope.html
    
    Appropriate scope_type for nova case:
    - https://specs.openstack.org/openstack/nova-specs/specs/ussuri/approved/policy-defaults-refresh.html#scope
    
    This commit introduce scope_type for os-assisted_volume_snapshots API policies
    as 'system'.
    
    Also adds the test case with scope_type enabled and verify we
    pass and fail the policy check with expected context.
    
    Partial implement blueprint policy-defaults-refresh
    
    Change-Id: I395ebec039f279521eca522e6e6dad1fb6653948

--- nova/tests/unit/policies/test_assisted_volume_snapshots.py
+++ nova/tests/unit/policies/test_assisted_volume_snapshots.py
@@ -83,3 +83,16 @@ class AssistedSnapshotScopeTypePolicyTest(AssistedVolumeSnapshotPolicyTest):
     def setUp(self):
         super(AssistedSnapshotScopeTypePolicyTest, self).setUp()
         self.flags(enforce_scope=True, group="oslo_policy")
+
+        # Check that system admin is able to take volume snapshot.
+        self.admin_authorized_contexts = [
+            self.system_admin_context]
+        # Check that non-system or non-admin is not able to take volume
+        # snapshot.
+        self.admin_unauthorized_contexts = [
+            self.legacy_admin_context, self.system_member_context,
+            self.system_reader_context, self.system_foo_context,
+            self.project_admin_context, self.project_member_context,
+            self.other_project_member_context,
+            self.project_foo_context, self.project_reader_context
+        ]
