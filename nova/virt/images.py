03d6eb500f85a417f84a7bf4a9f4ceb275c9a67e
Lee Yarwood <lyarwood@redhat.com>
2020-02-05 12:36:40 +0000

    images: Move qemu-img info calls into privsep
    This is mostly code motion from the nova.virt.images module into privsep
    to allow for both privileged and unprivileged calls to be made.
    
    A privileged_qemu_img_info function is introduced allowing QEMU to
    access devices requiring root privileges, such as host block devices.
    
    Change-Id: I5ac03f923d9d181d22d44d8ec8fbc31eb0c3999e

--- nova/virt/images.py
+++ nova/virt/images.py
@@ -19,14 +19,12 @@
 Handling of VM disk images.
 """
 
-import operator
 import os
 
 from oslo_concurrency import processutils
 from oslo_log import log as logging
 from oslo_utils import fileutils
 from oslo_utils import imageutils
-from oslo_utils import units
 
 from nova.compute import utils as compute_utils
 import nova.conf
@@ -35,19 +33,14 @@ from nova.i18n import _
 from nova.image import glance
 import nova.privsep.qemu
 
+# This is set by the libvirt driver on startup. The version is used to
+# determine what flags need to be set on the command line.
+QEMU_VERSION = None
+
 LOG = logging.getLogger(__name__)
 
 CONF = nova.conf.CONF
 IMAGE_API = glance.API()
-
-QEMU_IMG_LIMITS = processutils.ProcessLimits(
-    cpu_time=30,
-    address_space=1 * units.Gi)
-
-# This is set by the libvirt driver on startup. The version is used to
-# determine what flags need to be set on the command line.
-QEMU_VERSION = None
-QEMU_VERSION_REQ_SHARED = 2010000
 
 
 def qemu_img_info(path, format=None):
@@ -57,42 +50,9 @@ def qemu_img_info(path, format=None):
     if not os.path.exists(path) and CONF.libvirt.images_type != 'rbd':
         raise exception.DiskNotFound(location=path)
 
-    try:
-        # The following check is about ploop images that reside within
-        # directories and always have DiskDescriptor.xml file beside them
-        if (os.path.isdir(path) and
-            os.path.exists(os.path.join(path, "DiskDescriptor.xml"))):
-            path = os.path.join(path, "root.hds")
-
-        cmd = ('env', 'LC_ALL=C', 'LANG=C', 'qemu-img', 'info', path)
-        if format is not None:
-            cmd = cmd + ('-f', format)
-        # Check to see if the qemu version is >= 2.10 because if so, we need
-        # to add the --force-share flag.
-        if QEMU_VERSION and operator.ge(QEMU_VERSION, QEMU_VERSION_REQ_SHARED):
-            cmd = cmd + ('--force-share',)
-        out, err = processutils.execute(*cmd, prlimit=QEMU_IMG_LIMITS)
-    except processutils.ProcessExecutionError as exp:
-        if exp.exit_code == -9:
-            # this means we hit prlimits, make the exception more specific
-            msg = (_("qemu-img aborted by prlimits when inspecting "
-                    "%(path)s : %(exp)s") % {'path': path, 'exp': exp})
-        elif exp.exit_code == 1 and 'No such file or directory' in exp.stderr:
-            # The os.path.exists check above can race so this is a simple
-            # best effort at catching that type of failure and raising a more
-            # specific error.
-            raise exception.DiskNotFound(location=path)
-        else:
-            msg = (_("qemu-img failed to execute on %(path)s : %(exp)s") %
-                   {'path': path, 'exp': exp})
-        raise exception.InvalidDiskInfo(reason=msg)
-
-    if not out:
-        msg = (_("Failed to run qemu-img info on %(path)s : %(error)s") %
-               {'path': path, 'error': err})
-        raise exception.InvalidDiskInfo(reason=msg)
-
-    return imageutils.QemuImgInfo(out)
+    info = nova.privsep.qemu.unprivileged_qemu_img_info(
+        path, format=format, qemu_version=QEMU_VERSION)
+    return imageutils.QemuImgInfo(info)
 
 
 def convert_image(source, dest, in_format, out_format, run_as_root=False,
