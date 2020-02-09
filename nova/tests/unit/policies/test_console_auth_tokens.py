b055b5094eaca3191d066749ea6aff16dd6b9867
Ghanshyam Mann <gmann@ghanshyammann.com>
2020-02-08 20:14:35 -0600

    Add test coverage of existing os-console-auth-tokens policies
    Current tests do not have good test coverage of existing policies.
    Either tests for policies do not exist or if they exist then they
    do not cover the actual negative and positive testing.
    
    For Example, if any policy with default rule as admin only then
    test should verify:
    - policy check pass with context having admin or server owner
    - policy check fail with context having non-admin and not server owner
    
    As discussed in policy-defaults-refresh [1], to change the policies
    with new default roles and scope_type, we need to have the enough
    testing coverage of existing policy behavior.
    
    When we will add the scope_type in policies or new default roles,
    then these test coverage will be extended to adopt the new changes
    and also make sure we do not break the existing behavior.
    
    This commit covers the testing coverage of existing os-console-auth-tokens
    policies.
    
    Partial implement blueprint policy-defaults-refresh
    
    [1] https://specs.openstack.org/openstack/nova-specs/specs/ussuri/approved/policy-defaults-refresh.html#testing
    
    Change-Id: Iface875e2af24fd2dced0daa079bf7595762e999

---
+++ nova/tests/unit/policies/test_console_auth_tokens.py
@@ -0,0 +1,69 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from nova.api.openstack.compute import console_auth_tokens
+from nova.tests.unit.api.openstack import fakes
+from nova.tests.unit.policies import base
+
+
+class ConsoleAuthTokensPolicyTest(base.BasePolicyTest):
+    """Test Console Auth Tokens APIs policies with all possible context.
+
+    This class defines the set of context with different roles
+    which are allowed and not allowed to pass the policy checks.
+    With those set of context, it will call the API operation and
+    verify the expected behaviour.
+    """
+
+    def setUp(self):
+        super(ConsoleAuthTokensPolicyTest, self).setUp()
+        self.controller = console_auth_tokens.ConsoleAuthTokensController()
+        self.req = fakes.HTTPRequest.blank('', version='2.31')
+
+        # Check that admin is able to get console connection information.
+        self.admin_authorized_contexts = [
+            self.legacy_admin_context, self.system_admin_context,
+            self.project_admin_context]
+        # Check that non-admin is not able to get console connection
+        # information.
+        self.admin_unauthorized_contexts = [
+            self.system_member_context, self.system_reader_context,
+            self.system_foo_context, self.project_member_context,
+            self.other_project_member_context,
+            self.project_foo_context, self.project_reader_context
+        ]
+
+    @mock.patch('nova.objects.ConsoleAuthToken.validate')
+    def test_console_connect_info_token_policy(self, mock_validate):
+        rule_name = "os_compute_api:os-console-auth-tokens"
+        self.common_policy_check(self.admin_authorized_contexts,
+                                 self.admin_unauthorized_contexts,
+                                 rule_name, self.controller.show,
+                                 self.req, fakes.FAKE_UUID)
+
+
+class ConsoleAuthTokensScopeTypePolicyTest(ConsoleAuthTokensPolicyTest):
+    """Test Console Auth Tokens APIs policies with system scope enabled.
+
+    This class set the nova.conf [oslo_policy] enforce_scope to True
+    so that we can switch on the scope checking on oslo policy side.
+    It defines the set of context with scoped token
+    which are allowed and not allowed to pass the policy checks.
+    With those set of context, it will run the API operation and
+    verify the expected behaviour.
+    """
+
+    def setUp(self):
+        super(ConsoleAuthTokensScopeTypePolicyTest, self).setUp()
+        self.flags(enforce_scope=True, group="oslo_policy")
