48a66c56441861a206f9369b8c242cfd4dffd80d
Ghanshyam Mann <gmann@ghanshyammann.com>
2020-02-09 11:43:26 -0600

    Use stable constraint for Tempest pinned stable branches
    Stable branches till stable/rocky is using python
    version <py3.6. Tempest test those branch in venv
    but Tempest tox use the master upper-constraint[1]
    which block installation due to dependencies
    require >=py3.6. For exmaple, oslo.concurrency 4.0.0
    is not compatible for <py3.6.
    
    As we pin Tempest for EM stable brach, we should be
    able to use stable constraint for Tempest installation
    as well as while running during run-tempest playbook.
    
    tox.ini is hard coded to use master constraint[1] which force
    run-tempest to recreate the tox env and use the master constraint.
    Fix for that- https://review.opendev.org/#/c/705870/
    
    nova-live-migration test hook run_test.sh needs to use the stable
    u-c so that Tempest installation in venv will use stable
    branch constraint.
    
    Modify the irrelevant-files for nova-live-migration job to run
    for run_test.sh script.
    
    [1] https://opendev.org/openstack/tempest/src/commit/bc9fe8eca801f54915ff3eafa418e6e18ac2df63/tox.ini#L14
    
    Change-Id: I8190f93e0a754fa59ed848a3a230d1ef63a06abc

--- nova/tests/live_migration/hooks/utils.sh
+++ nova/tests/live_migration/hooks/utils.sh
@@ -3,7 +3,13 @@
 function run_tempest {
     local message=$1
     local tempest_regex=$2
-    sudo -H -u tempest tox -eall -- $tempest_regex --concurrency=$TEMPEST_CONCURRENCY
+    # NOTE(gmann): Use branch constraint because Tempest is pinned to the branch release
+    # instead of using master. We need to export it via env var UPPER_CONSTRAINTS_FILE
+    # so that initial creation of tempest tox use stable branch constraint
+    # instead of master constraint which is hard coded in tempest/tox.ini
+    export UPPER_CONSTRAINTS_FILE=$BASE/new/requirements/upper-constraints.txt
+
+    sudo -H -u tempest UPPER_CONSTRAINTS_FILE=$UPPER_CONSTRAINTS_FILE tox -eall -- $tempest_regex --concurrency=$TEMPEST_CONCURRENCY
     exitcode=$?
     if [[ $exitcode -ne 0 ]]; then
       die $LINENO "$message failure"
