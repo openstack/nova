abd67b9e5407e6728dbc9d23a8d3e3a17984992c
Ghanshyam Mann <gmann@ghanshyammann.com>
2020-02-10 20:23:59 -0600

    Add new default roles in os-create-backup policies
    This adds new defaults roles in os-admin-password API policies.
    
    This policy is default to owner or system admin role.
    
    Also add tests to simulates the future where we drop the deprecation
    fall back in the policy by overriding the rules with a version where
    there are no deprecated rule options. Operators can do the same by
    adding overrides in their policy files that match the default but
    stop the rule deprecation fallback from happening.
    
    Partial implement blueprint policy-defaults-refresh
    
    Change-Id: I023a13c0f7d205aa65b7f74589f243ea359345dc

--- nova/tests/unit/policies/test_create_backup.py
+++ nova/tests/unit/policies/test_create_backup.py
@@ -88,3 +88,29 @@ class CreateBackupScopeTypePolicyTest(CreateBackupPolicyTest):
     def setUp(self):
         super(CreateBackupScopeTypePolicyTest, self).setUp()
         self.flags(enforce_scope=True, group="oslo_policy")
+
+
+class CreateBackupNoLegacyPolicyTest(CreateBackupPolicyTest):
+    """Test Create Backup APIs policies with system scope enabled,
+    and no more deprecated rules that allow the legacy admin API to
+    access system_admin_or_owner APIs.
+    """
+    without_deprecated_rules = True
+
+    def setUp(self):
+        super(CreateBackupNoLegacyPolicyTest, self).setUp()
+        self.flags(enforce_scope=True, group="oslo_policy")
+
+        # Check that system or projct admin or owner is able to create
+        # server backup.
+        self.admin_authorized_contexts = [
+            self.system_admin_context,
+            self.project_admin_context, self.project_member_context]
+        # Check that non-system and non-admin/owner is not able to
+        # create server backup.
+        self.admin_unauthorized_contexts = [
+            self.legacy_admin_context, self.project_reader_context,
+            self.project_foo_context,
+            self.system_member_context, self.system_reader_context,
+            self.system_foo_context,
+            self.other_project_member_context]
