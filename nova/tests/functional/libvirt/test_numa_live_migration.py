openstack/nova

commit 6ec686c26b2c8b18bcff522633bfe9715e0feec3

Stop filtering out 'accepted' for in-progress migrations
Live migrations are created with an 'accepted' status. Resource claims
on the destination are done with the migration in 'accepted' status.
The status is set to 'preparing' a bit later, right before running
pre_live_migration(). Migrations with status 'accepted' are filtered
out by the database layer when getting in-progress migrations. Thus,
there's a time window after resource claims but before 'preparing'
during which resources have been claimed but the migration is not
considered in-progress by the database layer. During that window, the
instance's host is the source - that's only updated once the live
migration finishes. If the update available resources periodic task
runs during that window, it'll free the instance's resource from the
destination because neither the instance nor any of its in-progress
migrations are associated with the destination. This means that other
incoming instances are able to consume resources that should not be
available. This patch stops filtering out the 'accepted' status in the
database layer when retrieving in-progress migrations.

Change-Id: I4c56925ed35bc3275ca1ac6c30d7fd641ad84260
Closes-bug: 1845146

nova/db/sqlalchemy/api.py
@@ -4419,10 +4419,10 @@ def migration_get_in_progress_by_host_and_node(context, host, node):
                             models.Migration.source_node == node),
                        and_(models.Migration.dest_compute == host,
                             models.Migration.dest_node == node))).\
-            filter(~models.Migration.status.in_(['accepted', 'confirmed',
-                                                 'reverted', 'error',
-                                                 'failed', 'completed',
-                                                 'cancelled', 'done'])).\
+            filter(~models.Migration.status.in_(['confirmed', 'reverted',
+                                                 'error', 'failed',
+                                                 'completed', 'cancelled',
+                                                 'done'])).\
             options(_joinedload_all('instance.system_metadata')).\
             all()
 

nova/tests/unit/db/test_db_api.py
@@ -1153,7 +1153,6 @@ class MigrationTestCase(test.TestCase):
             self.assertNotEqual('reverted', migration['status'])
             self.assertNotEqual('error', migration['status'])
             self.assertNotEqual('failed', migration['status'])
-            self.assertNotEqual('accepted', migration['status'])
             self.assertNotEqual('done', migration['status'])
             self.assertNotEqual('cancelled', migration['status'])
 
@@ -1169,7 +1168,7 @@ class MigrationTestCase(test.TestCase):
         migrations = db.migration_get_in_progress_by_host_and_node(self.ctxt,
                 'host1', 'a')
         # 2 as source + 1 as dest
-        self.assertEqual(3, len(migrations))
+        self.assertEqual(4, len(migrations))
         self._assert_in_progress(migrations)
 
     def test_in_progress_host1_nodeb(self):
@@ -1182,7 +1181,7 @@ class MigrationTestCase(test.TestCase):
         migrations = db.migration_get_in_progress_by_host_and_node(self.ctxt,
                 'host2', 'b')
         # 2 as dest, 1 as source
-        self.assertEqual(3, len(migrations))
+        self.assertEqual(4, len(migrations))
         self._assert_in_progress(migrations)
 
     def test_instance_join(self):

commit 32713a4fe885ee55ef0fefc8ce6c78877f2f03e7

NUMA LM: Add func test for bug 1845146
Bug 1845146 was caused by the update available resources periodic task
running during a small window in which the migration was in 'accepted'
but resource claims had been done. 'accepted' migrations were not
considered in progress before the fix for 1845146 merged as commit
6ec686c26b, which caused the periodic task to incorrectly free the
migration's resources from the destination. This patch adds a test
that triggers this race by wrapping around the compute manager's
live_migration() (which sets the 'queued' migration status - this was
actually wrong in 6ec686c26b, as it talks about 'preparing') and
running the update available resources periodic task while the
migration is still in 'accepted'.

Related bug: 1845146

Change-Id: I78e79112a9c803fb45d828cfb4641456da66364a

nova/tests/functional/libvirt/test_numa_live_migration.py
@@ -18,6 +18,7 @@ import fixtures
 from oslo_config import cfg
 from oslo_log import log as logging
 
+from nova.compute import manager as compute_manager
 from nova import context
 from nova import objects
 from nova.tests.functional import integrated_helpers
@@ -259,6 +260,24 @@ class NUMALiveMigrationPositiveTests(NUMALiveMigrationPositiveBase):
 
     def test_numa_live_migration_dest_pinned(self):
         self._test(pin_dest=True)
+
+    def test_bug_1843639(self):
+        orig_live_migration = \
+            compute_manager.ComputeManager.live_migration
+
+        def live_migration(*args, **kwargs):
+            self._run_periodics()
+            # During the migration, server_a is consuming CPUs 0,1 on host_a,
+            # while all 4 of host_b's CPU are consumed by server_b and the
+            # incoming # migration.
+            self._assert_host_consumed_cpus('host_a', [0, 1])
+            self._assert_host_consumed_cpus('host_b', [0, 1, 2, 3])
+            return orig_live_migration(*args, **kwargs)
+
+        self.useFixture(fixtures.MonkeyPatch(
+            'nova.compute.manager.ComputeManager.live_migration',
+            live_migration))
+        self._test()
 
 
 class NUMALiveMigrationRollbackTests(NUMALiveMigrationPositiveBase):