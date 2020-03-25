1a41597b0c3300db32df3b3576a9eebef0b0f01b
Ghanshyam Mann <gmann@ghanshyammann.com>
2020-03-25 08:53:51 -0500

    Add test coverage of existing hypervisors policies
    Current tests do not have good test coverage of existing policies.
    Either tests for policies do not exist or if they exist then they
    do not cover the actual negative and positive testing.
    
    For Example, if any policy with default rule as admin only then
    test should verify:
    - policy check pass with context having admin or server owner
    - policy check fail with context having non-admin and not server owner
    
    As discussed in policy-defaults-refresh, to change the policies
    with new default roles and scope_type, we need to have the enough
    testing coverage of existing policy behavior.
    
    When we will add the scope_type in policies or new default roles,
    then these test coverage will be extended to adopt the new changes
    and also make sure we do not break the existing behavior.
    
    This commit covers the testing coverage of existing hypervisors policies.
    
    Partial implement blueprint policy-defaults-refresh
    
    Change-Id: Ieb5e27ccec000f6d6bb598aa00a4a17e0f334aad

---
+++ nova/tests/unit/policies/test_hypervisors.py
@@ -0,0 +1,116 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from nova.api.openstack.compute import hypervisors
+from nova.policies import hypervisors as hv_policies
+from nova.tests.unit.api.openstack import fakes
+from nova.tests.unit.policies import base
+
+
+class HypervisorsPolicyTest(base.BasePolicyTest):
+    """Test os-hypervisors APIs policies with all possible context.
+    This class defines the set of context with different roles
+    which are allowed and not allowed to pass the policy checks.
+    With those set of context, it will call the API operation and
+    verify the expected behaviour.
+    """
+
+    def setUp(self):
+        super(HypervisorsPolicyTest, self).setUp()
+        self.controller = hypervisors.HypervisorsController()
+        self.req = fakes.HTTPRequest.blank('')
+        self.controller._get_compute_nodes_by_name_pattern = mock.MagicMock()
+        self.controller.host_api.compute_node_get_all = mock.MagicMock()
+        self.controller.host_api.service_get_by_compute_host = mock.MagicMock()
+        self.controller.host_api.compute_node_get = mock.MagicMock()
+
+        # Check that admin is able to perform operations
+        # on hypervisors.
+        self.admin_authorized_contexts = [
+            self.legacy_admin_context, self.system_admin_context,
+            self.project_admin_context]
+        # Check that non-admin is not able to perform operations
+        # on hypervisors.
+        self.admin_unauthorized_contexts = [
+            self.system_member_context, self.system_reader_context,
+            self.system_foo_context, self.project_member_context,
+            self.other_project_member_context,
+            self.project_foo_context, self.project_reader_context
+        ]
+
+    def test_list_hypervisors_policy(self):
+        rule_name = hv_policies.BASE_POLICY_NAME
+        self.common_policy_check(self.admin_authorized_contexts,
+                                 self.admin_unauthorized_contexts,
+                                 rule_name, self.controller.index,
+                                 self.req)
+
+    def test_list_details_hypervisors_policy(self):
+        rule_name = hv_policies.BASE_POLICY_NAME
+        self.common_policy_check(self.admin_authorized_contexts,
+                                 self.admin_unauthorized_contexts,
+                                 rule_name, self.controller.detail,
+                                 self.req)
+
+    def test_show_hypervisors_policy(self):
+        rule_name = hv_policies.BASE_POLICY_NAME
+        self.common_policy_check(self.admin_authorized_contexts,
+                                 self.admin_unauthorized_contexts,
+                                 rule_name, self.controller.show,
+                                 self.req, 11111)
+
+    @mock.patch('nova.compute.api.HostAPI.get_host_uptime')
+    def test_uptime_hypervisors_policy(self, mock_uptime):
+        rule_name = hv_policies.BASE_POLICY_NAME
+        self.common_policy_check(self.admin_authorized_contexts,
+                                 self.admin_unauthorized_contexts,
+                                 rule_name, self.controller.uptime,
+                                 self.req, 11111)
+
+    def test_search_hypervisors_policy(self):
+        rule_name = hv_policies.BASE_POLICY_NAME
+        self.common_policy_check(self.admin_authorized_contexts,
+                                 self.admin_unauthorized_contexts,
+                                 rule_name, self.controller.search,
+                                 self.req, 11111)
+
+    def test_servers_hypervisors_policy(self):
+        rule_name = hv_policies.BASE_POLICY_NAME
+        self.common_policy_check(self.admin_authorized_contexts,
+                                 self.admin_unauthorized_contexts,
+                                 rule_name, self.controller.servers,
+                                 self.req, 11111)
+
+    @mock.patch('nova.compute.api.HostAPI.compute_node_statistics')
+    def test_statistics_hypervisors_policy(self, mock_statistics):
+        rule_name = hv_policies.BASE_POLICY_NAME
+        self.common_policy_check(self.admin_authorized_contexts,
+                                 self.admin_unauthorized_contexts,
+                                 rule_name, self.controller.statistics,
+                                 self.req)
+
+
+class HypervisorsScopeTypePolicyTest(HypervisorsPolicyTest):
+    """Test os-hypervisors APIs policies with system scope enabled.
+    This class set the nova.conf [oslo_policy] enforce_scope to True
+    so that we can switch on the scope checking on oslo policy side.
+    It defines the set of context with scoped token
+    which are allowed and not allowed to pass the policy checks.
+    With those set of context, it will run the API operation and
+    verify the expected behaviour.
+    """
+
+    def setUp(self):
+        super(HypervisorsScopeTypePolicyTest, self).setUp()
+        self.flags(enforce_scope=True, group="oslo_policy")
