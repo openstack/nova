258e38bd71a22d8c53281e9d20d23fa0f5bd07ae
Ghanshyam Mann <gmann@ghanshyammann.com>
2020-03-25 09:08:34 -0500

    Introduce scope_types in os-hypervisors
    oslo.policy introduced the scope_type feature which can
    control the access level at system-level and project-level.
     - https://docs.openstack.org/oslo.policy/latest/user/usage.html#setting-scope
     - http://specs.openstack.org/openstack/keystone-specs/specs/keystone/queens/system-scope.html
    
    Appropriate scope_type for nova case:
    - https://specs.openstack.org/openstack/nova-specs/specs/ussuri/approved/policy-defaults-refresh.html#scope
    
    This commit introduce scope_type for os-hypervisors API policies
    as 'system'.
    
    Also adds the test case with scope_type enabled and verify we
    pass and fail the policy check with expected context.
    
    Partial implement blueprint policy-defaults-refresh
    
    Change-Id: Id073345857fa26a0522804de42a133c588450330

--- nova/tests/unit/policies/test_hypervisors.py
+++ nova/tests/unit/policies/test_hypervisors.py
@@ -114,3 +114,17 @@ class HypervisorsScopeTypePolicyTest(HypervisorsPolicyTest):
     def setUp(self):
         super(HypervisorsScopeTypePolicyTest, self).setUp()
         self.flags(enforce_scope=True, group="oslo_policy")
+
+        # Check that system admin is able to perform operations
+        # on hypervisors.
+        self.admin_authorized_contexts = [
+            self.system_admin_context]
+        # Check that non-system-admin is not able to perform operations
+        # on hypervisors.
+        self.admin_unauthorized_contexts = [
+            self.legacy_admin_context, self.system_member_context,
+            self.system_reader_context, self.project_admin_context,
+            self.system_foo_context, self.project_member_context,
+            self.other_project_member_context,
+            self.project_foo_context, self.project_reader_context
+        ]
