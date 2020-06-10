840de3b8924612fa3fc47a4c9032a4723d536613
Stephen Finucane <stephenfin@redhat.com>
2020-06-10 10:56:38 +0100

    libvirt: Mark e1000e VIF as supported
    This is supported by the QEMU/KVM backends in libvirt. There's no reason
    not to support it in nova. This appears to have been an oversight.
    
    Change-Id: I12a5d28d75bc32a76a4f3765cb4db4cbc46c0c75
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>
    Closes-Bug: #1882919
    (cherry picked from commit 644cb5cb8bf44184d9b3f046cc67746b13550dd6)

--- nova/tests/unit/virt/libvirt/test_vif.py
+++ nova/tests/unit/virt/libvirt/test_vif.py
@@ -839,12 +839,14 @@ class LibvirtVifTestCase(test.NoDBTestCase):
                              network_model.VIF_MODEL_PCNET,
                              network_model.VIF_MODEL_RTL8139,
                              network_model.VIF_MODEL_E1000,
+                             network_model.VIF_MODEL_E1000E,
                              network_model.VIF_MODEL_SPAPR_VLAN)
             else:
                 supported = (network_model.VIF_MODEL_NE2K_PCI,
                              network_model.VIF_MODEL_PCNET,
                              network_model.VIF_MODEL_RTL8139,
                              network_model.VIF_MODEL_E1000,
+                             network_model.VIF_MODEL_E1000E,
                              network_model.VIF_MODEL_SPAPR_VLAN)
             for model in supported:
                 image_meta = objects.ImageMeta.from_dict(
