01e3c8778d8f3db19e7366201a9730f9316b2552
Balazs Gibizer <balazs.gibizer@est.tech>
2020-06-17 13:50:34 +0200

    Extend is_ipv6_supported() to cover more error cases
    It seems that with certain host configuration the ipv6 wsgi tests are
    failing as the test only skipped if no ipv6 interface is available but
    it does not check if the test will be able to bind to a port on that
    interface. So this patch extends the skip criteria by checking if
    binding is possible.
    
    Change-Id: Iffea0586f02e8d4020cadc8c513bafe984e07a6e
    Closes-Bug: #1883885

--- nova/tests/unit/test_test_utils.py
+++ nova/tests/unit/test_test_utils.py
@@ -14,7 +14,7 @@
 
 import errno
 import socket
-import tempfile
+from unittest import mock
 
 import fixtures
 
@@ -50,7 +50,7 @@ class TestUtilsTestCase(test.TestCase):
             raise e
 
         def fake_socket_ok(x, y):
-            return tempfile.TemporaryFile()
+            return mock.Mock()
 
         with fixtures.MonkeyPatch('socket.socket', fake_socket_fail):
             self.assertFalse(test_utils.is_ipv6_supported())
