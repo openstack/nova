24fe5d7ddae723638ac3d8966f29216d437d829a
Ghanshyam Mann <gmann@ghanshyammann.com>
2020-07-21 21:21:00 -0500

    Add new default roles in multinic policies
    This adds new defaults roles in multinic API policies.
    These policies are made granular and default to
    PROJECT_MEMBER_OR_SYSTEM_ADMIN.
    
    Partial implement blueprint policy-defaults-refresh-deprecated-apis
    
    Change-Id: I1b2c741e86431963fb4f0696509bed01351afac2

--- nova/tests/unit/policies/test_multinic.py
+++ nova/tests/unit/policies/test_multinic.py
@@ -17,6 +17,8 @@ from oslo_utils import timeutils
 
 from nova.api.openstack.compute import multinic
 from nova.compute import vm_states
+from nova.policies import base as base_policy
+from nova.policies import multinic as policies
 from nova.tests.unit.api.openstack import fakes
 from nova.tests.unit import fake_instance
 from nova.tests.unit.policies import base
@@ -59,7 +61,7 @@ class MultinicPolicyTest(base.BasePolicyTest):
 
     @mock.patch('nova.compute.api.API.add_fixed_ip')
     def test_add_fixed_ip_policy(self, mock_add):
-        rule_name = "os_compute_api:os-multinic"
+        rule_name = "os_compute_api:os-multinic:add"
         body = dict(addFixedIp=dict(networkId='test_net'))
         self.common_policy_check(self.admin_or_owner_authorized_contexts,
                                  self.admin_or_owner_unauthorized_contexts,
@@ -69,7 +71,7 @@ class MultinicPolicyTest(base.BasePolicyTest):
 
     @mock.patch('nova.compute.api.API.remove_fixed_ip')
     def test_remove_fixed_ip_policy(self, mock_remove):
-        rule_name = "os_compute_api:os-multinic"
+        rule_name = "os_compute_api:os-multinic:remove"
         body = dict(removeFixedIp=dict(address='1.2.3.4'))
         self.common_policy_check(self.admin_or_owner_authorized_contexts,
                                  self.admin_or_owner_unauthorized_contexts,
@@ -92,3 +94,33 @@ class MultinicScopeTypePolicyTest(MultinicPolicyTest):
     def setUp(self):
         super(MultinicScopeTypePolicyTest, self).setUp()
         self.flags(enforce_scope=True, group="oslo_policy")
+
+
+class MultinicNoLegacyPolicyTest(MultinicScopeTypePolicyTest):
+    """Test Multinic APIs policies with system scope enabled,
+    and no more deprecated rules.
+    """
+    without_deprecated_rules = True
+    rules_without_deprecation = {
+        policies.BASE_POLICY_NAME % 'add':
+            base_policy.PROJECT_MEMBER_OR_SYSTEM_ADMIN,
+        policies.BASE_POLICY_NAME % 'remove':
+            base_policy.PROJECT_MEMBER_OR_SYSTEM_ADMIN}
+
+    def setUp(self):
+        super(MultinicNoLegacyPolicyTest, self).setUp()
+        # Check that system admin or owner is able to
+        # add/delete Fixed IP to server.
+        self.admin_or_owner_authorized_contexts = [
+            self.system_admin_context,
+            self.project_admin_context, self.project_member_context,
+        ]
+        # Check that non-system and non-admin/owner is not able
+        # to add/delete Fixed IP to server.
+        self.admin_or_owner_unauthorized_contexts = [
+            self.legacy_admin_context, self.system_member_context,
+            self.system_reader_context, self.project_reader_context,
+            self.project_foo_context,
+            self.system_foo_context, self.other_project_member_context,
+            self.other_project_reader_context
+        ]
