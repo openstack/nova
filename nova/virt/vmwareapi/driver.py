498b41f65b88fd36a2ac4cb2c14df6fc0c612950
Balazs Gibizer <balazs.gibizer@est.tech>
2020-07-22 14:20:59 +0200

    Undeprecate the vmwareapi driver
    * 3pp CI is stable and produces results comparable to other nova 3pp
      CIs [1]
    * Yingji Sun seems to be actively maintaining the machinery (see
      comments in [2])
    
    [1] http://ciwatch.mmedvede.net/project?project=nova&time=7+days
    [2] https://review.opendev.org/#/c/734114
    
    Change-Id: Id61118f9374dcb51b8450b35c92950fb313df5a2

--- nova/virt/vmwareapi/driver.py
+++ nova/virt/vmwareapi/driver.py
@@ -202,13 +202,6 @@ class VMwareVCDriver(driver.ComputeDriver):
                 self._datastore_regex = None
 
     def init_host(self, host):
-        LOG.warning('The vmwareapi driver is deprecated and may be removed in '
-                    'a future release. The driver is not tested by the '
-                    'OpenStack project nor does it have clear maintainer(s) '
-                    'and thus its quality can not be ensured. If you are '
-                    'using the driver in production please let us know in '
-                    'freenode IRC and/or the openstack-discuss mailing list.')
-
         vim = self._session.vim
         if vim is None:
             self._session._create_session()
