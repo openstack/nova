260ebeebb8ff1074834236506f9a8acf242b93c9
Ghanshyam Mann <gmann@ghanshyammann.com>
2020-07-25 16:54:15 -0500

    Add test coverage of extensions policies
    Current tests do not have good test coverage of existing policies.
    Either tests for policies do not exist or if they exist then they
    do not cover the actual negative and positive testing.
    
    To adopt the keystone's scope_type and new defaults in deprecated
    API policies, we need to first write test coverage for the same to
    know the complete effect of policies changes.
    
    Partial implement blueprint policy-defaults-refresh-deprecated-apis
    
    Change-Id: Ib6d720bbfd473b0aa1ca55d38ab9bb6baaaa2d29

--- nova/tests/unit/api/openstack/compute/test_extension_info.py
+++ nova/tests/unit/api/openstack/compute/test_extension_info.py
@@ -12,12 +12,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import mock
 import webob
 
 from nova.api.openstack.compute import extension_info
-from nova import exception
-from nova import policy
 from nova import test
 from nova.tests.unit.api.openstack import fakes
 
@@ -27,35 +24,8 @@ class ExtensionInfoV21Test(test.NoDBTestCase):
     def setUp(self):
         super(ExtensionInfoV21Test, self).setUp()
         self.controller = extension_info.ExtensionInfoController()
-        patcher = mock.patch.object(policy, 'authorize', return_value=True)
-        patcher.start()
-        self.addCleanup(patcher.stop)
 
     def test_extension_info_show_servers_not_present(self):
         req = fakes.HTTPRequest.blank('/extensions/servers')
         self.assertRaises(webob.exc.HTTPNotFound, self.controller.show,
                           req, 'servers')
-
-
-class ExtensionInfoPolicyEnforcementV21(test.NoDBTestCase):
-    def setUp(self):
-        super(ExtensionInfoPolicyEnforcementV21, self).setUp()
-        self.controller = extension_info.ExtensionInfoController()
-        self.req = fakes.HTTPRequest.blank('')
-
-    def _test_extension_policy_failed(self, action, *args):
-        rule_name = "os_compute_api:extensions"
-        self.policy.set_rules({rule_name: "project:non_fake"})
-        exc = self.assertRaises(
-            exception.PolicyNotAuthorized,
-            getattr(self.controller, action), self.req, *args)
-
-        self.assertEqual(
-            "Policy doesn't allow %s to be performed." % rule_name,
-            exc.format_message())
-
-    def test_extension_index_policy_failed(self):
-        self._test_extension_policy_failed('index')
-
-    def test_extension_show_policy_failed(self):
-        self._test_extension_policy_failed('show', 1)
