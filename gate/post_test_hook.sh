d309e3cdf52bb4f8800c59f295a9c2ffb4069369
Balazs Gibizer <balazs.gibizer@est.tech>
2020-10-01 14:01:37 +0200

    Temporarily disable parts of heal port allocation test
    Due to bug 1894825 the nova-next job fails on master as the allocation
    key cannot be deleted from the binding:profile of a neutron port. This
    patch temporarily disable this test step while the bug is being fixed
    and a new neutronlib is released with the fix.
    
    Change-Id: I4dfebfb5c92dd8a5cdc779aac587e7477cd5fad6
    Related-Bug: #1894825
    Closes-Bug: #1898035

--- gate/post_test_hook.sh
+++ gate/post_test_hook.sh
@@ -185,8 +185,11 @@ fi
 echo "Deleting allocations in placement for the server"
 openstack resource provider allocation delete ${server_id}
 
-echo "Deleting allocation key from the binding:profile of the bandwidth aware port"
-openstack port unset --binding-profile allocation port-normal-qos
+# NOTE(gibi) disable this step until the bug
+# https://bugs.launchpad.net/neutron/+bug/1894825 in fixed and a new
+# neutron-lib version is released
+# echo "Deleting allocation key from the binding:profile of the bandwidth aware port"
+# openstack port unset --binding-profile allocation port-normal-qos
 
 # Make sure the allocations are gone.
 allocations=$(openstack resource provider allocation show ${server_id} \
@@ -196,12 +199,15 @@ if [[ "$allocations" != "" ]]; then
     exit 2
 fi
 
+# NOTE(gibi) disable this step until the bug
+# https://bugs.launchpad.net/neutron/+bug/1894825 in fixed and a new
+# neutron-lib version is released
 # Make sure that the binding:profile.allocation key is gone
-null_rp_uuid=$(get_binding_profile_value port-normal-qos "allocation")
-if [[ "$null_rp_uuid" != "" ]]; then
-    echo "Binding profile not updated for the bandwidth aware port."
-    exit 2
-fi
+# null_rp_uuid=$(get_binding_profile_value port-normal-qos "allocation")
+# if [[ "$null_rp_uuid" != "" ]]; then
+#    echo "Binding profile not updated for the bandwidth aware port."
+#    exit 2
+# fi
 
 # Make sure our extra key in the binding:profile is still there
 my_key=$(get_binding_profile_value port-normal-qos "my_key")
@@ -232,12 +238,16 @@ if [[ "$allocations" == "" ]]; then
     exit 2
 fi
 
+# NOTE(gibi) disable this step until the bug
+# https://bugs.launchpad.net/neutron/+bug/1894825 in fixed and a new
+# neutron-lib version is released
+
 # Make sure that the allocations contains bandwidth as well
-bandwidth_allocations=$(echo "$allocations" |  grep NET_BW_EGR_KILOBIT_PER_SEC)
-if [[ "$bandwidth_allocations" == "" ]]; then
-    echo "Failed to heal port allocations."
-    exit 2
-fi
+# bandwidth_allocations=$(echo "$allocations" |  grep NET_BW_EGR_KILOBIT_PER_SEC)
+# if [[ "$bandwidth_allocations" == "" ]]; then
+#     echo "Failed to heal port allocations."
+#     exit 2
+# fi
 
 # Make sure that the binding:profile.allocation key healed back
 healed_rp_uuid=$(get_binding_profile_value port-normal-qos "allocation")
