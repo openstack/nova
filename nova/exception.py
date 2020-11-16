eaecd1ceb0243b31cde9b3c909e8fb3fbef7e635
Stephen Finucane <stephenfin@redhat.com>
2020-11-16 17:35:07 +0000

    Add missing exception
    Change Idd49b0c70caedfcd42420ffa2ac926a6087d406e added support for
    discovery of PMEM devices by the libvirt driver. Some error handling
    code in this was expected to raise a 'GetPMEMNamespacesFailed'
    exception, however, a typo meant the exception was actually called
    'GetPMEMNamespaceFailed' (singular). This exception was later removed in
    change I6fd027fb51823b8a8a24ed7b864a2191c4e8e8c0 because it had no
    references.
    
    Re-introduce the exception, this time with the correct name, and add
    some unit tests to prevent us regressing.
    
    Conflicts:
    	nova/exception.py
    
    NOTE(stephenfin): Conflicts are because change
    I6fd027fb51823b8a8a24ed7b864a2191c4e8e8c0 doesn't exist on this branch,
    meaning the misnamed exception still exists and simply needs to be
    renamed.
    
    Change-Id: I3b597a46314a1b29a952fc0f7a9c4537341e37b8
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>
    Closes-Bug: #1904446
    (cherry picked from commit 160ed6ff652b80cd0a86d41dc3f66c15cee66290)
    (cherry picked from commit 82d415d200e39a5c11fe134e9ef62e427988f2ba)
    (cherry picked from commit 8f65de96a07623c6a8a9cd49db942ad6d4d3ab03)

--- nova/exception.py
+++ nova/exception.py
@@ -2541,7 +2541,7 @@ class PMEMNamespaceConfigInvalid(NovaException):
                 "please check your conf file. ")
 
 
-class GetPMEMNamespaceFailed(NovaException):
+class GetPMEMNamespacesFailed(NovaException):
     msg_fmt = _("Get PMEM namespaces on host failed: %(reason)s.")
 
 
