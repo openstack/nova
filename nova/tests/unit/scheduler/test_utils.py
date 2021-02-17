1862971faa5861bd4f5452b2d32b2ffce21d7c95
Stephen Finucane <stephenfin@redhat.com>
2021-02-17 21:31:13 +0000

    scheduler: Translate secure boot requests to trait
    You've got a 'os:secure_boot' flavor extra spec. Have a trait, you crazy
    guy!
    
    At the moment nothing will report this. That's coming soon.
    
    Blueprint: allow-secure-boot-for-qemu-kvm-guests
    Change-Id: Ie217063ba721d4b417e67e9a14e938948d050f4a
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- nova/tests/unit/scheduler/test_utils.py
+++ nova/tests/unit/scheduler/test_utils.py
@@ -1243,6 +1243,25 @@ class TestUtils(TestUtilsBase):
         rr = utils.ResourceRequest.from_request_spec(rs)
         self.assertResourceRequestsEqual(expected, rr)
 
+    def test_resource_request_from_request_spec_with_secure_boot(self):
+        flavor = objects.Flavor(
+            vcpus=1, memory_mb=1024, root_gb=10, ephemeral_gb=5, swap=0,
+            extra_specs={'os:secure_boot': 'required'},
+        )
+        expected = FakeResourceRequest()
+        expected._rg_by_id[None] = objects.RequestGroup(
+            use_same_provider=False,
+            required_traits={'COMPUTE_SECURITY_UEFI_SECURE_BOOT'},
+            resources={
+                'VCPU': 1,
+                'MEMORY_MB': 1024,
+                'DISK_GB': 15,
+            },
+        )
+        rs = objects.RequestSpec(flavor=flavor, is_bfv=False)
+        rr = utils.ResourceRequest.from_request_spec(rs)
+        self.assertResourceRequestsEqual(expected, rr)
+
     def test_resource_request_from_request_spec_with_vtpm_1_2(self):
         flavor = objects.Flavor(
             vcpus=1, memory_mb=1024, root_gb=10, ephemeral_gb=5, swap=0,
