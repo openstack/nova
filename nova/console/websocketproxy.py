9588cdbfd4649ea53d60303f2d10c5d62a070a07
Sean Mooney <work@seanmooney.info>
2021-08-23 15:37:48 +0100

    address open redirect with 3 forward slashes
    Ie36401c782f023d1d5f2623732619105dc2cfa24 was intended
    to address OSSA-2021-002 (CVE-2021-3654) however after its
    release it was discovered that the fix only worked
    for urls with 2 leading slashes or more then 4.
    
    This change adresses the missing edgecase for 3 leading slashes
    and also maintian support for rejecting 2+.
    
    Conflicts:
      nova/tests/unit/console/test_websocketproxy.py
    
    NOTE: conflict is due to I58b0382c86d4ef798572edb63d311e0e3e6937bb
    is missing in Victoria and Ie36401c782f023d1d5f2623732619105dc2cfa24
    backport contained conflicts and methods order was swapped.
    
    Change-Id: I95f68be76330ff09e5eabb5ef8dd9a18f5547866
    co-authored-by: Matteo Pozza
    Closes-Bug: #1927677
    (cherry picked from commit 6fbd0b758dcac71323f3be179b1a9d1c17a4acc5)
    (cherry picked from commit 47dad4836a26292e9d34e516e1525ecf00be127c)

--- nova/console/websocketproxy.py
+++ nova/console/websocketproxy.py
@@ -290,14 +290,9 @@ class NovaProxyRequestHandler(websockify.ProxyRequestHandler):
         if os.path.isdir(path):
             parts = urlparse.urlsplit(self.path)
             if not parts.path.endswith('/'):
-                # redirect browser - doing basically what apache does
-                new_parts = (parts[0], parts[1], parts[2] + '/',
-                              parts[3], parts[4])
-                new_url = urlparse.urlunsplit(new_parts)
-
                 # Browsers interpret "Location: //uri" as an absolute URI
                 # like "http://URI"
-                if new_url.startswith('//'):
+                if self.path.startswith('//'):
                     self.send_error(HTTPStatus.BAD_REQUEST,
                                     "URI must not start with //")
                     return None
