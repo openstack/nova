be82e9d2f3c5c91cc2c78dd3e972c03dc29d32cc
Fabian Wiesel <fabian.wiesel@sap.com>
2021-10-04 16:17:38 +0200

    Vmware: Handle missing propSet in list_vms
    If a vm has none of the requested properties,
    propSet will not be set. So, we need to skip over that instance
    
    Change-Id: Ia633fecb021bffe1557820e36c33ef53cf90db83
    (cherry picked from commit 6e8f9c725bd0d5697de0b001a60b94461176c984)

--- nova/virt/vmwareapi/vmops.py
+++ nova/virt/vmwareapi/vmops.py
@@ -2161,6 +2161,9 @@ class VMwareVMOps(object):
         with vutil.WithRetrieval(self._session.vim, retrieve_result) as \
                 objects:
             for vm in objects:
+                prop_set = getattr(vm, 'propSet', None)
+                if not prop_set:
+                    continue
                 vm_uuid = None
                 conn_state = None
                 for prop in vm.propSet:
