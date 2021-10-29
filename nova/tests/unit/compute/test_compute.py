33935c79847c2cfe2c4aa2f65d9089d80514e601
Balazs Gibizer <balazs.gibizer@est.tech>
2021-10-29 09:37:18 +0200

    Fix unit test for oslo.concurrency 4.5
    The signature of the lock() function gained a new kwargs in
    oslo.concurrency 4.5 which makes some nova unit test failing. This patch
    changes the mocking the unit test to mock in a higher level in the
    locking infra to avoid depending on the exact signature.
    
    This will enable
    https://review.opendev.org/c/openstack/requirements/+/814889/ to land.
    
    Change-Id: Icecb75d9a374b29f2cf70d3aa155dc6c92bf715f

--- nova/tests/unit/compute/test_compute.py
+++ nova/tests/unit/compute/test_compute.py
@@ -10223,7 +10223,9 @@ class ComputeAPITestCase(BaseTestCase):
         with test.nested(
             mock.patch.dict(self.compute.driver.capabilities,
                              supports_attach_interface=True),
-            mock.patch('oslo_concurrency.lockutils.lock'),
+            mock.patch(
+                'nova.utils.synchronized',
+                side_effect=lambda lockname: lambda f: f),
             mock.patch("nova.network.neutron.API.create_resource_requests"),
             mock.patch.object(
                 self.compute,
@@ -10253,9 +10255,7 @@ class ComputeAPITestCase(BaseTestCase):
                       action='interface_attach', phase='start'),
             mock.call(self.context, instance, self.compute.host,
                       action='interface_attach', phase='end')])
-        mock_lock.assert_called_once_with(lock_name, mock.ANY, mock.ANY,
-                mock.ANY, delay=mock.ANY, do_log=mock.ANY, fair=mock.ANY,
-                semaphores=mock.ANY)
+        mock_lock.assert_called_once_with(lock_name)
         mock_claim_pci.assert_called_once_with(
             self.context, instance,
             test.MatchType(objects.InstancePCIRequests))
@@ -10279,7 +10279,9 @@ class ComputeAPITestCase(BaseTestCase):
         with test.nested(
             mock.patch.dict(self.compute.driver.capabilities,
                              supports_attach_interface=True),
-            mock.patch('oslo_concurrency.lockutils.lock'),
+            mock.patch(
+                'nova.utils.synchronized',
+                side_effect=lambda lockname: lambda f: f),
             mock.patch("nova.network.neutron.API.create_resource_requests"),
             mock.patch.object(
                 self.compute,
@@ -10321,9 +10323,7 @@ class ComputeAPITestCase(BaseTestCase):
             mock.call(self.context, instance, self.compute.host,
                       action='interface_attach', phase='end')])
 
-        mock_lock.assert_called_once_with(lock_name, mock.ANY, mock.ANY,
-                mock.ANY, delay=mock.ANY, do_log=mock.ANY, fair=mock.ANY,
-                semaphores=mock.ANY)
+        mock_lock.assert_called_once_with(lock_name)
 
         # as this is an OVS port we don't call the pci claim but with an empty
         # request
@@ -11077,7 +11077,9 @@ class ComputeAPITestCase(BaseTestCase):
             mock.patch.object(self.compute.network_api,
                 'deallocate_port_for_instance',
                 return_value=([], port_allocation)),
-            mock.patch('oslo_concurrency.lockutils.lock'),
+            mock.patch(
+                'nova.utils.synchronized',
+                side_effect=lambda lockname: lambda f: f),
             mock.patch('nova.pci.request.get_instance_pci_request_from_vif',
                        return_value=pci_req),
             mock.patch.object(self.compute.rt, 'unclaim_pci_devices'),
@@ -11098,9 +11100,7 @@ class ComputeAPITestCase(BaseTestCase):
                       action='interface_detach', phase='start'),
             mock.call(self.context, instance, self.compute.host,
                       action='interface_detach', phase='end')])
-        mock_lock.assert_called_once_with(lock_name, mock.ANY, mock.ANY,
-                mock.ANY, delay=mock.ANY, do_log=mock.ANY, fair=mock.ANY,
-                semaphores=mock.ANY)
+        mock_lock.assert_called_once_with(lock_name)
         mock_unclaim_pci.assert_called_once_with(
             self.context, pci_dev, instance)
         self.assertNotIn(pci_req, instance.pci_requests.requests)
