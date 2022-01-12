cec9e7f1f13f62ff813df32d70d6455917398868
Iago Estrela <IagoFilipe.EstrelaBarros@windriver.com>
2022-01-12 16:06:14 -0300

    Remove deprecated opts from VNC conf
    This change aims to remove the deprecated vncserver_listen and
    vnserver_proxyclient_address opts from vnc conf.
    
    Story: 2009783
    Relates-To: https://review.opendev.org/c/starlingx/openstack-armada-app/+/824467
    
    Signed-off-by: Iago Estrela <IagoFilipe.EstrelaBarros@windriver.com>
    Change-Id: I9a6f26d16c74b0d5f38e16ba1e483eef0b578c21

--- nova/conf/vnc.py
+++ nova/conf/vnc.py
@@ -39,10 +39,6 @@ Guests will get created with graphical devices to support this. Clients
     cfg.HostAddressOpt(
         'server_listen',
         default='127.0.0.1',
-        deprecated_opts=[
-            cfg.DeprecatedOpt('vncserver_listen', group='DEFAULT'),
-            cfg.DeprecatedOpt('vncserver_listen', group='vnc'),
-        ],
         help="""
 The IP address or hostname on which an instance should listen to for
 incoming VNC connection requests on this node.
@@ -51,11 +47,6 @@ incoming VNC connection requests on this node.
     cfg.HostAddressOpt(
         'server_proxyclient_address',
         default='127.0.0.1',
-        deprecated_opts=[
-            cfg.DeprecatedOpt('vncserver_proxyclient_address',
-                              group='DEFAULT'),
-            cfg.DeprecatedOpt('vncserver_proxyclient_address', group='vnc'),
-        ],
         help="""
 Private, internal IP address or hostname of VNC console proxy.
 
