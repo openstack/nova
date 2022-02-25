edb8bcb0294fb1488c49ad76f2422378d0c495eb
Sean Mooney <work@seanmooney.info>
2022-02-25 11:09:50 +0000

    reenable greendns in nova.
    Back in the days of centos 6 and python 2.6 eventlet
    greendns monkeypatching broke ipv6. As a result nova
    has run without greendns monkey patching ever since.
    This removes that old workaround allowing modern
    eventlet to use greendns for non blocking dns lookups.
    
    Closes-Bug: #1964149
    Change-Id: Ia511879d2f5f50a3f63d180258abccf046a7264e
    (cherry picked from commit fe1ebe69f358cbed62434da3f1537a94390324bb)
    (cherry picked from commit a913ab1aabb827771e54ef64b579cebe44ad53d1)

--- nova/monkey_patch.py
+++ nova/monkey_patch.py
@@ -22,22 +22,11 @@ import os
 
 
 def _monkey_patch():
-    # See https://bugs.launchpad.net/nova/+bug/1164822
-    # TODO(mdbooth): This feature was deprecated and removed in eventlet at
-    # some point but brought back in version 0.21.0, presumably because some
-    # users still required it to work round issues. However, there have been a
-    # number of greendns fixes in eventlet since then. Specifically, it looks
-    # as though the originally reported IPv6 issue may have been fixed in
-    # version 0.24.0. We should remove this when we can confirm that the
-    # original issue is fixed.
-    # NOTE(artom) eventlet processes environment variables at import-time. We
-    # therefore set this here, before importing eventlet, in order to correctly
-    # disable greendns.
-    os.environ['EVENTLET_NO_GREENDNS'] = 'yes'
-
     # NOTE(mdbooth): Anything imported here will not be monkey patched. It is
     # important to take care not to import anything here which requires monkey
     # patching.
+    # NOTE(artom) eventlet processes environment variables at import-time.
+    # as such any eventlet configuration should happen here if needed.
     import eventlet
     import sys
 
