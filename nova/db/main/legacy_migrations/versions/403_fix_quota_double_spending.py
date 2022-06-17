822cda4caf0fb29416127b38f0671ea203e5bd1d
Kiran Pawar <kiran.pawar@sap.com>
2022-06-17 15:35:45 +0000

    add a uniqueness constraint to the quota_usages table - b6490ef id (#39)
    * Fixup migration

--- nova/db/main/legacy_migrations/versions/403_fix_quota_double_spending.py
+++ nova/db/main/legacy_migrations/versions/403_fix_quota_double_spending.py
@@ -18,5 +18,20 @@
 # http://lists.openstack.org/pipermail/openstack-dev/2013-March/006827.html
 
 
+from sqlalchemy import MetaData, Table
+from migrate.changeset.constraint import UniqueConstraint
+
+
+def _build_constraint(migrate_engine):
+    meta = MetaData()
+    meta.bind = migrate_engine
+    table = Table('quota_usages', meta, autoload=True)
+    return UniqueConstraint(
+        'project_id', 'user_id', 'resource', 'deleted',
+        table=table,
+    )
+
+
 def upgrade(migrate_engine):
-    pass
+    cons = _build_constraint(migrate_engine)
+    cons.create()
