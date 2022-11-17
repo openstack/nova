8c078b6383490a59eafdbd93ed57b250e39cdb75
Johannes Kulik <johannes.kulik@sap.com>
2022-11-17 12:31:51 +0100

    vmware: Add get_vmx_path helper
    This function returns the path to a VM's .vmx file parsed into a
    DatastorePath object.
    
    While this method is small enough for inlining it into any code, that
    code is easier to unit-test with this function.
    
    Change-Id: If37768910803a9b456c0328a6904c2d53b96cccf

--- nova/virt/vmwareapi/vm_util.py
+++ nova/virt/vmwareapi/vm_util.py
@@ -34,6 +34,7 @@ from oslo_utils import excutils
 from oslo_utils import units
 from oslo_utils.versionutils import convert_version_to_int
 from oslo_vmware import exceptions as vexc
+from oslo_vmware.objects import datastore as ds_obj
 from oslo_vmware import pbm
 from oslo_vmware import vim_util as vutil
 
@@ -2295,3 +2296,10 @@ def reconfigure_vm_device_change(session, vm_ref, device_change):
     config_spec = client_factory.create('ns0:VirtualMachineConfigSpec')
     config_spec.deviceChange = device_change
     reconfigure_vm(session, vm_ref, config_spec)
+
+
+def get_vmx_path(session, vm_ref):
+    """Return DatastorePath object of the vmx file of the VM"""
+    vmx_path = session._call_method(vutil, 'get_object_property',
+                                    vm_ref, 'config.files.vmPathName')
+    return ds_obj.DatastorePath.parse(vmx_path)
