b469e5585aaddd8ab42e6fd95ba71702b3c9e45a
Tharindu B. Hewage <tharindu.b.hewage@gmail.com>
2024-01-27 14:31:45 +1100

    PoC: green-fit impl
    fix green fit impl

--- nova/scheduler/weights/cpu.py
+++ nova/scheduler/weights/cpu.py
@@ -32,43 +32,50 @@ from ..manager import CORE_USAGE
 CONF = nova.conf.CONF
 
 
-def get_prefer_non_empty_machines_score(used_gc, free_gc, used_rc, free_rc):
-    if used_gc + used_rc > 0:
-        return 1
-    else:
+def get_prefer_non_empty_machines_score(usable_cores, used_cores):
+    free_cores = usable_cores - used_cores
+    if free_cores == 0:
         return 0
+    return 1
 
 
-def get_prefer_most_unused_green_cores_score(used_gc, free_gc, used_rc, free_rc):
-    if free_gc + used_gc == 0:
-        return 0
-    else:
-        return free_gc / (free_gc + used_gc)
+def get_prefer_most_unused_green_cores_score(gcpus_avl, gcpus_used):
+    MAX_CPUS = 16.0
+    free_gcpus = gcpus_avl - gcpus_used
+    score = free_gcpus / MAX_CPUS
+    return score
 
 
-def get_prefer_guranteed_renewable_draw_score(used_gc, free_gc, used_rc, free_rc, type, vm_vcpus):
+def get_prefer_guranteed_renewable_draw_score(type, rcpus_avl, rcpus_used, gcpus_avl, gcpus_used, vm_cpus):
     if type == 'regular':
         return 0
 
-    overflow = vm_vcpus - free_rc
-    if 0 < overflow <= free_gc:
+    rcpus_free = rcpus_avl - rcpus_used
+    gcpus_free = gcpus_avl - gcpus_used
+    rcpus_overflow = vm_cpus - rcpus_free
+
+    if 0 < rcpus_overflow <= gcpus_free:
         return 1
-    else:
+
+    return 0
+
+
+def get_worst_fit_on_green_cores_score(rcpus_avl, rcpus_used, gcpus_avl, gcpus_used, usable_cores, used_cores, vm_cpus):
+    rcpus_free = rcpus_avl - rcpus_used
+    gcpus_free = gcpus_avl - gcpus_used
+    is_alloc_on_gcpus = rcpus_free < vm_cpus <= (rcpus_free + gcpus_free)
+    if not is_alloc_on_gcpus:
         return 0
 
-
-def get_worst_fit_on_green_cores_score(used_gc, free_gc, used_rc, free_rc, type, vm_vcpus):
-    if free_gc + used_gc == 0:
-        return 0
-    overflow = vm_vcpus - free_rc
-    if 0 < overflow <= free_gc:
-        return free_gc / (free_gc + used_gc)
-    else:
-        return 0
+    score = 1 - get_best_fit_score(usable_cores=usable_cores, used_cores=used_cores, vm_cpus=vm_cpus)
+    return score
 
 
-def get_best_fit_on_green_cores_score(used_vcpu, free_vcpu):
-    return 1 - (free_vcpu / (used_vcpu + free_vcpu))
+def get_best_fit_score(usable_cores, used_cores, vm_cpus):
+    free_cores = usable_cores - used_cores
+    raw_score = free_cores - vm_cpus
+    score = 1 - raw_score / usable_cores
+    return score
 
 
 def get_cpu_attrs(host_state):
@@ -79,6 +86,44 @@ def get_cpu_attrs(host_state):
     gcpus_used = host_state.gcpus_used
     gcpus_free = (host_state.gcpus_total * 1.0 - host_state.gcpus_used)
     return gcpus_free, gcpus_used, rcpus_free, rcpus_used, vcpus_free, vcpus_used
+
+
+def get_final_weight(usable_cores, used_cores, gcpus_avl, gcpus_used, rcpus_avl, rcpus_used, type, vm_cpus):
+
+    w1 = math.pow(3, 4) * get_prefer_non_empty_machines_score(
+        usable_cores=usable_cores,
+        used_cores=used_cores
+    )
+    w2 = math.pow(3, 3) * get_prefer_most_unused_green_cores_score(
+        gcpus_avl=gcpus_avl,
+        gcpus_used=gcpus_used
+    )
+    w3 = math.pow(3, 2) * get_prefer_guranteed_renewable_draw_score(
+        type=type,
+        rcpus_used=rcpus_used,
+        gcpus_used=gcpus_used,
+        gcpus_avl=gcpus_avl,
+        rcpus_avl=rcpus_avl,
+        vm_cpus=vm_cpus
+    )
+    w4 = math.pow(3, 1) * get_worst_fit_on_green_cores_score(
+        usable_cores=usable_cores,
+        used_cores=used_cores,
+        gcpus_avl=gcpus_avl,
+        gcpus_used=gcpus_used,
+        rcpus_avl=rcpus_avl,
+        rcpus_used=rcpus_used,
+        vm_cpus=vm_cpus
+    )
+    w5 = math.pow(3, 0) * get_best_fit_score(
+        usable_cores=usable_cores,
+        used_cores=used_cores,
+        vm_cpus=vm_cpus
+    )
+
+    final_weight = w1 + w2 + w3 + w4 + w5
+
+    return final_weight
 
 
 class CPUWeigher(weights.BaseHostWeigher):
@@ -92,9 +137,11 @@ class CPUWeigher(weights.BaseHostWeigher):
 
     def _weigh_object(self, host_state, weight_properties):
         """Higher weights win.  We want spreading to be the default."""
-        LOG.debug("tharindu-green-cores: CORE_USAGE['core_usage'] %(weight_properties)s", {'weight_properties': CORE_USAGE['core_usage']})
+        LOG.debug("tharindu-green-cores: CORE_USAGE['core_usage'] %(weight_properties)s",
+                  {'weight_properties': CORE_USAGE['core_usage']})
         LOG.debug("tharindu-green-cores@weighter: host ip %(ip)s", {'ip': host_state.host_ip})
-        LOG.debug("tharindu-green-cores@weighter: weight_properties %(weight_properties)s", {'weight_properties': weight_properties})
+        LOG.debug("tharindu-green-cores@weighter: weight_properties %(weight_properties)s",
+                  {'weight_properties': weight_properties})
         # vcpus_free = (
         #     host_state.vcpus_total * host_state.cpu_allocation_ratio -
         #     host_state.vcpus_used)
@@ -110,19 +157,23 @@ class CPUWeigher(weights.BaseHostWeigher):
         gcpus_avl = core_usage['green-cores-avl']
         rcpus_used = core_usage['reg-cores-usg']
         gcpus_used = core_usage['green-cores-usg']
-        gcpus_free = gcpus_avl - gcpus_used
-        rcpus_free = rcpus_avl - rcpus_used
-        vcpus_used = rcpus_used + gcpus_used
-        vcpus_free = rcpus_free + gcpus_free
+
+        usable_cores = rcpus_avl + gcpus_avl
+        used_cores = rcpus_used + gcpus_used
 
         hints = weight_properties.scheduler_hints
-        type = hints['type'][0]
+        vm_type = hints['type'][0]
 
-        w1 = math.pow(3, 4) * get_prefer_non_empty_machines_score(gcpus_used, gcpus_free, rcpus_used, rcpus_free)
-        w2 = math.pow(3, 3) * get_prefer_most_unused_green_cores_score(gcpus_used, gcpus_free, rcpus_used, rcpus_free)
-        w3 = math.pow(3, 2) * get_prefer_guranteed_renewable_draw_score(gcpus_used, gcpus_free, rcpus_used, rcpus_free, type, weight_properties.vcpus)
-        w4 = math.pow(3, 1) * get_worst_fit_on_green_cores_score(gcpus_used, gcpus_free, rcpus_used, rcpus_free, type, weight_properties.vcpus)
-        w5 = math.pow(3, 0) * get_best_fit_on_green_cores_score(vcpus_used, vcpus_free)
+        vm_cpus = weight_properties.vcpus
 
-        final_weight = w1 + w2 + w3 + w4 + w5
+        final_weight = get_final_weight(
+            usable_cores=usable_cores,
+            used_cores=used_cores,
+            gcpus_avl=gcpus_avl,
+            gcpus_used=gcpus_used,
+            rcpus_avl=rcpus_avl,
+            rcpus_used=rcpus_used,
+            type=vm_type,
+            vm_cpus=vm_cpus
+        )
         return final_weight
