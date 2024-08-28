531ede50a3356208d660a69b352a92912dc22a05
Stephen Finucane <stephenfin@redhat.com>
2024-08-28 13:06:06 +0100

    api: Add response body schemas for hypervisors APIs (1/3)
    We split this one up due to its size, which itself is mainly due to the
    amount of aliasing that went on in early versions as well as the amount
    of changes that have been made over the years.
    
    This change focuses on the index view and related non-detailed views.
    
    Change-Id: I49828e17f9140da16c02fc44fe446c4e489b4d77
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- nova/api/openstack/compute/schemas/hypervisors.py
+++ nova/api/openstack/compute/schemas/hypervisors.py
@@ -12,7 +12,10 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import copy
+
 from nova.api.validation import parameter_types
+from nova.api.validation import response_types
 
 index_query = {
     'type': 'object',
@@ -69,3 +72,68 @@ statistics_query = {}
 search_query = {}
 servers_query = {}
 uptime_query = {}
+
+_servers_response = {
+    'type': 'array',
+    'items': {
+        'type': 'object',
+        'properties': {
+            'name': {'type': 'string'},
+            'uuid': {'type': 'string', 'format': 'uuid'},
+        },
+        'required': ['name', 'uuid'],
+        'additionalProperties': False,
+    },
+}
+
+_hypervisor_response = {
+    'type': 'object',
+    'properties': {
+        'id': {'type': 'integer'},
+        'hypervisor_hostname': {'type': 'string'},
+        'state': {'enum': ['up', 'down']},
+        'status': {'enum': ['enabled', 'disabled']},
+    },
+    'required': ['id', 'hypervisor_hostname', 'state', 'status'],
+    'additionalProperties': False,
+}
+
+_hypervisor_response_v253 = copy.deepcopy(_hypervisor_response)
+_hypervisor_response_v253['properties'].update({
+    'id': {'type': 'string', 'format': 'uuid'},
+    'servers': copy.deepcopy(_servers_response),
+})
+
+index_response = {
+    'type': 'object',
+    'properties': {
+        'hypervisors': {
+            'type': 'array',
+            'items': copy.deepcopy(_hypervisor_response),
+        },
+    },
+    'required': ['hypervisors'],
+    'additionalProperties': False,
+}
+
+# v2.33 adds the hypervisors_links field
+index_response_v233 = copy.deepcopy(index_response)
+index_response_v233['properties'].update({
+    'hypervisors_links': response_types.collection_links,
+})
+
+# v2.53 adds the 'servers' field but only if a user requests it via the
+# 'with_servers' query arg. It also changes the 'id' field to a UUID. Note that
+# v2.75 makes the 'servers' property always present even if empty, but that's
+# not something we can capture with jsonschema so we don't try
+index_response_v253 = copy.deepcopy(index_response_v233)
+index_response_v253['properties']['hypervisors']['items'] = (
+    _hypervisor_response_v253
+)
+
+search_response = copy.deepcopy(index_response)
+
+servers_response = copy.deepcopy(index_response)
+servers_response['properties']['hypervisors']['items']['properties'].update({
+    'servers': copy.deepcopy(_servers_response),
+})
