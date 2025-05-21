6d52ef14b602396d3bc5298fc7c827d6c56cc708
Sean Mooney <work@seanmooney.info>
2025-05-21 11:02:27 +0100

    move compile earlier
    This commit modifies the devstack plugin to wait for
    the compile of the mdev kernel modules earlier so that
    we can be sure its loaded before nova starts.
    
    the logic behind this change is that the mdev devices must be
    loaded in the kernel early enough such that libvirt can
    react to the udev event and update its cache before nova-compute
    tries to get the list of device in its internal cache.
    
    devstack has a set of hook point where plugins are invoked
    know as phases. the same phases exist within devstack and
    the native devstack logic always runs before the plugins in
    each phase.
    
    the main phases are
    
    * pre-install - Called after system (OS) setup is complete and before
    project source is installed.
    * install - Called after the layer 1 and 2
    projects source and their dependencies have been installed.
    * post-config  - Called after the layer 1 and 2 services have been configured.
    All configuration files for enabled services should exist at this point.
    * extra - Called near the end after layer 1 and 2 services have been
    started.
    * test-config - Called at the end of devstack used to configure
    tempest or any other test environments
    
    Previously the compile was started in install and we waited
    for it to complete in extra.
    That means there was a race between the compilation finishing
    and nova starting because we did not wait for it to complete
    in post-config. post-config is required because the services
    are started in extra before the plugin code runs.
    
    additionally this change prints the nodedev-list to the logs
    and adds a libvirtd restart to ensure the mdevs are visible
    in libvirt's nodedev cache.
    
    Change-Id: I0e85b3be04fb20814e63cbac55e95dae10f97f47
    Signed-off-by: jgilaber <jgilaber@redhat.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,3 +1,10 @@
+local xtrace=$(set +o | grep xtrace)
+
+if [ "$VERBOSE" == 'True' ]; then
+    # enabling verbosity on whole plugin - default behavior
+    set -o xtrace
+fi
+
 NOVA_PLUGINDIR=$(readlink -f $(dirname "${BASH_SOURCE[0]}"))
 
 source $NOVA_PLUGINDIR/lib/mdev_samples
@@ -9,12 +16,16 @@ if [[ $1 == "stack" ]]; then
             async_runfunc compile_mdev_samples
         fi
     ;;
-    extra)
+    post-config)
         if [[ "$NOVA_COMPILE_MDEV_SAMPLES" == True ]]; then
             async_wait compile_mdev_samples
+            sudo virsh nodedev-list
+            restart_service libvirtd
+            sudo virsh nodedev-list
         fi
     ;;
     esac
 elif [[ $1 == "clean" ]]; then
     rm -Rf $NOVA_KERNEL_TEMP
 fi
+$xtrace
