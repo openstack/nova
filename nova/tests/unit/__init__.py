b278240370493470b21cbb01ad25e143852b4b78
Balazs Gibizer <gibi@redhat.com>
2025-06-26 14:14:57 +0200

    Allow to start unit test without eventlet
    The end goals is to be able to run at least some of the unit tests
    without eventlet. But there are things preventing that for now.
    
    We need to make sure that the oslo.sevice backed is not initialized to
    eventlet by any early import code before our monkey_patch module can do
    the selective backed selection based on the env variable.
    
    The nova.tests.unit module had some import time code execution that is
    forcing imports that initialize the oslo.service backend too early,
    way before nova would do it in normal execution. We could remove
    objects.register_all() from nova/tests/unit/__init__.py as it seems
    tests are passing without it. Still that would not be enough so I
    eventually decide to keep it.
    
    The other issue is that the unit test discovery imports all modules
    under nova.tests.unit and that eventually imports oslo.messaging and
    that also forces oslo.service backend selection.
    
    So we injected an early call to our smart monkey_patch module to preempt
    that. This does not change the imported modules as monkey_patch module
    imported anyhow via nova.test module. Just changed the order to allow
    oslo.service backend selection explicitly.
    
    After this patch the unit test can be run via
    
      OS_NOVA_DISABLE_EVENTLET_PATCHING=true tox -e py312
    
    Most of the test will pass but there are a bunch of test timing out or
    hanging.
    
    Change-Id: I210cb6a30deaee779d55f88f0f57584c65b0dc05
    Signed-off-by: Balazs Gibizer <gibi@redhat.com>

--- nova/tests/unit/__init__.py
+++ nova/tests/unit/__init__.py
@@ -22,6 +22,21 @@
    :platform: Unix
 """
 
+# The import order in the test environment is different from the import
+# order in a real env. During unit testing nova.test.unit is imported first
+# to discover the unit tests, then the modules under it are imported. This
+# test discovery import forces the import of oslo.messaging eventually that
+# import time run code that uses the oslo.service backend which forces the
+# initialization of the backed to the default eventlet. This prevents
+# our tests to run with the threading backend. To avoid this we force the
+# backend registration early here by using our common monkey_patch mode
+# that already does smart backend selection based on env variables.
+# The monkey_patching would be used anyhow when nova.test is imported so
+# this does not change the end result just the order of imports
+# autopep8: off
+from nova import monkey_patch ; monkey_patch.patch()  # noqa
+# autopep8: on
+
 from nova import objects
 
 # NOTE(comstud): Make sure we have all of the objects loaded. We do this
